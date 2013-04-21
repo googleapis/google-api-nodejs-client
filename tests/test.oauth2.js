2eae25001d4190c0cb0e30c351e370a43e55e012
Burcu Dogan <jbd@google.com>
2013-04-21 21:03:16 +0200

    Unlinking expired cache files, organizing tests and adding a test for caching.

---
+++ tests/test.oauth2.js
@@ -0,0 +1,89 @@
+/**
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var url = require('url'),
+    assert = require('assert'),
+    qs = require('querystring'),
+    fs = require('fs');
+
+var googleapis = require('../lib/googleapis.js'),
+    MockTransporter = require('./mocks/transporters.js');
+
+describe('OAuth2 client', function() {
+
+  var CLIENT_ID = 'CLIENT_ID';
+  var CLIENT_SECRET = 'CLIENT_SECRET';
+  var REDIRECT_URI = 'REDIRECT';
+  var ACCESS_TYPE = 'offline';
+  var SCOPE = 'scopex';
+
+  var urlshortenerDiscoveryTransporter =
+      new MockTransporter(__dirname + '/data/discovery_urlshortener.json');
+
+  it('should generate a valid consent page url', function() {
+    var opts = {
+      access_type: ACCESS_TYPE,
+      scope: SCOPE,
+      response_type: 'code token'
+    };
+
+    var oauth2client =
+        new googleapis.OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+    var generated = oauth2client.generateAuthUrl(opts);
+    var parsed = url.parse(generated);
+    var query = qs.parse(parsed.query);
+
+    assert.equal(query.response_type, 'code token');
+    assert.equal(query.access_type, ACCESS_TYPE);
+    assert.equal(query.scope, SCOPE);
+    assert.equal(query.client_id, CLIENT_ID);
+    assert.equal(query.redirect_uri, REDIRECT_URI);
+  });
+
+  it('should set resonse_type param to code if none is given while' +
+      'generating the consent page url', function() {
+    var oauth2client =
+        new googleapis.OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+    var generated = oauth2client.generateAuthUrl();
+    var parsed = url.parse(generated);
+    var query = qs.parse(parsed.query);
+
+    assert.equal(query.response_type, 'code');
+  });
+
+  it('should throw exception no access token is set before making ' +
+      'a request', function() {
+    var oauth2client =
+      new googleapis.OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+    googleapis.Transporter = urlshortenerDiscoveryTransporter;
+    googleapis
+        .discover('urlshortener', 'v1')
+        .execute(function(err, client) {
+      assert.throws(function() {
+        client
+          .newRequest('dummy', {})
+          .withAuthClient(oauth2client)
+          .execute();
+      }, Error, 'No access token is set.');
+    });
+  });
+
+  it('should replay the request with a refreshed token if auth failed',
+    function() {
+    // TODO(burcud): Implement this unit test.
+  });
+
+});
--- tests/test.requests.js
+++ tests/test.requests.js
@@ -14,97 +14,13 @@
  * limitations under the License.
  */
 
-var MockTransporter = require('./mocks/transporters.js');
-var googleapis = require('../lib/googleapis.js');
-var url = require('url');
-var assert = require('assert');
-var qs = require('querystring');
+var url = require('url'),
+    assert = require('assert'),
+    qs = require('querystring'),
+    fs = require('fs');
 
-describe('Discovery', function() {
-  var plusMockTransporter =
-      new MockTransporter(__dirname + '/data/discovery_plus.json');
-  var urlshortenerDiscoveryTransporter =
-      new MockTransporter(__dirname + '/data/discovery_urlshortener.json');
-
-  it('should be able to load multiple APIs', function() {
-    googleapis
-      .discover('plus', 'v3')
-      .discover('urlshortener', 'v1')
-      .execute(function(err, client) {
-        assert.equal(typeof client.plus, 'Object');
-        assert.equal(typeof client.urlshortener, 'Object');
-      });
-  });
-
-});
-
-describe('Clients', function() {
-
-  var mockTransporter =
-        new MockTransporter(__dirname + '/data/discovery_plus.json');
-
-  it('should create request helpers according ' +
-        'to the resource on discovery API response', function() {
-    googleapis.Transporter = mockTransporter;
-    googleapis
-        .discover('plus', 'v3')
-        .execute(function(err, client) {
-      assert.equal(typeof client.plus.people.get, 'function');
-      assert.equal(typeof client.plus.activities.search, 'function');
-      assert.equal(typeof client.plus.comments.list, 'function');
-    });
-  });
-
-  it('should be able to generate default discovery url', function() {
-    var api = { name: 'plus', version: 'v3', opts: {} };
-    var discoveryUrl =
-        googleapis.generateDiscoveryUrl(api);
-    var parsed = url.parse(discoveryUrl);
-    assert.equal(parsed.protocol, 'https:');
-    assert.equal(parsed.host, 'www.googleapis.com');
-    assert.equal(parsed.path, '/discovery/v1/apis/plus/v3/rpc');
-    assert.equal(parsed.query, null);
-  });
-
-  it('should be able to generate default discovery url with custom ' +
-      'base url and parameters configuration', function() {
-    var api = { name: 'plus', version: 'v3', opts: {
-      baseDiscoveryUrl: 'http://mydeployment/discovery/',
-      discoveryParams: { a: 'hello', b: 'hi' }
-    }};
-    var discoveryUrl = googleapis.generateDiscoveryUrl(api);
-    var parsed = url.parse(discoveryUrl);
-    assert.equal(parsed.protocol, 'http:');
-    assert.equal(parsed.host, 'mydeployment');
-    assert.equal(parsed.pathname, '/discovery/plus/v3/rpc');
-    assert.equal(parsed.query, 'a=hello&b=hi');
-  });
-
-});
-
-describe('Transporters', function() {
-
-  var urlshortenerDiscoveryTransporter =
-    new MockTransporter(__dirname + '/data/discovery_urlshortener.json');
-  var defaultUserAgent = 'google-api-nodejs-client/0.2.5-alpha';
-
-  it('should set default client user agent if none is set', function() {
-    var opts = urlshortenerDiscoveryTransporter.configure({});
-    assert.equal(
-        opts.headers['User-Agent'], defaultUserAgent);
-  });
-
-  it('should append default client user agent to the existing user agent',
-    function() {
-    var applicationName = 'MyTestApplication-1.0';
-    var opts = urlshortenerDiscoveryTransporter.configure({
-      headers: { 'User-Agent': applicationName }
-    });
-    assert.equal(
-        opts.headers['User-Agent'], applicationName + ' ' + defaultUserAgent);
-  });
-
-});
+var googleapis = require('../lib/googleapis.js'),
+    MockTransporter = require('./mocks/transporters.js');
 
 describe('Requests', function() {
 
@@ -234,72 +150,6 @@ describe('Requests', function() {
         assert.notEqual(results[1].result, null);
       });
     });
-  });
-
-});
-
-describe('OAuth2 client', function() {
-
-  var CLIENT_ID = 'CLIENT_ID';
-  var CLIENT_SECRET = 'CLIENT_SECRET';
-  var REDIRECT_URI = 'REDIRECT';
-  var ACCESS_TYPE = 'offline';
-  var SCOPE = 'scopex';
-
-  var urlshortenerDiscoveryTransporter =
-      new MockTransporter(__dirname + '/data/discovery_urlshortener.json');
-
-  it('should generate a valid consent page url', function() {
-    var opts = {
-      access_type: ACCESS_TYPE,
-      scope: SCOPE,
-      response_type: 'code token'
-    };
-
-    var oauth2client =
-        new googleapis.OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
-    var generated = oauth2client.generateAuthUrl(opts);
-    var parsed = url.parse(generated);
-    var query = qs.parse(parsed.query);
-
-    assert.equal(query.response_type, 'code token');
-    assert.equal(query.access_type, ACCESS_TYPE);
-    assert.equal(query.scope, SCOPE);
-    assert.equal(query.client_id, CLIENT_ID);
-    assert.equal(query.redirect_uri, REDIRECT_URI);
-  });
-
-  it('should set resonse_type param to code if none is given while' +
-      'generating the consent page url', function() {
-    var oauth2client =
-        new googleapis.OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
-    var generated = oauth2client.generateAuthUrl();
-    var parsed = url.parse(generated);
-    var query = qs.parse(parsed.query);
-
-    assert.equal(query.response_type, 'code');
-  });
-
-  it('should throw exception no access token is set before making ' +
-      'a request', function() {
-    var oauth2client =
-      new googleapis.OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
-    googleapis.Transporter = urlshortenerDiscoveryTransporter;
-    googleapis
-        .discover('urlshortener', 'v1')
-        .execute(function(err, client) {
-      assert.throws(function() {
-        client
-          .newRequest('dummy', {})
-          .withAuthClient(oauth2client)
-          .execute();
-      }, Error, 'No access token is set.');
-    });
-  });
-
-  it('should replay the request with a refreshed token if auth failed',
-    function() {
-    // TODO(burcud): Implement this unit test.
   });
 
 });
