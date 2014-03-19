31ea7c33bf0e72c8644190256410529315810f42
Robert Rossmann <rr.rossmann@me.com>
2014-03-19 22:53:34 +0100

    Update gapitoken to version 0.1.2
    This adds support for the "sub" attribute which is necessary to
    perform impersonation.

--- examples/jwt.js
+++ examples/jwt.js
@@ -16,11 +16,29 @@
 
 var googleapis = require('../lib/googleapis.js');
 
+/**
+ * The JWT authorization is ideal for performing server-to-server communication without
+ * asking for user consent.
+ *
+ * Suggested reading for Admin SDK users using service accounts:
+ * https://developers.google.com/admin-sdk/directory/v1/guides/delegation
+ *
+ * Note on the private_key.pem:
+ * Node.js currently does not support direct access to the keys stored within PKCS12 file
+ * (see issue comment https://github.com/joyent/node/issues/4050#issuecomment-8816304)
+ * so the private key must be extracted and converted to a passphrase-less RSA key:
+ * Convert the .p12 file to .pem: openssl pkcs12 -in key.p12 -out key.pem -nocerts
+ * Remove the passphrase from the .pem file: openssl rsa -in key.pem -out key.pem
+ */
 var authClient = googleapis.auth.JWT(
     'service-account-email@developer.gserviceaccount.com',
-    'path to private_key.pem',
-    'keyphrase',
-    ['https://www.googleapis.com/auth/drive.readonly']);
+    'path/to/private_key.pem',
+    // Contents of private_key.pem if you want to load the pem file yourself
+    // (do not use the path parameter above if using this param)
+    'key',
+    ['https://www.googleapis.com/auth/drive.readonly'],
+    // User to impersonate (leave empty if no impersonation needed)
+    'subject-account-email@example.com');
 
 authClient.authorize(function(err, tokens) {
   if (err) {
