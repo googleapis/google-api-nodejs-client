7e3ee4052c1138d2b5f7c4078880f94663370167
Jason Allor <jasonall@google.com>
2014-10-09 15:21:26 -0700

    Added tests for _tryGetApplicationCredentialsFromEnvironmentVariable

--- test/test.googlecredentials.js
+++ test/test.googlecredentials.js
@@ -24,6 +24,7 @@ var fs = require('fs');
 
 nock.disableNetConnect();
 
+// Creates a standard JSON credentials object for testing.
 function createJSON() {
   return {
     "private_key_id": "key123",
@@ -34,8 +35,24 @@ function createJSON() {
   };
 }
 
+// Matches the ending of a string.
 function stringEndsWith(str, suffix) {
   return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+// Executes the doneCallback after the nTH call.
+function doneWhen(doneCallback, count) {
+  var i = 0;
+
+  return function() {
+    ++i;
+
+    if (i == count) {
+      doneCallback();
+    } else if (i > count) {
+      throw new Error('Called too many times. Test error?');
+    }
+  };
 }
 
 describe('googleCredentials', function() {
@@ -180,6 +197,7 @@ describe('googleCredentials', function() {
       var directory = './test/fixtures';
       assert.equal(true, fs.lstatSync(directory).isDirectory());
 
+      // Execute.
       googleapis.credentials._getApplicationCredentialsFromFilePath(directory, function (err, result) {
         assert.equal(true, err instanceof Error);
         done();
@@ -187,11 +205,13 @@ describe('googleCredentials', function() {
     });
 
     it('should handle errors thrown from createReadStream', function (done) {
+      // Set up a mock to throw from the createReadStream method.
       var gc = new googleCredentials();
-      gc.internals.createReadStream = function(filePath) {
+      gc._createReadStream = function(filePath) {
         throw new Error('Hans and Chewbacca');
       }
 
+      // Execute.
       gc._getApplicationCredentialsFromFilePath('./test/fixtures/private.json', function (err, result) {
         assert.equal(true, stringEndsWith(err.message, 'Hans and Chewbacca'));
         done();
@@ -199,11 +219,13 @@ describe('googleCredentials', function() {
     });
 
     it('should handle errors thrown from fromStream', function (done) {
+      // Set up a mock to throw from the fromStream method.
       var gc = new googleCredentials();
       gc.fromStream = function(stream, callback) {
         throw new Error('Darth Maul');
       }
 
+      // Execute.
       gc._getApplicationCredentialsFromFilePath('./test/fixtures/private.json', function (err, result) {
         assert.equal(true, stringEndsWith(err.message, 'Darth Maul'));
         done();
@@ -211,11 +233,13 @@ describe('googleCredentials', function() {
     });
 
     it('should handle errors passed from fromStream', function (done) {
+      // Set up a mock to return an error from the fromStream method.
       var gc = new googleCredentials();
       gc.fromStream = function(stream, callback) {
         callback(new Error('Princess Leia'));
       }
 
+      // Execute.
       gc._getApplicationCredentialsFromFilePath('./test/fixtures/private.json', function (err, result) {
         assert.equal(true, stringEndsWith(err.message, 'Princess Leia'));
         done();
@@ -237,6 +261,98 @@ describe('googleCredentials', function() {
         assert.equal(null, result.scope);
         done();
       });
+    });
+  });
+
+  describe('._tryGetApplicationCredentialsFromEnvironmentVariable', function () {
+
+    it('should return false when env var is not set', function (done) {
+      // Set up a mock to return a null path string.
+      var gc = new googleCredentials();
+      gc._getEnvironmentVariable = function(name) {
+        return null;
+      }
+
+      // The test ends successfully after 1 step has completed.
+      var step = doneWhen(done, 1);
+
+      // Execute.
+      var handled = gc._tryGetApplicationCredentialsFromEnvironmentVariable(function (err, result) {
+        step(); // This should not get called.
+      });
+
+      assert.equal(false, handled);
+      step(); // This should get called.
+    });
+
+    it('should return false when env var is empty string', function (done) {
+      // Set up a mock to return an empty path string.
+      var gc = new googleCredentials();
+      gc._getEnvironmentVariable = function(name) {
+        return '';
+      }
+
+      // The test ends successfully after 1 step has completed.
+      var step = doneWhen(done, 1);
+
+      // Execute.
+      var handled = gc._tryGetApplicationCredentialsFromEnvironmentVariable(function (err, result) {
+        step(); // This should not get called.
+      });
+
+      assert.equal(false, handled);
+      step(); // This should get called.
+    });
+
+    it('should handle invalid environment variable', function (done) {
+      // Set up a mock to return a path to an invalid file.
+      var gc = new googleCredentials();
+      gc._getEnvironmentVariable = function(name) {
+        return './nonexistantfile.json';
+      }
+
+      // The test ends successfully after 2 steps have completed.
+      var step = doneWhen(done, 2);
+
+      // Execute.
+      var handled = gc._tryGetApplicationCredentialsFromEnvironmentVariable(function (err, result) {
+        assert.equal(true, err instanceof Error);
+        step();
+      });
+
+      assert.equal(true, handled);
+      step();
+    });
+
+    it('should handle valid environment variable', function (done) {
+      // Set up a mock to return path to a valid credentials file.
+      var gc = new googleCredentials();
+      gc._getEnvironmentVariable = function(name) {
+        return './test/fixtures/private.json';
+      }
+
+      var x = 1;
+
+      // Read the contents of the file into a json object.
+      var fileContents = fs.readFileSync('./test/fixtures/private.json', 'utf-8');
+      var json = JSON.parse(fileContents);
+
+      // The test ends successfully after 2 steps have completed.
+      var step = doneWhen(done, 2);
+
+      // Execute.
+      var handled = gc._tryGetApplicationCredentialsFromEnvironmentVariable(function (err, result) {
+        assert.equal(null, err);
+        assert.equal(json.private_key, result.key);
+        assert.equal(json.client_email, result.email);
+        assert.equal(null, result.keyFile);
+        assert.equal(null, result.subject);
+        assert.equal(null, result.scope);
+        step();
+      });
+
+      assert.equal(true, handled);
+      step();
     });
   });
 });
