googleapis/google-api-nodejs-client

commit 4d920bf592ac7852b311ede14398953a575aa9ca

Refactor apirequest.js

lib/apirequest.js
@@ -19,34 +19,38 @@
 var Multipart = require('multipart-stream');
 var utils = require('./utils.js');
 var DefaultTransporter = require('./transporters.js');
-var transporter = new DefaultTransporter();
 var stream = require('stream');
 var parseString = require('string-template');
 var querystring = require('querystring');
 
 function isReadableStream(obj) {
-  return obj instanceof stream.Stream && typeof obj._read === 'function' &&
-      typeof obj._readableState === 'object';
+  return obj instanceof stream.Stream &&
+    typeof obj._read === 'function' &&
+    typeof obj._readableState === 'object';
 }
 
-function logErrorOnly(err) {
+function logError(err) {
   if (err) {
     console.error(err);
   }
 }
 
 function createCallback(callback) {
-  return typeof callback === 'function' ? callback : logErrorOnly;
+  return typeof callback === 'function' ? callback : logError;
 }
 
-function isValidParams(params, keys, callback) {
-  for (var i = 0, len = keys.length; i < len; i++) {
-    if (!params[keys[i]]) {
-      callback(new Error('Missing required parameter: ' + keys[i]), null);
-      return false;
+function getMissingParams(params, required) {
+  var missing = [];
+
+  required.forEach(function(param) {
+    // Is the required param in the params object?
+    if (! params[param]) {
+      missing.push(param);
     }
-  }
-  return true;
+  });
+
+  // If there are any required params missing, return their names in array, otherwise return null
+  return missing.length > 0 ? missing : null;
 }
 
 /**
@@ -80,35 +84,41 @@ function parsePath(path, params) {
  * @return {Request}             Returns Request object or null
  */
 function createAPIRequest(parameters, callback) {
-  var req, body;
-  var context = parameters.context;
+  var req, body, missingParams;
   var params = parameters.params;
-  var options = parameters.options || {};
-  var requiredParams = parameters.requiredParams || [];
-  var pathParams = parameters.pathParams || [];
+  var options = utils.extend({}, parameters.options);
 
   /**
    * If the params are not present, and callback was passed instead,
    * use params as the callback and create empty params.
    */
-  if (typeof(params) === 'function') {
+  if (typeof params === 'function') {
     callback = params;
     params = {};
   } else {
+    // Create a new params object so it can no longer be modified from outside code
     params = utils.extend({}, params);
   }
 
-  // Redefining callback var
+  // Normalize callback
   callback = createCallback(callback);
 
-  if (!isValidParams(params, requiredParams, callback)) {
+  // Check for missing required parameters in the API request
+  missingParams = getMissingParams(params, parameters.requiredParams);
+  if (missingParams) {
+    // Some params are missing - stop further operations and inform the developer which required
+    // params are not included in the request
+    callback(new Error('Missing required parameters: ', missingParams.join(', ')));
+
     return null;
   }
 
-  var method = options.method;
   var media = params.media || {};
   var resource = params.resource;
-  var authClient = params.auth || context._options.auth || context.google._options.auth;
+  var authClient = params.auth ||
+    parameters.context._options.auth ||
+    parameters.context.google._options.auth;
+
   var defaultMime = typeof media.body === 'string' ? 'text/plain' : 'application/octet-stream';
   delete params.media;
   delete params.resource;
@@ -118,10 +128,10 @@ function createAPIRequest(parameters, callback) {
   options.url = parsePath(options.url, params);
   parameters.mediaUrl = parsePath(parameters.mediaUrl, params);
 
-  // delete required parameters
-  for (var i = 0, len = pathParams.length; i < len; i++) {
-    delete params[pathParams[i]];
-  }
+  // delete path parameters from the params object so they do not end up in query
+  parameters.pathParams.forEach(function(param) {
+    delete params[param];
+  });
 
   // if authClient is actually a string, use it as an API KEY
   if (typeof authClient === 'string') {
@@ -129,18 +139,17 @@ function createAPIRequest(parameters, callback) {
     authClient = null;
   }
 
-  if (parameters.mediaUrl && media && media.body) {
+  if (parameters.mediaUrl && media.body) {
     options.url = parameters.mediaUrl;
     if (resource) {
       // Create a boundary identifier and multipart read stream
-      var boundary = Math.random().toString(36).slice(2);
-      body = new Multipart(boundary);
+      body = new Multipart();
 
       // Use multipart upload
       params.uploadType = 'multipart';
 
       options.headers = {
-        'Content-Type': 'multipart/related; boundary="' + boundary + '"'
+        'Content-Type': 'multipart/related; boundary="' + body.boundary + '"'
       };
 
       // Add parts to multipart request
@@ -170,21 +179,27 @@ function createAPIRequest(parameters, callback) {
       }
     }
   } else {
-    options.json = resource || ((method === 'GET' || method === 'DELETE') ? true : {});
+    options.json = resource || (
+      (options.method === 'GET' || options.method === 'DELETE') ? true : {}
+    );
   }
 
   if(Object.keys(params).length) {
     options.url += '?' + querystring.stringify(params);
   }
 
-  options = utils.extend({}, context.google._options, context._options, options);
+  options = utils.extend({},
+    parameters.context.google._options,
+    parameters.context._options,
+    options
+  );
   delete options.auth; // is overridden by our auth code
 
   // create request (using authClient or otherwise and return request obj)
   if (authClient) {
     req = authClient.request(options, callback);
   } else {
-    req = transporter.request(options, callback);
+    req = new DefaultTransporter().request(options, callback);
   }
 
   if (body) {
@@ -194,9 +209,7 @@ function createAPIRequest(parameters, callback) {
 }
 
 /**
- * Exports helper functionsj
- * @type {object}
+ * Exports createAPIRequest
+ * @type {Function}
  */
-module.exports = {
-  createAPIRequest: createAPIRequest
-};
+module.exports = createAPIRequest;

templates/api-endpoint.js
@@ -18,8 +18,7 @@
 
 'use strict';
 
-var apirequest = require('../../lib/apirequest');
-var createAPIRequest = apirequest.createAPIRequest;
+var createAPIRequest = require('../../lib/apirequest');
 
 {% set Name = name|capitalize %}
 {% set Version = version|replace('\.', '_')|capitalize %}

templates/method-partial.js
@@ -34,8 +34,8 @@
     },
     params: params,
     {%- if m.mediaUpload.protocols.simple.path -%}mediaUrl: {{ m.mediaUpload.protocols.simple.path|buildurl }},{%- endif -%}
-    {%- if m.parameterOrder.length -%}requiredParams: ['{{ m.parameterOrder|join("', '")|safe }}'],{%- endif -%}
-    {%- if pathParams.length -%}pathParams: ['{{ pathParams|join("', '")|safe }}'],{%- endif -%}
+    requiredParams: [{%- if m.parameterOrder.length -%}'{{ m.parameterOrder|join("', '")|safe }}'{%- endif -%}],
+    pathParams: [{%- if pathParams.length -%}'{{ pathParams|join("', '")|safe }}'{%- endif -%}],
     context: self
   };
 

commit 42826da390a6bfb9ce0d145e7666b71aaf520f3b

Fix missing params not being shown in Error message
Bug introduced in 4d920bf

lib/apirequest.js
@@ -108,7 +108,7 @@ function createAPIRequest(parameters, callback) {
   if (missingParams) {
     // Some params are missing - stop further operations and inform the developer which required
     // params are not included in the request
-    callback(new Error('Missing required parameters: ', missingParams.join(', ')));
+    callback(new Error('Missing required parameters: ' + missingParams.join(', ')));
 
     return null;
   }

test/test.path.js
@@ -43,6 +43,13 @@ describe('Path params', function() {
     });
   });
 
+  it('should be mentioned in err.message when missing', function (done) {
+    drive.files.get({}, function(err) {
+      assert.notEqual(err.message.indexOf('fileId'), -1, 'Missing param not mentioned in error');
+      done();
+    });
+  });
+
   it('should return null response object if not included and required', function(done) {
     drive.files.get({}, function(err, resp) {
       assert.equal(resp, null);