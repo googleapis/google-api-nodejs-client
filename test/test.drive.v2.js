e4c77c20e92a5f5b1dbc3bb34681297aa0bf9657
Robert Rossmann <rr.rossmann@me.com>
2015-01-14 17:30:35 +0100

    Do not output stuff to console during testing
    It looks ugly.:)

--- test/test.drive.v2.js
+++ test/test.drive.v2.js
@@ -99,11 +99,21 @@ describe('drive:v2', function() {
         assert.equal(req.constructor.name, 'Request');
       });
 
-      it('should use logError callback if no callback specified', function() {
+      it('should use logError callback if no callback specified', function(done) {
         var drive = google.drive('v2');
         nock('https://www.googleapis.com')
         .get('/drive/v2/files?q=hello')
         .reply(501, { error: 'not a real error' });
+
+        // logError internally uses console.error - let's monkey-patch the function to intercept
+        // calls to it, then restore the original function once we are done testing
+        var origFn = console.error;
+        console.error = function(err) {
+          assert.equal(err.code, 501);
+          console.error = origFn;
+          done();
+        };
+
         assert.doesNotThrow(function() {
           drive.files.list({ q: 'hello' });
         });
