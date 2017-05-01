4941cf1adfe9b6106684cab47489dc300c8eaa4f
Justin Beckwith <beckwith@google.com>
2017-05-01 10:12:28 -0700

    a few updates

--- lib/discovery.ts
+++ lib/discovery.ts
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const generatorUtils = require('./generator_utils');
+import generatorUtils from '../scripts/generator_utils';
 const handleError = generatorUtils.handleError;
 const async = require('async');
 const fs = require('fs');
