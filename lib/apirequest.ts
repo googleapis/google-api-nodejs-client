1400e2b46fca43e4f8dd4a3fd3e6b43f9d914de3
Sai Cheemalapati <saicheems@google.com>
2017-06-15 15:01:02 -0700

    Address PR feedback

--- lib/apirequest.ts
+++ lib/apirequest.ts
@@ -38,7 +38,7 @@ function getMissingParams (params, required) {
 
   required.forEach(function (param) {
     // Is the required param in the params object?
-    if (!params.hasOwnProperty(param)) {
+    if (params[param] === undefined) {
       missing.push(param);
     }
   });
