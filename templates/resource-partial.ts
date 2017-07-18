b4c694b8a96de90c4ea414c4df27ada449a698ab
Sai Cheemalapati <saicheems@gmail.com>
2017-07-17 22:03:23 -0700

    Make the generator deterministic (#779)

--- templates/resource-partial.ts
+++ templates/resource-partial.ts
@@ -1,11 +1,13 @@
 {% if r.methods %}
-{% for mname, m in methods -%}
+{% for mname in methods|sort -%}
+{% set m = methods[mname] %}
   {% include "./method-partial.ts" with m %}
 {%- endfor -%}
 {%- endif -%}{%- if r.methods && r.resources -%},{%- endif -%}
 
 {% if r.resources %}
-{%- for rname, r in resources %}
+{%- for rname in resources|sort -%}
+{%- set r = resources[rname] -%}
 
   {{ rname }}: {
     {%- include "./resource-partial.ts" with r -%}
