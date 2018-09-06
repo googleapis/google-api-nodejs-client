55c5f39c3affe4529ec03959c987023ccb75c52d
Justin Beckwith <justin.beckwith@gmail.com>
2018-09-06 15:28:23 -0700

    extend

--- test/test.kitchen.ts
+++ test/test.kitchen.ts
@@ -61,7 +61,7 @@ describe('kitchen sink', async () => {
     await mvp(tarball, `${stagingPath}/googleapis.tgz`);
     await ncpp('test/fixtures/kitchen', `${stagingPath}/`);
     await spawnp('npm', ['install'], {cwd: `${stagingPath}/`});
-  }).timeout(40000);
+  }).timeout(80000);
 });
 
 /**
