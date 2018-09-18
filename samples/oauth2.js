8cb10f267ce18c475650f5dc0dae06babea22d1f
Justin Beckwith <justin.beckwith@gmail.com>
2018-09-17 22:25:46 -0700

    Use prettier and eslint on samples (#1341)

--- samples/oauth2.js
+++ samples/oauth2.js
@@ -28,7 +28,7 @@ const plus = google.plus('v1');
  * To use OAuth2 authentication, we need access to a a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI.  To get these credentials for your application, visit https://console.cloud.google.com/apis/credentials.
  */
 const keyPath = path.join(__dirname, 'oauth2.keys.json');
-let keys = { redirect_uris: [''] };
+let keys = {redirect_uris: ['']};
 if (fs.existsSync(keyPath)) {
   keys = require(keyPath).web;
 }
@@ -45,42 +45,44 @@ const oauth2Client = new google.auth.OAuth2(
 /**
  * This is one of the many ways you can configure googleapis to use authentication credentials.  In this method, we're setting a global reference for all APIs.  Any other API you use here, like google.drive('v3'), will now use this auth client. You can also override the auth client at the service and method call levels.
  */
-google.options({ auth: oauth2Client });
+google.options({auth: oauth2Client});
 
 /**
  * Open an http server to accept the oauth callback. In this simple example, the only request to our webserver is to /callback?code=<code>
  */
-async function authenticate (scopes) {
+async function authenticate(scopes) {
   return new Promise((resolve, reject) => {
     // grab the url that will be used for authorization
     const authorizeUrl = oauth2Client.generateAuthUrl({
       access_type: 'offline',
-      scope: scopes.join(' ')
+      scope: scopes.join(' '),
     });
-    const server = http.createServer(async (req, res) => {
-      try {
-        if (req.url.indexOf('/oauth2callback') > -1) {
-          const qs = querystring.parse(url.parse(req.url).query);
-          res.end('Authentication successful! Please return to the console.');
-          server.destroy();
-          const {tokens} = await oauth2Client.getToken(qs.code);
-          oauth2Client.credentials = tokens;
-          resolve(oauth2Client);
+    const server = http
+      .createServer(async (req, res) => {
+        try {
+          if (req.url.indexOf('/oauth2callback') > -1) {
+            const qs = querystring.parse(url.parse(req.url).query);
+            res.end('Authentication successful! Please return to the console.');
+            server.destroy();
+            const {tokens} = await oauth2Client.getToken(qs.code);
+            oauth2Client.credentials = tokens;
+            resolve(oauth2Client);
+          }
+        } catch (e) {
+          reject(e);
         }
-      } catch (e) {
-        reject(e);
-      }
-    }).listen(3000, () => {
-      // open the browser to the authorize url to start the workflow
-      opn(authorizeUrl, {wait: false}).then(cp => cp.unref());
-    });
+      })
+      .listen(3000, () => {
+        // open the browser to the authorize url to start the workflow
+        opn(authorizeUrl, {wait: false}).then(cp => cp.unref());
+      });
     destroyer(server);
   });
 }
 
-async function runSample () {
+async function runSample() {
   // retrieve user profile
-  const res = await plus.people.get({ userId: 'me' });
+  const res = await plus.people.get({userId: 'me'});
   console.log(res.data);
 }
 
