633e89d42b937de3a43c0af47b53b998cfbdaf32
Edwardzyc <edward.chen92@gmail.com>
2018-12-13 19:53:30 -0500

    Update append.js

--- samples/sheets/append.js
+++ samples/sheets/append.js
@@ -26,7 +26,7 @@ async function runSample(spreadsheetId, range) {
     spreadsheetId,
     range,
     valueInputOption: 'USER_ENTERED',
-    requestBody: {
+    resource: {
       values: [
         ['Justin', '1/1/2001', 'Website'],
         ['Node.js', '2018-03-14', 'Fun'],
