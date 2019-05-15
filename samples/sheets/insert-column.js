14fa774381ba81a6e61c983f2713cd72d58636cf
Tyler Johnson <mail@tyler-johnson.ca>
2019-05-15 18:38:31 -0400

    Fix linting

--- samples/sheets/insert-column.js
+++ samples/sheets/insert-column.js
@@ -17,28 +17,27 @@ async function runSample(spreadsheetId, sheetId, startIndex, endIndex) {
           insertDimension: {
             range: {
               sheetId,
-              dimension: "COLUMNS",
+              dimension: 'COLUMNS',
               startIndex,
-              endIndex
+              endIndex,
             },
             inheritFromBefore: false,
-          }
-        }
-      ]
-    }
+          },
+        },
+      ],
+    },
   });
+  console.info(res);
 }
 
 if (module === require.main) {
   if (process.argv.length !== 6) {
-    const cliParams = "spreadsheetId sheetId startIndex endIndex";
+    const cliParams = 'spreadsheetId sheetId startIndex endIndex';
     throw new Error(`Usage: node samples/sheets/insert-column.js ${cliParams}`);
   }
 
   const [spreadsheetId, sheetId, startIndex, endIndex] = process.argv.slice(2);
-  const scopes = [
-    'https://www.googleapis.com/auth/spreadsheets'
-  ];
+  const scopes = ['https://www.googleapis.com/auth/spreadsheets'];
 
   sampleClient
     .authenticate(scopes)
