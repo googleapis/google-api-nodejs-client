bd5789f634c58772e83009709d6766eac55388bc
Benjamin Coe <bencoe@google.com>
2019-05-29 12:04:48 -0700

    build: remove verbose logging from test scripts

--- .kokoro/lint.sh
+++ .kokoro/lint.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -xeo pipefail
+set -eo pipefail
 
 export NPM_CONFIG_PREFIX=/home/node/.npm-global
 
