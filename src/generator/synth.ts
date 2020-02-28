94410ba70ecde394537cfb47c87f905e5e347f8c
Justin Beckwith <justin.beckwith@gmail.com>
2020-02-28 08:47:57 -0800

    build: auto-run generation and submit prs (#1905)

---
+++ src/generator/synth.ts
@@ -0,0 +1,68 @@
+// Copyright 2019 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as execa from 'execa';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as gaxios from 'gaxios';
+
+async function main() {
+  const statusResult = await execa('git', ['status']);
+  const status = statusResult.stdout;
+  const apiDir = path.resolve('./src/apis');
+  const files = fs.readdirSync(apiDir);
+  const token = process.env.GITHUB_TOKEN;
+  if (!token) {
+    throw new Error('please include a GITHUB_TOKEN');
+  }
+  const dirs = files.filter(f => {
+    return (
+      fs.statSync(path.join(apiDir, f)).isDirectory() &&
+      status.includes(`src/apis/${f}`)
+    );
+  });
+  console.log(`Changes found in ${dirs.length} APIs`);
+  for (const dir of dirs) {
+    try {
+      console.log(`Submitting change for ${dir}...`);
+      const branch = `api-${dir}`;
+      const title = `feat(${dir}): update the API`;
+      await execa('git', ['checkout', '-B', branch]);
+      await execa('git', ['add', path.join('src/apis', dir)]);
+      await execa('git', ['commit', '-m', title]);
+      await execa('git', ['push', 'origin', branch, '--force']);
+      await gaxios.request({
+        method: 'POST',
+        headers: {
+          Authorization: `token ${token}`,
+        },
+        url:
+          'https://api.github.com/repos/googleapis/google-api-nodejs-client/pulls',
+        data: {
+          title,
+          head: branch,
+          base: 'master',
+        },
+      });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await execa('git', ['checkout', 'master']);
+    }
+  }
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exit(2);
+});
--- synth.py
+++ synth.py
@@ -13,10 +13,8 @@
 # limitations under the License.
 
 import synthtool as s
-import synthtool.log as log
-import synthtool.shell as shell
-import synthtool.sources.git as git
 import synthtool.gcp as gcp
+import subprocess
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -25,12 +23,3 @@ logging.basicConfig(level=logging.DEBUG)
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library()
 s.copy(templates, excludes=[".github/CONTRIBUTING.md", "README.md"])
-
-# repository_url = "https://github.com/googleapis/google-api-nodejs-client.git"
-# log.debug(f"Cloning {repository_url}.")
-# repository = git.clone(repository_url, depth=1)
-# log.debug("Installing dependencies.")
-# shell.run(["npm", "install"], cwd=repository)
-# log.debug("Generating all libraries...")
-# shell.run(["npm", "run", "generate"], cwd=repository)
-# s.copy(repository / "src")
