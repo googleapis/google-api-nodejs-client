a05a827cdb00b1542eec4956b4272ee406983dcb
yoshi-automation <yoshi-automation@google.com>
2020-06-11 04:36:19 -0700

    feat: regenerate index files

--- .kokoro/populate-secrets.sh
+++ .kokoro/populate-secrets.sh
@@ -15,12 +15,19 @@
 
 set -eo pipefail
 
+function now { date +"%Y-%m-%d %H:%M:%S" | tr -d '\n' ;}
+function msg { println "$*" >&2 ;}
+function println { printf '%s\n' "$(now) $*" ;}
+
+
 # Populates requested secrets set in SECRET_MANAGER_KEYS from service account:
 # kokoro-trampoline@cloud-devrel-kokoro-resources.iam.gserviceaccount.com
 SECRET_LOCATION="${KOKORO_GFILE_DIR}/secret_manager"
+msg "Creating folder on disk for secrets: ${SECRET_LOCATION}"
 mkdir -p ${SECRET_LOCATION}
 for key in $(echo ${SECRET_MANAGER_KEYS} | sed "s/,/ /g")
 do
+  msg "Retrieving secret ${key}"
   docker run --entrypoint=gcloud \
     --volume=${KOKORO_GFILE_DIR}:${KOKORO_GFILE_DIR} \
     gcr.io/google.com/cloudsdktool/cloud-sdk \
@@ -29,4 +36,9 @@ do
     --project cloud-devrel-kokoro-resources \
     --secret $key > \
     "$SECRET_LOCATION/$key"
+  if [[ $? == 0 ]]; then
+    msg "Secret written to ${SECRET_LOCATION}/${key}"
+  else
+    msg "Error retrieving secret ${key}"
+  fi
 done
