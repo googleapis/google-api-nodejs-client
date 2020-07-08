163106077696c056582f0b201947dc328947b9ba
yoshi-automation <yoshi-automation@google.com>
2020-07-08 04:25:39 -0700

    feat(managedidentities): update the API
    #### managedidentities:v1alpha1
    The following keys were added:
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1Instance.properties.maintenanceSettings.$ref
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1Instance.properties.maintenanceSettings.description
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.description
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.id
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.properties.exclude.description
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.properties.exclude.type
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.type
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.global.resources.domains.methods.getIamPolicy.parameters.options.requestedPolicyVersion.description
    - schemas.Binding.properties.condition.description
    - schemas.Policy.description
    - schemas.Policy.properties.version.description
    
    #### managedidentities:v1
    The following keys were added:
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1Instance.properties.maintenanceSettings.$ref
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1Instance.properties.maintenanceSettings.description
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.description
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.id
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.properties.exclude.description
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.properties.exclude.type
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.type
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.global.resources.domains.methods.getIamPolicy.parameters.options.requestedPolicyVersion.description
    - schemas.Binding.properties.condition.description
    - schemas.Policy.description
    - schemas.Policy.properties.version.description
    
    #### managedidentities:v1beta1
    The following keys were added:
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1Instance.properties.maintenanceSettings.$ref
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1Instance.properties.maintenanceSettings.description
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.description
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.id
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.properties.exclude.description
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.properties.exclude.type
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.type
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.global.resources.domains.methods.getIamPolicy.parameters.options.requestedPolicyVersion.description
    - schemas.Binding.properties.condition.description
    - schemas.Policy.description
    - schemas.Policy.properties.version.description

--- src/apis/managedidentities/v1.ts
+++ src/apis/managedidentities/v1.ts
@@ -141,7 +141,7 @@ export namespace managedidentities_v1 {
    */
   export interface Schema$Binding {
     /**
-     * The condition that is associated with this binding. NOTE: An unsatisfied condition will not allow user access via current binding. Different bindings, including their conditions, are examined independently.
+     * The condition that is associated with this binding.  If the condition evaluates to `true`, then this binding applies to the current request.  If the condition evaluates to `false`, then this binding does not apply to the current request. However, a different role binding might grant the same role to one or more of the members in this binding.  To learn which resources support conditions in their IAM policies, see the [IAM documentation](https://cloud.google.com/iam/help/conditions/resource-policies).
      */
     condition?: Schema$Expr;
     /**
@@ -357,6 +357,10 @@ export namespace managedidentities_v1 {
       ]: Schema$GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSchedule;
     } | null;
     /**
+     * Optional. The MaintenanceSettings associated with instance.
+     */
+    maintenanceSettings?: Schema$GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings;
+    /**
      * Unique name of the resource. It uses the form:  `projects/{project_id}/locations/{location_id}/instances/{instance_id}`
      */
     name?: string | null;
@@ -413,6 +417,15 @@ export namespace managedidentities_v1 {
      * The scheduled start time for the maintenance.
      */
     startTime?: string | null;
+  }
+  /**
+   * Maintenance settings associated with instance. Allows service producers and end users to assign settings that controls maintenance on this instance.
+   */
+  export interface Schema$GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings {
+    /**
+     * Optional. Exclude instance from maintenance. When true, rollout service will not attempt maintenance on the instance. Rollout service will include the instance in reported rollout progress as not attempted.
+     */
+    exclude?: boolean | null;
   }
   /**
    * Node information for custom per-node SLO implementations. SSA does not support per-node SLO, but producers can populate per-node information in SloMetadata for custom precomputations. SSA Eligibility Exporter will emit per-node metric based on this information.
@@ -626,7 +639,7 @@ export namespace managedidentities_v1 {
     verb?: string | null;
   }
   /**
-   * An Identity and Access Management (IAM) policy, which specifies access controls for Google Cloud resources.   A `Policy` is a collection of `bindings`. A `binding` binds one or more `members` to a single `role`. Members can be user accounts, service accounts, Google groups, and domains (such as G Suite). A `role` is a named list of permissions; each `role` can be an IAM predefined role or a user-created custom role.  Optionally, a `binding` can specify a `condition`, which is a logical expression that allows access to a resource only if the expression evaluates to `true`. A condition can add constraints based on attributes of the request, the resource, or both.  **JSON example:**      {       &quot;bindings&quot;: [         {           &quot;role&quot;: &quot;roles/resourcemanager.organizationAdmin&quot;,           &quot;members&quot;: [             &quot;user:mike@example.com&quot;,             &quot;group:admins@example.com&quot;,             &quot;domain:google.com&quot;,             &quot;serviceAccount:my-project-id@appspot.gserviceaccount.com&quot;           ]         },         {           &quot;role&quot;: &quot;roles/resourcemanager.organizationViewer&quot;,           &quot;members&quot;: [&quot;user:eve@example.com&quot;],           &quot;condition&quot;: {             &quot;title&quot;: &quot;expirable access&quot;,             &quot;description&quot;: &quot;Does not grant access after Sep 2020&quot;,             &quot;expression&quot;: &quot;request.time &lt; timestamp(&#39;2020-10-01T00:00:00.000Z&#39;)&quot;,           }         }       ],       &quot;etag&quot;: &quot;BwWWja0YfJA=&quot;,       &quot;version&quot;: 3     }  **YAML example:**      bindings:     - members:       - user:mike@example.com       - group:admins@example.com       - domain:google.com       - serviceAccount:my-project-id@appspot.gserviceaccount.com       role: roles/resourcemanager.organizationAdmin     - members:       - user:eve@example.com       role: roles/resourcemanager.organizationViewer       condition:         title: expirable access         description: Does not grant access after Sep 2020         expression: request.time &lt; timestamp(&#39;2020-10-01T00:00:00.000Z&#39;)     - etag: BwWWja0YfJA=     - version: 3  For a description of IAM and its features, see the [IAM documentation](https://cloud.google.com/iam/docs/).
+   * An Identity and Access Management (IAM) policy, which specifies access controls for Google Cloud resources.   A `Policy` is a collection of `bindings`. A `binding` binds one or more `members` to a single `role`. Members can be user accounts, service accounts, Google groups, and domains (such as G Suite). A `role` is a named list of permissions; each `role` can be an IAM predefined role or a user-created custom role.  For some types of Google Cloud resources, a `binding` can also specify a `condition`, which is a logical expression that allows access to a resource only if the expression evaluates to `true`. A condition can add constraints based on attributes of the request, the resource, or both. To learn which resources support conditions in their IAM policies, see the [IAM documentation](https://cloud.google.com/iam/help/conditions/resource-policies).  **JSON example:**      {       &quot;bindings&quot;: [         {           &quot;role&quot;: &quot;roles/resourcemanager.organizationAdmin&quot;,           &quot;members&quot;: [             &quot;user:mike@example.com&quot;,             &quot;group:admins@example.com&quot;,             &quot;domain:google.com&quot;,             &quot;serviceAccount:my-project-id@appspot.gserviceaccount.com&quot;           ]         },         {           &quot;role&quot;: &quot;roles/resourcemanager.organizationViewer&quot;,           &quot;members&quot;: [             &quot;user:eve@example.com&quot;           ],           &quot;condition&quot;: {             &quot;title&quot;: &quot;expirable access&quot;,             &quot;description&quot;: &quot;Does not grant access after Sep 2020&quot;,             &quot;expression&quot;: &quot;request.time &lt; timestamp(&#39;2020-10-01T00:00:00.000Z&#39;)&quot;,           }         }       ],       &quot;etag&quot;: &quot;BwWWja0YfJA=&quot;,       &quot;version&quot;: 3     }  **YAML example:**      bindings:     - members:       - user:mike@example.com       - group:admins@example.com       - domain:google.com       - serviceAccount:my-project-id@appspot.gserviceaccount.com       role: roles/resourcemanager.organizationAdmin     - members:       - user:eve@example.com       role: roles/resourcemanager.organizationViewer       condition:         title: expirable access         description: Does not grant access after Sep 2020         expression: request.time &lt; timestamp(&#39;2020-10-01T00:00:00.000Z&#39;)     - etag: BwWWja0YfJA=     - version: 3  For a description of IAM and its features, see the [IAM documentation](https://cloud.google.com/iam/docs/).
    */
   export interface Schema$Policy {
     /**
@@ -638,7 +651,7 @@ export namespace managedidentities_v1 {
      */
     etag?: string | null;
     /**
-     * Specifies the format of the policy.  Valid values are `0`, `1`, and `3`. Requests that specify an invalid value are rejected.  Any operation that affects conditional role bindings must specify version `3`. This requirement applies to the following operations:  * Getting a policy that includes a conditional role binding * Adding a conditional role binding to a policy * Changing a conditional role binding in a policy * Removing any role binding, with or without a condition, from a policy   that includes conditions  **Important:** If you use IAM Conditions, you must include the `etag` field whenever you call `setIamPolicy`. If you omit this field, then IAM allows you to overwrite a version `3` policy with a version `1` policy, and all of the conditions in the version `3` policy are lost.  If a policy does not include any conditions, operations on that policy may specify any valid version or leave the field unset.
+     * Specifies the format of the policy.  Valid values are `0`, `1`, and `3`. Requests that specify an invalid value are rejected.  Any operation that affects conditional role bindings must specify version `3`. This requirement applies to the following operations:  * Getting a policy that includes a conditional role binding * Adding a conditional role binding to a policy * Changing a conditional role binding in a policy * Removing any role binding, with or without a condition, from a policy   that includes conditions  **Important:** If you use IAM Conditions, you must include the `etag` field whenever you call `setIamPolicy`. If you omit this field, then IAM allows you to overwrite a version `3` policy with a version `1` policy, and all of the conditions in the version `3` policy are lost.  If a policy does not include any conditions, operations on that policy may specify any valid version or leave the field unset.  To learn which resources support conditions in their IAM policies, see the [IAM documentation](https://cloud.google.com/iam/help/conditions/resource-policies).
      */
     version?: number | null;
   }
@@ -1903,6 +1916,10 @@ export namespace managedidentities_v1 {
      *       // Requests for policies with any conditional bindings must specify version 3.
      *       // Policies without any conditional bindings may specify any valid value or
      *       // leave the field unset.
+     *       //
+     *       // To learn which resources support conditions in their IAM policies, see the
+     *       // [IAM
+     *       // documentation](https://cloud.google.com/iam/help/conditions/resource-policies).
      *       'options.requestedPolicyVersion': 'placeholder-value',
      *       // REQUIRED: The resource for which the policy is being requested.
      *       // See the operation documentation for the appropriate value for this field.
@@ -1928,7 +1945,7 @@ export namespace managedidentities_v1 {
      * @memberOf! ()
      *
      * @param {object} params Parameters for request
-     * @param {integer=} params.options.requestedPolicyVersion Optional. The policy format version to be returned.  Valid values are 0, 1, and 3. Requests specifying an invalid value will be rejected.  Requests for policies with any conditional bindings must specify version 3. Policies without any conditional bindings may specify any valid value or leave the field unset.
+     * @param {integer=} params.options.requestedPolicyVersion Optional. The policy format version to be returned.  Valid values are 0, 1, and 3. Requests specifying an invalid value will be rejected.  Requests for policies with any conditional bindings must specify version 3. Policies without any conditional bindings may specify any valid value or leave the field unset.  To learn which resources support conditions in their IAM policies, see the [IAM documentation](https://cloud.google.com/iam/help/conditions/resource-policies).
      * @param {string} params.resource_ REQUIRED: The resource for which the policy is being requested. See the operation documentation for the appropriate value for this field.
      * @param {object} [options] Optionally override request options, such as `url`, `method`, and `encoding`.
      * @param {callback} callback The callback that handles the response.
@@ -3137,7 +3154,7 @@ export namespace managedidentities_v1 {
   export interface Params$Resource$Projects$Locations$Global$Domains$Getiampolicy
     extends StandardParameters {
     /**
-     * Optional. The policy format version to be returned.  Valid values are 0, 1, and 3. Requests specifying an invalid value will be rejected.  Requests for policies with any conditional bindings must specify version 3. Policies without any conditional bindings may specify any valid value or leave the field unset.
+     * Optional. The policy format version to be returned.  Valid values are 0, 1, and 3. Requests specifying an invalid value will be rejected.  Requests for policies with any conditional bindings must specify version 3. Policies without any conditional bindings may specify any valid value or leave the field unset.  To learn which resources support conditions in their IAM policies, see the [IAM documentation](https://cloud.google.com/iam/help/conditions/resource-policies).
      */
     'options.requestedPolicyVersion'?: number;
     /**
