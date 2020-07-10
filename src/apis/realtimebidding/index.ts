6145bfca68cce0925172b6555c9b7c01d8dcfd08
yoshi-automation <yoshi-automation@google.com>
2020-07-10 04:25:03 -0700

    fix(realtimebidding): update the API

---
+++ src/apis/realtimebidding/index.ts
@@ -0,0 +1,37 @@
+// Copyright 2020 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*! THIS FILE IS AUTO-GENERATED */
+
+import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
+import {realtimebidding_v1} from './v1';
+
+export const VERSIONS = {
+  v1: realtimebidding_v1.Realtimebidding,
+};
+
+export function realtimebidding(
+  version: 'v1'
+): realtimebidding_v1.Realtimebidding;
+export function realtimebidding(
+  options: realtimebidding_v1.Options
+): realtimebidding_v1.Realtimebidding;
+export function realtimebidding<T = realtimebidding_v1.Realtimebidding>(
+  this: GoogleConfigurable,
+  versionOrOptions: 'v1' | realtimebidding_v1.Options
+) {
+  return getAPI<T>('realtimebidding', versionOrOptions, VERSIONS, this);
+}
+
+const auth = new AuthPlus();
+export {auth};
