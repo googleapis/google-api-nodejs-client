ea3c2808f7bb8493b8dc7c7766624a4d771df4a3
yoshi-automation <yoshi-automation@google.com>
2020-08-03 04:29:05 -0700

    fix(storagetransfer): update the API
    #### storagetransfer:v1
    The following keys were changed:
    - resources.transferOperations.methods.cancel.description

--- src/apis/storagetransfer/v1.ts
+++ src/apis/storagetransfer/v1.ts
@@ -1456,7 +1456,7 @@ export namespace storagetransfer_v1 {
 
     /**
      * storagetransfer.transferOperations.cancel
-     * @desc Cancels a transfer. Use the get method to check whether the cancellation succeeded or whether the operation completed despite cancellation.
+     * @desc Cancels a transfer. Use the transferOperations.get method to check if the cancellation succeeded or if the operation completed despite the `cancel` request.  When you cancel an operation, the currently running transfer is interrupted.  For recurring transfer jobs, the next instance of the transfer job will still run.  For example, if your job is configured to run every day at 1pm and you cancel Monday's operation at 1:05pm, Monday's transfer will stop. However, a transfer job will still be attempted on Tuesday.  This applies only to currently running operations. If an operation is not currently running, `cancel` does nothing.  <aside class="caution"> <b>Caution:</b> Canceling a transfer job can leave your data in an unknown state. We recommend that you restore the state at both the destination and the source after the `cancel` request completes so that your data is in a consistent state. </aside>  When you cancel a job, the next job computes a delta of files and may repair any inconsistent state. For instance, if you run a job every day, and today's job found 10 new files and transferred five files before you canceled the job, tomorrow's transfer operation will compute a new delta with the five files that were not copied today plus any new files discovered tomorrow.
      * @example
      * // Before running the sample:
      * // - Enable the API at:
