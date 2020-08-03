231b4b4615a140afafa2800329ac29668279cc4b
yoshi-automation <yoshi-automation@google.com>
2020-08-03 04:29:05 -0700

    fix(servicecontrol): update the API

--- src/apis/servicecontrol/index.ts
+++ src/apis/servicecontrol/index.ts
@@ -15,18 +15,30 @@
 
 import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
 import {servicecontrol_v1} from './v1';
+import {servicecontrol_v2} from './v2';
 
 export const VERSIONS = {
   v1: servicecontrol_v1.Servicecontrol,
+  v2: servicecontrol_v2.Servicecontrol,
 };
 
 export function servicecontrol(version: 'v1'): servicecontrol_v1.Servicecontrol;
 export function servicecontrol(
   options: servicecontrol_v1.Options
 ): servicecontrol_v1.Servicecontrol;
-export function servicecontrol<T = servicecontrol_v1.Servicecontrol>(
+export function servicecontrol(version: 'v2'): servicecontrol_v2.Servicecontrol;
+export function servicecontrol(
+  options: servicecontrol_v2.Options
+): servicecontrol_v2.Servicecontrol;
+export function servicecontrol<
+  T = servicecontrol_v1.Servicecontrol | servicecontrol_v2.Servicecontrol
+>(
   this: GoogleConfigurable,
-  versionOrOptions: 'v1' | servicecontrol_v1.Options
+  versionOrOptions:
+    | 'v1'
+    | servicecontrol_v1.Options
+    | 'v2'
+    | servicecontrol_v2.Options
 ) {
   return getAPI<T>('servicecontrol', versionOrOptions, VERSIONS, this);
 }
