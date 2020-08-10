4a5961e64397425438b3c0e9c4d31f3769202872
yoshi-automation <yoshi-automation@google.com>
2020-08-10 04:22:16 -0700

    feat(oslogin): update the API
    #### oslogin:v1alpha
    The following keys were added:
    - resources.users.methods.getLoginProfile.parameters.operatingSystemType.enumDescriptions
    - resources.users.resources.projects.methods.delete.parameters.operatingSystemType.enumDescriptions
    - schemas.PosixAccount.properties.accountId.readOnly
    - schemas.PosixAccount.properties.name.readOnly
    - schemas.SshPublicKey.properties.fingerprint.readOnly
    - schemas.SshPublicKey.properties.name.readOnly
    
    The following keys were changed:
    - resources.users.methods.getLoginProfile.description
    - resources.users.methods.importSshPublicKey.description
    - resources.users.resources.projects.methods.delete.parameters.name.description
    - resources.users.resources.sshPublicKeys.methods.delete.parameters.name.description
    - resources.users.resources.sshPublicKeys.methods.get.parameters.name.description
    - resources.users.resources.sshPublicKeys.methods.patch.description
    - resources.users.resources.sshPublicKeys.methods.patch.parameters.name.description
    - schemas.Empty.description
    - schemas.LoginProfile.description
    - schemas.PosixAccount.properties.operatingSystemType.enumDescriptions
    - schemas.PosixAccount.properties.systemId.description
    - schemas.SshPublicKey.properties.key.description

--- src/apis/oslogin/v1alpha.ts
+++ src/apis/oslogin/v1alpha.ts
@@ -128,7 +128,7 @@ export namespace oslogin_v1alpha {
   }
 
   /**
-   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance:      service Foo {       rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty);     }  The JSON representation for `Empty` is empty JSON object `{}`.
+   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); } The JSON representation for `Empty` is empty JSON object `{}`.
    */
   export interface Schema$Empty {}
   /**
@@ -219,7 +219,7 @@ export namespace oslogin_v1alpha {
      */
     fingerprint?: string | null;
     /**
-     * Public key text in SSH format, defined by &lt;a href=&quot;https://www.ietf.org/rfc/rfc4253.txt&quot; target=&quot;_blank&quot;&gt;RFC4253&lt;/a&gt; section 6.6.
+     * Public key text in SSH format, defined by RFC4253 section 6.6.
      */
     key?: string | null;
     /**
@@ -614,9 +614,7 @@ export namespace oslogin_v1alpha {
      *
      *   // Do the magic
      *   const res = await oslogin.users.projects.delete({
-     *     // A reference to the POSIX account to update. POSIX accounts are identified
-     *     // by the project ID they are associated with. A reference to the POSIX
-     *     // account is in format `users/{user}/projects/{project}`.
+     *     // A reference to the POSIX account to update. POSIX accounts are identified by the project ID they are associated with. A reference to the POSIX account is in format `users/{user}/projects/{project}`.
      *     name: 'users/my-user/projects/my-project',
      *     // The type of operating system associated with the account.
      *     operatingSystemType: 'placeholder-value',
@@ -767,9 +765,7 @@ export namespace oslogin_v1alpha {
      *
      *   // Do the magic
      *   const res = await oslogin.users.sshPublicKeys.delete({
-     *     // The fingerprint of the public key to update. Public keys are identified by
-     *     // their SHA-256 fingerprint. The fingerprint of the public key is in format
-     *     // `users/{user}/sshPublicKeys/{fingerprint}`.
+     *     // The fingerprint of the public key to update. Public keys are identified by their SHA-256 fingerprint. The fingerprint of the public key is in format `users/{user}/sshPublicKeys/{fingerprint}`.
      *     name: 'users/my-user/sshPublicKeys/my-sshPublicKey',
      *   });
      *   console.log(res.data);
@@ -898,9 +894,7 @@ export namespace oslogin_v1alpha {
      *
      *   // Do the magic
      *   const res = await oslogin.users.sshPublicKeys.get({
-     *     // The fingerprint of the public key to retrieve. Public keys are identified
-     *     // by their SHA-256 fingerprint. The fingerprint of the public key is in
-     *     // format `users/{user}/sshPublicKeys/{fingerprint}`.
+     *     // The fingerprint of the public key to retrieve. Public keys are identified by their SHA-256 fingerprint. The fingerprint of the public key is in format `users/{user}/sshPublicKeys/{fingerprint}`.
      *     name: 'users/my-user/sshPublicKeys/my-sshPublicKey',
      *   });
      *   console.log(res.data);
@@ -1034,9 +1028,7 @@ export namespace oslogin_v1alpha {
      *
      *   // Do the magic
      *   const res = await oslogin.users.sshPublicKeys.patch({
-     *     // The fingerprint of the public key to update. Public keys are identified by
-     *     // their SHA-256 fingerprint. The fingerprint of the public key is in format
-     *     // `users/{user}/sshPublicKeys/{fingerprint}`.
+     *     // The fingerprint of the public key to update. Public keys are identified by their SHA-256 fingerprint. The fingerprint of the public key is in format `users/{user}/sshPublicKeys/{fingerprint}`.
      *     name: 'users/my-user/sshPublicKeys/my-sshPublicKey',
      *     // Mask to control which fields get updated. Updates all if not present.
      *     updateMask: 'placeholder-value',
