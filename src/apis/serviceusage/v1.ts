f1170139d941d5e736c901f41c8bcb25e45fc288
yoshi-automation <yoshi-automation@google.com>
2020-08-15 04:21:19 -0700

    fix(serviceusage): update the API
    #### serviceusage:v1
    The following keys were changed:
    - resources.services.methods.list.description

--- src/apis/serviceusage/v1.ts
+++ src/apis/serviceusage/v1.ts
@@ -2928,7 +2928,7 @@ export namespace serviceusage_v1 {
 
     /**
      * serviceusage.services.list
-     * @desc List all services available to the specified project, and the current state of those services with respect to the project. The list includes all public services, all services for which the calling user has the `servicemanagement.services.bind` permission, and all services that have already been enabled on the project. The list can be filtered to only include services in a specific state, for example to only include services enabled on the project.
+     * @desc List all services available to the specified project, and the current state of those services with respect to the project. The list includes all public services, all services for which the calling user has the `servicemanagement.services.bind` permission, and all services that have already been enabled on the project. The list can be filtered to only include services in a specific state, for example to only include services enabled on the project. WARNING: If you need to query enabled services frequently or across an organization, you should use [Cloud Asset Inventory API](https://cloud.google.com/asset-inventory/docs/apis), which provides higher throughput and richer filtering capability.
      * @example
      * // Before running the sample:
      * // - Enable the API at:
