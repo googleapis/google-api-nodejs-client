dc767f5b2c3acb360c6ea5d874bc75b7ae1280d2
Sofia Leon <sofialeon@google.com>
2020-08-25 11:29:17 -0700

    fix(securitycenter): update the API
    #### securitycenter:v1
    The following keys were changed:
    - schemas.Finding.properties.eventTime.description
    - schemas.GoogleCloudSecuritycenterV1p1beta1Finding.properties.eventTime.description
    
    #### securitycenter:v1beta1
    The following keys were changed:
    - schemas.Finding.properties.eventTime.description
    - schemas.GoogleCloudSecuritycenterV1beta1Finding.properties.eventTime.description
    - schemas.GoogleCloudSecuritycenterV1p1beta1Finding.properties.eventTime.description

--- src/apis/securitycenter/index.ts
+++ src/apis/securitycenter/index.ts
@@ -16,12 +16,14 @@
 import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
 import {securitycenter_v1} from './v1';
 import {securitycenter_v1beta1} from './v1beta1';
+import {securitycenter_v1beta2} from './v1beta2';
 import {securitycenter_v1p1alpha1} from './v1p1alpha1';
 import {securitycenter_v1p1beta1} from './v1p1beta1';
 
 export const VERSIONS = {
   v1: securitycenter_v1.Securitycenter,
   v1beta1: securitycenter_v1beta1.Securitycenter,
+  v1beta2: securitycenter_v1beta2.Securitycenter,
   v1p1alpha1: securitycenter_v1p1alpha1.Securitycenter,
   v1p1beta1: securitycenter_v1p1beta1.Securitycenter,
 };
@@ -36,6 +38,12 @@ export function securitycenter(
 export function securitycenter(
   options: securitycenter_v1beta1.Options
 ): securitycenter_v1beta1.Securitycenter;
+export function securitycenter(
+  version: 'v1beta2'
+): securitycenter_v1beta2.Securitycenter;
+export function securitycenter(
+  options: securitycenter_v1beta2.Options
+): securitycenter_v1beta2.Securitycenter;
 export function securitycenter(
   version: 'v1p1alpha1'
 ): securitycenter_v1p1alpha1.Securitycenter;
@@ -52,6 +60,7 @@ export function securitycenter<
   T =
     | securitycenter_v1.Securitycenter
     | securitycenter_v1beta1.Securitycenter
+    | securitycenter_v1beta2.Securitycenter
     | securitycenter_v1p1alpha1.Securitycenter
     | securitycenter_v1p1beta1.Securitycenter
 >(
@@ -61,6 +70,8 @@ export function securitycenter<
     | securitycenter_v1.Options
     | 'v1beta1'
     | securitycenter_v1beta1.Options
+    | 'v1beta2'
+    | securitycenter_v1beta2.Options
     | 'v1p1alpha1'
     | securitycenter_v1p1alpha1.Options
     | 'v1p1beta1'
