3e98a0d2548579417c0f7781a5d6ffdc3c0e804c
Harry Chen <45508624+HachimanHiki@users.noreply.github.com>
2020-09-23 06:05:34 +0800

    fix: fixed compile error of invalid export (#2362)

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,9 @@
 import {GoogleApis} from './googleapis';
 const google = new GoogleApis();
 export {google, GoogleApis};
-export * as Common from 'googleapis-common';
-export * as Auth from 'google-auth-library';
+import * as Common from 'googleapis-common';
+import * as Auth from 'google-auth-library';
+export {Common, Auth};
 
 export {abusiveexperiencereport_v1} from './apis/abusiveexperiencereport/v1';
 export {acceleratedmobilepageurl_v1} from './apis/acceleratedmobilepageurl/v1';
