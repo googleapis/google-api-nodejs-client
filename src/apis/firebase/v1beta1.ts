70d49ab3a7c2ca38569b71a5ff974617a7e8379f
Justin Beckwith <justin.beckwith@gmail.com>
2020-10-05 13:15:05 -0700

    fix(firebase): update the API
    #### firebase:v1beta1
    The following keys were changed:
    - schemas.WebAppConfig.properties.measurementId.description

--- src/apis/firebase/v1beta1.ts
+++ src/apis/firebase/v1beta1.ts
@@ -639,7 +639,7 @@ export namespace firebase_v1beta1 {
      */
     locationId?: string | null;
     /**
-     * The unique Google-assigned identifier of the Google Analytics web stream associated with the Firebase Web App. Firebase SDKs use this ID to interact with Google Analytics APIs. This field is only present if the App is linked to a web stream in a Google Analytics App + Web property. Learn more about this ID and Google Analytics web streams in the [Analytics documentation](https://support.google.com/analytics/topic/9303475). To generate a `measurementId` and link the Web App with a Google Analytics web stream, call [`AddGoogleAnalytics`](../../v1beta1/projects/addGoogleAnalytics).
+     * The unique Google-assigned identifier of the Google Analytics web stream associated with the `WebApp`. Firebase SDKs use this ID to interact with Google Analytics APIs. This field is only present if the `WebApp` is linked to a web stream in a Google Analytics App + Web property. Learn more about this ID and Google Analytics web streams in the [Analytics documentation](https://support.google.com/analytics/topic/9303475). To generate a `measurementId` and link the `WebApp` with a Google Analytics web stream, call [`AddGoogleAnalytics`](../../v1beta1/projects/addGoogleAnalytics). For apps using the Firebase JavaScript SDK v7.20.0 and later, Firebase dynamically fetches the `measurementId` when your app initializes Analytics. Having this ID in your config object is optional, but it does serve as a fallback in the rare case that the dynamic fetch fails.
      */
     measurementId?: string | null;
     /**
