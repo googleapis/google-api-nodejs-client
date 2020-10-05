cf7db546fcf293f9af93540b5b31e0a83a43a80a
Justin Beckwith <justin.beckwith@gmail.com>
2020-10-05 13:15:04 -0700

    feat(displayvideo): update the API
    #### displayvideo:v1
    The following keys were added:
    - schemas.LineItem.properties.warningMessages.description
    - schemas.LineItem.properties.warningMessages.items.enum
    - schemas.LineItem.properties.warningMessages.items.enumDescriptions
    - schemas.LineItem.properties.warningMessages.items.type
    - schemas.LineItem.properties.warningMessages.readOnly
    - schemas.LineItem.properties.warningMessages.type
    
    The following keys were changed:
    - resources.advertisers.methods.list.parameters.filter.description
    - schemas.PerformanceGoal.properties.performanceGoalType.enum
    - schemas.PerformanceGoal.properties.performanceGoalType.enumDescriptions

--- src/apis/displayvideo/index.ts
+++ src/apis/displayvideo/index.ts
@@ -15,18 +15,54 @@
 
 import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
 import {displayvideo_v1} from './v1';
+import {displayvideo_v1beta} from './v1beta';
+import {displayvideo_v1beta2} from './v1beta2';
+import {displayvideo_v1dev} from './v1dev';
 
 export const VERSIONS = {
   v1: displayvideo_v1.Displayvideo,
+  v1beta: displayvideo_v1beta.Displayvideo,
+  v1beta2: displayvideo_v1beta2.Displayvideo,
+  v1dev: displayvideo_v1dev.Displayvideo,
 };
 
 export function displayvideo(version: 'v1'): displayvideo_v1.Displayvideo;
 export function displayvideo(
   options: displayvideo_v1.Options
 ): displayvideo_v1.Displayvideo;
-export function displayvideo<T = displayvideo_v1.Displayvideo>(
+export function displayvideo(
+  version: 'v1beta'
+): displayvideo_v1beta.Displayvideo;
+export function displayvideo(
+  options: displayvideo_v1beta.Options
+): displayvideo_v1beta.Displayvideo;
+export function displayvideo(
+  version: 'v1beta2'
+): displayvideo_v1beta2.Displayvideo;
+export function displayvideo(
+  options: displayvideo_v1beta2.Options
+): displayvideo_v1beta2.Displayvideo;
+export function displayvideo(version: 'v1dev'): displayvideo_v1dev.Displayvideo;
+export function displayvideo(
+  options: displayvideo_v1dev.Options
+): displayvideo_v1dev.Displayvideo;
+export function displayvideo<
+  T =
+    | displayvideo_v1.Displayvideo
+    | displayvideo_v1beta.Displayvideo
+    | displayvideo_v1beta2.Displayvideo
+    | displayvideo_v1dev.Displayvideo
+>(
   this: GoogleConfigurable,
-  versionOrOptions: 'v1' | displayvideo_v1.Options
+  versionOrOptions:
+    | 'v1'
+    | displayvideo_v1.Options
+    | 'v1beta'
+    | displayvideo_v1beta.Options
+    | 'v1beta2'
+    | displayvideo_v1beta2.Options
+    | 'v1dev'
+    | displayvideo_v1dev.Options
 ) {
   return getAPI<T>('displayvideo', versionOrOptions, VERSIONS, this);
 }
