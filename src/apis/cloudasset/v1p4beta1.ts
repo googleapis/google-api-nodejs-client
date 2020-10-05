d46b0c6de892995f1a8b78d1f275eafbd3b20f45
Justin Beckwith <justin.beckwith@gmail.com>
2020-10-05 13:14:57 -0700

    feat(cloudasset)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### cloudasset:v1p4beta1
    The following keys were changed:
    - schemas.AccessSelector.description
    
    #### cloudasset:v1p5beta1
    The following keys were deleted:
    - schemas.GoogleIdentityAccesscontextmanagerV1DevicePolicy.properties.allowedDeviceManagementLevels.enumDescriptions
    - schemas.GoogleIdentityAccesscontextmanagerV1DevicePolicy.properties.allowedEncryptionStatuses.enumDescriptions
    
    #### cloudasset:v1
    The following keys were deleted:
    - schemas.GoogleIdentityAccesscontextmanagerV1DevicePolicy.properties.allowedDeviceManagementLevels.enumDescriptions
    - schemas.GoogleIdentityAccesscontextmanagerV1DevicePolicy.properties.allowedEncryptionStatuses.enumDescriptions
    
    The following keys were added:
    - schemas.BigQueryDestination.properties.partitionSpec.$ref
    - schemas.BigQueryDestination.properties.partitionSpec.description
    - schemas.BigQueryDestination.properties.separateTablesPerAssetType.description
    - schemas.BigQueryDestination.properties.separateTablesPerAssetType.type
    - schemas.PartitionSpec.description
    - schemas.PartitionSpec.id
    - schemas.PartitionSpec.properties.partitionKey.description
    - schemas.PartitionSpec.properties.partitionKey.enum
    - schemas.PartitionSpec.properties.partitionKey.enumDescriptions
    - schemas.PartitionSpec.properties.partitionKey.type
    - schemas.PartitionSpec.type
    
    #### cloudasset:v1beta1
    The following keys were deleted:
    - schemas.GoogleIdentityAccesscontextmanagerV1DevicePolicy.properties.allowedDeviceManagementLevels.enumDescriptions
    - schemas.GoogleIdentityAccesscontextmanagerV1DevicePolicy.properties.allowedEncryptionStatuses.enumDescriptions

--- src/apis/cloudasset/v1p4beta1.ts
+++ src/apis/cloudasset/v1p4beta1.ts
@@ -128,7 +128,7 @@ export namespace cloudasset_v1p4beta1 {
   }
 
   /**
-   * Specifies roles and/or permissions to analyze, to determine both the identities possessing them and the resources they control. If multiple values are specified, results will include identities and resources matching any of them.
+   * Specifies roles and/or permissions to analyze, to determine both the identities possessing them and the resources they control. If multiple values are specified, results will include identities and resources matching any of them. The total number of roles and permissions should be equal or less than 10.
    */
   export interface Schema$AccessSelector {
     /**
