8fd603f78101f801c605cc5f9dd215de883fdcad
yoshi-automation <yoshi-automation@google.com>
2020-10-06 05:23:02 -0700

    feat: regenerate index files

--- .kokoro/publish.sh
+++ .kokoro/publish.sh
@@ -16,7 +16,7 @@
 
 set -eo pipefail
 
-export NPM_CONFIG_PREFIX=/home/node/.npm-global
+export NPM_CONFIG_PREFIX=${HOME}/.npm-global
 
 # Start the releasetool reporter
 python3 -m pip install gcp-releasetool
--- .kokoro/samples-test.sh
+++ .kokoro/samples-test.sh
@@ -16,7 +16,7 @@
 
 set -eo pipefail
 
-export NPM_CONFIG_PREFIX=/home/node/.npm-global
+export NPM_CONFIG_PREFIX=${HOME}/.npm-global
 
 # Setup service account credentials.
 export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/service-account.json
