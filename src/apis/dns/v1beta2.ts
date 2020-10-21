74408232729349b2ae83109ca3edb0d911e22217
yoshi-automation <yoshi-automation@google.com>
2020-10-21 09:48:09 -0700

    feat(dns)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    #### dns:v1
    The following keys were changed:
    - documentationLink
    
    #### dns:v1beta2
    The following keys were deleted:
    - schemas.ResourceRecordSetsListResponse.properties.kind.description
    
    The following keys were changed:
    - documentationLink

--- src/apis/dns/v1beta2.ts
+++ src/apis/dns/v1beta2.ts
@@ -711,9 +711,6 @@ export namespace dns_v1beta2 {
   }
   export interface Schema$ResourceRecordSetsListResponse {
     header?: Schema$ResponseHeader;
-    /**
-     * Type of resource.
-     */
     kind?: string | null;
     /**
      * The presence of this field indicates that there exist more results following your last page of results in pagination order. To fetch them, make another list request using this value as your pagination token. In this way you can retrieve the complete contents of even very large collections one page at a time. However, if the contents of the collection change between the first and last paginated list request, the set of all elements returned will be an inconsistent view of the collection. There is no way to retrieve a consistent snapshot of a collection larger than the maximum page size.
