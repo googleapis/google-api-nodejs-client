e1c3c69dd7c2d79d45e1be30e582ee7169c62edf
yoshi-automation <yoshi-automation@google.com>
2020-10-28 05:21:23 -0700

    feat(monitoring): update the API
    #### monitoring:v3
    The following keys were added:
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.alignmentPeriod.description
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.alignmentPeriod.format
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.alignmentPeriod.location
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.alignmentPeriod.type
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.crossSeriesReducer.description
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.crossSeriesReducer.enum
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.crossSeriesReducer.enumDescriptions
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.crossSeriesReducer.location
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.crossSeriesReducer.type
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.groupByFields.description
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.groupByFields.location
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.groupByFields.repeated
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.groupByFields.type
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.perSeriesAligner.description
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.perSeriesAligner.enum
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.perSeriesAligner.enumDescriptions
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.perSeriesAligner.location
    - resources.projects.resources.timeSeries.methods.list.parameters.secondaryAggregation.perSeriesAligner.type
    
    The following keys were changed:
    - schemas.HttpCheck.properties.body.description
    - schemas.HttpCheck.properties.contentType.description
    - schemas.HttpCheck.properties.contentType.enumDescriptions
    - schemas.MetricDescriptor.properties.launchStage.enumDescriptions
    - schemas.MetricDescriptorMetadata.properties.launchStage.enumDescriptions
    - schemas.MonitoredResourceDescriptor.properties.launchStage.enumDescriptions
    - schemas.MonitoringQueryLanguageCondition.description
    - schemas.MonitoringQueryLanguageCondition.properties.query.description
    - schemas.NotificationChannelDescriptor.properties.launchStage.enumDescriptions

--- src/apis/monitoring/v3.ts
+++ src/apis/monitoring/v3.ts
@@ -723,11 +723,11 @@ export namespace monitoring_v3 {
      */
     authInfo?: Schema$BasicAuthentication;
     /**
-     * The request body associated with the HTTP POST request. If content_type is URL_ENCODED, the body passed in must be URL-encoded. Users can provide a Content-Length header via the headers field or the API will do so. If the request_method is GET and body is not empty, the API will return an error. The maximum byte size is 1 megabyte. Note: As with all bytes fields JSON representations are base64 encoded. e.g.: &quot;foo=bar&quot; in URL-encoded form is &quot;foo%3Dbar&quot; and in base64 encoding is &quot;Zm9vJTI1M0RiYXI=&quot;.
+     * The request body associated with the HTTP POST request. If content_type is URL_ENCODED, the body passed in must be URL-encoded. Users can provide a Content-Length header via the headers field or the API will do so. If the request_method is GET and body is not empty, the API will return an error. The maximum byte size is 1 megabyte. Note: As with all bytes fields, JSON representations are base64 encoded. e.g.: &quot;foo=bar&quot; in URL-encoded form is &quot;foo%3Dbar&quot; and in base64 encoding is &quot;Zm9vJTI1M0RiYXI=&quot;.
      */
     body?: string | null;
     /**
-     * The content type to use for the check.
+     * The content type header to use for the check. The following configurations result in errors: 1. Content type is specified in both the headers field and the content_type field. 2. Request method is GET and content_type is not TYPE_UNSPECIFIED 3. Request method is POST and content_type is TYPE_UNSPECIFIED. 4. Request method is POST and a &quot;Content-Type&quot; header is provided via headers field. The content_type field should be used instead.
      */
     contentType?: string | null;
     /**
@@ -1251,7 +1251,7 @@ export namespace monitoring_v3 {
     userLabels?: {[key: string]: string} | null;
   }
   /**
-   * A condition type that allows alert policies to be defined using Monitoring Query Language.
+   * A condition type that allows alert policies to be defined using Monitoring Query Language (https://cloud.google.com/monitoring/mql).
    */
   export interface Schema$MonitoringQueryLanguageCondition {
     /**
@@ -1259,7 +1259,7 @@ export namespace monitoring_v3 {
      */
     duration?: string | null;
     /**
-     * Monitoring Query Language query that outputs a boolean stream.
+     * Monitoring Query Language (https://cloud.google.com/monitoring/mql) query that outputs a boolean stream.
      */
     query?: string | null;
     /**
@@ -6791,6 +6791,14 @@ export namespace monitoring_v3 {
      *     pageSize: 'placeholder-value',
      *     // If this field is not empty then it must contain the nextPageToken value returned by a previous call to this method. Using this field causes the method to return additional results from the previous method call.
      *     pageToken: 'placeholder-value',
+     *     // The alignment_period specifies a time interval, in seconds, that is used to divide the data in all the time series into consistent blocks of time. This will be done before the per-series aligner can be applied to the data.The value must be at least 60 seconds. If a per-series aligner other than ALIGN_NONE is specified, this field is required or an error is returned. If no per-series aligner is specified, or the aligner ALIGN_NONE is specified, then this field is ignored.The maximum value of the alignment_period is 104 weeks (2 years) for charts, and 90,000 seconds (25 hours) for alerting policies.
+     *     'secondaryAggregation.alignmentPeriod': 'placeholder-value',
+     *     // The reduction operation to be used to combine time series into a single time series, where the value of each data point in the resulting series is a function of all the already aligned values in the input time series.Not all reducer operations can be applied to all time series. The valid choices depend on the metric_kind and the value_type of the original time series. Reduction can yield a time series with a different metric_kind or value_type than the input time series.Time series data must first be aligned (see per_series_aligner) in order to perform cross-time series reduction. If cross_series_reducer is specified, then per_series_aligner must be specified, and must not be ALIGN_NONE. An alignment_period must also be specified; otherwise, an error is returned.
+     *     'secondaryAggregation.crossSeriesReducer': 'placeholder-value',
+     *     // The set of fields to preserve when cross_series_reducer is specified. The group_by_fields determine how the time series are partitioned into subsets prior to applying the aggregation operation. Each subset contains time series that have the same value for each of the grouping fields. Each individual time series is a member of exactly one subset. The cross_series_reducer is applied to each subset of time series. It is not possible to reduce across different resource types, so this field implicitly contains resource.type. Fields not specified in group_by_fields are aggregated away. If group_by_fields is not specified and all the time series have the same resource type, then the time series are aggregated into a single output time series. If cross_series_reducer is not defined, this field is ignored.
+     *     'secondaryAggregation.groupByFields': 'placeholder-value',
+     *     // An Aligner describes how to bring the data points in a single time series into temporal alignment. Except for ALIGN_NONE, all alignments cause all the data points in an alignment_period to be mathematically grouped together, resulting in a single data point for each alignment_period with end timestamp at the end of the period.Not all alignment operations may be applied to all time series. The valid choices depend on the metric_kind and value_type of the original time series. Alignment can change the metric_kind or the value_type of the time series.Time series data must be aligned in order to perform cross-time series reduction. If cross_series_reducer is specified, then per_series_aligner must be specified and not equal to ALIGN_NONE and alignment_period must be specified; otherwise, an error is returned.
+     *     'secondaryAggregation.perSeriesAligner': 'placeholder-value',
      *     // Required. Specifies which information is returned about the time series.
      *     view: 'placeholder-value',
      *   });
@@ -6825,6 +6833,10 @@ export namespace monitoring_v3 {
      * @param {string=} params.orderBy Unsupported: must be left blank. The points in each time series are currently returned in reverse time order (most recent to oldest).
      * @param {integer=} params.pageSize A positive number that is the maximum number of results to return. If page_size is empty or more than 100,000 results, the effective page_size is 100,000 results. If view is set to FULL, this is the maximum number of Points returned. If view is set to HEADERS, this is the maximum number of TimeSeries returned.
      * @param {string=} params.pageToken If this field is not empty then it must contain the nextPageToken value returned by a previous call to this method. Using this field causes the method to return additional results from the previous method call.
+     * @param {string=} params.secondaryAggregation.alignmentPeriod The alignment_period specifies a time interval, in seconds, that is used to divide the data in all the time series into consistent blocks of time. This will be done before the per-series aligner can be applied to the data.The value must be at least 60 seconds. If a per-series aligner other than ALIGN_NONE is specified, this field is required or an error is returned. If no per-series aligner is specified, or the aligner ALIGN_NONE is specified, then this field is ignored.The maximum value of the alignment_period is 104 weeks (2 years) for charts, and 90,000 seconds (25 hours) for alerting policies.
+     * @param {string=} params.secondaryAggregation.crossSeriesReducer The reduction operation to be used to combine time series into a single time series, where the value of each data point in the resulting series is a function of all the already aligned values in the input time series.Not all reducer operations can be applied to all time series. The valid choices depend on the metric_kind and the value_type of the original time series. Reduction can yield a time series with a different metric_kind or value_type than the input time series.Time series data must first be aligned (see per_series_aligner) in order to perform cross-time series reduction. If cross_series_reducer is specified, then per_series_aligner must be specified, and must not be ALIGN_NONE. An alignment_period must also be specified; otherwise, an error is returned.
+     * @param {string=} params.secondaryAggregation.groupByFields The set of fields to preserve when cross_series_reducer is specified. The group_by_fields determine how the time series are partitioned into subsets prior to applying the aggregation operation. Each subset contains time series that have the same value for each of the grouping fields. Each individual time series is a member of exactly one subset. The cross_series_reducer is applied to each subset of time series. It is not possible to reduce across different resource types, so this field implicitly contains resource.type. Fields not specified in group_by_fields are aggregated away. If group_by_fields is not specified and all the time series have the same resource type, then the time series are aggregated into a single output time series. If cross_series_reducer is not defined, this field is ignored.
+     * @param {string=} params.secondaryAggregation.perSeriesAligner An Aligner describes how to bring the data points in a single time series into temporal alignment. Except for ALIGN_NONE, all alignments cause all the data points in an alignment_period to be mathematically grouped together, resulting in a single data point for each alignment_period with end timestamp at the end of the period.Not all alignment operations may be applied to all time series. The valid choices depend on the metric_kind and value_type of the original time series. Alignment can change the metric_kind or the value_type of the time series.Time series data must be aligned in order to perform cross-time series reduction. If cross_series_reducer is specified, then per_series_aligner must be specified and not equal to ALIGN_NONE and alignment_period must be specified; otherwise, an error is returned.
      * @param {string=} params.view Required. Specifies which information is returned about the time series.
      * @param {object} [options] Optionally override request options, such as `url`, `method`, and `encoding`.
      * @param {callback} callback The callback that handles the response.
@@ -7127,6 +7139,22 @@ export namespace monitoring_v3 {
      * If this field is not empty then it must contain the nextPageToken value returned by a previous call to this method. Using this field causes the method to return additional results from the previous method call.
      */
     pageToken?: string;
+    /**
+     * The alignment_period specifies a time interval, in seconds, that is used to divide the data in all the time series into consistent blocks of time. This will be done before the per-series aligner can be applied to the data.The value must be at least 60 seconds. If a per-series aligner other than ALIGN_NONE is specified, this field is required or an error is returned. If no per-series aligner is specified, or the aligner ALIGN_NONE is specified, then this field is ignored.The maximum value of the alignment_period is 104 weeks (2 years) for charts, and 90,000 seconds (25 hours) for alerting policies.
+     */
+    'secondaryAggregation.alignmentPeriod'?: string;
+    /**
+     * The reduction operation to be used to combine time series into a single time series, where the value of each data point in the resulting series is a function of all the already aligned values in the input time series.Not all reducer operations can be applied to all time series. The valid choices depend on the metric_kind and the value_type of the original time series. Reduction can yield a time series with a different metric_kind or value_type than the input time series.Time series data must first be aligned (see per_series_aligner) in order to perform cross-time series reduction. If cross_series_reducer is specified, then per_series_aligner must be specified, and must not be ALIGN_NONE. An alignment_period must also be specified; otherwise, an error is returned.
+     */
+    'secondaryAggregation.crossSeriesReducer'?: string;
+    /**
+     * The set of fields to preserve when cross_series_reducer is specified. The group_by_fields determine how the time series are partitioned into subsets prior to applying the aggregation operation. Each subset contains time series that have the same value for each of the grouping fields. Each individual time series is a member of exactly one subset. The cross_series_reducer is applied to each subset of time series. It is not possible to reduce across different resource types, so this field implicitly contains resource.type. Fields not specified in group_by_fields are aggregated away. If group_by_fields is not specified and all the time series have the same resource type, then the time series are aggregated into a single output time series. If cross_series_reducer is not defined, this field is ignored.
+     */
+    'secondaryAggregation.groupByFields'?: string[];
+    /**
+     * An Aligner describes how to bring the data points in a single time series into temporal alignment. Except for ALIGN_NONE, all alignments cause all the data points in an alignment_period to be mathematically grouped together, resulting in a single data point for each alignment_period with end timestamp at the end of the period.Not all alignment operations may be applied to all time series. The valid choices depend on the metric_kind and value_type of the original time series. Alignment can change the metric_kind or the value_type of the time series.Time series data must be aligned in order to perform cross-time series reduction. If cross_series_reducer is specified, then per_series_aligner must be specified and not equal to ALIGN_NONE and alignment_period must be specified; otherwise, an error is returned.
+     */
+    'secondaryAggregation.perSeriesAligner'?: string;
     /**
      * Required. Specifies which information is returned about the time series.
      */
