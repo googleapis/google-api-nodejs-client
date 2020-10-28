5069b698d528ca33e3a20734629fa550b66aea7a
yoshi-automation <yoshi-automation@google.com>
2020-10-28 05:21:23 -0700

    fix(logging): update the API
    #### logging:v2
    The following keys were changed:
    - schemas.LogMetric.description
    - schemas.MetricDescriptor.properties.launchStage.enumDescriptions
    - schemas.MetricDescriptorMetadata.properties.launchStage.enumDescriptions
    - schemas.MonitoredResourceDescriptor.properties.launchStage.enumDescriptions

--- src/apis/logging/v2.ts
+++ src/apis/logging/v2.ts
@@ -653,7 +653,7 @@ export namespace logging_v2 {
     time?: string | null;
   }
   /**
-   * Describes a logs-based metric. The value of the metric is the number of log entries that match a logs filter in a given time interval.Logs-based metric can also be used to extract values from logs and create a a distribution of the values. The distribution records the statistics of the extracted values along with an optional histogram of the values as specified by the bucket options.
+   * Describes a logs-based metric. The value of the metric is the number of log entries that match a logs filter in a given time interval.Logs-based metrics can also be used to extract values from logs and create a distribution of the values. The distribution records the statistics of the extracted values along with an optional histogram of the values as specified by the bucket options.
    */
   export interface Schema$LogMetric {
     /**
