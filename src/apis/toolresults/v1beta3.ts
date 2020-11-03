9e034e4fc53b8de1955cc696446d5551d9fd56a5
yoshi-automation <yoshi-automation@google.com>
2020-11-03 05:22:10 -0800

    feat(toolresults): update the API
    #### toolresults:v1beta3
    The following keys were added:
    - schemas.DeviceOutOfMemory.description
    - schemas.DeviceOutOfMemory.id
    - schemas.DeviceOutOfMemory.type
    
    The following keys were changed:
    - schemas.TestIssue.properties.type.enum
    - schemas.TestIssue.properties.type.enumDescriptions

--- src/apis/toolresults/v1beta3.ts
+++ src/apis/toolresults/v1beta3.ts
@@ -308,6 +308,10 @@ export namespace toolresults_v1beta3 {
     crashPackage?: string | null;
   }
   /**
+   * A warning that device ran out of memory
+   */
+  export interface Schema$DeviceOutOfMemory {}
+  /**
    *  A Duration represents a signed, fixed-length span of time represented as a count of seconds and fractions of seconds at nanosecond resolution. It is independent of any calendar and concepts like "day" or "month". It is related to Timestamp in that the difference between two Timestamp values is a Duration and it can be added or subtracted from a Timestamp. Range is approximately +-10,000 years.
    */
   export interface Schema$Duration {
