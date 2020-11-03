bc35329473e887d3a752614f867aa9f7bd97dd1e
yoshi-automation <yoshi-automation@google.com>
2020-11-03 05:22:08 -0800

    feat(billingbudgets): update the API
    #### billingbudgets:v1beta1
    The following keys were added:
    - schemas.GoogleCloudBillingBudgetsV1beta1Filter.properties.creditTypes.description
    - schemas.GoogleCloudBillingBudgetsV1beta1Filter.properties.creditTypes.items.type
    - schemas.GoogleCloudBillingBudgetsV1beta1Filter.properties.creditTypes.type
    
    The following keys were changed:
    - schemas.GoogleCloudBillingBudgetsV1beta1Filter.properties.creditTypesTreatment.enum
    - schemas.GoogleCloudBillingBudgetsV1beta1Filter.properties.creditTypesTreatment.enumDescriptions
    
    #### billingbudgets:v1
    The following keys were added:
    - schemas.GoogleCloudBillingBudgetsV1Filter.properties.creditTypes.description
    - schemas.GoogleCloudBillingBudgetsV1Filter.properties.creditTypes.items.type
    - schemas.GoogleCloudBillingBudgetsV1Filter.properties.creditTypes.type
    
    The following keys were changed:
    - schemas.GoogleCloudBillingBudgetsV1Filter.properties.creditTypesTreatment.enum
    - schemas.GoogleCloudBillingBudgetsV1Filter.properties.creditTypesTreatment.enumDescriptions

--- src/apis/billingbudgets/v1beta1.ts
+++ src/apis/billingbudgets/v1beta1.ts
@@ -204,6 +204,10 @@ export namespace billingbudgets_v1beta1 {
    */
   export interface Schema$GoogleCloudBillingBudgetsV1beta1Filter {
     /**
+     * Optional. A list of credit types to be subtracted from gross cost to determine the spend for threshold calculations if and only if credit_types_treatment is INCLUDE_SPECIFIED_CREDITS. If credit_types_treatment is not INCLUDE_SPECIFIED_CREDITS, this field must be empty. See credits.type at https://cloud.google.com/billing/docs/how-to/export-data-bigquery-tables#data-schema for a list of acceptable credit type values in this field.
+     */
+    creditTypes?: string[] | null;
+    /**
      * Optional. If not set, default behavior is `INCLUDE_ALL_CREDITS`.
      */
     creditTypesTreatment?: string | null;
