d84cac44535dd692089e113f4005958183d24ba0
yoshi-automation <yoshi-automation@google.com>
2020-11-05 05:24:13 -0800

    feat(networkmanagement)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    #### networkmanagement:v1beta1
    The following keys were deleted:
    - schemas.Binding.properties.bindingId.type
    - schemas.LatencyDistribution.description
    - schemas.LatencyDistribution.id
    - schemas.LatencyDistribution.properties.latencyPercentiles.description
    - schemas.LatencyDistribution.properties.latencyPercentiles.items.$ref
    - schemas.LatencyDistribution.properties.latencyPercentiles.type
    - schemas.LatencyDistribution.type
    - schemas.LatencyPercentile.description
    - schemas.LatencyPercentile.id
    - schemas.LatencyPercentile.properties.latencyMicros.description
    - schemas.LatencyPercentile.properties.latencyMicros.format
    - schemas.LatencyPercentile.properties.latencyMicros.type
    - schemas.LatencyPercentile.properties.percent.description
    - schemas.LatencyPercentile.properties.percent.format
    - schemas.LatencyPercentile.properties.percent.type
    - schemas.LatencyPercentile.type
    - schemas.ProbingDetails.properties.probingLatency.$ref
    - schemas.ProbingDetails.properties.probingLatency.description

--- src/apis/networkmanagement/v1beta1.ts
+++ src/apis/networkmanagement/v1beta1.ts
@@ -166,7 +166,6 @@ export namespace networkmanagement_v1beta1 {
    * Associates `members` with a `role`.
    */
   export interface Schema$Binding {
-    bindingId?: string | null;
     /**
      * The condition that is associated with this binding. If the condition evaluates to `true`, then this binding applies to the current request. If the condition evaluates to `false`, then this binding does not apply to the current request. However, a different role binding might grant the same role to one or more of the members in this binding. To learn which resources support conditions in their IAM policies, see the [IAM documentation](https://cloud.google.com/iam/help/conditions/resource-policies).
      */
@@ -529,28 +528,6 @@ export namespace networkmanagement_v1beta1 {
     uri?: string | null;
   }
   /**
-   * Describes measured latency distribution.
-   */
-  export interface Schema$LatencyDistribution {
-    /**
-     * Representative latency percentiles.
-     */
-    latencyPercentiles?: Schema$LatencyPercentile[];
-  }
-  /**
-   * Latency percentile rank and value.
-   */
-  export interface Schema$LatencyPercentile {
-    /**
-     * percent-th percentile of latency observed, in microseconds. Fraction of percent/100 of samples have latency lower or equal to the value of this field.
-     */
-    latencyMicros?: string | null;
-    /**
-     * Percentage of samples this data point applies to.
-     */
-    percent?: number | null;
-  }
-  /**
    * Response for the `ListConnectivityTests` method.
    */
   export interface Schema$ListConnectivityTestsResponse {
@@ -780,10 +757,6 @@ export namespace networkmanagement_v1beta1 {
      * The details of an internal failure or a cancellation of reachability analysis.
      */
     error?: Schema$Status;
-    /**
-     * One way probing latency distribution. The latency is measured as duration of packet traversal of Google Cloud network, from source to destination endpoint.
-     */
-    probingLatency?: Schema$LatencyDistribution;
     /**
      * The overall reachability result of the test.
      */
