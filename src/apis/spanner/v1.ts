7d504fee11a64793693bf4ddc6b02c597c934073
yoshi-automation <yoshi-automation@google.com>
2020-11-09 05:20:30 -0800

    feat(spanner)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    #### spanner:v1
    The following keys were deleted:
    - schemas.Binding.properties.bindingId.description
    
    The following keys were changed:
    - schemas.Field.properties.name.description

--- src/apis/spanner/v1.ts
+++ src/apis/spanner/v1.ts
@@ -208,9 +208,6 @@ export namespace spanner_v1 {
    * Associates `members` with a `role`.
    */
   export interface Schema$Binding {
-    /**
-     * A client-specified ID for this binding. Expected to be globally unique to support the internal bindings-by-ID API.
-     */
     bindingId?: string | null;
     /**
      * The condition that is associated with this binding. If the condition evaluates to `true`, then this binding applies to the current request. If the condition evaluates to `false`, then this binding does not apply to the current request. However, a different role binding might grant the same role to one or more of the members in this binding. To learn which resources support conditions in their IAM policies, see the [IAM documentation](https://cloud.google.com/iam/help/conditions/resource-policies).
@@ -489,7 +486,7 @@ export namespace spanner_v1 {
    */
   export interface Schema$Field {
     /**
-     * The name of the field. For reads, this is the column name. For SQL queries, it is the column alias (e.g., `"Word"` in the query `"SELECT 'hello' AS Word"`), or the column name (e.g., `"ColName"` in the query `"SELECT ColName FROM Table"`). Some columns might have an empty name (e.g., !"SELECT UPPER(ColName)"`). Note that a query result can contain multiple fields with the same name.
+     * The name of the field. For reads, this is the column name. For SQL queries, it is the column alias (e.g., `"Word"` in the query `"SELECT 'hello' AS Word"`), or the column name (e.g., `"ColName"` in the query `"SELECT ColName FROM Table"`). Some columns might have an empty name (e.g., `"SELECT UPPER(ColName)"`). Note that a query result can contain multiple fields with the same name.
      */
     name?: string | null;
     /**
