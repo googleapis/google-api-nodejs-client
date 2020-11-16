3ff6030fb9fdc4e2146ce49256f85f6e581f9af6
yoshi-automation <yoshi-automation@google.com>
2020-11-16 05:20:16 -0800

    fix(servicedirectory): update the API
    #### servicedirectory:v1beta1
    The following keys were changed:
    - schemas.Endpoint.properties.metadata.description
    - schemas.Service.properties.metadata.description

--- src/apis/servicedirectory/v1beta1.ts
+++ src/apis/servicedirectory/v1beta1.ts
@@ -154,7 +154,7 @@ export namespace servicedirectory_v1beta1 {
      */
     address?: string | null;
     /**
-     * Optional. Metadata for the endpoint. This data can be consumed by service clients. The entire metadata dictionary may contain up to 512 characters, spread accoss all key-value pairs. Metadata that goes beyond any these limits will be rejected.
+     * Optional. Metadata for the endpoint. This data can be consumed by service clients. Restrictions: - The entire metadata dictionary may contain up to 512 characters, spread accoss all key-value pairs. Metadata that goes beyond any these limits will be rejected. - Valid metadata keys have two segments: an optional prefix and name, separated by a slash (/). The name segment is required and must be 63 characters or less, beginning and ending with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_), dots (.), and alphanumerics between. The prefix is optional. If specified, the prefix must be a DNS subdomain: a series of DNS labels separated by dots (.), not longer than 253 characters in total, followed by a slash (/). Metadata that fails to meet these requirements will be rejected. - The '(*.)google.com/' and '(*.)googleapis.com/' prefixes are reserved for system metadata managed by Service Directory. If the user tries to write to these keyspaces, those entries will be silently ignored by the system.
      */
     metadata?: {[key: string]: string} | null;
     /**
@@ -340,7 +340,7 @@ export namespace servicedirectory_v1beta1 {
      */
     endpoints?: Schema$Endpoint[];
     /**
-     * Optional. Metadata for the service. This data can be consumed by service clients. The entire metadata dictionary may contain up to 2000 characters, spread across all key-value pairs. Metadata that goes beyond any these limits will be rejected.
+     * Optional. Metadata for the service. This data can be consumed by service clients. Restrictions: - The entire metadata dictionary may contain up to 2000 characters, spread accoss all key-value pairs. Metadata that goes beyond any these limits will be rejected. - Valid metadata keys have two segments: an optional prefix and name, separated by a slash (/). The name segment is required and must be 63 characters or less, beginning and ending with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_), dots (.), and alphanumerics between. The prefix is optional. If specified, the prefix must be a DNS subdomain: a series of DNS labels separated by dots (.), not longer than 253 characters in total, followed by a slash (/). Metadata that fails to meet these requirements will be rejected. - The '(*.)google.com/' and '(*.)googleapis.com/' prefixes are reserved for system metadata managed by Service Directory. If the user tries to write to these keyspaces, those entries will be silently ignored by the system.
      */
     metadata?: {[key: string]: string} | null;
     /**
