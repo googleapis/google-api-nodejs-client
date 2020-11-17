b23043722446f117c555692e6e1ea1aa7e48040e
yoshi-automation <yoshi-automation@google.com>
2020-11-17 05:20:39 -0800

    fix(assuredworkloads): update the API

--- src/apis/assuredworkloads/index.ts
+++ src/apis/assuredworkloads/index.ts
@@ -14,27 +14,44 @@
 /*! THIS FILE IS AUTO-GENERATED */
 
 import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
+import {assuredworkloads_v1} from './v1';
 import {assuredworkloads_v1beta1} from './v1beta1';
 
 export const VERSIONS = {
+  v1: assuredworkloads_v1.Assuredworkloads,
   v1beta1: assuredworkloads_v1beta1.Assuredworkloads,
 };
 
+export function assuredworkloads(
+  version: 'v1'
+): assuredworkloads_v1.Assuredworkloads;
+export function assuredworkloads(
+  options: assuredworkloads_v1.Options
+): assuredworkloads_v1.Assuredworkloads;
 export function assuredworkloads(
   version: 'v1beta1'
 ): assuredworkloads_v1beta1.Assuredworkloads;
 export function assuredworkloads(
   options: assuredworkloads_v1beta1.Options
 ): assuredworkloads_v1beta1.Assuredworkloads;
-export function assuredworkloads<T = assuredworkloads_v1beta1.Assuredworkloads>(
+export function assuredworkloads<
+  T =
+    | assuredworkloads_v1.Assuredworkloads
+    | assuredworkloads_v1beta1.Assuredworkloads
+>(
   this: GoogleConfigurable,
-  versionOrOptions: 'v1beta1' | assuredworkloads_v1beta1.Options
+  versionOrOptions:
+    | 'v1'
+    | assuredworkloads_v1.Options
+    | 'v1beta1'
+    | assuredworkloads_v1beta1.Options
 ) {
   return getAPI<T>('assuredworkloads', versionOrOptions, VERSIONS, this);
 }
 
 const auth = new AuthPlus();
 export {auth};
+export {assuredworkloads_v1};
 export {assuredworkloads_v1beta1};
 export {
   AuthPlus,
