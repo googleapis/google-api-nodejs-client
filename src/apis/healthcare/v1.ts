aceb09a9875fc05c50e07d9a84ec7a2462dcc3bf
yoshi-automation <yoshi-automation@google.com>
2020-11-30 05:19:45 -0800

    fix(healthcare): update the API
    #### healthcare:v1beta1
    The following keys were changed:
    - schemas.FhirStore.properties.disableReferentialIntegrity.description
    - schemas.FhirStore.properties.disableResourceVersioning.description
    - schemas.FhirStore.properties.version.description
    
    #### healthcare:v1
    The following keys were changed:
    - schemas.FhirStore.properties.disableReferentialIntegrity.description
    - schemas.FhirStore.properties.disableResourceVersioning.description
    - schemas.FhirStore.properties.version.description

--- src/apis/healthcare/v1.ts
+++ src/apis/healthcare/v1.ts
@@ -421,11 +421,11 @@ export namespace healthcare_v1 {
    */
   export interface Schema$FhirStore {
     /**
-     * Whether to disable referential integrity in this FHIR store. This field is immutable after FHIR store creation. The default value is false, meaning that the API enforces referential integrity and fails the requests that result in inconsistent state in the FHIR store. When this field is set to true, the API skips referential integrity checks. Consequently, operations that rely on references, such as GetPatientEverything, do not return all the results if broken references exist.
+     * Immutable. Whether to disable referential integrity in this FHIR store. This field is immutable after FHIR store creation. The default value is false, meaning that the API enforces referential integrity and fails the requests that result in inconsistent state in the FHIR store. When this field is set to true, the API skips referential integrity checks. Consequently, operations that rely on references, such as GetPatientEverything, do not return all the results if broken references exist.
      */
     disableReferentialIntegrity?: boolean | null;
     /**
-     * Whether to disable resource versioning for this FHIR store. This field can not be changed after the creation of FHIR store. If set to false, which is the default behavior, all write operations cause historical versions to be recorded automatically. The historical versions can be fetched through the history APIs, but cannot be updated. If set to true, no historical versions are kept. The server sends errors for attempts to read the historical versions.
+     * Immutable. Whether to disable resource versioning for this FHIR store. This field can not be changed after the creation of FHIR store. If set to false, which is the default behavior, all write operations cause historical versions to be recorded automatically. The historical versions can be fetched through the history APIs, but cannot be updated. If set to true, no historical versions are kept. The server sends errors for attempts to read the historical versions.
      */
     disableResourceVersioning?: boolean | null;
     /**
@@ -449,7 +449,7 @@ export namespace healthcare_v1 {
      */
     streamConfigs?: Schema$StreamConfig[];
     /**
-     * The FHIR specification version that this FHIR store supports natively. This field is immutable after store creation. Requests are rejected if they contain FHIR resources of a different version. Version is required for every FHIR store.
+     * Immutable. The FHIR specification version that this FHIR store supports natively. This field is immutable after store creation. Requests are rejected if they contain FHIR resources of a different version. Version is required for every FHIR store.
      */
     version?: string | null;
   }
