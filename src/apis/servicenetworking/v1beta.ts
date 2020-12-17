a1d1442547e4cf0117e7fd902a8faa6b1b90ed1f
yoshi-automation <yoshi-automation@google.com>
2020-12-17 05:18:52 -0800

    fix(servicenetworking): update the API
    #### servicenetworking:v1beta
    
    The following keys were changed:
    - schemas.Endpoint.description
    - schemas.Service.properties.configVersion.description
    
    #### servicenetworking:v1
    
    The following keys were changed:
    - schemas.Endpoint.description
    - schemas.Service.properties.configVersion.description

--- src/apis/servicenetworking/v1beta.ts
+++ src/apis/servicenetworking/v1beta.ts
@@ -611,7 +611,7 @@ export namespace servicenetworking_v1beta {
     selector?: string | null;
   }
   /**
-   * `Endpoint` describes a network endpoint that serves a set of APIs. A service may expose any number of endpoints, and all endpoints share the same service configuration, such as quota configuration and monitoring configuration. Example service configuration: name: library-example.googleapis.com endpoints: # Below entry makes 'google.example.library.v1.Library' # API be served from endpoint address library-example.googleapis.com. # It also allows HTTP OPTIONS calls to be passed to the backend, for # it to decide whether the subsequent cross-origin request is # allowed to proceed. - name: library-example.googleapis.com allow_cors: true
+   * `Endpoint` describes a network endpoint of a service that serves a set of APIs. It is commonly known as a service endpoint. A service may expose any number of service endpoints, and all service endpoints share the same service definition, such as quota limits and monitoring metrics. Example service configuration: name: library-example.googleapis.com endpoints: # Below entry makes 'google.example.library.v1.Library' # API be served from endpoint address library-example.googleapis.com. # It also allows HTTP OPTIONS calls to be passed to the backend, for # it to decide whether the subsequent cross-origin request is # allowed to proceed. - name: library-example.googleapis.com allow_cors: true
    */
   export interface Schema$Endpoint {
     /**
@@ -1330,7 +1330,7 @@ export namespace servicenetworking_v1beta {
      */
     billing?: Schema$Billing;
     /**
-     * This field is obsolete. Its value must be set to `3`.
+     * Deprecated. The service config compiler always sets this field to `3`.
      */
     configVersion?: number | null;
     /**
