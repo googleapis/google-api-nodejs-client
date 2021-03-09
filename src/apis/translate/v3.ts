ddd342c29d3b63ea14f66c2555ea5014b16faac5
yoshi-automation <yoshi-automation@google.com>
2021-03-09 05:18:22 -0800

    fix(translate): update the API
    #### translate:v3beta1
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.operations.methods.wait.description
    - schemas.BatchTranslateTextRequest.properties.inputConfigs.description
    
    #### translate:v3
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.operations.methods.wait.description
    - schemas.BatchTranslateTextRequest.properties.inputConfigs.description

--- src/apis/translate/v3.ts
+++ src/apis/translate/v3.ts
@@ -134,7 +134,7 @@ export namespace translate_v3 {
      */
     glossaries?: {[key: string]: Schema$TranslateTextGlossaryConfig} | null;
     /**
-     * Required. Input configurations. The total number of files matched should be <= 1000. The total content size should be <= 100M Unicode codepoints. The files must use UTF-8 encoding.
+     * Required. Input configurations. The total number of files matched should be <= 100. The total content size should be <= 100M Unicode codepoints. The files must use UTF-8 encoding.
      */
     inputConfigs?: Schema$InputConfig[];
     /**
@@ -3148,7 +3148,7 @@ export namespace translate_v3 {
     }
 
     /**
-     * Waits for the specified long-running operation until it is done or reaches at most a specified timeout, returning the latest state. If the operation is already done, the latest state is immediately returned. If the timeout specified is greater than the default HTTP/RPC timeout, the HTTP/RPC timeout is used. If the server does not support this method, it returns `google.rpc.Code.UNIMPLEMENTED`. Note that this method is on a best-effort basis. It may return the latest state before the specified timeout (including immediately), meaning even an immediate response is no guarantee that the operation is done.
+     * Waits until the specified long-running operation is done or reaches at most a specified timeout, returning the latest state. If the operation is already done, the latest state is immediately returned. If the timeout specified is greater than the default HTTP/RPC timeout, the HTTP/RPC timeout is used. If the server does not support this method, it returns `google.rpc.Code.UNIMPLEMENTED`. Note that this method is on a best-effort basis. It may return the latest state before the specified timeout (including immediately), meaning even an immediate response is no guarantee that the operation is done.
      * @example
      * ```js
      * // Before running the sample:
