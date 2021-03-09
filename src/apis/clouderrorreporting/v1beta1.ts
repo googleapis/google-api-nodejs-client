66392e635bee0b6e8edc2dea79e79ac6e069fa9b
yoshi-automation <yoshi-automation@google.com>
2021-03-09 05:18:10 -0800

    fix(clouderrorreporting): update the API
    #### clouderrorreporting:v1beta1
    
    The following keys were changed:
    - resources.projects.resources.events.methods.report.description
    - resources.projects.resources.groupStats.methods.list.parameters.projectName.description
    - schemas.ErrorGroup.properties.resolutionStatus.enumDescriptions

--- src/apis/clouderrorreporting/v1beta1.ts
+++ src/apis/clouderrorreporting/v1beta1.ts
@@ -707,7 +707,7 @@ export namespace clouderrorreporting_v1beta1 {
     }
 
     /**
-     * Report an individual error event. This endpoint accepts **either** an OAuth token, **or** an [API key](https://support.google.com/cloud/answer/6158862) for authentication. To use an API key, append it to the URL as the value of a `key` parameter. For example: `POST https://clouderrorreporting.googleapis.com/v1beta1/{projectName\}/events:report?key=123ABC456`
+     * Report an individual error event and record the event to a log. This endpoint accepts **either** an OAuth token, **or** an [API key](https://support.google.com/cloud/answer/6158862) for authentication. To use an API key, append it to the URL as the value of a `key` parameter. For example: `POST https://clouderrorreporting.googleapis.com/v1beta1/{projectName\}/events:report?key=123ABC456` **Note:** [Error Reporting](/error-reporting) is a global service built on Cloud Logging and doesn't analyze logs stored in regional log buckets or logs routed to other Google Cloud projects. For more information, see [Using Error Reporting with regionalized logs](/error-reporting/docs/regionalization).
      * @example
      * ```js
      * // Before running the sample:
@@ -1247,7 +1247,7 @@ export namespace clouderrorreporting_v1beta1 {
      *     pageSize: 'placeholder-value',
      *     // Optional. A `next_page_token` provided by a previous response. To view additional results, pass this token along with the identical query parameters as the first request.
      *     pageToken: 'placeholder-value',
-     *     // Required. The resource name of the Google Cloud Platform project. Written as `projects/{projectID\}`, where `{projectID\}` is the [Google Cloud Platform project ID](https://support.google.com/cloud/answer/6158840). Example: `projects/my-project-123`.
+     *     // Required. The resource name of the Google Cloud Platform project. Written as `projects/{projectID\}` or `projects/{projectNumber\}`, where `{projectID\}` and `{projectNumber\}` can be found in the [Google Cloud Console](https://support.google.com/cloud/answer/6158840). Examples: `projects/my-project-123`, `projects/5551234`.
      *     projectName: 'projects/my-project',
      *     // Optional. The exact value to match against [`ServiceContext.resource_type`](/error-reporting/reference/rest/v1beta1/ServiceContext#FIELDS.resource_type).
      *     'serviceFilter.resourceType': 'placeholder-value',
@@ -1395,7 +1395,7 @@ export namespace clouderrorreporting_v1beta1 {
      */
     pageToken?: string;
     /**
-     * Required. The resource name of the Google Cloud Platform project. Written as `projects/{projectID\}`, where `{projectID\}` is the [Google Cloud Platform project ID](https://support.google.com/cloud/answer/6158840). Example: `projects/my-project-123`.
+     * Required. The resource name of the Google Cloud Platform project. Written as `projects/{projectID\}` or `projects/{projectNumber\}`, where `{projectID\}` and `{projectNumber\}` can be found in the [Google Cloud Console](https://support.google.com/cloud/answer/6158840). Examples: `projects/my-project-123`, `projects/5551234`.
      */
     projectName?: string;
     /**
