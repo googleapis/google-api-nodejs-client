a5399044f56cdfbdf1e19eba1636c6b8b1e12cd4
yoshi-automation <yoshi-automation@google.com>
2021-03-25 01:57:20 -0700

    fix(cloudtasks): update the API
    #### cloudtasks:v2beta2
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.filter.description
    - resources.projects.resources.locations.methods.list.parameters.pageSize.description
    - resources.projects.resources.locations.methods.list.parameters.pageToken.description
    
    #### cloudtasks:v2beta3
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.filter.description
    - resources.projects.resources.locations.methods.list.parameters.pageSize.description
    - resources.projects.resources.locations.methods.list.parameters.pageToken.description
    
    #### cloudtasks:v2
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.filter.description
    - resources.projects.resources.locations.methods.list.parameters.pageSize.description
    - resources.projects.resources.locations.methods.list.parameters.pageToken.description

--- src/apis/cloudtasks/v2beta2.ts
+++ src/apis/cloudtasks/v2beta2.ts
@@ -851,13 +851,13 @@ export namespace cloudtasks_v2beta2 {
      *
      *   // Do the magic
      *   const res = await cloudtasks.projects.locations.list({
-     *     // The standard list filter.
+     *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like "displayName=tokyo", and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
      *     // The resource that owns the locations collection, if applicable.
      *     name: 'projects/my-project',
-     *     // The standard list page size.
+     *     // The maximum number of results to return. If not set, the service will select a default.
      *     pageSize: 'placeholder-value',
-     *     // The standard list page token.
+     *     // A page token received from the `next_page_token` field in the response. Send that page token to receive the subsequent page.
      *     pageToken: 'placeholder-value',
      *   });
      *   console.log(res.data);
@@ -976,7 +976,7 @@ export namespace cloudtasks_v2beta2 {
   export interface Params$Resource$Projects$Locations$List
     extends StandardParameters {
     /**
-     * The standard list filter.
+     * A filter to narrow down results to a preferred subset. The filtering language accepts strings like "displayName=tokyo", and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      */
     filter?: string;
     /**
@@ -984,11 +984,11 @@ export namespace cloudtasks_v2beta2 {
      */
     name?: string;
     /**
-     * The standard list page size.
+     * The maximum number of results to return. If not set, the service will select a default.
      */
     pageSize?: number;
     /**
-     * The standard list page token.
+     * A page token received from the `next_page_token` field in the response. Send that page token to receive the subsequent page.
      */
     pageToken?: string;
   }
