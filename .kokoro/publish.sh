f152704a9305ef0ea2a42221f7839e8396ef92b8
Benjamin E. Coe <bencoe@google.com>
2021-03-25 10:02:16 -0700

    build: publish packages for each individual API (#2557)

--- .kokoro/publish.sh
+++ .kokoro/publish.sh
@@ -16,7 +16,13 @@
 
 set -eo pipefail
 
-export NPM_CONFIG_PREFIX=${HOME}/.npm-global
+if [ -z "${AUTORELEASE_PR}" ]
+then
+  echo "Need to specify AUTORELEASE_PR environment variable"
+  exit 1
+fi
+
+export NPM_CONFIG_PREFIX=/home/node/.npm-global
 
 # Start the releasetool reporter
 python3 -m pip install gcp-releasetool
@@ -24,8 +30,7 @@ python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /
 
 cd $(dirname $0)/..
 
-NPM_TOKEN=$(cat $KOKORO_GFILE_DIR/secret_manager/npm_publish_token)
-echo "//wombat-dressing-room.appspot.com/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+NPM_TOKEN=$(cat "${KOKORO_GFILE_DIR}/secret_manager/repo_automation_bots_npm_publish_token")
+printf "//wombat-dressing-room.appspot.com/:_authToken=${NPM_TOKEN}\nregistry=https://wombat-dressing-room.appspot.com" > ~/.npmrc
 
-npm install
-npm publish --access=public --registry=https://wombat-dressing-room.appspot.com
+npx @google-cloud/mono-repo-publish --pr-url="${AUTORELEASE_PR}"
