62d1e8d3affac5fa99992fb3a52e5f7577759b65
yoshi-automation <yoshi-automation@google.com>
2021-03-25 01:57:20 -0700

    feat(bigquery): update the API
    #### bigquery:v2
    
    The following keys were added:
    - schemas.JobStatistics.properties.sessionInfoTemplate.$ref
    - schemas.JobStatistics.properties.sessionInfoTemplate.description
    - schemas.QueryResponse.properties.sessionInfoTemplate.$ref
    - schemas.QueryResponse.properties.sessionInfoTemplate.description
    - schemas.SessionInfo.id
    - schemas.SessionInfo.properties.sessionId.description
    - schemas.SessionInfo.properties.sessionId.type
    - schemas.SessionInfo.type
    - schemas.TableFieldSchema.properties.maxLength.description
    - schemas.TableFieldSchema.properties.maxLength.format
    - schemas.TableFieldSchema.properties.maxLength.type
    - schemas.TableFieldSchema.properties.precision.description
    - schemas.TableFieldSchema.properties.precision.format
    - schemas.TableFieldSchema.properties.precision.type
    - schemas.TableFieldSchema.properties.scale.description
    - schemas.TableFieldSchema.properties.scale.format
    - schemas.TableFieldSchema.properties.scale.type
    
    The following keys were changed:
    - auth.oauth2.scopes.https://www.googleapis.com/auth/cloud-platform.description
    - schemas.StandardSqlDataType.properties.typeKind.enum
    - schemas.StandardSqlDataType.properties.typeKind.enumDescriptions

--- src/apis/bigquery/v2.ts
+++ src/apis/bigquery/v2.ts
@@ -1753,6 +1753,10 @@ export namespace bigquery_v2 {
      */
     scriptStatistics?: Schema$ScriptStatistics;
     /**
+     * [Output-only] [Preview] Information of the session if this job is part of one.
+     */
+    sessionInfoTemplate?: Schema$SessionInfo;
+    /**
      * [Output-only] Start time of this job, in milliseconds since the epoch. This field will be present when the job transitions from the PENDING state to either RUNNING or DONE.
      */
     startTime?: string | null;
@@ -2329,6 +2333,10 @@ export namespace bigquery_v2 {
      */
     schema?: Schema$TableSchema;
     /**
+     * [Output-only] [Preview] Information of the session if this job is part of one.
+     */
+    sessionInfoTemplate?: Schema$SessionInfo;
+    /**
      * The total number of bytes processed for this query. If this query was a dry run, this is the number of bytes that would be processed if the query were run.
      */
     totalBytesProcessed?: string | null;
@@ -2583,6 +2591,12 @@ export namespace bigquery_v2 {
      * Stack trace showing the line/column/procedure name of each frame on the stack at the point where the current evaluation happened. The leaf frame is first, the primary script is last. Never empty.
      */
     stackFrames?: Schema$ScriptStackFrame[];
+  }
+  export interface Schema$SessionInfo {
+    /**
+     * [Output-only] // [Preview] Id of the session.
+     */
+    sessionId?: string | null;
   }
   /**
    * Request message for `SetIamPolicy` method.
@@ -2852,6 +2866,10 @@ export namespace bigquery_v2 {
      */
     fields?: Schema$TableFieldSchema[];
     /**
+     * [Optional] Maximum length of values of this field for STRINGS or BYTES. If max_length is not specified, no maximum length constraint is imposed on this field. If type = "STRING", then max_length represents the maximum UTF-8 length of strings in this field. If type = "BYTES", then max_length represents the maximum number of bytes in this field. It is invalid to set this field if type ≠ "STRING" and ≠ "BYTES".
+     */
+    maxLength?: string | null;
+    /**
      * [Optional] The field mode. Possible values include NULLABLE, REQUIRED and REPEATED. The default value is NULLABLE.
      */
     mode?: string | null;
@@ -2860,6 +2878,14 @@ export namespace bigquery_v2 {
      */
     name?: string | null;
     policyTags?: {names?: string[]} | null;
+    /**
+     * [Optional] Precision (maximum number of total digits in base 10) and scale (maximum number of digits in the fractional part in base 10) constraints for values of this field for NUMERIC or BIGNUMERIC. It is invalid to set precision or scale if type ≠ "NUMERIC" and ≠ "BIGNUMERIC". If precision and scale are not specified, no value range constraint is imposed on this field insofar as values are permitted by the type. Values of this NUMERIC or BIGNUMERIC field must be in this range when: - Precision (P) and scale (S) are specified: [-10P-S + 10-S, 10P-S - 10-S] - Precision (P) is specified but not scale (and thus scale is interpreted to be equal to zero): [-10P + 1, 10P - 1]. Acceptable values for precision and scale if both are specified: - If type = "NUMERIC": 1 ≤ precision - scale ≤ 29 and 0 ≤ scale ≤ 9. - If type = "BIGNUMERIC": 1 ≤ precision - scale ≤ 38 and 0 ≤ scale ≤ 38. Acceptable values for precision if only precision is specified but not scale (and thus scale is interpreted to be equal to zero): - If type = "NUMERIC": 1 ≤ precision ≤ 29. - If type = "BIGNUMERIC": 1 ≤ precision ≤ 38. If scale is specified but not precision, then it is invalid.
+     */
+    precision?: string | null;
+    /**
+     * [Optional] See documentation for precision.
+     */
+    scale?: string | null;
     /**
      * [Required] The field data type. Possible values include STRING, BYTES, INTEGER, INT64 (same as INTEGER), FLOAT, FLOAT64 (same as FLOAT), NUMERIC, BIGNUMERIC, BOOLEAN, BOOL (same as BOOLEAN), TIMESTAMP, DATE, TIME, DATETIME, RECORD (where RECORD indicates that the field contains a nested schema) or STRUCT (same as RECORD).
      */
@@ -5061,6 +5087,7 @@ export namespace bigquery_v2 {
      *   //   "pageToken": "my_pageToken",
      *   //   "rows": [],
      *   //   "schema": {},
+     *   //   "sessionInfoTemplate": {},
      *   //   "totalBytesProcessed": "my_totalBytesProcessed",
      *   //   "totalRows": "my_totalRows"
      *   // }
