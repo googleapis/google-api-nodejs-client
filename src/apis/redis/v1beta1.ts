2ae24f6a6673e5120311387a2af3fc513dd58f3f
yoshi-automation <yoshi-automation@google.com>
2021-03-26 01:59:03 -0700

    fix(redis): update the API
    #### redis:v1beta1
    
    The following keys were changed:
    - schemas.Instance.properties.reservedIpRange.description
    
    #### redis:v1
    
    The following keys were changed:
    - schemas.Instance.properties.reservedIpRange.description

--- src/apis/redis/v1beta1.ts
+++ src/apis/redis/v1beta1.ts
@@ -300,7 +300,7 @@ export namespace redis_v1beta1 {
      */
     redisVersion?: string | null;
     /**
-     * Optional. The CIDR range of internal addresses that are reserved for this instance. If not provided, the service will choose an unused /29 block, for example, 10.0.0.0/29 or 192.168.0.0/29. Ranges must be unique and non-overlapping with existing subnets in an authorized network.
+     * Optional. For DIRECT_PEERING mode, the CIDR range of internal addresses that are reserved for this instance. Range must be unique and non-overlapping with existing subnets in an authorized network. For PRIVATE_SERVICE_ACCESS mode, the name of one allocated IP address ranges associated with this private service access connection. If not provided, the service will choose an unused /29 block, for example, 10.0.0.0/29 or 192.168.0.0/29.
      */
     reservedIpRange?: string | null;
     /**
