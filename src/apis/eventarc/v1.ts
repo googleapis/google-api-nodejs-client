f206776e6fcd4aa97e2140cca4f596f30c3f1647
yoshi-automation <yoshi-automation@google.com>
2021-03-26 01:59:02 -0700

    fix(eventarc): update the API
    #### eventarc:v1
    
    The following keys were changed:
    - schemas.CloudRun.properties.service.description

--- src/apis/eventarc/v1.ts
+++ src/apis/eventarc/v1.ts
@@ -181,7 +181,7 @@ export namespace eventarc_v1 {
      */
     region?: string | null;
     /**
-     * Required. The name of the Cloud run service being addressed (see https://cloud.google.com/run/docs/reference/rest/v1/namespaces.services). Only services located in the same project of the trigger object can be addressed.
+     * Required. The name of the Cloud Run service being addressed. See https://cloud.google.com/run/docs/reference/rest/v1/namespaces.services. Only services located in the same project of the trigger object can be addressed.
      */
     service?: string | null;
   }
