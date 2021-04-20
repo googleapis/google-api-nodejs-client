504d86050b2283450a73601e3f002ab8c8c3303a
yoshi-automation <yoshi-automation@google.com>
2021-04-20 02:02:35 -0700

    feat(cloudresourcemanager): update the API
    #### cloudresourcemanager:v1beta1
    
    The following keys were added:
    - schemas.CreateTagBindingMetadata.description
    - schemas.CreateTagBindingMetadata.id
    - schemas.CreateTagBindingMetadata.type
    - schemas.DeleteTagBindingMetadata.description
    - schemas.DeleteTagBindingMetadata.id
    - schemas.DeleteTagBindingMetadata.type
    
    #### cloudresourcemanager:v2beta1
    
    The following keys were added:
    - schemas.CreateTagBindingMetadata.description
    - schemas.CreateTagBindingMetadata.id
    - schemas.CreateTagBindingMetadata.type
    - schemas.DeleteTagBindingMetadata.description
    - schemas.DeleteTagBindingMetadata.id
    - schemas.DeleteTagBindingMetadata.type
    
    #### cloudresourcemanager:v1
    
    The following keys were added:
    - schemas.CreateTagBindingMetadata.description
    - schemas.CreateTagBindingMetadata.id
    - schemas.CreateTagBindingMetadata.type
    - schemas.DeleteTagBindingMetadata.description
    - schemas.DeleteTagBindingMetadata.id
    - schemas.DeleteTagBindingMetadata.type
    
    #### cloudresourcemanager:v2
    
    The following keys were added:
    - schemas.CreateTagBindingMetadata.description
    - schemas.CreateTagBindingMetadata.id
    - schemas.CreateTagBindingMetadata.type
    - schemas.DeleteTagBindingMetadata.description
    - schemas.DeleteTagBindingMetadata.id
    - schemas.DeleteTagBindingMetadata.type
    
    #### cloudresourcemanager:v3
    
    The following keys were added:
    - schemas.CreateTagBindingMetadata.description
    - schemas.CreateTagBindingMetadata.id
    - schemas.CreateTagBindingMetadata.type
    - schemas.DeleteTagBindingMetadata.description
    - schemas.DeleteTagBindingMetadata.id
    - schemas.DeleteTagBindingMetadata.type
    
    The following keys were changed:
    - resources.projects.methods.move.description
    - resources.projects.methods.search.parameters.query.description

--- src/apis/cloudresourcemanager/v3.ts
+++ src/apis/cloudresourcemanager/v3.ts
@@ -255,6 +255,10 @@ export namespace cloudresourcemanager_v3 {
     ready?: boolean | null;
   }
   /**
+   * Runtime operation information for creating a TagValue.
+   */
+  export interface Schema$CreateTagBindingMetadata {}
+  /**
    * Runtime operation information for creating a TagKey.
    */
   export interface Schema$CreateTagKeyMetadata {}
@@ -274,6 +278,10 @@ export namespace cloudresourcemanager_v3 {
    * A status object which is used as the `metadata` field for the Operation returned by `DeleteProject`.
    */
   export interface Schema$DeleteProjectMetadata {}
+  /**
+   * Runtime operation information for deleting a TagBinding.
+   */
+  export interface Schema$DeleteTagBindingMetadata {}
   /**
    * Runtime operation information for deleting a TagKey.
    */
@@ -4785,7 +4793,7 @@ export namespace cloudresourcemanager_v3 {
     }
 
     /**
-     * Move a project to another place in your resource hierarchy, under a new resource parent. Returns an operation which can be used to track the process of the project move workflow. Upon success, the `Operation.response` field will be populated with the moved project. The caller must have `resourcemanager.projects.update` permission on the project and have `resourcemanager.projects.move` permission on the project's current and proposed new parent.
+     * Move a project to another place in your resource hierarchy, under a new resource parent. Returns an operation which can be used to track the process of the project move workflow. Upon success, the `Operation.response` field will be populated with the moved project. The caller must have `resourcemanager.projects.update` permission on the project and have `resourcemanager.projects.move` permission on the project's current and proposed new parent. If project has no current parent, or it currently does not have an associated organization resource, you will also need the `resourcemanager.projects.setIamPolicy` permission in the project.
      * @example
      * ```js
      * // Before running the sample:
@@ -5102,7 +5110,7 @@ export namespace cloudresourcemanager_v3 {
      *     pageSize: 'placeholder-value',
      *     // Optional. A pagination token returned from a previous call to ListProjects that indicates from where listing should continue.
      *     pageToken: 'placeholder-value',
-     *     // Optional. A query string for searching for projects that the caller has `resourcemanager.projects.get` permission to. If multiple fields are included in the query, the it will return results that match any of the fields. Some eligible fields are: | Field | Description | |-------------------------|----------------------------------------------| | displayName, name | Filters by displayName. | | parent | Project's parent. (for example: folders/123, organizations/x) Prefer parent field over parent.type and parent.id. | | parent.type | Parent's type: `folder` or `organization`. | | parent.id | Parent's id number (for example: 123) | | id, projectId | Filters by projectId. | | state, lifecycleState | Filters by state. | | labels | Filters by label name or value. | | labels. (where *key* is the name of a label) | Filters by label name. | Search expressions are case insensitive. Some examples queries: | Query | Description | |------------------|-----------------------------------------------------| | name:how* | The project's name starts with "how". | | name:Howl | The project's name is `Howl` or `howl`. | | name:HOWL | Equivalent to above. | | NAME:howl | Equivalent to above. | | labels.color:* | The project has the label `color`. | | labels.color:red | The project's label `color` has the value `red`. | | labels.color:red labels.size:big | The project's label `color` has the value `red` and its label `size` has the value `big`. | If no query is specified, the call will return projects for which the user has the `resourcemanager.projects.get` permission.
+     *     // Optional. A query string for searching for projects that the caller has `resourcemanager.projects.get` permission to. If multiple fields are included in the query, the it will return results that match any of the fields. Some eligible fields are: | Field | Description | |-------------------------|----------------------------------------------| | displayName, name | Filters by displayName. | | parent | Project's parent (for example: folders/123, organizations/x). Prefer parent field over parent.type and parent.id.| | parent.type | Parent's type: `folder` or `organization`. | | parent.id | Parent's id number (for example: 123) | | id, projectId | Filters by projectId. | | state, lifecycleState | Filters by state. | | labels | Filters by label name or value. | | labels.\ (where *key* is the name of a label) | Filters by label name.| Search expressions are case insensitive. Some examples queries: | Query | Description | |------------------|-----------------------------------------------------| | name:how* | The project's name starts with "how". | | name:Howl | The project's name is `Howl` or `howl`. | | name:HOWL | Equivalent to above. | | NAME:howl | Equivalent to above. | | labels.color:* | The project has the label `color`. | | labels.color:red | The project's label `color` has the value `red`. | | labels.color:red labels.size:big | The project's label `color` has the value `red` and its label `size` has the value `big`.| If no query is specified, the call will return projects for which the user has the `resourcemanager.projects.get` permission.
      *     query: 'placeholder-value',
      *   });
      *   console.log(res.data);
@@ -5725,7 +5733,7 @@ export namespace cloudresourcemanager_v3 {
      */
     pageToken?: string;
     /**
-     * Optional. A query string for searching for projects that the caller has `resourcemanager.projects.get` permission to. If multiple fields are included in the query, the it will return results that match any of the fields. Some eligible fields are: | Field | Description | |-------------------------|----------------------------------------------| | displayName, name | Filters by displayName. | | parent | Project's parent. (for example: folders/123, organizations/x) Prefer parent field over parent.type and parent.id. | | parent.type | Parent's type: `folder` or `organization`. | | parent.id | Parent's id number (for example: 123) | | id, projectId | Filters by projectId. | | state, lifecycleState | Filters by state. | | labels | Filters by label name or value. | | labels. (where *key* is the name of a label) | Filters by label name. | Search expressions are case insensitive. Some examples queries: | Query | Description | |------------------|-----------------------------------------------------| | name:how* | The project's name starts with "how". | | name:Howl | The project's name is `Howl` or `howl`. | | name:HOWL | Equivalent to above. | | NAME:howl | Equivalent to above. | | labels.color:* | The project has the label `color`. | | labels.color:red | The project's label `color` has the value `red`. | | labels.color:red labels.size:big | The project's label `color` has the value `red` and its label `size` has the value `big`. | If no query is specified, the call will return projects for which the user has the `resourcemanager.projects.get` permission.
+     * Optional. A query string for searching for projects that the caller has `resourcemanager.projects.get` permission to. If multiple fields are included in the query, the it will return results that match any of the fields. Some eligible fields are: | Field | Description | |-------------------------|----------------------------------------------| | displayName, name | Filters by displayName. | | parent | Project's parent (for example: folders/123, organizations/x). Prefer parent field over parent.type and parent.id.| | parent.type | Parent's type: `folder` or `organization`. | | parent.id | Parent's id number (for example: 123) | | id, projectId | Filters by projectId. | | state, lifecycleState | Filters by state. | | labels | Filters by label name or value. | | labels.\ (where *key* is the name of a label) | Filters by label name.| Search expressions are case insensitive. Some examples queries: | Query | Description | |------------------|-----------------------------------------------------| | name:how* | The project's name starts with "how". | | name:Howl | The project's name is `Howl` or `howl`. | | name:HOWL | Equivalent to above. | | NAME:howl | Equivalent to above. | | labels.color:* | The project has the label `color`. | | labels.color:red | The project's label `color` has the value `red`. | | labels.color:red labels.size:big | The project's label `color` has the value `red` and its label `size` has the value `big`.| If no query is specified, the call will return projects for which the user has the `resourcemanager.projects.get` permission.
      */
     query?: string;
   }
