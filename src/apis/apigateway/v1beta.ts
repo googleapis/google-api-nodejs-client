5b357a82a968c3b0bb90f7878ee2765e9fb77368
yoshi-automation <yoshi-automation@google.com>
2021-04-20 02:02:34 -0700

    fix(apigateway): update the API
    #### apigateway:v1beta
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.pageSize.description
    
    #### apigateway:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.pageSize.description

--- src/apis/apigateway/v1beta.ts
+++ src/apis/apigateway/v1beta.ts
@@ -807,7 +807,7 @@ export namespace apigateway_v1beta {
      *     filter: 'placeholder-value',
      *     // The resource that owns the locations collection, if applicable.
      *     name: 'projects/my-project',
-     *     // The maximum number of results to return. If not set, the service will select a default.
+     *     // The maximum number of results to return. If not set, the service selects a default.
      *     pageSize: 'placeholder-value',
      *     // A page token received from the `next_page_token` field in the response. Send that page token to receive the subsequent page.
      *     pageToken: 'placeholder-value',
@@ -940,7 +940,7 @@ export namespace apigateway_v1beta {
      */
     name?: string;
     /**
-     * The maximum number of results to return. If not set, the service will select a default.
+     * The maximum number of results to return. If not set, the service selects a default.
      */
     pageSize?: number;
     /**
