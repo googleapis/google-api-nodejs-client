5b27edd3a7b16d94480ec1a770cd6dcefb47f403
yoshi-automation <yoshi-automation@google.com>
2021-04-20 02:02:38 -0700

    fix(file): update the API
    #### file:v1beta1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.pageSize.description
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1Instance.properties.name.description
    
    #### file:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.pageSize.description
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1Instance.properties.name.description

--- src/apis/file/v1.ts
+++ src/apis/file/v1.ts
@@ -280,7 +280,7 @@ export namespace file_v1 {
      */
     maintenanceSettings?: Schema$GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings;
     /**
-     * Unique name of the resource. It uses the form: `projects/{project_id\}/locations/{location_id\}/instances/{instance_id\}`
+     * Unique name of the resource. It uses the form: `projects/{project_id|project_number\}/locations/{location_id\}/instances/{instance_id\}` Note: Either project_id or project_number and be used, but keep it consistent with other APIs (e.g. RescheduleUpdate)
      */
     name?: string | null;
     /**
@@ -1021,7 +1021,7 @@ export namespace file_v1 {
      *     includeUnrevealedLocations: 'placeholder-value',
      *     // The resource that owns the locations collection, if applicable.
      *     name: 'projects/my-project',
-     *     // The maximum number of results to return. If not set, the service will select a default.
+     *     // The maximum number of results to return. If not set, the service selects a default.
      *     pageSize: 'placeholder-value',
      *     // A page token received from the `next_page_token` field in the response. Send that page token to receive the subsequent page.
      *     pageToken: 'placeholder-value',
@@ -1154,7 +1154,7 @@ export namespace file_v1 {
      */
     name?: string;
     /**
-     * The maximum number of results to return. If not set, the service will select a default.
+     * The maximum number of results to return. If not set, the service selects a default.
      */
     pageSize?: number;
     /**
