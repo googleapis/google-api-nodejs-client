0e25f2f66db1e4ce8222c32775d032d7c4d8699d
yoshi-automation <yoshi-automation@google.com>
2021-04-20 02:02:41 -0700

    fix(translate): update the API
    #### translate:v3beta1
    
    The following keys were changed:
    - auth.oauth2.scopes.https://www.googleapis.com/auth/cloud-platform.description
    - resources.projects.resources.locations.methods.list.parameters.pageSize.description
    
    #### translate:v3
    
    The following keys were changed:
    - auth.oauth2.scopes.https://www.googleapis.com/auth/cloud-platform.description
    - resources.projects.resources.locations.methods.list.parameters.pageSize.description

--- src/apis/translate/v3beta1.ts
+++ src/apis/translate/v3beta1.ts
@@ -1649,7 +1649,7 @@ export namespace translate_v3beta1 {
      *     filter: 'placeholder-value',
      *     // The resource that owns the locations collection, if applicable.
      *     name: 'projects/my-project',
-     *     // The maximum number of results to return. If not set, the service will select a default.
+     *     // The maximum number of results to return. If not set, the service selects a default.
      *     pageSize: 'placeholder-value',
      *     // A page token received from the `next_page_token` field in the response. Send that page token to receive the subsequent page.
      *     pageToken: 'placeholder-value',
@@ -1971,7 +1971,7 @@ export namespace translate_v3beta1 {
      */
     name?: string;
     /**
-     * The maximum number of results to return. If not set, the service will select a default.
+     * The maximum number of results to return. If not set, the service selects a default.
      */
     pageSize?: number;
     /**
