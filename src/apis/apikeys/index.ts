81835e930f95faef13ae2807108329a91e83966d
yoshi-automation <yoshi-automation@google.com>
2021-04-21 03:23:16 -0700

    fix(apikeys): update the API

---
+++ src/apis/apikeys/index.ts
@@ -0,0 +1,44 @@
+// Copyright 2020 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*! THIS FILE IS AUTO-GENERATED */
+
+import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
+import {apikeys_v2} from './v2';
+
+export const VERSIONS = {
+  v2: apikeys_v2.Apikeys,
+};
+
+export function apikeys(version: 'v2'): apikeys_v2.Apikeys;
+export function apikeys(options: apikeys_v2.Options): apikeys_v2.Apikeys;
+export function apikeys<T = apikeys_v2.Apikeys>(
+  this: GoogleConfigurable,
+  versionOrOptions: 'v2' | apikeys_v2.Options
+) {
+  return getAPI<T>('apikeys', versionOrOptions, VERSIONS, this);
+}
+
+const auth = new AuthPlus();
+export {auth};
+export {apikeys_v2};
+export {
+  AuthPlus,
+  GlobalOptions,
+  APIRequestContext,
+  GoogleConfigurable,
+  StreamMethodOptions,
+  GaxiosPromise,
+  MethodOptions,
+  BodyResponseCallback,
+} from 'googleapis-common';
