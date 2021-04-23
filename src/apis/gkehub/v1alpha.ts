16e0f84b88f2af8a9fd70cb0d24562334175fa65
yoshi-automation <yoshi-automation@google.com>
2021-04-23 03:23:44 -0700

    fix(gkehub): update the API
    #### gkehub:v1alpha
    
    The following keys were changed:
    - schemas.Feature.properties.membershipSpecs.description
    
    #### gkehub:v1beta
    
    The following keys were changed:
    - schemas.Feature.properties.membershipSpecs.description

--- src/apis/gkehub/v1alpha.ts
+++ src/apis/gkehub/v1alpha.ts
@@ -697,7 +697,7 @@ export namespace gkehub_v1alpha {
      */
     labels?: {[key: string]: string} | null;
     /**
-     * Optional. Membership-specific configuration for this Feature. If this Feature does not support any per-Membership configuration, this field may be unused. The keys indicate which Membership the configuration is for, in the form: projects/{p\}/locations/{l\}/memberships/{m\} Where {p\} is the project number, {l\} is a valid location and {m\} is a valid Membership in this project at that location. {p\} MUST match the Feature's project number.
+     * Optional. Membership-specific configuration for this Feature. If this Feature does not support any per-Membership configuration, this field may be unused. The keys indicate which Membership the configuration is for, in the form: projects/{p\}/locations/{l\}/memberships/{m\} Where {p\} is the project, {l\} is a valid location and {m\} is a valid Membership in this project at that location. {p\} WILL match the Feature's project. {p\} will always be returned as the project number, but the project ID is also accepted during input. If the same Membership is specified in the map twice (using the project ID form, and the project number form), exactly ONE of the entries will be saved, with no guarantees as to which. For this reason, it is recommended the same format be used for all entries when mutating a Feature.
      */
     membershipSpecs?: {[key: string]: Schema$MembershipFeatureSpec} | null;
     /**
