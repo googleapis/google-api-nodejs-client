3fd6fb98f37073933961974afb110965110ec6cf
yoshi-automation <yoshi-automation@google.com>
2021-05-03 03:23:29 -0700

    fix(binaryauthorization): update the API
    #### binaryauthorization:v1beta1
    
    The following keys were changed:
    - schemas.AdmissionWhitelistPattern.properties.namePattern.description
    
    #### binaryauthorization:v1
    
    The following keys were changed:
    - schemas.AdmissionWhitelistPattern.properties.namePattern.description

--- src/apis/binaryauthorization/v1beta1.ts
+++ src/apis/binaryauthorization/v1beta1.ts
@@ -149,7 +149,7 @@ export namespace binaryauthorization_v1beta1 {
    */
   export interface Schema$AdmissionWhitelistPattern {
     /**
-     * An image name pattern to allowlist, in the form `registry/path/to/image`. This supports a trailing `*` as a wildcard, but this is allowed only in text after the `registry/` part. Also wildcards do not match `/`, i.e., gcr.io/nginx* matches gcr.io/nginx@latest, but it does not match gcr.io/nginx/image.
+     * An image name pattern to allowlist, in the form `registry/path/to/image`. This supports a trailing `*` as a wildcard, but this is allowed only in text after the `registry/` part. `*` wildcard does not match `/`, i.e., gcr.io/nginx* matches gcr.io/nginx@latest, but it does not match gcr.io/nginx/image. This also supports a trailing `**` wildcard which matches subdirectories, i.e., gcr.io/nginx** matches gcr.io/nginx/image.
      */
     namePattern?: string | null;
   }
