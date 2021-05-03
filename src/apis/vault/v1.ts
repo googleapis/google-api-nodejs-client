18f094d00f6767001e2e7b75f78b845c7e5d2162
yoshi-automation <yoshi-automation@google.com>
2021-05-03 03:23:35 -0700

    fix(vault): update the API
    #### vault:v1
    
    The following keys were changed:
    - description

--- src/apis/vault/v1.ts
+++ src/apis/vault/v1.ts
@@ -103,7 +103,7 @@ export namespace vault_v1 {
   /**
    * G Suite Vault API
    *
-   * Archiving and eDiscovery for G Suite.
+   * Retention and eDiscovery for Google Workspace. To work with Vault resources, the account must have the [required Vault privileges] (https://support.google.com/vault/answer/2799699) and access to the matter. To access a matter, the account must have created the matter, have the matter shared with them, or have the **View All Matters** privilege. For example, to download an export, an account needs the **Manage Exports** privilege and the matter shared with them.
    *
    * @example
    * ```js
