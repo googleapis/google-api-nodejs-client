090133b4fcb559e81a2b0e1c3c4cfe1cc8752cf0
yoshi-automation <yoshi-automation@google.com>
2021-05-21 01:57:34 -0700

    fix(baremetalsolution): update the API

---
+++ src/apis/baremetalsolution/v1.ts
@@ -0,0 +1,318 @@
+// Copyright 2020 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/class-name-casing */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-interface */
+/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable no-irregular-whitespace */
+
+import {
+  OAuth2Client,
+  JWT,
+  Compute,
+  UserRefreshClient,
+  BaseExternalAccountClient,
+  GaxiosPromise,
+  GoogleConfigurable,
+  createAPIRequest,
+  MethodOptions,
+  StreamMethodOptions,
+  GlobalOptions,
+  GoogleAuth,
+  BodyResponseCallback,
+  APIRequestContext,
+} from 'googleapis-common';
+import {Readable} from 'stream';
+
+export namespace baremetalsolution_v1 {
+  export interface Options extends GlobalOptions {
+    version: 'v1';
+  }
+
+  interface StandardParameters {
+    /**
+     * Auth client or API Key for the request
+     */
+    auth?:
+      | string
+      | OAuth2Client
+      | JWT
+      | Compute
+      | UserRefreshClient
+      | BaseExternalAccountClient
+      | GoogleAuth;
+
+    /**
+     * V1 error format.
+     */
+    '$.xgafv'?: string;
+    /**
+     * OAuth access token.
+     */
+    access_token?: string;
+    /**
+     * Data format for response.
+     */
+    alt?: string;
+    /**
+     * JSONP
+     */
+    callback?: string;
+    /**
+     * Selector specifying which fields to include in a partial response.
+     */
+    fields?: string;
+    /**
+     * API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+     */
+    key?: string;
+    /**
+     * OAuth 2.0 token for the current user.
+     */
+    oauth_token?: string;
+    /**
+     * Returns response with indentations and line breaks.
+     */
+    prettyPrint?: boolean;
+    /**
+     * Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+     */
+    quotaUser?: string;
+    /**
+     * Legacy upload protocol for media (e.g. "media", "multipart").
+     */
+    uploadType?: string;
+    /**
+     * Upload protocol for media (e.g. "raw", "multipart").
+     */
+    upload_protocol?: string;
+  }
+
+  /**
+   * Bare Metal Solution API
+   *
+   * Bare Metal Solution provides hardware to run specialized workloads with low latency on Google Cloud.
+   *
+   * @example
+   * ```js
+   * const {google} = require('googleapis');
+   * const baremetalsolution = google.baremetalsolution('v1');
+   * ```
+   */
+  export class Baremetalsolution {
+    context: APIRequestContext;
+    projects: Resource$Projects;
+
+    constructor(options: GlobalOptions, google?: GoogleConfigurable) {
+      this.context = {
+        _options: options || {},
+        google,
+      };
+
+      this.projects = new Resource$Projects(this.context);
+    }
+  }
+
+  /**
+   * Request for ResetInstance.
+   */
+  export interface Schema$ResetInstanceRequest {}
+  /**
+   * Response for ResetInstance.
+   */
+  export interface Schema$ResetInstanceResponse {}
+
+  export class Resource$Projects {
+    context: APIRequestContext;
+    locations: Resource$Projects$Locations;
+    constructor(context: APIRequestContext) {
+      this.context = context;
+      this.locations = new Resource$Projects$Locations(this.context);
+    }
+  }
+
+  export class Resource$Projects$Locations {
+    context: APIRequestContext;
+    instances: Resource$Projects$Locations$Instances;
+    constructor(context: APIRequestContext) {
+      this.context = context;
+      this.instances = new Resource$Projects$Locations$Instances(this.context);
+    }
+  }
+
+  export class Resource$Projects$Locations$Instances {
+    context: APIRequestContext;
+    constructor(context: APIRequestContext) {
+      this.context = context;
+    }
+
+    /**
+     * Perform an ungraceful, hard reset on a machine (equivalent to shutting the power off, and then turning it back on).
+     * @example
+     * ```js
+     * // Before running the sample:
+     * // - Enable the API at:
+     * //   https://console.developers.google.com/apis/api/baremetalsolution.googleapis.com
+     * // - Login into gcloud by running:
+     * //   `$ gcloud auth application-default login`
+     * // - Install the npm module by running:
+     * //   `$ npm install googleapis`
+     *
+     * const {google} = require('googleapis');
+     * const baremetalsolution = google.baremetalsolution('v1');
+     *
+     * async function main() {
+     *   const auth = new google.auth.GoogleAuth({
+     *     // Scopes can be specified either as an array or as a single, space-delimited string.
+     *     scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+     *   });
+     *
+     *   // Acquire an auth client, and bind it to all future calls
+     *   const authClient = await auth.getClient();
+     *   google.options({auth: authClient});
+     *
+     *   // Do the magic
+     *   const res =
+     *     await baremetalsolution.projects.locations.instances.resetInstance({
+     *       // Required. Name of the instance to reset.
+     *       instance:
+     *         'projects/my-project/locations/my-location/instances/my-instance',
+     *
+     *       // Request body metadata
+     *       requestBody: {
+     *         // request body parameters
+     *         // {}
+     *       },
+     *     });
+     *   console.log(res.data);
+     *
+     *   // Example response
+     *   // {}
+     * }
+     *
+     * main().catch(e => {
+     *   console.error(e);
+     *   throw e;
+     * });
+     *
+     * ```
+     *
+     * @param params - Parameters for request
+     * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
+     * @param callback - Optional callback that handles the response.
+     * @returns A promise if used with async/await, or void if used with a callback.
+     */
+    resetInstance(
+      params: Params$Resource$Projects$Locations$Instances$Resetinstance,
+      options: StreamMethodOptions
+    ): GaxiosPromise<Readable>;
+    resetInstance(
+      params?: Params$Resource$Projects$Locations$Instances$Resetinstance,
+      options?: MethodOptions
+    ): GaxiosPromise<Schema$ResetInstanceResponse>;
+    resetInstance(
+      params: Params$Resource$Projects$Locations$Instances$Resetinstance,
+      options: StreamMethodOptions | BodyResponseCallback<Readable>,
+      callback: BodyResponseCallback<Readable>
+    ): void;
+    resetInstance(
+      params: Params$Resource$Projects$Locations$Instances$Resetinstance,
+      options:
+        | MethodOptions
+        | BodyResponseCallback<Schema$ResetInstanceResponse>,
+      callback: BodyResponseCallback<Schema$ResetInstanceResponse>
+    ): void;
+    resetInstance(
+      params: Params$Resource$Projects$Locations$Instances$Resetinstance,
+      callback: BodyResponseCallback<Schema$ResetInstanceResponse>
+    ): void;
+    resetInstance(
+      callback: BodyResponseCallback<Schema$ResetInstanceResponse>
+    ): void;
+    resetInstance(
+      paramsOrCallback?:
+        | Params$Resource$Projects$Locations$Instances$Resetinstance
+        | BodyResponseCallback<Schema$ResetInstanceResponse>
+        | BodyResponseCallback<Readable>,
+      optionsOrCallback?:
+        | MethodOptions
+        | StreamMethodOptions
+        | BodyResponseCallback<Schema$ResetInstanceResponse>
+        | BodyResponseCallback<Readable>,
+      callback?:
+        | BodyResponseCallback<Schema$ResetInstanceResponse>
+        | BodyResponseCallback<Readable>
+    ):
+      | void
+      | GaxiosPromise<Schema$ResetInstanceResponse>
+      | GaxiosPromise<Readable> {
+      let params = (paramsOrCallback ||
+        {}) as Params$Resource$Projects$Locations$Instances$Resetinstance;
+      let options = (optionsOrCallback || {}) as MethodOptions;
+
+      if (typeof paramsOrCallback === 'function') {
+        callback = paramsOrCallback;
+        params =
+          {} as Params$Resource$Projects$Locations$Instances$Resetinstance;
+        options = {};
+      }
+
+      if (typeof optionsOrCallback === 'function') {
+        callback = optionsOrCallback;
+        options = {};
+      }
+
+      const rootUrl =
+        options.rootUrl || 'https://baremetalsolution.googleapis.com/';
+      const parameters = {
+        options: Object.assign(
+          {
+            url: (rootUrl + '/v1/{+instance}:resetInstance').replace(
+              /([^:]\/)\/+/g,
+              '$1'
+            ),
+            method: 'POST',
+          },
+          options
+        ),
+        params,
+        requiredParams: ['instance'],
+        pathParams: ['instance'],
+        context: this.context,
+      };
+      if (callback) {
+        createAPIRequest<Schema$ResetInstanceResponse>(
+          parameters,
+          callback as BodyResponseCallback<unknown>
+        );
+      } else {
+        return createAPIRequest<Schema$ResetInstanceResponse>(parameters);
+      }
+    }
+  }
+
+  export interface Params$Resource$Projects$Locations$Instances$Resetinstance
+    extends StandardParameters {
+    /**
+     * Required. Name of the instance to reset.
+     */
+    instance?: string;
+
+    /**
+     * Request body metadata
+     */
+    requestBody?: Schema$ResetInstanceRequest;
+  }
+}
