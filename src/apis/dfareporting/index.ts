6257d455a46952469cf6aa722682ee27e54c6814
yoshi-automation <yoshi-automation@google.com>
2021-05-23 05:12:55 -0700

    fix(dfareporting): update the API
    #### dfareporting:v3.3
    
    The following keys were changed:
    - schemas.CreativeAssetMetadata.properties.warnedValidationRules.items.enum
    - schemas.CreativeAssetMetadata.properties.warnedValidationRules.items.enumDescriptions
    - schemas.UserRolePermission.properties.availability.enum
    - schemas.UserRolePermission.properties.availability.enumDescriptions
    
    #### dfareporting:v3.4
    
    The following keys were changed:
    - schemas.CreativeAssetMetadata.properties.warnedValidationRules.items.enum
    - schemas.CreativeAssetMetadata.properties.warnedValidationRules.items.enumDescriptions
    - schemas.UserRolePermission.properties.availability.enum
    - schemas.UserRolePermission.properties.availability.enumDescriptions

--- src/apis/dfareporting/index.ts
+++ src/apis/dfareporting/index.ts
@@ -16,10 +16,12 @@
 import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
 import {dfareporting_v3_3} from './v3.3';
 import {dfareporting_v3_4} from './v3.4';
+import {dfareporting_v3_5} from './v3.5';
 
 export const VERSIONS = {
   'v3.3': dfareporting_v3_3.Dfareporting,
   'v3.4': dfareporting_v3_4.Dfareporting,
+  'v3.5': dfareporting_v3_5.Dfareporting,
 };
 
 export function dfareporting(version: 'v3.3'): dfareporting_v3_3.Dfareporting;
@@ -30,8 +32,15 @@ export function dfareporting(version: 'v3.4'): dfareporting_v3_4.Dfareporting;
 export function dfareporting(
   options: dfareporting_v3_4.Options
 ): dfareporting_v3_4.Dfareporting;
+export function dfareporting(version: 'v3.5'): dfareporting_v3_5.Dfareporting;
+export function dfareporting(
+  options: dfareporting_v3_5.Options
+): dfareporting_v3_5.Dfareporting;
 export function dfareporting<
-  T = dfareporting_v3_3.Dfareporting | dfareporting_v3_4.Dfareporting
+  T =
+    | dfareporting_v3_3.Dfareporting
+    | dfareporting_v3_4.Dfareporting
+    | dfareporting_v3_5.Dfareporting
 >(
   this: GoogleConfigurable,
   versionOrOptions:
@@ -39,6 +48,8 @@ export function dfareporting<
     | dfareporting_v3_3.Options
     | 'v3.4'
     | dfareporting_v3_4.Options
+    | 'v3.5'
+    | dfareporting_v3_5.Options
 ) {
   return getAPI<T>('dfareporting', versionOrOptions, VERSIONS, this);
 }
@@ -47,6 +58,7 @@ const auth = new AuthPlus();
 export {auth};
 export {dfareporting_v3_3};
 export {dfareporting_v3_4};
+export {dfareporting_v3_5};
 export {
   AuthPlus,
   GlobalOptions,
