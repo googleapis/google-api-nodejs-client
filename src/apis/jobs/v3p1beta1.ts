f917ed15d6187f4759cb08ec18fd7847479a50ed
yoshi-automation <yoshi-automation@google.com>
2021-05-25 01:58:36 -0700

    fix(jobs): update the API
    #### jobs:v3p1beta1
    
    The following keys were changed:
    - schemas.LatLng.description
    
    #### jobs:v3
    
    The following keys were changed:
    - schemas.LatLng.description
    
    #### jobs:v4
    
    The following keys were changed:
    - schemas.LatLng.description

--- src/apis/jobs/v3p1beta1.ts
+++ src/apis/jobs/v3p1beta1.ts
@@ -840,7 +840,7 @@ export namespace jobs_v3p1beta1 {
     queryLanguageCode?: string | null;
   }
   /**
-   * An object that represents a latitude/longitude pair. This is expressed as a pair of doubles to represent degrees latitude and degrees longitude. Unless specified otherwise, this must conform to the WGS84 standard. Values must be within normalized ranges.
+   * An object that represents a latitude/longitude pair. This is expressed as a pair of doubles to represent degrees latitude and degrees longitude. Unless specified otherwise, this object must conform to the WGS84 standard. Values must be within normalized ranges.
    */
   export interface Schema$LatLng {
     /**
