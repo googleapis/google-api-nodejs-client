d569fa6a5a2bb65470ff33fc0700ea4f38e7fc8a
yoshi-automation <yoshi-automation@google.com>
2021-05-26 02:00:16 -0700

    fix(osconfig): update the API

--- src/apis/osconfig/index.ts
+++ src/apis/osconfig/index.ts
@@ -15,24 +15,37 @@
 
 import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
 import {osconfig_v1} from './v1';
+import {osconfig_v1alpha} from './v1alpha';
 import {osconfig_v1beta} from './v1beta';
 
 export const VERSIONS = {
   v1: osconfig_v1.Osconfig,
+  v1alpha: osconfig_v1alpha.Osconfig,
   v1beta: osconfig_v1beta.Osconfig,
 };
 
 export function osconfig(version: 'v1'): osconfig_v1.Osconfig;
 export function osconfig(options: osconfig_v1.Options): osconfig_v1.Osconfig;
+export function osconfig(version: 'v1alpha'): osconfig_v1alpha.Osconfig;
+export function osconfig(
+  options: osconfig_v1alpha.Options
+): osconfig_v1alpha.Osconfig;
 export function osconfig(version: 'v1beta'): osconfig_v1beta.Osconfig;
 export function osconfig(
   options: osconfig_v1beta.Options
 ): osconfig_v1beta.Osconfig;
-export function osconfig<T = osconfig_v1.Osconfig | osconfig_v1beta.Osconfig>(
+export function osconfig<
+  T =
+    | osconfig_v1.Osconfig
+    | osconfig_v1alpha.Osconfig
+    | osconfig_v1beta.Osconfig
+>(
   this: GoogleConfigurable,
   versionOrOptions:
     | 'v1'
     | osconfig_v1.Options
+    | 'v1alpha'
+    | osconfig_v1alpha.Options
     | 'v1beta'
     | osconfig_v1beta.Options
 ) {
@@ -42,6 +55,7 @@ export function osconfig<T = osconfig_v1.Osconfig | osconfig_v1beta.Osconfig>(
 const auth = new AuthPlus();
 export {auth};
 export {osconfig_v1};
+export {osconfig_v1alpha};
 export {osconfig_v1beta};
 export {
   AuthPlus,
