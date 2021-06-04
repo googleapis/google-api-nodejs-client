f08fac6ec92d666b5c1cc2274d3bda84e641424e
yoshi-automation <yoshi-automation@google.com>
2021-06-04 02:33:30 -0700

    fix(spanner): update the API
    #### spanner:v1
    
    The following keys were changed:
    - schemas.CommitStats.properties.mutationCount.description

--- src/apis/spanner/v1.ts
+++ src/apis/spanner/v1.ts
@@ -301,7 +301,7 @@ export namespace spanner_v1 {
    */
   export interface Schema$CommitStats {
     /**
-     * The total number of mutations for the transaction. Knowing the `mutation_count` value can help you maximize the number of mutations in a transaction and minimize the number of API round trips. You can also monitor this value to prevent transactions from exceeding the system [limit](https://cloud.google.com/spanner/quotas#limits_for_creating_reading_updating_and_deleting_data). If the number of mutations exceeds the limit, the server returns [INVALID_ARGUMENT](https://cloud.google.com/spanner/docs/reference/rest/v1/Code#ENUM_VALUES.INVALID_ARGUMENT).
+     * The total number of mutations for the transaction. Knowing the `mutation_count` value can help you maximize the number of mutations in a transaction and minimize the number of API round trips. You can also monitor this value to prevent transactions from exceeding the system [limit](http://cloud.google.com/spanner/quotas#limits_for_creating_reading_updating_and_deleting_data). If the number of mutations exceeds the limit, the server returns [INVALID_ARGUMENT](http://cloud.google.com/spanner/docs/reference/rest/v1/Code#ENUM_VALUES.INVALID_ARGUMENT).
      */
     mutationCount?: string | null;
   }
