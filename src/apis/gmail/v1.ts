86f29db6a50474fdc9665f13491cbc705c6a4a51
yoshi-automation <yoshi-automation@google.com>
2021-06-04 02:33:30 -0700

    fix(gmail): update the API
    #### gmail:v1
    
    The following keys were changed:
    - schemas.Label.description

--- src/apis/gmail/v1.ts
+++ src/apis/gmail/v1.ts
@@ -347,7 +347,7 @@ export namespace gmail_v1 {
     maxFolderSize?: number | null;
   }
   /**
-   * Labels are used to categorize messages and threads within the user's mailbox.
+   * Labels are used to categorize messages and threads within the user's mailbox. The maximum number of labels supported for a user's mailbox is 10,000.
    */
   export interface Schema$Label {
     /**
