ba41041ea5023a0ed127c9ae334682f1650a8514
yoshi-automation <yoshi-automation@google.com>
2021-06-08 02:34:09 -0700

    feat(container)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### container:v1beta1
    
    The following keys were deleted:
    - schemas.UpgradeSettings.description
    
    The following keys were added:
    - schemas.AutoprovisioningNodePoolDefaults.properties.imageType.description
    - schemas.AutoprovisioningNodePoolDefaults.properties.imageType.type
    
    The following keys were changed:
    - schemas.NodePool.description

--- src/apis/container/v1beta1.ts
+++ src/apis/container/v1beta1.ts
@@ -226,6 +226,10 @@ export namespace container_v1beta1 {
      */
     diskType?: string | null;
     /**
+     * The image type to use for NAP created node.
+     */
+    imageType?: string | null;
+    /**
      * NodeManagement configuration for this NodePool.
      */
     management?: Schema$NodeManagement;
@@ -1633,7 +1637,7 @@ export namespace container_v1beta1 {
     podRange?: string | null;
   }
   /**
-   * NodePool contains the name and configuration for a cluster's node pool. Node pools are a set of nodes (i.e. VM's), with a common configuration and specification, under the control of the cluster master. They may have a set of Kubernetes labels applied to them, which may be used to reference them during pod scheduling. They may also be resized up or down, to accommodate the workload.
+   * NodePool contains the name and configuration for a cluster's node pool. Node pools are a set of nodes (i.e. VM's), with a common configuration and specification, under the control of the cluster master. They may have a set of Kubernetes labels applied to them, which may be used to reference them during pod scheduling. They may also be resized up or down, to accommodate the workload. These upgrade settings control the level of parallelism and the level of disruption caused by an upgrade. maxUnavailable controls the number of nodes that can be simultaneously unavailable. maxSurge controls the number of additional nodes that can be added to the node pool temporarily for the time of the upgrade to increase the number of available nodes. (maxUnavailable + maxSurge) determines the level of parallelism (how many nodes are being upgraded at the same time). Note: upgrades inevitably introduce some disruption since workloads need to be moved from old nodes to new, upgraded ones. Even if maxUnavailable=0, this holds true. (Disruption stays within the limits of PodDisruptionBudget, if it is configured.) Consider a hypothetical node pool with 5 nodes having maxSurge=2, maxUnavailable=1. This means the upgrade process upgrades 3 nodes simultaneously. It creates 2 additional (upgraded) nodes, then it brings down 3 old (not yet upgraded) nodes at the same time. This ensures that there are always at least 4 nodes available.
    */
   export interface Schema$NodePool {
     /**
@@ -2677,9 +2681,6 @@ export namespace container_v1beta1 {
      */
     targetVersion?: string | null;
   }
-  /**
-   * These upgrade settings control the level of parallelism and the level of disruption caused by an upgrade. maxUnavailable controls the number of nodes that can be simultaneously unavailable. maxSurge controls the number of additional nodes that can be added to the node pool temporarily for the time of the upgrade to increase the number of available nodes. (maxUnavailable + maxSurge) determines the level of parallelism (how many nodes are being upgraded at the same time). Note: upgrades inevitably introduce some disruption since workloads need to be moved from old nodes to new, upgraded ones. Even if maxUnavailable=0, this holds true. (Disruption stays within the limits of PodDisruptionBudget, if it is configured.) Consider a hypothetical node pool with 5 nodes having maxSurge=2, maxUnavailable=1. This means the upgrade process upgrades 3 nodes simultaneously. It creates 2 additional (upgraded) nodes, then it brings down 3 old (not yet upgraded) nodes at the same time. This ensures that there are always at least 4 nodes available.
-   */
   export interface Schema$UpgradeSettings {
     /**
      * The maximum number of nodes that can be created beyond the current size of the node pool during the upgrade process.
