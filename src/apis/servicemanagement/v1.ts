2abc22a142e4db69555721c6a9aac2ee277ada87
yoshi-automation <yoshi-automation@google.com>
2021-06-10 02:34:02 -0700

    feat(servicemanagement): update the API
    #### servicemanagement:v1
    
    The following keys were added:
    - schemas.OperationInfo.description
    - schemas.OperationInfo.id
    - schemas.OperationInfo.properties.metadataType.description
    - schemas.OperationInfo.properties.metadataType.type
    - schemas.OperationInfo.properties.responseType.description
    - schemas.OperationInfo.properties.responseType.type
    - schemas.OperationInfo.type

--- src/apis/servicemanagement/v1.ts
+++ src/apis/servicemanagement/v1.ts
@@ -1188,6 +1188,19 @@ export namespace servicemanagement_v1 {
     response?: {[key: string]: any} | null;
   }
   /**
+   * A message representing the message types used by a long-running operation. Example: rpc Export(ExportRequest) returns (google.longrunning.Operation) { option (google.longrunning.operation_info) = { response_type: "ExportResponse" metadata_type: "ExportMetadata" \}; \}
+   */
+  export interface Schema$OperationInfo {
+    /**
+     * Required. The message name of the metadata type for this long-running operation. If the response is in a different package from the rpc, a fully-qualified message name must be used (e.g. `google.protobuf.Struct`). Note: Altering this value constitutes a breaking change.
+     */
+    metadataType?: string | null;
+    /**
+     * Required. The message name of the primary return type for this long-running operation. This type will be used to deserialize the LRO's response. If the response is in a different package from the rpc, a fully-qualified message name must be used (e.g. `google.protobuf.Struct`). Note: Altering this value constitutes a breaking change.
+     */
+    responseType?: string | null;
+  }
+  /**
    * The metadata associated with a long running operation resource.
    */
   export interface Schema$OperationMetadata {
