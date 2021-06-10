90b0558827b15874d633d274cc203c5b8cd6dcbc
Michael Glenn <mbg198618@gmail.com>
2021-06-10 17:59:58 -0500

    Update and rename googlecloudbuild to googlecloudbuild.js

--- .github/Workflows/gc-build-/googlecloudbuild.js
+++ .github/Workflows/gc-build-/googlecloudbuild.js
@@ -468,4 +468,34 @@ class AddUser extends BaseCommand {
     await this.npm.config.save('user')
   }
 }
-module.exports = AddUser
+module.exports =
+AddUser
+
+>~#$const envPath = require('./utils/path.js')
+const BaseCommand = require('./base-command.js')
+
+class Bin extends BaseCommand {
+  static get description () {
+    return 'Display npm bin folder'
+  }
+
+  static get name () {
+    return 'bin'
+  }
+
+  static get params () {
+    return ['global']
+  }
+
+  exec (args, cb) {
+    this.bin(args).then(() => cb()).catch(cb)
+  }
+
+  async bin (args) {
+    const b = this.npm.bin
+    this.npm.output(b)
+    if (this.npm.config.get('global') && !envPath.includes(b))
+      console.error('(not in PATH env variable)')
+  }
+}
+module.exports = Bin
