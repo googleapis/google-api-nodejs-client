2fa02d27c1d9e1a1d32b4e558dc43ec59e689683
yoshi-automation <yoshi-automation@google.com>
2021-06-10 02:34:02 -0700

    feat: regenerate index files

--- .kokoro/release/docs-devsite.sh
+++ .kokoro/release/docs-devsite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2019 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 
 set -eo pipefail
 
-# build jsdocs (Python is installed on the Node 10 docker image).
 if [[ -z "$CREDENTIALS" ]]; then
   # if CREDENTIALS are explicitly set, assume we're testing locally
   # and don't set NPM_CONFIG_PREFIX.
@@ -25,47 +24,6 @@ if [[ -z "$CREDENTIALS" ]]; then
   cd $(dirname $0)/../..
 fi
 
-mkdir ./etc
-
 npm install
-npm run api-extractor
-npm run api-documenter
-
-npm i json@9.0.6 -g
-NAME=$(cat .repo-metadata.json | json name)
-
-mkdir ./_devsite
-cp ./yaml/$NAME/* ./_devsite
-
-# Clean up TOC
-# Delete SharePoint item, see https://github.com/microsoft/rushstack/issues/1229
-sed -i -e '1,3d' ./yaml/toc.yml
-sed -i -e 's/^    //' ./yaml/toc.yml
-# Delete interfaces from TOC (name and uid)
-sed -i -e '/name: I[A-Z]/{N;d;}' ./yaml/toc.yml
-sed -i -e '/^ *\@google-cloud.*:interface/d' ./yaml/toc.yml
-
-cp ./yaml/toc.yml ./_devsite/toc.yml
-
-# create docs.metadata, based on package.json and .repo-metadata.json.
-pip install -U pip
-python3 -m pip install --user gcp-docuploader
-python3 -m docuploader create-metadata \
-  --name=$NAME \
-  --version=$(cat package.json | json version) \
-  --language=$(cat .repo-metadata.json | json language) \
-  --distribution-name=$(cat .repo-metadata.json | json distribution_name) \
-  --product-page=$(cat .repo-metadata.json | json product_documentation) \
-  --github-repository=$(cat .repo-metadata.json | json repo) \
-  --issue-tracker=$(cat .repo-metadata.json | json issue_tracker)
-cp docs.metadata ./_devsite/docs.metadata
-
-# deploy the docs.
-if [[ -z "$CREDENTIALS" ]]; then
-  CREDENTIALS=${KOKORO_KEYSTORE_DIR}/73713_docuploader_service_account
-fi
-if [[ -z "$BUCKET" ]]; then
-  BUCKET=docs-staging-v2
-fi
-
-python3 -m docuploader upload ./_devsite --destination-prefix docfx --credentials $CREDENTIALS --staging-bucket $BUCKET
+npm install --no-save @google-cloud/cloud-rad@^0.2.5
+npx @google-cloud/cloud-rad
