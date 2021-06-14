abe6998d9029239ac1e1aaea18396940b0d77330
yoshi-automation <yoshi-automation@google.com>
2021-06-14 02:34:23 -0700

    fix(storagetransfer): update the API
    #### storagetransfer:v1
    
    The following keys were changed:
    - schemas.AwsS3Data.properties.awsAccessKey.description

--- src/apis/storagetransfer/v1.ts
+++ src/apis/storagetransfer/v1.ts
@@ -149,7 +149,7 @@ export namespace storagetransfer_v1 {
    */
   export interface Schema$AwsS3Data {
     /**
-     * Input only. AWS access key used to sign the API requests to the AWS S3 bucket. Permissions on the bucket must be granted to the access ID of the AWS access key. For information on our data retention policy for user credentials, see [User credentials](/storage-transfer/docs/data-retention#user-credentials).
+     * Input only. AWS access key used to sign the API requests to the AWS S3 bucket. Permissions on the bucket must be granted to the access ID of the AWS access key. This field is required. For information on our data retention policy for user credentials, see [User credentials](/storage-transfer/docs/data-retention#user-credentials).
      */
     awsAccessKey?: Schema$AwsAccessKey;
     /**
