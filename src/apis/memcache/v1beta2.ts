982ba32fc76448d058085d5d96052adcead2220b
Yoshi Automation <yoshi-automation@google.com>
2021-06-18 01:34:40 +0000

    feat(memcache)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### memcache:v1beta2
    
    The following keys were deleted:
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1SloMetadata.properties.eligibility.$ref
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1SloMetadata.properties.eligibility.description
    
    The following keys were changed:
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1SloMetadata.properties.perSliEligibility.description

--- src/apis/memcache/v1beta2.ts
+++ src/apis/memcache/v1beta2.ts
@@ -441,10 +441,6 @@ export namespace memcache_v1beta2 {
    */
   export interface Schema$GoogleCloudSaasacceleratorManagementProvidersV1SloMetadata {
     /**
-     * Optional. Global per-instance SLI eligibility which applies to all defined SLIs. Exactly one of 'eligibility' and 'per_sli_eligibility' fields must be used.
-     */
-    eligibility?: Schema$GoogleCloudSaasacceleratorManagementProvidersV1SloEligibility;
-    /**
      * List of SLO exclusion windows. When multiple entries in the list match (matching the exclusion time-window against current time point) the exclusion reason used in the first matching entry will be published. It is not needed to include expired exclusion in this list, as only the currently applicable exclusions are taken into account by the eligibility exporting subsystem (the historical state of exclusions will be reflected in the historically produced timeseries regardless of the current state). This field can be used to mark the instance as temporary ineligible for the purpose of SLO calculation. For permanent instance SLO exclusion, use of custom instance eligibility is recommended. See 'eligibility' field below.
      */
     exclusions?: Schema$GoogleCloudSaasacceleratorManagementProvidersV1SloExclusion[];
@@ -453,7 +449,7 @@ export namespace memcache_v1beta2 {
      */
     nodes?: Schema$GoogleCloudSaasacceleratorManagementProvidersV1NodeSloMetadata[];
     /**
-     * Optional. Multiple per-instance SLI eligibilities which apply for individual SLIs. Exactly one of 'eligibility' and 'per_sli_eligibility' fields must be used.
+     * Optional. Multiple per-instance SLI eligibilities which apply for individual SLIs.
      */
     perSliEligibility?: Schema$GoogleCloudSaasacceleratorManagementProvidersV1PerSliSloEligibility;
     /**
