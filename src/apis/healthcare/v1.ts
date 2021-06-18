ec9e726426790b7d956fc00ce31991744d291335
Yoshi Automation <yoshi-automation@google.com>
2021-06-18 01:34:40 +0000

    fix(healthcare): update the API
    #### healthcare:v1beta1
    
    The following keys were changed:
    - schemas.DeidentifyDatasetRequest.properties.destinationDataset.description
    - schemas.DeidentifyDicomStoreRequest.properties.destinationStore.description
    - schemas.DeidentifyFhirStoreRequest.properties.destinationStore.description
    
    #### healthcare:v1
    
    The following keys were changed:
    - schemas.DeidentifyDatasetRequest.properties.destinationDataset.description
    - schemas.DeidentifyDicomStoreRequest.properties.destinationStore.description
    - schemas.DeidentifyFhirStoreRequest.properties.destinationStore.description

--- src/apis/healthcare/v1.ts
+++ src/apis/healthcare/v1.ts
@@ -473,7 +473,7 @@ export namespace healthcare_v1 {
      */
     config?: Schema$DeidentifyConfig;
     /**
-     * The name of the dataset resource to create and write the redacted data to. * The destination dataset must not exist. * The destination dataset must be in the same project and location as the source dataset. De-identifying data across multiple projects or locations is not supported.
+     * The name of the dataset resource to create and write the redacted data to. * The destination dataset must not exist. * The destination dataset must be in the same location as the source dataset. De-identifying data across multiple locations is not supported.
      */
     destinationDataset?: string | null;
   }
@@ -486,7 +486,7 @@ export namespace healthcare_v1 {
      */
     config?: Schema$DeidentifyConfig;
     /**
-     * The name of the DICOM store to create and write the redacted data to. For example, `projects/{project_id\}/locations/{location_id\}/datasets/{dataset_id\}/dicomStores/{dicom_store_id\}`. * The destination dataset must exist. * The source dataset and destination dataset must both reside in the same project. De-identifying data across multiple projects is not supported. * The destination DICOM store must not exist. * The caller must have the necessary permissions to create the destination DICOM store.
+     * The name of the DICOM store to create and write the redacted data to. For example, `projects/{project_id\}/locations/{location_id\}/datasets/{dataset_id\}/dicomStores/{dicom_store_id\}`. * The destination dataset must exist. * The source dataset and destination dataset must both reside in the same location. De-identifying data across multiple locations is not supported. * The destination DICOM store must not exist. * The caller must have the necessary permissions to create the destination DICOM store.
      */
     destinationStore?: string | null;
     /**
@@ -503,7 +503,7 @@ export namespace healthcare_v1 {
      */
     config?: Schema$DeidentifyConfig;
     /**
-     * The name of the FHIR store to create and write the redacted data to. For example, `projects/{project_id\}/locations/{location_id\}/datasets/{dataset_id\}/fhirStores/{fhir_store_id\}`. * The destination dataset must exist. * The source dataset and destination dataset must both reside in the same project. De-identifying data across multiple projects is not supported. * The destination FHIR store must exist. * The caller must have the healthcare.fhirResources.update permission to write to the destination FHIR store.
+     * The name of the FHIR store to create and write the redacted data to. For example, `projects/{project_id\}/locations/{location_id\}/datasets/{dataset_id\}/fhirStores/{fhir_store_id\}`. * The destination dataset must exist. * The source dataset and destination dataset must both reside in the same location. De-identifying data across multiple locations is not supported. * The destination FHIR store must exist. * The caller must have the healthcare.fhirResources.update permission to write to the destination FHIR store.
      */
     destinationStore?: string | null;
     /**
