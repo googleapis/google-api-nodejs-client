603a0b1b30fe53242ff270a5f98d54e07c871468
Yoshi Automation <yoshi-automation@google.com>
2021-06-21 01:34:57 +0000

    fix(networkmanagement): update the API
    #### networkmanagement:v1beta1
    
    The following keys were changed:
    - schemas.Trace.properties.endpointInfo.description
    
    #### networkmanagement:v1
    
    The following keys were changed:
    - schemas.Trace.properties.endpointInfo.description

--- src/apis/networkmanagement/v1.ts
+++ src/apis/networkmanagement/v1.ts
@@ -947,7 +947,7 @@ export namespace networkmanagement_v1 {
    */
   export interface Schema$Trace {
     /**
-     * Derived from the source and destination endpoints definition, and validated by the data plane model. If there are multiple traces starting from different source locations, then the endpoint_info may be different between traces.
+     * Derived from the source and destination endpoints definition specified by user request, and validated by the data plane model. If there are multiple traces starting from different source locations, then the endpoint_info may be different between traces.
      */
     endpointInfo?: Schema$EndpointInfo;
     /**
