a9ad82fab3fd3cd7e030f22f615a62465c576354
Yoshi Automation <yoshi-automation@google.com>
2021-06-25 01:32:17 +0000

    fix(billingbudgets): update the API
    #### billingbudgets:v1beta1
    
    The following keys were changed:
    - schemas.GoogleCloudBillingBudgetsV1beta1Budget.properties.thresholdRules.description
    - schemas.GoogleCloudBillingBudgetsV1beta1ThresholdRule.description
    
    #### billingbudgets:v1
    
    The following keys were changed:
    - schemas.GoogleCloudBillingBudgetsV1Budget.properties.thresholdRules.description
    - schemas.GoogleCloudBillingBudgetsV1ThresholdRule.description

--- src/apis/billingbudgets/v1beta1.ts
+++ src/apis/billingbudgets/v1beta1.ts
@@ -175,7 +175,7 @@ export namespace billingbudgets_v1beta1 {
      */
     name?: string | null;
     /**
-     * Optional. Rules that trigger alerts (notifications of thresholds being crossed) when spend exceeds the specified percentages of the budget.
+     * Optional. Rules that trigger alerts (notifications of thresholds being crossed) when spend exceeds the specified percentages of the budget. Optional for `pubsubTopic` notifications. Required if using email notifications.
      */
     thresholdRules?: Schema$GoogleCloudBillingBudgetsV1beta1ThresholdRule[];
   }
@@ -269,7 +269,7 @@ export namespace billingbudgets_v1beta1 {
     nextPageToken?: string | null;
   }
   /**
-   * ThresholdRule contains a definition of a threshold which triggers an alert (a notification of a threshold being crossed) to be sent when spend goes above the specified amount. Alerts are automatically e-mailed to users with the Billing Account Administrator role or the Billing Account User role. The thresholds here have no effect on notifications sent to anything configured under `Budget.all_updates_rule`.
+   * ThresholdRule contains the definition of a threshold. Threshold rules define the triggering events used to generate a budget notification email. When a threshold is crossed (spend exceeds the specified percentages of the budget), budget alert emails are sent to the email recipients you specify in the [NotificationsRule](#notificationsrule). Threshold rules also affect the fields included in the [JSON data object](https://cloud.google.com/billing/docs/how-to/budgets-programmatic-notifications#notification_format) sent to a Pub/Sub topic. Threshold rules are _required_ if using email notifications. Threshold rules are _optional_ if only setting a [`pubsubTopic` NotificationsRule](#NotificationsRule), unless you want your JSON data object to include data about the thresholds you set. For more information, see [set budget threshold rules and actions](https://cloud.google.com/billing/docs/how-to/budgets#budget-actions).
    */
   export interface Schema$GoogleCloudBillingBudgetsV1beta1ThresholdRule {
     /**
