64d501fa20b3a541370e801a8937cdf7213bcab4
Yoshi Automation <yoshi-automation@google.com>
2021-06-30 15:59:57 +0000

    fix(sqladmin): update the API

--- src/apis/sqladmin/index.ts
+++ src/apis/sqladmin/index.ts
@@ -14,25 +14,34 @@
 /*! THIS FILE IS AUTO-GENERATED */
 
 import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
+import {sqladmin_v1} from './v1';
 import {sqladmin_v1beta4} from './v1beta4';
 
 export const VERSIONS = {
+  v1: sqladmin_v1.Sqladmin,
   v1beta4: sqladmin_v1beta4.Sqladmin,
 };
 
+export function sqladmin(version: 'v1'): sqladmin_v1.Sqladmin;
+export function sqladmin(options: sqladmin_v1.Options): sqladmin_v1.Sqladmin;
 export function sqladmin(version: 'v1beta4'): sqladmin_v1beta4.Sqladmin;
 export function sqladmin(
   options: sqladmin_v1beta4.Options
 ): sqladmin_v1beta4.Sqladmin;
-export function sqladmin<T = sqladmin_v1beta4.Sqladmin>(
+export function sqladmin<T = sqladmin_v1.Sqladmin | sqladmin_v1beta4.Sqladmin>(
   this: GoogleConfigurable,
-  versionOrOptions: 'v1beta4' | sqladmin_v1beta4.Options
+  versionOrOptions:
+    | 'v1'
+    | sqladmin_v1.Options
+    | 'v1beta4'
+    | sqladmin_v1beta4.Options
 ) {
   return getAPI<T>('sqladmin', versionOrOptions, VERSIONS, this);
 }
 
 const auth = new AuthPlus();
 export {auth};
+export {sqladmin_v1};
 export {sqladmin_v1beta4};
 export {
   AuthPlus,
