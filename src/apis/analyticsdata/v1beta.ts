9bd79dd545d99b68a54f9e94f19231169faca41f
Yoshi Automation <yoshi-automation@google.com>
2021-06-30 15:59:53 +0000

    feat(analyticsdata): update the API
    #### analyticsdata:v1beta
    
    The following keys were added:
    - schemas.MinuteRange.description
    - schemas.MinuteRange.id
    - schemas.MinuteRange.properties.endMinutesAgo.description
    - schemas.MinuteRange.properties.endMinutesAgo.format
    - schemas.MinuteRange.properties.endMinutesAgo.type
    - schemas.MinuteRange.properties.name.description
    - schemas.MinuteRange.properties.name.type
    - schemas.MinuteRange.properties.startMinutesAgo.description
    - schemas.MinuteRange.properties.startMinutesAgo.format
    - schemas.MinuteRange.properties.startMinutesAgo.type
    - schemas.MinuteRange.type
    - schemas.RunRealtimeReportRequest.properties.minuteRanges.description
    - schemas.RunRealtimeReportRequest.properties.minuteRanges.items.$ref
    - schemas.RunRealtimeReportRequest.properties.minuteRanges.type

--- src/apis/analyticsdata/v1beta.ts
+++ src/apis/analyticsdata/v1beta.ts
@@ -534,6 +534,23 @@ export namespace analyticsdata_v1beta {
     value?: string | null;
   }
   /**
+   * A contiguous set of minutes: startMinutesAgo, startMinutesAgo + 1, ..., endMinutesAgo. Requests are allowed up to 2 minute ranges.
+   */
+  export interface Schema$MinuteRange {
+    /**
+     * The inclusive end minute for the query as a number of minutes before now. Cannot be before `startMinutesAgo`. For example, `"endMinutesAgo": 15` specifies the report should include event data from prior to 15 minutes ago. If unspecified, `endMinutesAgo` is defaulted to 0. Standard Analytics properties can request any minute in the last 30 minutes of event data (`endMinutesAgo <= 29`), and 360 Analytics properties can request any minute in the last 60 minutes of event data (`endMinutesAgo <= 59`).
+     */
+    endMinutesAgo?: number | null;
+    /**
+     * Assigns a name to this minute range. The dimension `dateRange` is valued to this name in a report response. If set, cannot begin with `date_range_` or `RESERVED_`. If not set, minute ranges are named by their zero based index in the request: `date_range_0`, `date_range_1`, etc.
+     */
+    name?: string | null;
+    /**
+     * The inclusive start minute for the query as a number of minutes before now. For example, `"startMinutesAgo": 29` specifies the report should include event data from 29 minutes ago and after. Cannot be after `endMinutesAgo`. If unspecified, `startMinutesAgo` is defaulted to 29. Standard Analytics properties can request up to the last 30 minutes of event data (`startMinutesAgo <= 29`), and 360 Analytics properties can request up to the last 60 minutes of event data (`startMinutesAgo <= 59`).
+     */
+    startMinutesAgo?: number | null;
+  }
+  /**
    * Filters for numeric or date values.
    */
   export interface Schema$NumericFilter {
@@ -827,6 +844,10 @@ export namespace analyticsdata_v1beta {
      * The metrics requested and displayed.
      */
     metrics?: Schema$Metric[];
+    /**
+     * The minute ranges of event data to read. If unspecified, one minute range for the last 30 minutes will be used. If multiple minute ranges are requested, each response row will contain a zero based minute range index. If two minute ranges overlap, the event data for the overlapping minutes is included in the response rows for both minute ranges.
+     */
+    minuteRanges?: Schema$MinuteRange[];
     /**
      * Specifies how rows are ordered in the response.
      */
@@ -1647,6 +1668,7 @@ export namespace analyticsdata_v1beta {
      *       //   "metricAggregations": [],
      *       //   "metricFilter": {},
      *       //   "metrics": [],
+     *       //   "minuteRanges": [],
      *       //   "orderBys": [],
      *       //   "returnPropertyQuota": false
      *       // }
