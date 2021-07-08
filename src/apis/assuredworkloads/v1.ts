53464e25bc879fb7019e82d912459446a3d2df20
Yoshi Automation <yoshi-automation@google.com>
2021-07-08 01:30:21 +0000

    fix(assuredworkloads): update the API
    #### assuredworkloads:v1
    
    The following keys were changed:
    - schemas.GoogleCloudAssuredworkloadsV1beta1Workload.properties.billingAccount.description
    - schemas.GoogleCloudAssuredworkloadsV1beta1WorkloadResourceInfo.properties.resourceType.enum
    - schemas.GoogleCloudAssuredworkloadsV1beta1WorkloadResourceInfo.properties.resourceType.enumDescriptions
    - schemas.GoogleCloudAssuredworkloadsV1beta1WorkloadResourceSettings.properties.resourceType.enum
    - schemas.GoogleCloudAssuredworkloadsV1beta1WorkloadResourceSettings.properties.resourceType.enumDescriptions

--- src/apis/assuredworkloads/v1.ts
+++ src/apis/assuredworkloads/v1.ts
@@ -155,7 +155,7 @@ export namespace assuredworkloads_v1 {
    */
   export interface Schema$GoogleCloudAssuredworkloadsV1beta1Workload {
     /**
-     * Required. Input only. The billing account used for the resources which are direct children of workload. This billing account is initially associated with the resources created as part of Workload creation. After the initial creation of these resources, the customer can change the assigned billing account. The resource name has the form `billingAccounts/{billing_account_id\}`. For example, `billingAccounts/012345-567890-ABCDEF`.
+     * Input only. The billing account used for the resources which are direct children of workload. This billing account is initially associated with the resources created as part of Workload creation. After the initial creation of these resources, the customer can change the assigned billing account. The resource name has the form `billingAccounts/{billing_account_id\}`. For example, `billingAccounts/012345-567890-ABCDEF`.
      */
     billingAccount?: string | null;
     /**
