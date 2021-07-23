97fc5af22b7877d2c2cbcf0f23a0b46c6c8f2d1c
Yoshi Automation <yoshi-automation@google.com>
2021-07-23 01:32:03 +0000

    fix(calendar): update the API
    #### calendar:v3
    
    The following keys were changed:
    - schemas.ConferenceSolutionKey.properties.type.description

--- src/apis/calendar/v3.ts
+++ src/apis/calendar/v3.ts
@@ -493,7 +493,7 @@ export namespace calendar_v3 {
      * If a client encounters an unfamiliar or empty type, it should still be able to display the entry points. However, it should disallow modifications.
      * The possible values are:
      * - "eventHangout" for Hangouts for consumers (http://hangouts.google.com)
-     * - "eventNamedHangout" for classic Hangouts for Google Workspace users (http://hangouts.google.com)
+     * - "eventNamedHangout" for classic Hangouts for Google Workspace users (deprecated; http://hangouts.google.com)
      * - "hangoutsMeet" for Google Meet (http://meet.google.com)
      * - "addOn" for 3P conference providers
      */
