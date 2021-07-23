ce2292e8bb7b5bab54c9985c049eb54285cafed5
Yoshi Automation <yoshi-automation@google.com>
2021-07-23 01:32:08 +0000

    feat(gkehub): update the API
    #### gkehub:v1alpha
    
    The following keys were changed:
    - schemas.Feature.properties.membershipSpecs.description
    - schemas.Feature.properties.membershipStates.description
    - schemas.MultiClusterIngressFeatureSpec.properties.billing.description
    
    #### gkehub:v1alpha2
    
    The following keys were added:
    - schemas.GkeCluster.properties.clusterMissing.description
    - schemas.GkeCluster.properties.clusterMissing.readOnly
    - schemas.GkeCluster.properties.clusterMissing.type
    
    #### gkehub:v1beta
    
    The following keys were changed:
    - schemas.Feature.properties.membershipSpecs.description
    - schemas.Feature.properties.membershipStates.description
    - schemas.MultiClusterIngressFeatureSpec.properties.billing.description
    
    #### gkehub:v1beta1
    
    The following keys were added:
    - schemas.GkeCluster.properties.clusterMissing.description
    - schemas.GkeCluster.properties.clusterMissing.readOnly
    - schemas.GkeCluster.properties.clusterMissing.type
    
    #### gkehub:v1
    
    The following keys were added:
    - schemas.GkeCluster.properties.clusterMissing.description
    - schemas.GkeCluster.properties.clusterMissing.readOnly
    - schemas.GkeCluster.properties.clusterMissing.type
    
    The following keys were changed:
    - schemas.Feature.properties.membershipSpecs.description
    - schemas.Feature.properties.membershipStates.description

--- src/apis/gkehub/v1beta.ts
+++ src/apis/gkehub/v1beta.ts
@@ -680,11 +680,11 @@ export namespace gkehub_v1beta {
      */
     labels?: {[key: string]: string} | null;
     /**
-     * Optional. Membership-specific configuration for this Feature. If this Feature does not support any per-Membership configuration, this field may be unused. The keys indicate which Membership the configuration is for, in the form: projects/{p\}/locations/{l\}/memberships/{m\} Where {p\} is the project, {l\} is a valid location and {m\} is a valid Membership in this project at that location. {p\} WILL match the Feature's project. {p\} will always be returned as the project number, but the project ID is also accepted during input. If the same Membership is specified in the map twice (using the project ID form, and the project number form), exactly ONE of the entries will be saved, with no guarantees as to which. For this reason, it is recommended the same format be used for all entries when mutating a Feature.
+     * Optional. Membership-specific configuration for this Feature. If this Feature does not support any per-Membership configuration, this field may be unused. The keys indicate which Membership the configuration is for, in the form: `projects/{p\}/locations/{l\}/memberships/{m\}` Where {p\} is the project, {l\} is a valid location and {m\} is a valid Membership in this project at that location. {p\} WILL match the Feature's project. {p\} will always be returned as the project number, but the project ID is also accepted during input. If the same Membership is specified in the map twice (using the project ID form, and the project number form), exactly ONE of the entries will be saved, with no guarantees as to which. For this reason, it is recommended the same format be used for all entries when mutating a Feature.
      */
     membershipSpecs?: {[key: string]: Schema$MembershipFeatureSpec} | null;
     /**
-     * Output only. Membership-specific Feature status. If this Feature does report any per-Membership status, this field may be unused. The keys indicate which Membership the state is for, in the form: projects/{p\}/locations/{l\}/memberships/{m\} Where {p\} is the project number, {l\} is a valid location and {m\} is a valid Membership in this project at that location. {p\} MUST match the Feature's project number.
+     * Output only. Membership-specific Feature status. If this Feature does report any per-Membership status, this field may be unused. The keys indicate which Membership the state is for, in the form: `projects/{p\}/locations/{l\}/memberships/{m\}` Where {p\} is the project number, {l\} is a valid location and {m\} is a valid Membership in this project at that location. {p\} MUST match the Feature's project number.
      */
     membershipStates?: {[key: string]: Schema$MembershipFeatureState} | null;
     /**
@@ -859,7 +859,7 @@ export namespace gkehub_v1beta {
    */
   export interface Schema$MultiClusterIngressFeatureSpec {
     /**
-     * Customer's billing structure
+     * Deprecated: This field will be ignored and should not be set. Customer's billing structure.
      */
     billing?: string | null;
     /**
