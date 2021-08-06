d7fe66a031c8252fbf4c76f3b3748b16798d744f
Yoshi Automation <yoshi-automation@google.com>
2021-08-06 01:31:42 +0000

    feat(iam): update the API
    #### iam:v1
    
    The following keys were added:
    - schemas.PolicyOperationMetadata.description
    - schemas.PolicyOperationMetadata.id
    - schemas.PolicyOperationMetadata.properties.createTime.description
    - schemas.PolicyOperationMetadata.properties.createTime.format
    - schemas.PolicyOperationMetadata.properties.createTime.type
    - schemas.PolicyOperationMetadata.type
    
    The following keys were changed:
    - schemas.SignJwtRequest.properties.payload.description

--- src/apis/iam/v1.ts
+++ src/apis/iam/v1.ts
@@ -540,6 +540,15 @@ export namespace iam_v1 {
     bindingDeltas?: Schema$BindingDelta[];
   }
   /**
+   * Metadata for long-running Policy operations.
+   */
+  export interface Schema$PolicyOperationMetadata {
+    /**
+     * Timestamp when the google.longrunning.Operation was created.
+     */
+    createTime?: string | null;
+  }
+  /**
    * A request to get the list of auditable services for a resource.
    */
   export interface Schema$QueryAuditableServicesRequest {
@@ -773,7 +782,7 @@ export namespace iam_v1 {
    */
   export interface Schema$SignJwtRequest {
     /**
-     * Required. Deprecated. [Migrate to Service Account Credentials API](https://cloud.google.com/iam/help/credentials/migrate-api). The JWT payload to sign. Must be a serialized JSON object that contains a JWT Claims Set. For example: `{"sub": "user@example.com", "iat": 313435\}` If the JWT Claims Set contains an expiration time (`exp`) claim, it must be an integer timestamp that is not in the past and no more than 1 hour in the future. If the JWT Claims Set does not contain an expiration time (`exp`) claim, this claim is added automatically, with a timestamp that is 1 hour in the future.
+     * Required. Deprecated. [Migrate to Service Account Credentials API](https://cloud.google.com/iam/help/credentials/migrate-api). The JWT payload to sign. Must be a serialized JSON object that contains a JWT Claims Set. For example: `{"sub": "user@example.com", "iat": 313435\}` If the JWT Claims Set contains an expiration time (`exp`) claim, it must be an integer timestamp that is not in the past and no more than 12 hours in the future. If the JWT Claims Set does not contain an expiration time (`exp`) claim, this claim is added automatically, with a timestamp that is 1 hour in the future.
      */
     payload?: string | null;
   }
