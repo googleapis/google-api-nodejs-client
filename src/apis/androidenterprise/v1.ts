92ee5fff341440503c9a767f6951f6bc0a215f24
Yoshi Automation <yoshi-automation@google.com>
2021-08-06 01:31:39 +0000

    fix(androidenterprise): update the API
    #### androidenterprise:v1
    
    The following keys were changed:
    - resources.devices.methods.update.description

--- src/apis/androidenterprise/v1.ts
+++ src/apis/androidenterprise/v1.ts
@@ -2152,7 +2152,7 @@ export namespace androidenterprise_v1 {
     }
 
     /**
-     * Updates the device policy. To ensure the policy is properly enforced, you need to prevent unmanaged accounts from accessing Google Play by setting the allowed_accounts in the managed configuration for the Google Play package. See restrict accounts in Google Play.
+     * Updates the device policy. To ensure the policy is properly enforced, you need to prevent unmanaged accounts from accessing Google Play by setting the allowed_accounts in the managed configuration for the Google Play package. See restrict accounts in Google Play. When provisioning a new device, you should set the device policy using this method before adding the managed Google Play Account to the device, otherwise the policy will not be applied for a short period of time after adding the account to the device.
      * @example
      * ```js
      * // Before running the sample:
