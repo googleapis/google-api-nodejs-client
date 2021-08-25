312f32ffcf316061490cb704b6e242cf3f45f7c9
Yoshi Automation <yoshi-automation@google.com>
2021-08-25 01:29:01 +0000

    fix(workflows): update the API
    #### workflows:v1beta
    
    The following keys were changed:
    - auth.oauth2.scopes.https://www.googleapis.com/auth/cloud-platform.description
    - schemas.Workflow.properties.serviceAccount.description
    
    #### workflows:v1
    
    The following keys were changed:
    - auth.oauth2.scopes.https://www.googleapis.com/auth/cloud-platform.description
    - schemas.Workflow.properties.serviceAccount.description

--- src/apis/workflows/v1beta.ts
+++ src/apis/workflows/v1beta.ts
@@ -293,7 +293,7 @@ export namespace workflows_v1beta {
      */
     revisionId?: string | null;
     /**
-     * Name of the service account associated with the latest workflow version. This service account represents the identity of the workflow and determines what permissions the workflow has. Format: projects/{project\}/serviceAccounts/{account\} Using `-` as a wildcard for the `{project\}` will infer the project from the account. The `{account\}` value can be the `email` address or the `unique_id` of the service account. If not provided, workflow will use the project's default service account. Modifying this field for an existing workflow results in a new workflow revision.
+     * The service account associated with the latest workflow version. This service account represents the identity of the workflow and determines what permissions the workflow has. Format: projects/{project\}/serviceAccounts/{account\} or {account\} Using `-` as a wildcard for the `{project\}` or not providing one at all will infer the project from the account. The `{account\}` value can be the `email` address or the `unique_id` of the service account. If not provided, workflow will use the project's default service account. Modifying this field for an existing workflow results in a new workflow revision.
      */
     serviceAccount?: string | null;
     /**
