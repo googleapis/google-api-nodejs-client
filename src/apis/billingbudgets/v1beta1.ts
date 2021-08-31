0b979982a8523882173f94a7c3ca0ffb13e7c13b
Yoshi Automation <yoshi-automation@google.com>
2021-08-31 19:36:00 +0000

    fix(billingbudgets): update the API
    #### billingbudgets:v1beta1
    
    The following keys were changed:
    - auth.oauth2.scopes.https://www.googleapis.com/auth/cloud-platform.description
    - schemas.GoogleCloudBillingBudgetsV1beta1Filter.properties.labels.description
    
    #### billingbudgets:v1
    
    The following keys were changed:
    - auth.oauth2.scopes.https://www.googleapis.com/auth/cloud-platform.description
    - schemas.GoogleCloudBillingBudgetsV1Budget.properties.etag.description
    - schemas.GoogleCloudBillingBudgetsV1Filter.properties.calendarPeriod.description
    - schemas.GoogleCloudBillingBudgetsV1Filter.properties.labels.description
    - schemas.GoogleCloudBillingBudgetsV1Filter.properties.projects.description
    - schemas.GoogleCloudBillingBudgetsV1Filter.properties.services.description
    - schemas.GoogleCloudBillingBudgetsV1Filter.properties.subaccounts.description
    - schemas.GoogleCloudBillingBudgetsV1LastPeriodAmount.description
    - schemas.GoogleCloudBillingBudgetsV1NotificationsRule.properties.monitoringNotificationChannels.description
    - schemas.GoogleCloudBillingBudgetsV1NotificationsRule.properties.pubsubTopic.description

--- src/apis/billingbudgets/v1beta1.ts
+++ src/apis/billingbudgets/v1beta1.ts
@@ -235,7 +235,7 @@ export namespace billingbudgets_v1beta1 {
      */
     customPeriod?: Schema$GoogleCloudBillingBudgetsV1beta1CustomPeriod;
     /**
-     * Optional. A single label and value pair specifying that usage from only this set of labeled resources should be included in the budget. Currently, multiple entries or multiple values per entry are not allowed. If omitted, the report will include all labeled and unlabeled usage.
+     * Optional. A single label and value pair specifying that usage from only this set of labeled resources should be included in the budget. If omitted, the report will include all labeled and unlabeled usage. An object containing a single `"key": value` pair. Example: `{ "name": "wrench" \}`. _Currently, multiple entries or multiple values per entry are not allowed._
      */
     labels?: {[key: string]: any[]} | null;
     /**
