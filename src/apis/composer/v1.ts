901734a9f7968e4e6a9255f84ca2cdcf0df1bd89
Yoshi Automation <yoshi-automation@google.com>
2021-08-31 01:29:17 +0000

    feat(composer): update the API
    #### composer:v1beta1
    
    The following keys were added:
    - schemas.NodeConfig.properties.enableIpMasqAgent.description
    - schemas.NodeConfig.properties.enableIpMasqAgent.type
    - schemas.PrivateEnvironmentConfig.properties.enablePrivatelyUsedPublicIps.description
    - schemas.PrivateEnvironmentConfig.properties.enablePrivatelyUsedPublicIps.type
    
    The following keys were changed:
    - auth.oauth2.scopes.https://www.googleapis.com/auth/cloud-platform.description
    - schemas.IPAllocationPolicy.properties.clusterIpv4CidrBlock.description
    - schemas.IPAllocationPolicy.properties.servicesIpv4CidrBlock.description
    
    #### composer:v1
    
    The following keys were changed:
    - auth.oauth2.scopes.https://www.googleapis.com/auth/cloud-platform.description
    - schemas.IPAllocationPolicy.properties.clusterIpv4CidrBlock.description
    - schemas.IPAllocationPolicy.properties.servicesIpv4CidrBlock.description

--- src/apis/composer/v1.ts
+++ src/apis/composer/v1.ts
@@ -318,7 +318,7 @@ export namespace composer_v1 {
    */
   export interface Schema$IPAllocationPolicy {
     /**
-     * Optional. The IP address range used to allocate IP addresses to pods in the GKE cluster. This field is applicable only when `use_ip_aliases` is true. Set to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have GKE choose a range with a specific netmask. Set to a [CIDR](http://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to use.
+     * Optional. The IP address range used to allocate IP addresses to pods in the GKE cluster. This field is applicable only when `use_ip_aliases` is true. Set to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have GKE choose a range with a specific netmask. Set to a [CIDR](https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to use.
      */
     clusterIpv4CidrBlock?: string | null;
     /**
@@ -326,7 +326,7 @@ export namespace composer_v1 {
      */
     clusterSecondaryRangeName?: string | null;
     /**
-     * Optional. The IP address range of the services IP addresses in this GKE cluster. This field is applicable only when `use_ip_aliases` is true. Set to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have GKE choose a range with a specific netmask. Set to a [CIDR](http://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to use.
+     * Optional. The IP address range of the services IP addresses in this GKE cluster. This field is applicable only when `use_ip_aliases` is true. Set to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have GKE choose a range with a specific netmask. Set to a [CIDR](https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to use.
      */
     servicesIpv4CidrBlock?: string | null;
     /**
