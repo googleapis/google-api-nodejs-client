b75f36cbfed0f190f1728ce4f569dfd8846e9695
Yoshi Automation <yoshi-automation@google.com>
2021-09-28 01:32:44 +0000

    fix(composer): update the API
    #### composer:v1beta1
    
    The following keys were changed:
    - schemas.IPAllocationPolicy.properties.clusterIpv4CidrBlock.description
    - schemas.IPAllocationPolicy.properties.clusterSecondaryRangeName.description
    - schemas.IPAllocationPolicy.properties.servicesIpv4CidrBlock.description
    - schemas.IPAllocationPolicy.properties.servicesSecondaryRangeName.description
    
    #### composer:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.environments.methods.patch.parameters.updateMask.description
    - schemas.DatabaseConfig.description
    - schemas.EncryptionConfig.description
    - schemas.EnvironmentConfig.properties.databaseConfig.description
    - schemas.EnvironmentConfig.properties.encryptionConfig.description
    - schemas.EnvironmentConfig.properties.nodeCount.description
    - schemas.EnvironmentConfig.properties.webServerNetworkAccessControl.description
    - schemas.IPAllocationPolicy.properties.clusterIpv4CidrBlock.description
    - schemas.IPAllocationPolicy.properties.clusterSecondaryRangeName.description
    - schemas.IPAllocationPolicy.properties.servicesIpv4CidrBlock.description
    - schemas.IPAllocationPolicy.properties.servicesSecondaryRangeName.description
    - schemas.IPAllocationPolicy.properties.useIpAliases.description
    - schemas.NodeConfig.properties.diskSizeGb.description
    - schemas.NodeConfig.properties.location.description
    - schemas.NodeConfig.properties.machineType.description
    - schemas.NodeConfig.properties.oauthScopes.description
    - schemas.NodeConfig.properties.tags.description
    - schemas.PrivateEnvironmentConfig.properties.enablePrivateEnvironment.description
    - schemas.PrivateEnvironmentConfig.properties.webServerIpv4CidrBlock.description
    - schemas.PrivateEnvironmentConfig.properties.webServerIpv4ReservedRange.description
    - schemas.SoftwareConfig.properties.pythonVersion.description
    - schemas.WebServerConfig.description
    - schemas.WebServerNetworkAccessControl.description

--- src/apis/composer/v1beta1.ts
+++ src/apis/composer/v1beta1.ts
@@ -339,19 +339,19 @@ export namespace composer_v1beta1 {
    */
   export interface Schema$IPAllocationPolicy {
     /**
-     * Optional. The IP address range used to allocate IP addresses to pods in the cluster. This field is applicable only when `use_ip_aliases` is true. For Cloud Composer environments in versions composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true. Set to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have GKE choose a range with a specific netmask. Set to a [CIDR](https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to use. Specify `cluster_secondary_range_name` or `cluster_ipv4_cidr_block` but not both.
+     * Optional. The IP address range used to allocate IP addresses to pods in the cluster. For Cloud Composer environments in versions composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true. Set to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have GKE choose a range with a specific netmask. Set to a [CIDR](https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to use. Specify `cluster_secondary_range_name` or `cluster_ipv4_cidr_block` but not both.
      */
     clusterIpv4CidrBlock?: string | null;
     /**
-     * Optional. The name of the cluster's secondary range used to allocate IP addresses to pods. Specify either `cluster_secondary_range_name` or `cluster_ipv4_cidr_block` but not both. This field is applicable only when `use_ip_aliases` is true. For Cloud Composer environments in versions composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true.
+     * Optional. The name of the cluster's secondary range used to allocate IP addresses to pods. Specify either `cluster_secondary_range_name` or `cluster_ipv4_cidr_block` but not both. For Cloud Composer environments in versions composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true.
      */
     clusterSecondaryRangeName?: string | null;
     /**
-     * Optional. The IP address range of the services IP addresses in this cluster. This field is applicable only when `use_ip_aliases` is true. For Cloud Composer environments in versions composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true. Set to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have GKE choose a range with a specific netmask. Set to a [CIDR](https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to use. Specify `services_secondary_range_name` or `services_ipv4_cidr_block` but not both.
+     * Optional. The IP address range of the services IP addresses in this cluster. For Cloud Composer environments in versions composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true. Set to blank to have GKE choose a range with the default size. Set to /netmask (e.g. `/14`) to have GKE choose a range with a specific netmask. Set to a [CIDR](https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) from the RFC-1918 private networks (e.g. `10.0.0.0/8`, `172.16.0.0/12`, `192.168.0.0/16`) to pick a specific range to use. Specify `services_secondary_range_name` or `services_ipv4_cidr_block` but not both.
      */
     servicesIpv4CidrBlock?: string | null;
     /**
-     * Optional. The name of the services' secondary range used to allocate IP addresses to the cluster. Specify either `services_secondary_range_name` or `services_ipv4_cidr_block` but not both. This field is applicable only when `use_ip_aliases` is true. For Cloud Composer environments in versions composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true.
+     * Optional. The name of the services' secondary range used to allocate IP addresses to the cluster. Specify either `services_secondary_range_name` or `services_ipv4_cidr_block` but not both. For Cloud Composer environments in versions composer-1.*.*-airflow-*.*.*, this field is applicable only when `use_ip_aliases` is true.
      */
     servicesSecondaryRangeName?: string | null;
     /**
