af0a9b510bbbf9207d7a97bbc4ff51fa340bf2fb
Yoshi Automation <yoshi-automation@google.com>
2021-10-01 01:33:34 +0000

    feat(bigtableadmin)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### bigtableadmin:v2
    
    The following keys were deleted:
    - schemas.MultiClusterRoutingUseAny.properties.clusterIds.description
    - schemas.MultiClusterRoutingUseAny.properties.clusterIds.items.type
    - schemas.MultiClusterRoutingUseAny.properties.clusterIds.type

--- src/apis/bigtableadmin/v2.ts
+++ src/apis/bigtableadmin/v2.ts
@@ -762,12 +762,7 @@ export namespace bigtableadmin_v2 {
   /**
    * Read/write requests are routed to the nearest cluster in the instance, and will fail over to the nearest cluster that is available in the event of transient errors or delays. Clusters in a region are considered equidistant. Choosing this option sacrifices read-your-writes consistency to improve availability.
    */
-  export interface Schema$MultiClusterRoutingUseAny {
-    /**
-     * The set of clusters to route to. The order is ignored; clusters will be tried in order of distance. If left empty, all clusters are eligible.
-     */
-    clusterIds?: string[] | null;
-  }
+  export interface Schema$MultiClusterRoutingUseAny {}
   /**
    * This resource represents a long-running operation that is the result of a network API call.
    */
