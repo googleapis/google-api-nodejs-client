7de8325f86c0307869a64010c4490243754fd5be
Yoshi Automation <yoshi-automation@google.com>
2021-10-01 01:33:34 +0000

    feat(artifactregistry): update the API
    #### artifactregistry:v1beta2
    
    The following keys were added:
    - schemas.MavenRepositoryConfig.description
    - schemas.MavenRepositoryConfig.id
    - schemas.MavenRepositoryConfig.properties.allowSnapshotOverwrites.description
    - schemas.MavenRepositoryConfig.properties.allowSnapshotOverwrites.type
    - schemas.MavenRepositoryConfig.properties.versionPolicy.description
    - schemas.MavenRepositoryConfig.properties.versionPolicy.enum
    - schemas.MavenRepositoryConfig.properties.versionPolicy.enumDescriptions
    - schemas.MavenRepositoryConfig.properties.versionPolicy.type
    - schemas.MavenRepositoryConfig.type
    - schemas.Repository.properties.mavenConfig.$ref
    - schemas.Repository.properties.mavenConfig.description
    
    #### artifactregistry:v1
    
    The following keys were changed:
    - schemas.DockerImage.properties.buildTime.description

--- src/apis/artifactregistry/v1.ts
+++ src/apis/artifactregistry/v1.ts
@@ -165,7 +165,7 @@ export namespace artifactregistry_v1 {
    */
   export interface Schema$DockerImage {
     /**
-     * The time this image was built. This field is returned as the 'metadata.buildTime' field in the Version resource. The build time is returned to the client as an RFC 3339 string, which can be easily used with the JavaScript Date constructor and keeps the Version timestamps returned via OnePlatform consistent, as JSON marshals google.protobuf.Timestamp into an RFC 3339 string.
+     * The time this image was built. This field is returned as the 'metadata.buildTime' field in the Version resource. The build time is returned to the client as an RFC 3339 string, which can be easily used with the JavaScript Date constructor.
      */
     buildTime?: string | null;
     /**
