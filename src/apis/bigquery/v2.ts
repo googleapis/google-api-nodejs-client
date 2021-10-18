0de2e241c038d86f10ad11e6a1bcf61ee85b865a
Yoshi Automation <yoshi-automation@google.com>
2021-10-18 01:32:45 +0000

    feat(bigquery): update the API
    #### bigquery:v2
    
    The following keys were added:
    - schemas.DestinationTableProperties.properties.expirationTimestampMillis.description
    - schemas.DestinationTableProperties.properties.expirationTimestampMillis.format
    - schemas.DestinationTableProperties.properties.expirationTimestampMillis.type

--- src/apis/bigquery/v2.ts
+++ src/apis/bigquery/v2.ts
@@ -880,6 +880,10 @@ export namespace bigquery_v2 {
      */
     description?: string | null;
     /**
+     * [Optional] The expiration timestamp for the destination table. If this field is set: For a new table, it will set the table's expiration time (even if there is a dataset level default table expiration time). For an existing table, it will update the table's expiration time. If this field is not set: For a new table, if dataset level default table expiration time is present, that will be applied. For an existing table, no change is made to the table's expiration time. Additionally this field is only applied when data is written to an empty table (WRITE_EMPTY) or data is overwritten to a table (WRITE_TRUNCATE).
+     */
+    expirationTimestampMillis?: string | null;
+    /**
      * [Optional] The friendly name for the destination table. This will only be used if the destination table is newly created. If the table already exists and a value different than the current friendly name is provided, the job will fail.
      */
     friendlyName?: string | null;
