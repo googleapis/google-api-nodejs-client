494dc78ff8ed728a8da25fa036b2833ddf347327
Yoshi Automation <yoshi-automation@google.com>
2021-10-28 01:30:46 +0000

    fix(sqladmin): update the API
    #### sqladmin:v1beta4
    
    The following keys were changed:
    - schemas.DatabaseInstance.properties.databaseVersion.description
    
    #### sqladmin:v1
    
    The following keys were changed:
    - schemas.ConnectSettings.properties.databaseVersion.enum
    - schemas.ConnectSettings.properties.databaseVersion.enumDescriptions
    - schemas.DatabaseInstance.properties.databaseVersion.description
    - schemas.DatabaseInstance.properties.databaseVersion.enum
    - schemas.DatabaseInstance.properties.databaseVersion.enumDescriptions
    - schemas.Flag.properties.appliesTo.items.enum
    - schemas.Flag.properties.appliesTo.items.enumDescriptions

--- src/apis/sqladmin/v1.ts
+++ src/apis/sqladmin/v1.ts
@@ -477,7 +477,7 @@ export namespace sqladmin_v1 {
      */
     currentDiskSize?: string | null;
     /**
-     * The database engine type and version. The **databaseVersion** field cannot be changed after instance creation. * **MySQL instances**: MYSQL_8_0, MYSQL_5_7 (default), or MYSQL_5_6. * **PostgreSQL instances**: POSTGRES_9_6, POSTGRES_10, POSTGRES_11, POSTGRES_12, POSTGRES_13 (default). * **SQL Server instances**: SQLSERVER_2019_STANDARD, SQLSERVER_2019_ENTERPRISE, SQLSERVER_2019_EXPRESS, or SQLSERVER_2019_WEB, SQLSERVER_2017_STANDARD (default), SQLSERVER_2017_ENTERPRISE, SQLSERVER_2017_EXPRESS, or SQLSERVER_2017_WEB.
+     * The database engine type and version. The **databaseVersion** field cannot be changed after instance creation. * **MySQL instances**: MYSQL_8_0, MYSQL_5_7 (default), or MYSQL_5_6. * **PostgreSQL instances**: POSTGRES_9_6, POSTGRES_10, POSTGRES_11, POSTGRES_12, POSTGRES_13 (default), POSTGRES_14. * **SQL Server instances**: SQLSERVER_2019_STANDARD, SQLSERVER_2019_ENTERPRISE, SQLSERVER_2019_EXPRESS, or SQLSERVER_2019_WEB, SQLSERVER_2017_STANDARD (default), SQLSERVER_2017_ENTERPRISE, SQLSERVER_2017_EXPRESS, or SQLSERVER_2017_WEB.
      */
     databaseVersion?: string | null;
     /**
