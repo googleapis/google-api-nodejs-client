d864c2743cc2d7c3478499f68cb83439fb448215
Yoshi Automation <yoshi-automation@google.com>
2021-11-08 01:31:15 +0000

    feat(memcache)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### memcache:v1beta2
    
    The following keys were deleted:
    - schemas.MemcacheParameters.description
    
    The following keys were changed:
    - schemas.Instance.properties.parameters.description
    - schemas.MemcacheParameters.properties.id.description
    
    #### memcache:v1
    
    The following keys were deleted:
    - schemas.MemcacheParameters.description
    
    The following keys were changed:
    - schemas.Instance.properties.parameters.description
    - schemas.MemcacheParameters.properties.id.description

--- src/apis/memcache/v1.ts
+++ src/apis/memcache/v1.ts
@@ -472,7 +472,7 @@ export namespace memcache_v1 {
      */
     nodeCount?: number | null;
     /**
-     * Optional: User defined parameters to apply to the memcached process on each node.
+     * User defined parameters to apply to the memcached process on each node.
      */
     parameters?: Schema$MemcacheParameters;
     /**
@@ -621,12 +621,9 @@ export namespace memcache_v1 {
      */
     weeklyCycle?: Schema$WeeklyCycle;
   }
-  /**
-   * The unique ID associated with this set of parameters. Users can use this id to determine if the parameters associated with the instance differ from the parameters associated with the nodes. A discrepancy between parameter ids can inform users that they may need to take action to apply parameters on nodes.
-   */
   export interface Schema$MemcacheParameters {
     /**
-     * Output only.
+     * Output only. The unique ID associated with this set of parameters. Users can use this id to determine if the parameters associated with the instance differ from the parameters associated with the nodes. A discrepancy between parameter ids can inform users that they may need to take action to apply parameters on nodes.
      */
     id?: string | null;
     /**
