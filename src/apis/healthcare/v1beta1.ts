a9bf5d13fe4241fe1f382bcf89cec290360b624e
Yoshi Automation <yoshi-automation@google.com>
2021-11-08 01:31:14 +0000

    feat(healthcare): update the API
    #### healthcare:v1beta1
    
    The following keys were added:
    - schemas.ValidationConfig.properties.disableFhirpathValidation.description
    - schemas.ValidationConfig.properties.disableFhirpathValidation.type
    - schemas.ValidationConfig.properties.disableReferenceTypeValidation.description
    - schemas.ValidationConfig.properties.disableReferenceTypeValidation.type
    - schemas.ValidationConfig.properties.disableRequiredFieldValidation.description
    - schemas.ValidationConfig.properties.disableRequiredFieldValidation.type
    
    #### healthcare:v1
    
    The following keys were added:
    - schemas.FhirStore.properties.validationConfig.$ref
    - schemas.FhirStore.properties.validationConfig.description
    - schemas.ValidationConfig.description
    - schemas.ValidationConfig.id
    - schemas.ValidationConfig.properties.disableFhirpathValidation.description
    - schemas.ValidationConfig.properties.disableFhirpathValidation.type
    - schemas.ValidationConfig.properties.disableReferenceTypeValidation.description
    - schemas.ValidationConfig.properties.disableReferenceTypeValidation.type
    - schemas.ValidationConfig.properties.disableRequiredFieldValidation.description
    - schemas.ValidationConfig.properties.disableRequiredFieldValidation.type
    - schemas.ValidationConfig.type

--- src/apis/healthcare/v1beta1.ts
+++ src/apis/healthcare/v1beta1.ts
@@ -2328,9 +2328,21 @@ export namespace healthcare_v1beta1 {
    */
   export interface Schema$ValidationConfig {
     /**
+     * Whether to disable FHIRPath validation for incoming resources. Set this to true to disable checking incoming resources for conformance against FHIRPath requirement defined in the FHIR specification. This property only affects resource types that do not have profiles configured for them, any rules in enabled implementation guides will still be enforced.
+     */
+    disableFhirpathValidation?: boolean | null;
+    /**
      * Whether to disable profile validation for this FHIR store. Set this to true to disable checking incoming resources for conformance against StructureDefinitions in this FHIR store.
      */
     disableProfileValidation?: boolean | null;
+    /**
+     * Whether to disable reference type validation for incoming resources. Set this to true to disable checking incoming resources for conformance against reference type requirement defined in the FHIR specification. This property only affects resource types that do not have profiles configured for them, any rules in enabled implementation guides will still be enforced.
+     */
+    disableReferenceTypeValidation?: boolean | null;
+    /**
+     * Whether to disable required fields validation for incoming resources. Set this to true to disable checking incoming resources for conformance against required fields requirement defined in the FHIR specification. This property only affects resource types that do not have profiles configured for them, any rules in enabled implementation guides will still be enforced.
+     */
+    disableRequiredFieldValidation?: boolean | null;
     /**
      * A list of ImplementationGuide URLs in this FHIR store that are used to configure the profiles to use for validation. For example, to use the US Core profiles for validation, set `enabled_implementation_guides` to `["http://hl7.org/fhir/us/core/ImplementationGuide/ig"]`. If `enabled_implementation_guides` is empty or omitted, then incoming resources are only required to conform to the base FHIR profiles. Otherwise, a resource must conform to at least one profile listed in the `global` property of one of the enabled ImplementationGuides. The Cloud Healthcare API does not currently enforce all of the rules in a StructureDefinition. The following rules are supported: - min/max - minValue/maxValue - maxLength - type - fixed[x] - pattern[x] on simple types - slicing, when using "value" as the discriminator type When a URL cannot be resolved (for example, in a type assertion), the server does not return an error.
      */
