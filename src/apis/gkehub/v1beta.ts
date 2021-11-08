546baf32bb80781fc632f0ca9972554518bed3ea
Yoshi Automation <yoshi-automation@google.com>
2021-11-08 01:31:14 +0000

    feat(gkehub): update the API
    #### gkehub:v1alpha
    
    The following keys were added:
    - schemas.AppDevExperienceFeatureSpec.description
    - schemas.AppDevExperienceFeatureSpec.id
    - schemas.AppDevExperienceFeatureSpec.type
    - schemas.AppDevExperienceFeatureState.description
    - schemas.AppDevExperienceFeatureState.id
    - schemas.AppDevExperienceFeatureState.properties.networkingInstallSucceeded.$ref
    - schemas.AppDevExperienceFeatureState.properties.networkingInstallSucceeded.description
    - schemas.AppDevExperienceFeatureState.type
    - schemas.CommonFeatureSpec.properties.appdevexperience.$ref
    - schemas.CommonFeatureSpec.properties.appdevexperience.description
    - schemas.CommonFeatureState.properties.appdevexperience.$ref
    - schemas.CommonFeatureState.properties.appdevexperience.description
    - schemas.MembershipFeatureState.properties.appdevexperience.$ref
    - schemas.MembershipFeatureState.properties.appdevexperience.description
    - schemas.Status.description
    - schemas.Status.id
    - schemas.Status.properties.code.description
    - schemas.Status.properties.code.enum
    - schemas.Status.properties.code.enumDescriptions
    - schemas.Status.properties.code.type
    - schemas.Status.properties.description.description
    - schemas.Status.properties.description.type
    - schemas.Status.type
    
    #### gkehub:v1beta
    
    The following keys were added:
    - schemas.AppDevExperienceFeatureSpec.description
    - schemas.AppDevExperienceFeatureSpec.id
    - schemas.AppDevExperienceFeatureSpec.type
    - schemas.AppDevExperienceFeatureState.description
    - schemas.AppDevExperienceFeatureState.id
    - schemas.AppDevExperienceFeatureState.properties.networkingInstallSucceeded.$ref
    - schemas.AppDevExperienceFeatureState.properties.networkingInstallSucceeded.description
    - schemas.AppDevExperienceFeatureState.type
    - schemas.CommonFeatureSpec.properties.appdevexperience.$ref
    - schemas.CommonFeatureSpec.properties.appdevexperience.description
    - schemas.CommonFeatureState.properties.appdevexperience.$ref
    - schemas.CommonFeatureState.properties.appdevexperience.description
    - schemas.MembershipFeatureState.properties.appdevexperience.$ref
    - schemas.MembershipFeatureState.properties.appdevexperience.description
    - schemas.Status.description
    - schemas.Status.id
    - schemas.Status.properties.code.description
    - schemas.Status.properties.code.enum
    - schemas.Status.properties.code.enumDescriptions
    - schemas.Status.properties.code.type
    - schemas.Status.properties.description.description
    - schemas.Status.properties.description.type
    - schemas.Status.type
    
    #### gkehub:v1
    
    The following keys were added:
    - schemas.AppDevExperienceFeatureSpec.description
    - schemas.AppDevExperienceFeatureSpec.id
    - schemas.AppDevExperienceFeatureSpec.type
    - schemas.AppDevExperienceFeatureState.description
    - schemas.AppDevExperienceFeatureState.id
    - schemas.AppDevExperienceFeatureState.properties.networkingInstallSucceeded.$ref
    - schemas.AppDevExperienceFeatureState.properties.networkingInstallSucceeded.description
    - schemas.AppDevExperienceFeatureState.type
    - schemas.CommonFeatureSpec.properties.appdevexperience.$ref
    - schemas.CommonFeatureSpec.properties.appdevexperience.description
    - schemas.CommonFeatureState.properties.appdevexperience.$ref
    - schemas.CommonFeatureState.properties.appdevexperience.description
    - schemas.MembershipFeatureState.properties.appdevexperience.$ref
    - schemas.MembershipFeatureState.properties.appdevexperience.description
    - schemas.Status.description
    - schemas.Status.id
    - schemas.Status.properties.code.description
    - schemas.Status.properties.code.enum
    - schemas.Status.properties.code.enumDescriptions
    - schemas.Status.properties.code.type
    - schemas.Status.properties.description.description
    - schemas.Status.properties.description.type
    - schemas.Status.type

--- src/apis/gkehub/v1beta.ts
+++ src/apis/gkehub/v1beta.ts
@@ -126,6 +126,19 @@ export namespace gkehub_v1beta {
   }
 
   /**
+   * Spec for App Dev Experience Feature.
+   */
+  export interface Schema$AppDevExperienceFeatureSpec {}
+  /**
+   * State for App Dev Exp Feature.
+   */
+  export interface Schema$AppDevExperienceFeatureState {
+    /**
+     * Status of subcomponent that detects configured Service Mesh resources.
+     */
+    networkingInstallSucceeded?: Schema$Status;
+  }
+  /**
    * Specifies the audit configuration for a service. The configuration determines which permission types are logged, and what identities, if any, are exempted from logging. An AuditConfig must have one or more AuditLogConfigs. If there are AuditConfigs for both `allServices` and a specific service, the union of the two AuditConfigs is used for that service: the log_types specified in each AuditConfig are enabled, and the exempted_members in each AuditLogConfig are exempted. Example Policy with multiple AuditConfigs: { "audit_configs": [ { "service": "allServices", "audit_log_configs": [ { "log_type": "DATA_READ", "exempted_members": [ "user:jose@example.com" ] \}, { "log_type": "DATA_WRITE" \}, { "log_type": "ADMIN_READ" \} ] \}, { "service": "sampleservice.googleapis.com", "audit_log_configs": [ { "log_type": "DATA_READ" \}, { "log_type": "DATA_WRITE", "exempted_members": [ "user:aliya@example.com" ] \} ] \} ] \} For sampleservice, this policy enables DATA_READ, DATA_WRITE and ADMIN_READ logging. It also exempts jose@example.com from DATA_READ logging, and aliya@example.com from DATA_WRITE logging.
    */
   export interface Schema$AuditConfig {
@@ -177,6 +190,10 @@ export namespace gkehub_v1beta {
    */
   export interface Schema$CommonFeatureSpec {
     /**
+     * Appdevexperience specific spec.
+     */
+    appdevexperience?: Schema$AppDevExperienceFeatureSpec;
+    /**
      * Multicluster Ingress-specific spec.
      */
     multiclusteringress?: Schema$MultiClusterIngressFeatureSpec;
@@ -185,6 +202,10 @@ export namespace gkehub_v1beta {
    * CommonFeatureState contains Hub-wide Feature status information.
    */
   export interface Schema$CommonFeatureState {
+    /**
+     * Appdevexperience specific state.
+     */
+    appdevexperience?: Schema$AppDevExperienceFeatureState;
     /**
      * Output only. The "running state" of the Feature in this Hub.
      */
@@ -941,6 +962,10 @@ export namespace gkehub_v1beta {
    */
   export interface Schema$MembershipFeatureState {
     /**
+     * Appdevexperience specific state.
+     */
+    appdevexperience?: Schema$AppDevExperienceFeatureState;
+    /**
      * Config Management-specific state.
      */
     configmanagement?: Schema$ConfigManagementMembershipState;
@@ -1074,6 +1099,19 @@ export namespace gkehub_v1beta {
      * OPTIONAL: A FieldMask specifying which fields of the policy to modify. Only the fields in the mask will be modified. If no mask is provided, the following default mask is used: `paths: "bindings, etag"`
      */
     updateMask?: string | null;
+  }
+  /**
+   * Status specifies state for the subcomponent.
+   */
+  export interface Schema$Status {
+    /**
+     * Code specifies AppDevExperienceFeature's subcomponent ready state.
+     */
+    code?: string | null;
+    /**
+     * Description is populated if Code is Failed, explaining why it has failed.
+     */
+    description?: string | null;
   }
   /**
    * Request message for `TestIamPermissions` method.
