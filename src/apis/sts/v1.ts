576ed9aed1a7e26361debdcbc99e1e859a242441
Yoshi Automation <yoshi-automation@google.com>
2021-11-25 01:30:16 +0000

    fix(sts): update the API
    #### sts:v1
    
    The following keys were changed:
    - resources.v1.methods.token.description

--- src/apis/sts/v1.ts
+++ src/apis/sts/v1.ts
@@ -508,7 +508,7 @@ export namespace sts_v1 {
     }
 
     /**
-     * Exchanges a credential for a Google OAuth 2.0 access token. The token asserts an external identity within a workload identity pool, or it applies a Credential Access Boundary to a Google access token. When you call this method, do not send the `Authorization` HTTP header in the request. This method does not require the `Authorization` header, and using the header can cause the request to fail.
+     * Exchanges a credential for a Google OAuth 2.0 access token. The token asserts an external identity within an identity pool, or it applies a Credential Access Boundary to a Google access token. When you call this method, do not send the `Authorization` HTTP header in the request. This method does not require the `Authorization` header, and using the header can cause the request to fail.
      * @example
      * ```js
      * // Before running the sample:
