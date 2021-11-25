3d36c11a785e2fc91e150362f05de05dd1583e7b
Yoshi Automation <yoshi-automation@google.com>
2021-11-25 01:30:13 +0000

    feat(healthcare): update the API
    #### healthcare:v1beta1
    
    The following keys were added:
    - schemas.FhirConfig.properties.defaultKeepExtensions.description
    - schemas.FhirConfig.properties.defaultKeepExtensions.type
    
    #### healthcare:v1
    
    The following keys were added:
    - schemas.FhirConfig.properties.defaultKeepExtensions.description
    - schemas.FhirConfig.properties.defaultKeepExtensions.type

--- src/apis/healthcare/v1beta1.ts
+++ src/apis/healthcare/v1beta1.ts
@@ -977,6 +977,10 @@ export namespace healthcare_v1beta1 {
    */
   export interface Schema$FhirConfig {
     /**
+     * The behaviour for handling FHIR extensions that aren't otherwise specified for de-identification. If true, all extensions are preserved during de-identification by default. If false or unspecified, all extensions are removed during de-identification by default.
+     */
+    defaultKeepExtensions?: boolean | null;
+    /**
      * Specifies FHIR paths to match and how to transform them. Any field that is not matched by a FieldMetadata is passed through to the output dataset unmodified. All extensions will be processed according to `default_keep_extensions`. If a field can be matched by more than one FieldMetadata, the first FieldMetadata.Action is applied.
      */
     fieldMetadataList?: Schema$FieldMetadata[];
