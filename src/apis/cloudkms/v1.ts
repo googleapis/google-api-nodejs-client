4acf712068481a607f8322ac4d8481a1d966cf47
Yoshi Automation <yoshi-automation@google.com>
2022-01-11 01:35:15 +0000

    fix(cloudkms): update the API
    #### cloudkms:v1
    
    The following keys were changed:
    - schemas.GenerateRandomBytesRequest.properties.protectionLevel.description
    - schemas.ImportCryptoKeyVersionRequest.properties.rsaAesWrappedKey.description

--- src/apis/cloudkms/v1.ts
+++ src/apis/cloudkms/v1.ts
@@ -540,7 +540,7 @@ export namespace cloudkms_v1 {
      */
     lengthBytes?: number | null;
     /**
-     * The ProtectionLevel to use when generating the random data. Defaults to SOFTWARE.
+     * The ProtectionLevel to use when generating the random data. Currently, only HSM protection level is supported.
      */
     protectionLevel?: string | null;
   }
@@ -574,7 +574,7 @@ export namespace cloudkms_v1 {
      */
     importJob?: string | null;
     /**
-     * Wrapped key material produced with RSA_OAEP_3072_SHA1_AES_256 or RSA_OAEP_4096_SHA1_AES_256. This field contains the concatenation of two wrapped keys: 1. An ephemeral AES-256 wrapping key wrapped with the public_key using RSAES-OAEP with SHA-1, MGF1 with SHA-1, and an empty label. 2. The key to be imported, wrapped with the ephemeral AES-256 key using AES-KWP (RFC 5649). If importing symmetric key material, it is expected that the unwrapped key contains plain bytes. If importing asymmetric key material, it is expected that the unwrapped key is in PKCS#8-encoded DER format (the PrivateKeyInfo structure from RFC 5208). This format is the same as the format produced by PKCS#11 mechanism CKM_RSA_AES_KEY_WRAP.
+     * Wrapped key material produced with RSA_OAEP_3072_SHA1_AES_256 or RSA_OAEP_4096_SHA1_AES_256. This field contains the concatenation of two wrapped keys: 1. An ephemeral AES-256 wrapping key wrapped with the public_key using RSAES-OAEP with SHA-1/SHA-256, MGF1 with SHA-1/SHA-256, and an empty label. 2. The key to be imported, wrapped with the ephemeral AES-256 key using AES-KWP (RFC 5649). If importing symmetric key material, it is expected that the unwrapped key contains plain bytes. If importing asymmetric key material, it is expected that the unwrapped key is in PKCS#8-encoded DER format (the PrivateKeyInfo structure from RFC 5208). This format is the same as the format produced by PKCS#11 mechanism CKM_RSA_AES_KEY_WRAP.
      */
     rsaAesWrappedKey?: string | null;
   }
