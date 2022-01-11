c5e291586e91eb9103d857e153ee4d233130bb03
Yoshi Automation <yoshi-automation@google.com>
2022-01-11 01:35:19 +0000

    feat(texttospeech)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### texttospeech:v1
    
    The following keys were deleted:
    - auth.oauth2.scopes.https://www.googleapis.com/auth/dialogflow.description
    - resources.projects.resources.locations.resources.datasets.methods.import.description
    - resources.projects.resources.locations.resources.datasets.methods.import.flatPath
    - resources.projects.resources.locations.resources.datasets.methods.import.httpMethod
    - resources.projects.resources.locations.resources.datasets.methods.import.id
    - resources.projects.resources.locations.resources.datasets.methods.import.parameterOrder
    - resources.projects.resources.locations.resources.datasets.methods.import.parameters.name.description
    - resources.projects.resources.locations.resources.datasets.methods.import.parameters.name.location
    - resources.projects.resources.locations.resources.datasets.methods.import.parameters.name.pattern
    - resources.projects.resources.locations.resources.datasets.methods.import.parameters.name.required
    - resources.projects.resources.locations.resources.datasets.methods.import.parameters.name.type
    - resources.projects.resources.locations.resources.datasets.methods.import.path
    - resources.projects.resources.locations.resources.datasets.methods.import.request.$ref
    - resources.projects.resources.locations.resources.datasets.methods.import.response.$ref
    - resources.projects.resources.locations.resources.datasets.methods.import.scopes
    - schemas.ImportDataRequest.description
    - schemas.ImportDataRequest.id
    - schemas.ImportDataRequest.properties.csvCloudStorageUri.description
    - schemas.ImportDataRequest.properties.csvCloudStorageUri.type
    - schemas.ImportDataRequest.type
    - schemas.Operation.description
    - schemas.Operation.id
    - schemas.Operation.properties.done.description
    - schemas.Operation.properties.done.type
    - schemas.Operation.properties.error.$ref
    - schemas.Operation.properties.error.description
    - schemas.Operation.properties.metadata.additionalProperties.description
    - schemas.Operation.properties.metadata.additionalProperties.type
    - schemas.Operation.properties.metadata.description
    - schemas.Operation.properties.metadata.type
    - schemas.Operation.properties.name.description
    - schemas.Operation.properties.name.type
    - schemas.Operation.properties.response.additionalProperties.description
    - schemas.Operation.properties.response.additionalProperties.type
    - schemas.Operation.properties.response.description
    - schemas.Operation.properties.response.type
    - schemas.Operation.type
    - schemas.Status.description
    - schemas.Status.id
    - schemas.Status.properties.code.description
    - schemas.Status.properties.code.format
    - schemas.Status.properties.code.type
    - schemas.Status.properties.details.description
    - schemas.Status.properties.details.items.additionalProperties.description
    - schemas.Status.properties.details.items.additionalProperties.type
    - schemas.Status.properties.details.items.type
    - schemas.Status.properties.details.type
    - schemas.Status.properties.message.description
    - schemas.Status.properties.message.type
    - schemas.Status.type

--- src/apis/texttospeech/v1.ts
+++ src/apis/texttospeech/v1.ts
@@ -113,7 +113,6 @@ export namespace texttospeech_v1 {
    */
   export class Texttospeech {
     context: APIRequestContext;
-    projects: Resource$Projects;
     text: Resource$Text;
     voices: Resource$Voices;
 
@@ -123,7 +122,6 @@ export namespace texttospeech_v1 {
         google,
       };
 
-      this.projects = new Resource$Projects(this.context);
       this.text = new Resource$Text(this.context);
       this.voices = new Resource$Voices(this.context);
     }
@@ -159,15 +157,6 @@ export namespace texttospeech_v1 {
     volumeGainDb?: number | null;
   }
   /**
-   * A request to import data.
-   */
-  export interface Schema$ImportDataRequest {
-    /**
-     * Customer provide a Cloud Storage link which point to a .csv file which stores all the truth text and Cloud Storage link of audio data.
-     */
-    csvCloudStorageUri?: string | null;
-  }
-  /**
    * The message returned to the client by the `ListVoices` method.
    */
   export interface Schema$ListVoicesResponse {
@@ -175,48 +164,6 @@ export namespace texttospeech_v1 {
      * The list of voices.
      */
     voices?: Schema$Voice[];
-  }
-  /**
-   * This resource represents a long-running operation that is the result of a network API call.
-   */
-  export interface Schema$Operation {
-    /**
-     * If the value is `false`, it means the operation is still in progress. If `true`, the operation is completed, and either `error` or `response` is available.
-     */
-    done?: boolean | null;
-    /**
-     * The error result of the operation in case of failure or cancellation.
-     */
-    error?: Schema$Status;
-    /**
-     * Service-specific metadata associated with the operation. It typically contains progress information and common metadata such as create time. Some services might not provide such metadata. Any method that returns a long-running operation should document the metadata type, if any.
-     */
-    metadata?: {[key: string]: any} | null;
-    /**
-     * The server-assigned name, which is only unique within the same service that originally returns it. If you use the default HTTP mapping, the `name` should be a resource name ending with `operations/{unique_id\}`.
-     */
-    name?: string | null;
-    /**
-     * The normal response of the operation in case of success. If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`. If the original method is standard `Get`/`Create`/`Update`, the response should be the resource. For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name. For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
-     */
-    response?: {[key: string]: any} | null;
-  }
-  /**
-   * The `Status` type defines a logical error model that is suitable for different programming environments, including REST APIs and RPC APIs. It is used by [gRPC](https://github.com/grpc). Each `Status` message contains three pieces of data: error code, error message, and error details. You can find out more about this error model and how to work with it in the [API Design Guide](https://cloud.google.com/apis/design/errors).
-   */
-  export interface Schema$Status {
-    /**
-     * The status code, which should be an enum value of google.rpc.Code.
-     */
-    code?: number | null;
-    /**
-     * A list of messages that carry the error details. There is a common set of message types for APIs to use.
-     */
-    details?: Array<{[key: string]: any}> | null;
-    /**
-     * A developer-facing error message, which should be in English. Any user-facing error message should be localized and sent in the google.rpc.Status.details field, or localized by the client.
-     */
-    message?: string | null;
   }
   /**
    * Contains text input to be synthesized. Either `text` or `ssml` must be supplied. Supplying both or neither returns google.rpc.Code.INVALID_ARGUMENT. The input size is limited to 5000 characters.
@@ -294,185 +241,6 @@ export namespace texttospeech_v1 {
      * The preferred gender of the voice. If not set, the service will choose a voice based on the other parameters such as language_code and name. Note that this is only a preference, not requirement; if a voice of the appropriate gender is not available, the synthesizer should substitute a voice with a different gender rather than failing the request.
      */
     ssmlGender?: string | null;
-  }
-
-  export class Resource$Projects {
-    context: APIRequestContext;
-    locations: Resource$Projects$Locations;
-    constructor(context: APIRequestContext) {
-      this.context = context;
-      this.locations = new Resource$Projects$Locations(this.context);
-    }
-  }
-
-  export class Resource$Projects$Locations {
-    context: APIRequestContext;
-    datasets: Resource$Projects$Locations$Datasets;
-    constructor(context: APIRequestContext) {
-      this.context = context;
-      this.datasets = new Resource$Projects$Locations$Datasets(this.context);
-    }
-  }
-
-  export class Resource$Projects$Locations$Datasets {
-    context: APIRequestContext;
-    constructor(context: APIRequestContext) {
-      this.context = context;
-    }
-
-    /**
-     * Imports audio+text data for training custom voice.
-     * @example
-     * ```js
-     * // Before running the sample:
-     * // - Enable the API at:
-     * //   https://console.developers.google.com/apis/api/texttospeech.googleapis.com
-     * // - Login into gcloud by running:
-     * //   `$ gcloud auth application-default login`
-     * // - Install the npm module by running:
-     * //   `$ npm install googleapis`
-     *
-     * const {google} = require('googleapis');
-     * const texttospeech = google.texttospeech('v1');
-     *
-     * async function main() {
-     *   const auth = new google.auth.GoogleAuth({
-     *     // Scopes can be specified either as an array or as a single, space-delimited string.
-     *     scopes: [
-     *       'https://www.googleapis.com/auth/cloud-platform',
-     *       'https://www.googleapis.com/auth/dialogflow',
-     *     ],
-     *   });
-     *
-     *   // Acquire an auth client, and bind it to all future calls
-     *   const authClient = await auth.getClient();
-     *   google.options({auth: authClient});
-     *
-     *   // Do the magic
-     *   const res = await texttospeech.projects.locations.datasets.import({
-     *     // The name of the Dataset resource. Format: `projects/{project\}/locations/{location\}/datasets/{dataset\}`
-     *     name: 'projects/my-project/locations/my-location/datasets/my-dataset',
-     *
-     *     // Request body metadata
-     *     requestBody: {
-     *       // request body parameters
-     *       // {
-     *       //   "csvCloudStorageUri": "my_csvCloudStorageUri"
-     *       // }
-     *     },
-     *   });
-     *   console.log(res.data);
-     *
-     *   // Example response
-     *   // {
-     *   //   "done": false,
-     *   //   "error": {},
-     *   //   "metadata": {},
-     *   //   "name": "my_name",
-     *   //   "response": {}
-     *   // }
-     * }
-     *
-     * main().catch(e => {
-     *   console.error(e);
-     *   throw e;
-     * });
-     *
-     * ```
-     *
-     * @param params - Parameters for request
-     * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
-     * @param callback - Optional callback that handles the response.
-     * @returns A promise if used with async/await, or void if used with a callback.
-     */
-    import(
-      params: Params$Resource$Projects$Locations$Datasets$Import,
-      options: StreamMethodOptions
-    ): GaxiosPromise<Readable>;
-    import(
-      params?: Params$Resource$Projects$Locations$Datasets$Import,
-      options?: MethodOptions
-    ): GaxiosPromise<Schema$Operation>;
-    import(
-      params: Params$Resource$Projects$Locations$Datasets$Import,
-      options: StreamMethodOptions | BodyResponseCallback<Readable>,
-      callback: BodyResponseCallback<Readable>
-    ): void;
-    import(
-      params: Params$Resource$Projects$Locations$Datasets$Import,
-      options: MethodOptions | BodyResponseCallback<Schema$Operation>,
-      callback: BodyResponseCallback<Schema$Operation>
-    ): void;
-    import(
-      params: Params$Resource$Projects$Locations$Datasets$Import,
-      callback: BodyResponseCallback<Schema$Operation>
-    ): void;
-    import(callback: BodyResponseCallback<Schema$Operation>): void;
-    import(
-      paramsOrCallback?:
-        | Params$Resource$Projects$Locations$Datasets$Import
-        | BodyResponseCallback<Schema$Operation>
-        | BodyResponseCallback<Readable>,
-      optionsOrCallback?:
-        | MethodOptions
-        | StreamMethodOptions
-        | BodyResponseCallback<Schema$Operation>
-        | BodyResponseCallback<Readable>,
-      callback?:
-        | BodyResponseCallback<Schema$Operation>
-        | BodyResponseCallback<Readable>
-    ): void | GaxiosPromise<Schema$Operation> | GaxiosPromise<Readable> {
-      let params = (paramsOrCallback ||
-        {}) as Params$Resource$Projects$Locations$Datasets$Import;
-      let options = (optionsOrCallback || {}) as MethodOptions;
-
-      if (typeof paramsOrCallback === 'function') {
-        callback = paramsOrCallback;
-        params = {} as Params$Resource$Projects$Locations$Datasets$Import;
-        options = {};
-      }
-
-      if (typeof optionsOrCallback === 'function') {
-        callback = optionsOrCallback;
-        options = {};
-      }
-
-      const rootUrl = options.rootUrl || 'https://texttospeech.googleapis.com/';
-      const parameters = {
-        options: Object.assign(
-          {
-            url: (rootUrl + '/v1/{+name}:import').replace(/([^:]\/)\/+/g, '$1'),
-            method: 'POST',
-          },
-          options
-        ),
-        params,
-        requiredParams: ['name'],
-        pathParams: ['name'],
-        context: this.context,
-      };
-      if (callback) {
-        createAPIRequest<Schema$Operation>(
-          parameters,
-          callback as BodyResponseCallback<unknown>
-        );
-      } else {
-        return createAPIRequest<Schema$Operation>(parameters);
-      }
-    }
-  }
-
-  export interface Params$Resource$Projects$Locations$Datasets$Import
-    extends StandardParameters {
-    /**
-     * The name of the Dataset resource. Format: `projects/{project\}/locations/{location\}/datasets/{dataset\}`
-     */
-    name?: string;
-
-    /**
-     * Request body metadata
-     */
-    requestBody?: Schema$ImportDataRequest;
   }
 
   export class Resource$Text {
