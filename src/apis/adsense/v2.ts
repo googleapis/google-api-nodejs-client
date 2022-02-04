745360d0e086d3dccbb9bbf143378a1f6bf40290
Yoshi Automation <yoshi-automation@google.com>
2022-02-04 01:29:19 +0000

    fix(adsense): update the API
    #### adsense:v2
    
    The following keys were changed:
    - schemas.Date.description
    - schemas.Payment.properties.name.description

--- src/apis/adsense/v2.ts
+++ src/apis/adsense/v2.ts
@@ -283,7 +283,7 @@ export namespace adsense_v2 {
     reportingDimensionId?: string | null;
   }
   /**
-   * Represents a whole or partial calendar date, such as a birthday. The time of day and time zone are either specified elsewhere or are insignificant. The date is relative to the Gregorian Calendar. This can represent one of the following: * A full date, with non-zero year, month, and day values * A month and day value, with a zero year, such as an anniversary * A year on its own, with zero month and day values * A year and month value, with a zero day, such as a credit card expiration date Related types are google.type.TimeOfDay and `google.protobuf.Timestamp`.
+   * Represents a whole or partial calendar date, such as a birthday. The time of day and time zone are either specified elsewhere or are insignificant. The date is relative to the Gregorian Calendar. This can represent one of the following: * A full date, with non-zero year, month, and day values * A month and day, with a zero year (e.g., an anniversary) * A year on its own, with a zero month and a zero day * A year and month, with a zero day (e.g., a credit card expiration date) Related types: * google.type.TimeOfDay * google.type.DateTime * google.protobuf.Timestamp
    */
   export interface Schema$Date {
     /**
@@ -494,7 +494,7 @@ export namespace adsense_v2 {
      */
     date?: Schema$Date;
     /**
-     * Resource name of the payment. Format: accounts/{account\}/payments/unpaid for unpaid (current) earnings. accounts/{account\}/payments/yyyy-MM-dd for paid earnings.
+     * Resource name of the payment. Format: - accounts/{account\}/payments/unpaid for unpaid (current) AdSense earnings. - accounts/{account\}/payments/youtube-unpaid for unpaid (current) YouTube earnings. - accounts/{account\}/payments/yyyy-MM-dd for paid AdSense earnings. - accounts/{account\}/payments/youtube-yyyy-MM-dd for paid YouTube earnings.
      */
     name?: string | null;
   }
