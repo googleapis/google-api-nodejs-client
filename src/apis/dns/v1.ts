fe651f17a20ae70d226668a639fe4e322a75783d
Yoshi Automation <yoshi-automation@google.com>
2022-02-04 01:29:24 +0000

    feat(dns): update the API
    #### dns:v1beta2
    
    The following keys were added:
    - schemas.Quota.properties.peeringZonesPerTargetNetwork.description
    - schemas.Quota.properties.peeringZonesPerTargetNetwork.format
    - schemas.Quota.properties.peeringZonesPerTargetNetwork.type
    - schemas.RRSetRoutingPolicyGeoPolicy.description
    - schemas.RRSetRoutingPolicyGeoPolicyGeoPolicyItem.description
    - schemas.RRSetRoutingPolicyWrrPolicy.description
    - schemas.RRSetRoutingPolicyWrrPolicyWrrPolicyItem.description
    
    The following keys were changed:
    - schemas.RRSetRoutingPolicyGeoPolicyGeoPolicyItem.properties.signatureRrdatas.description
    - schemas.RRSetRoutingPolicyWrrPolicyWrrPolicyItem.properties.signatureRrdatas.description
    
    #### dns:v1
    
    The following keys were added:
    - schemas.Quota.properties.peeringZonesPerTargetNetwork.description
    - schemas.Quota.properties.peeringZonesPerTargetNetwork.format
    - schemas.Quota.properties.peeringZonesPerTargetNetwork.type
    - schemas.RRSetRoutingPolicyGeoPolicy.description
    - schemas.RRSetRoutingPolicyGeoPolicyGeoPolicyItem.description
    - schemas.RRSetRoutingPolicyWrrPolicy.description
    - schemas.RRSetRoutingPolicyWrrPolicyWrrPolicyItem.description
    
    The following keys were changed:
    - schemas.RRSetRoutingPolicyGeoPolicyGeoPolicyItem.properties.signatureRrdatas.description
    - schemas.RRSetRoutingPolicyWrrPolicyWrrPolicyItem.properties.signatureRrdatas.description

--- src/apis/dns/v1.ts
+++ src/apis/dns/v1.ts
@@ -663,6 +663,10 @@ export namespace dns_v1 {
      */
     networksPerPolicy?: number | null;
     /**
+     * Maximum allowed number of consumer peering zones per target network owned by this producer project
+     */
+    peeringZonesPerTargetNetwork?: number | null;
+    /**
      * Maximum allowed number of policies per project.
      */
     policies?: number | null;
@@ -857,6 +861,9 @@ export namespace dns_v1 {
     kind?: string | null;
     wrr?: Schema$RRSetRoutingPolicyWrrPolicy;
   }
+  /**
+   * Configures a RRSetRoutingPolicy that routes based on the geo location of the querying user.
+   */
   export interface Schema$RRSetRoutingPolicyGeoPolicy {
     /**
      * The primary geo routing configuration. If there are multiple items with the same location, an error is returned instead.
@@ -864,6 +871,9 @@ export namespace dns_v1 {
     items?: Schema$RRSetRoutingPolicyGeoPolicyGeoPolicyItem[];
     kind?: string | null;
   }
+  /**
+   * ResourceRecordSet data for one geo location.
+   */
   export interface Schema$RRSetRoutingPolicyGeoPolicyGeoPolicyItem {
     kind?: string | null;
     /**
@@ -872,19 +882,25 @@ export namespace dns_v1 {
     location?: string | null;
     rrdatas?: string[] | null;
     /**
-     * DNSSEC generated signatures for the above geo_rrdata.
+     * DNSSEC generated signatures for all the rrdata within this item. Note that if health checked targets are provided for DNSSEC enabled zones, there's a restriction of 1 ip per item. .
      */
     signatureRrdatas?: string[] | null;
   }
+  /**
+   * Configures a RRSetRoutingPolicy that routes in a weighted round robin fashion.
+   */
   export interface Schema$RRSetRoutingPolicyWrrPolicy {
     items?: Schema$RRSetRoutingPolicyWrrPolicyWrrPolicyItem[];
     kind?: string | null;
   }
+  /**
+   * A routing block which contains the routing information for one WRR item.
+   */
   export interface Schema$RRSetRoutingPolicyWrrPolicyWrrPolicyItem {
     kind?: string | null;
     rrdatas?: string[] | null;
     /**
-     * DNSSEC generated signatures for the above wrr_rrdata.
+     * DNSSEC generated signatures for all the rrdata within this item. Note that if health checked targets are provided for DNSSEC enabled zones, there's a restriction of 1 ip per item. .
      */
     signatureRrdatas?: string[] | null;
     /**
