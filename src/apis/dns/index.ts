0f8d2fc3043510629c4e46ac5559c69602c52de3
Yoshi Automation <yoshi-automation@google.com>
2022-03-08 01:36:40 +0000

    fix(dns): update the API

--- src/apis/dns/index.ts
+++ src/apis/dns/index.ts
@@ -16,11 +16,13 @@
 import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
 import {dns_v1} from './v1';
 import {dns_v1beta2} from './v1beta2';
+import {dns_v2} from './v2';
 import {dns_v2beta1} from './v2beta1';
 
 export const VERSIONS = {
   v1: dns_v1.Dns,
   v1beta2: dns_v1beta2.Dns,
+  v2: dns_v2.Dns,
   v2beta1: dns_v2beta1.Dns,
 };
 
@@ -28,15 +30,21 @@ export function dns(version: 'v1'): dns_v1.Dns;
 export function dns(options: dns_v1.Options): dns_v1.Dns;
 export function dns(version: 'v1beta2'): dns_v1beta2.Dns;
 export function dns(options: dns_v1beta2.Options): dns_v1beta2.Dns;
+export function dns(version: 'v2'): dns_v2.Dns;
+export function dns(options: dns_v2.Options): dns_v2.Dns;
 export function dns(version: 'v2beta1'): dns_v2beta1.Dns;
 export function dns(options: dns_v2beta1.Options): dns_v2beta1.Dns;
-export function dns<T = dns_v1.Dns | dns_v1beta2.Dns | dns_v2beta1.Dns>(
+export function dns<
+  T = dns_v1.Dns | dns_v1beta2.Dns | dns_v2.Dns | dns_v2beta1.Dns
+>(
   this: GoogleConfigurable,
   versionOrOptions:
     | 'v1'
     | dns_v1.Options
     | 'v1beta2'
     | dns_v1beta2.Options
+    | 'v2'
+    | dns_v2.Options
     | 'v2beta1'
     | dns_v2beta1.Options
 ) {
@@ -47,6 +55,7 @@ const auth = new AuthPlus();
 export {auth};
 export {dns_v1};
 export {dns_v1beta2};
+export {dns_v2};
 export {dns_v2beta1};
 export {
   AuthPlus,
