0a161e98a9884f8fd43844b9ed7f65156de15058
Yoshi Automation <yoshi-automation@google.com>
2022-03-08 01:36:45 +0000

    feat(servicemanagement)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### servicemanagement:v1
    
    The following keys were deleted:
    - schemas.EnableServiceResponse.description
    - schemas.EnableServiceResponse.id
    - schemas.EnableServiceResponse.type
    
    The following keys were changed:
    - schemas.MetricDescriptor.properties.launchStage.enumDescriptions
    - schemas.MetricDescriptorMetadata.properties.launchStage.enumDescriptions
    - schemas.MonitoredResourceDescriptor.properties.launchStage.enumDescriptions
    - schemas.Rollout.properties.createdBy.description

--- src/apis/servicemanagement/v1.ts
+++ src/apis/servicemanagement/v1.ts
@@ -576,10 +576,6 @@ export namespace servicemanagement_v1 {
     selector?: string | null;
   }
   /**
-   * Operation payload for EnableService method.
-   */
-  export interface Schema$EnableServiceResponse {}
-  /**
    * `Endpoint` describes a network address of a service that serves a set of APIs. It is commonly known as a service endpoint. A service may expose any number of service endpoints, and all service endpoints share the same service definition, such as quota limits and monitoring metrics. Example: type: google.api.Service name: library-example.googleapis.com endpoints: # Declares network address `https://library-example.googleapis.com` # for service `library-example.googleapis.com`. The `https` scheme # is implicit for all service endpoints. Other schemes may be # supported in the future. - name: library-example.googleapis.com allow_cors: false - name: content-staging-library-example.googleapis.com # Allows HTTP OPTIONS calls to be passed to the API frontend, for it # to decide whether the subsequent cross-origin request is allowed # to proceed. allow_cors: true
    */
   export interface Schema$Endpoint {
@@ -1348,7 +1344,7 @@ export namespace servicemanagement_v1 {
    */
   export interface Schema$Rollout {
     /**
-     * This field is deprecated and will be deleted. Please remove usage of this field.
+     * The user who created the Rollout. Readonly.
      */
     createdBy?: string | null;
     /**
