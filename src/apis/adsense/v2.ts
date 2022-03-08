844ef06564151ac078354ad611a47abbf7f9300f
Yoshi Automation <yoshi-automation@google.com>
2022-03-08 01:36:34 +0000

    fix(adsense): update the API
    #### adsense:v2
    
    The following keys were changed:
    - schemas.Date.description
    - schemas.Payment.description

--- src/apis/adsense/v2.ts
+++ src/apis/adsense/v2.ts
@@ -283,7 +283,7 @@ export namespace adsense_v2 {
     reportingDimensionId?: string | null;
   }
   /**
-   * Represents a whole or partial calendar date, such as a birthday. The time of day and time zone are either specified elsewhere or are insignificant. The date is relative to the Gregorian Calendar. This can represent one of the following: * A full date, with non-zero year, month, and day values * A month and day, with a zero year (e.g., an anniversary) * A year on its own, with a zero month and a zero day * A year and month, with a zero day (e.g., a credit card expiration date) Related types: * google.type.TimeOfDay * google.type.DateTime * google.protobuf.Timestamp
+   * Represents a whole or partial calendar date, such as a birthday. The time of day and time zone are either specified elsewhere or are insignificant. The date is relative to the Gregorian Calendar. This can represent one of the following: * A full date, with non-zero year, month, and day values. * A month and day, with a zero year (for example, an anniversary). * A year on its own, with a zero month and a zero day. * A year and month, with a zero day (for example, a credit card expiration date). Related types: * google.type.TimeOfDay * google.type.DateTime * google.protobuf.Timestamp
    */
   export interface Schema$Date {
     /**
@@ -482,7 +482,7 @@ export namespace adsense_v2 {
     urlChannels?: Schema$UrlChannel[];
   }
   /**
-   * Representation of an unpaid or paid payment. See [Payment timelines for AdSense](https://support.google.com/adsense/answer/7164703) for more information about payments.
+   * Representation of an unpaid or paid payment. See [Payment timelines for AdSense](https://support.google.com/adsense/answer/7164703) for more information about payments and the [YouTube homepage and payments account](https://support.google.com/adsense/answer/11622510) article for information about dedicated payments accounts for YouTube.
    */
   export interface Schema$Payment {
     /**
