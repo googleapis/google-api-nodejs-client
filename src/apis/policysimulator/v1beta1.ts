908a249a7456a418b63ee9e660cfd2776e8dbec6
Yoshi Automation <yoshi-automation@google.com>
2022-03-08 01:36:43 +0000

    feat(policysimulator): update the API
    #### policysimulator:v1beta1
    
    The following keys were added:
    - schemas.GoogleTypeDate.description
    
    #### policysimulator:v1
    
    The following keys were added:
    - schemas.GoogleTypeDate.description

--- src/apis/policysimulator/v1beta1.ts
+++ src/apis/policysimulator/v1beta1.ts
@@ -571,6 +571,9 @@ export namespace policysimulator_v1beta1 {
      */
     message?: string | null;
   }
+  /**
+   * Represents a whole or partial calendar date, such as a birthday. The time of day and time zone are either specified elsewhere or are insignificant. The date is relative to the Gregorian Calendar. This can represent one of the following: * A full date, with non-zero year, month, and day values. * A month and day, with a zero year (for example, an anniversary). * A year on its own, with a zero month and a zero day. * A year and month, with a zero day (for example, a credit card expiration date). Related types: * google.type.TimeOfDay * google.type.DateTime * google.protobuf.Timestamp
+   */
   export interface Schema$GoogleTypeDate {
     /**
      * Day of a month. Must be from 1 to 31 and valid for the year and month, or 0 to specify a year by itself or a year and month where the day isn't significant.
