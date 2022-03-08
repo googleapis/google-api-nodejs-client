47875cd0097f0b430943d9fe953083a2cfd6fe60
Yoshi Automation <yoshi-automation@google.com>
2022-03-08 01:36:40 +0000

    fix(dlp): update the API
    #### dlp:v2
    
    The following keys were changed:
    - schemas.GoogleTypeDate.description

--- src/apis/dlp/v2.ts
+++ src/apis/dlp/v2.ts
@@ -3135,7 +3135,7 @@ export namespace dlp_v2 {
     message?: string | null;
   }
   /**
-   * Represents a whole or partial calendar date, such as a birthday. The time of day and time zone are either specified elsewhere or are insignificant. The date is relative to the Gregorian Calendar. This can represent one of the following: * A full date, with non-zero year, month, and day values * A month and day value, with a zero year, such as an anniversary * A year on its own, with zero month and day values * A year and month value, with a zero day, such as a credit card expiration date Related types are google.type.TimeOfDay and `google.protobuf.Timestamp`.
+   * Represents a whole or partial calendar date, such as a birthday. The time of day and time zone are either specified elsewhere or are insignificant. The date is relative to the Gregorian Calendar. This can represent one of the following: * A full date, with non-zero year, month, and day values * A month and day, with a zero year (e.g., an anniversary) * A year on its own, with a zero month and a zero day * A year and month, with a zero day (e.g., a credit card expiration date) Related types: * google.type.TimeOfDay * google.type.DateTime * google.protobuf.Timestamp
    */
   export interface Schema$GoogleTypeDate {
     /**
