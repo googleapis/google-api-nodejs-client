20fb1b97a5714f4b47b9c37a2d88fc857204ab68
Yoshi Automation <yoshi-automation@google.com>
2022-03-08 01:36:37 +0000

    feat(composer)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### composer:v1beta1
    
    The following keys were deleted:
    - schemas.LoadEnvironmentStateResponse.description
    - schemas.LoadEnvironmentStateResponse.id
    - schemas.LoadEnvironmentStateResponse.type
    - schemas.StoreEnvironmentStateResponse.description
    - schemas.StoreEnvironmentStateResponse.id
    - schemas.StoreEnvironmentStateResponse.properties.snapshotLocation.description
    - schemas.StoreEnvironmentStateResponse.properties.snapshotLocation.type
    - schemas.StoreEnvironmentStateResponse.type
    
    The following keys were added:
    - schemas.LoadSnapshotResponse.description
    - schemas.LoadSnapshotResponse.id
    - schemas.LoadSnapshotResponse.type
    - schemas.SaveSnapshotResponse.description
    - schemas.SaveSnapshotResponse.id
    - schemas.SaveSnapshotResponse.properties.snapshotPath.description
    - schemas.SaveSnapshotResponse.properties.snapshotPath.type
    - schemas.SaveSnapshotResponse.type
    
    The following keys were changed:
    - schemas.Date.description
    - schemas.OperationMetadata.properties.operationType.enum
    - schemas.OperationMetadata.properties.operationType.enumDescriptions
    
    #### composer:v1
    
    The following keys were added:
    - schemas.PrivateEnvironmentConfig.properties.cloudComposerConnectionSubnetwork.description
    - schemas.PrivateEnvironmentConfig.properties.cloudComposerConnectionSubnetwork.type
    
    The following keys were changed:
    - schemas.Date.description
    - schemas.OperationMetadata.properties.operationType.enum
    - schemas.OperationMetadata.properties.operationType.enumDescriptions

--- src/apis/composer/v1beta1.ts
+++ src/apis/composer/v1beta1.ts
@@ -195,7 +195,7 @@ export namespace composer_v1beta1 {
     machineType?: string | null;
   }
   /**
-   * Represents a whole or partial calendar date, such as a birthday. The time of day and time zone are either specified elsewhere or are insignificant. The date is relative to the Gregorian Calendar. This can represent one of the following: * A full date, with non-zero year, month, and day values * A month and day value, with a zero year, such as an anniversary * A year on its own, with zero month and day values * A year and month value, with a zero day, such as a credit card expiration date Related types are google.type.TimeOfDay and `google.protobuf.Timestamp`.
+   * Represents a whole or partial calendar date, such as a birthday. The time of day and time zone are either specified elsewhere or are insignificant. The date is relative to the Gregorian Calendar. This can represent one of the following: * A full date, with non-zero year, month, and day values * A month and day, with a zero year (e.g., an anniversary) * A year on its own, with a zero month and a zero day * A year and month, with a zero day (e.g., a credit card expiration date) Related types: * google.type.TimeOfDay * google.type.DateTime * google.protobuf.Timestamp
    */
   export interface Schema$Date {
     /**
@@ -416,9 +416,9 @@ export namespace composer_v1beta1 {
     operations?: Schema$Operation[];
   }
   /**
-   * Load environment state response.
+   * Response to LoadSnapshotRequest.
    */
-  export interface Schema$LoadEnvironmentStateResponse {}
+  export interface Schema$LoadSnapshotResponse {}
   /**
    * The configuration settings for Cloud Composer maintenance window. The following example: ``` { "startTime":"2019-08-01T01:00:00Z" "endTime":"2019-08-01T07:00:00Z" "recurrence":"FREQ=WEEKLY;BYDAY=TU,WE" \} ``` would define a maintenance window between 01 and 07 hours UTC during each Tuesday and Wednesday.
    */
@@ -615,6 +615,15 @@ export namespace composer_v1beta1 {
    */
   export interface Schema$RestartWebServerRequest {}
   /**
+   * Response to SaveSnapshotRequest.
+   */
+  export interface Schema$SaveSnapshotResponse {
+    /**
+     * The fully-resolved Cloud Storage path of the created snapshot, e.g.: "gs://my-bucket/snapshots/project_location_environment_timestamp". This field is populated only if the snapshot creation was successful.
+     */
+    snapshotPath?: string | null;
+  }
+  /**
    * Configuration for resources used by Airflow schedulers.
    */
   export interface Schema$SchedulerResource {
@@ -680,15 +689,6 @@ export namespace composer_v1beta1 {
      * A developer-facing error message, which should be in English. Any user-facing error message should be localized and sent in the google.rpc.Status.details field, or localized by the client.
      */
     message?: string | null;
-  }
-  /**
-   * Store environment state response.
-   */
-  export interface Schema$StoreEnvironmentStateResponse {
-    /**
-     * The fully-resolved Cloud Storage location of the created snapshot, e.g.: "gs://my-bucket/snapshots/project_id/location/environment_uuid/timestamp". This field is populated only if the snapshot creation was successful.
-     */
-    snapshotLocation?: string | null;
   }
   /**
    * The configuration settings for the Airflow web server App Engine instance. Supported for Cloud Composer environments in versions composer-1.*.*-airflow-*.*.*.
