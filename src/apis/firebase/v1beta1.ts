e71758645413af0e2767ec1167ab5c74d111382f
Yoshi Automation <yoshi-automation@google.com>
2022-03-23 01:41:50 +0000

    fix(firebase): update the API
    #### firebase:v1beta1
    
    The following keys were changed:
    - schemas.Empty.description

--- src/apis/firebase/v1beta1.ts
+++ src/apis/firebase/v1beta1.ts
@@ -257,7 +257,7 @@ export namespace firebase_v1beta1 {
     storageBucket?: string | null;
   }
   /**
-   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \} The JSON representation for `Empty` is empty JSON object `{\}`.
+   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \}
    */
   export interface Schema$Empty {}
   export interface Schema$FinalizeDefaultLocationRequest {
