aecc15b30c6bd98291fbda870c0a70e29adc3c01
Yoshi Automation <yoshi-automation@google.com>
2022-03-23 01:41:52 +0000

    fix(privateca): update the API
    #### privateca:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.caPools.resources.certificateAuthorities.methods.delete.parameters.skipGracePeriod.description

--- src/apis/privateca/v1.ts
+++ src/apis/privateca/v1.ts
@@ -3424,7 +3424,7 @@ export namespace privateca_v1 {
      *       name: 'projects/my-project/locations/my-location/caPools/my-caPool/certificateAuthorities/my-certificateAuthoritie',
      *       // Optional. An ID to identify requests. Specify a unique request ID so that if you must retry your request, the server will know to ignore the request if it has already been completed. The server will guarantee that for at least 60 minutes since the first request. For example, consider a situation where you make an initial request and t he request times out. If you make the request again with the same request ID, the server can check if original operation with the same request ID was received, and if so, will ignore the second request. This prevents clients from accidentally creating duplicate commitments. The request ID must be a valid UUID with the exception that zero UUID is not supported (00000000-0000-0000-0000-000000000000).
      *       requestId: 'placeholder-value',
-     *       // Optional. This field sets the CA to have a pending delete duration of 0.
+     *       // Optional. If this flag is set, the Certificate Authority will be deleted as soon as possible without a 30-day grace period where undeletion would have been allowed. If you proceed, there will be no way to recover this CA.
      *       skipGracePeriod: 'placeholder-value',
      *     });
      *   console.log(res.data);
@@ -4598,7 +4598,7 @@ export namespace privateca_v1 {
      */
     requestId?: string;
     /**
-     * Optional. This field sets the CA to have a pending delete duration of 0.
+     * Optional. If this flag is set, the Certificate Authority will be deleted as soon as possible without a 30-day grace period where undeletion would have been allowed. If you proceed, there will be no way to recover this CA.
      */
     skipGracePeriod?: boolean;
   }
