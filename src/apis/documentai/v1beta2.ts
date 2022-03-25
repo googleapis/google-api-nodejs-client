e6b21a460edad1c08ab9c0da85f6a00296bef891
Yoshi Automation <yoshi-automation@google.com>
2022-03-25 01:38:15 +0000

    feat(documentai): update the API
    #### documentai:v1beta2
    
    The following keys were added:
    - schemas.GoogleCloudDocumentaiV1beta1DocumentEntity.properties.nonPresent.description
    - schemas.GoogleCloudDocumentaiV1beta1DocumentEntity.properties.nonPresent.type
    - schemas.GoogleCloudDocumentaiV1beta2DocumentEntity.properties.nonPresent.description
    - schemas.GoogleCloudDocumentaiV1beta2DocumentEntity.properties.nonPresent.type
    
    The following keys were changed:
    - schemas.GoogleCloudDocumentaiV1beta1Document.description
    - schemas.GoogleCloudDocumentaiV1beta2Document.description
    - schemas.GoogleProtobufEmpty.description
    
    #### documentai:v1beta3
    
    The following keys were added:
    - schemas.GoogleCloudDocumentaiV1beta1DocumentEntity.properties.nonPresent.description
    - schemas.GoogleCloudDocumentaiV1beta1DocumentEntity.properties.nonPresent.type
    - schemas.GoogleCloudDocumentaiV1beta2DocumentEntity.properties.nonPresent.description
    - schemas.GoogleCloudDocumentaiV1beta2DocumentEntity.properties.nonPresent.type
    - schemas.GoogleCloudDocumentaiV1beta3DocumentEntity.properties.nonPresent.description
    - schemas.GoogleCloudDocumentaiV1beta3DocumentEntity.properties.nonPresent.type
    
    The following keys were changed:
    - schemas.GoogleCloudDocumentaiV1beta1Document.description
    - schemas.GoogleCloudDocumentaiV1beta2Document.description
    - schemas.GoogleCloudDocumentaiV1beta3Document.description
    - schemas.GoogleProtobufEmpty.description
    
    #### documentai:v1
    
    The following keys were added:
    - schemas.GoogleCloudDocumentaiV1DocumentEntity.properties.nonPresent.description
    - schemas.GoogleCloudDocumentaiV1DocumentEntity.properties.nonPresent.type
    - schemas.GoogleCloudDocumentaiV1beta1DocumentEntity.properties.nonPresent.description
    - schemas.GoogleCloudDocumentaiV1beta1DocumentEntity.properties.nonPresent.type
    - schemas.GoogleCloudDocumentaiV1beta2DocumentEntity.properties.nonPresent.description
    - schemas.GoogleCloudDocumentaiV1beta2DocumentEntity.properties.nonPresent.type
    
    The following keys were changed:
    - schemas.GoogleCloudDocumentaiV1Document.description
    - schemas.GoogleCloudDocumentaiV1beta1Document.description
    - schemas.GoogleCloudDocumentaiV1beta2Document.description
    - schemas.GoogleProtobufEmpty.description

--- src/apis/documentai/v1beta2.ts
+++ src/apis/documentai/v1beta2.ts
@@ -618,7 +618,7 @@ export namespace documentai_v1beta2 {
     vertices?: Schema$GoogleCloudDocumentaiV1beta1Vertex[];
   }
   /**
-   * Document represents the canonical document resource in Document Understanding AI. It is an interchange format that provides insights into documents and allows for collaboration between users and Document Understanding AI to iterate and optimize for quality.
+   * Document represents the canonical document resource in Document AI. It is an interchange format that provides insights into documents and allows for collaboration between users and Document AI to iterate and optimize for quality.
    */
   export interface Schema$GoogleCloudDocumentaiV1beta1Document {
     /**
@@ -690,6 +690,10 @@ export namespace documentai_v1beta2 {
      * Optional. Text value in the document e.g. `1600 Amphitheatre Pkwy`. If the entity is not present in the document, this field will be empty.
      */
     mentionText?: string | null;
+    /**
+     * Optional. This attribute indicates that the processing didn't actually identify this entity, but a confidence score was assigned that represent the potential that this could be a false negative. A non-present entity should have an empty mention_text and text_anchor.
+     */
+    nonPresent?: boolean | null;
     /**
      * Optional. Normalized entity value. Absent if the extracted value could not be converted or the type (e.g. address) is not supported for certain parsers. This field is also only populated for certain supported document types.
      */
@@ -1493,7 +1497,7 @@ export namespace documentai_v1beta2 {
     vertices?: Schema$GoogleCloudDocumentaiV1beta2Vertex[];
   }
   /**
-   * Document represents the canonical document resource in Document Understanding AI. It is an interchange format that provides insights into documents and allows for collaboration between users and Document Understanding AI to iterate and optimize for quality.
+   * Document represents the canonical document resource in Document AI. It is an interchange format that provides insights into documents and allows for collaboration between users and Document AI to iterate and optimize for quality.
    */
   export interface Schema$GoogleCloudDocumentaiV1beta2Document {
     /**
@@ -1569,6 +1573,10 @@ export namespace documentai_v1beta2 {
      * Optional. Text value in the document e.g. `1600 Amphitheatre Pkwy`. If the entity is not present in the document, this field will be empty.
      */
     mentionText?: string | null;
+    /**
+     * Optional. This attribute indicates that the processing didn't actually identify this entity, but a confidence score was assigned that represent the potential that this could be a false negative. A non-present entity should have an empty mention_text and text_anchor.
+     */
+    nonPresent?: boolean | null;
     /**
      * Optional. Normalized entity value. Absent if the extracted value could not be converted or the type (e.g. address) is not supported for certain parsers. This field is also only populated for certain supported document types.
      */
@@ -2869,7 +2877,7 @@ export namespace documentai_v1beta2 {
     response?: {[key: string]: any} | null;
   }
   /**
-   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \} The JSON representation for `Empty` is empty JSON object `{\}`.
+   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \}
    */
   export interface Schema$GoogleProtobufEmpty {}
   /**
