b92570880784fb751545a0549f03e71912578206
Yoshi Automation <yoshi-automation@google.com>
2022-03-25 01:38:15 +0000

    feat(firestore): update the API
    #### firestore:v1beta1
    
    The following keys were added:
    - schemas.ListCollectionIdsRequest.properties.readTime.description
    - schemas.ListCollectionIdsRequest.properties.readTime.format
    - schemas.ListCollectionIdsRequest.properties.readTime.type
    - schemas.PartitionQueryRequest.properties.readTime.description
    - schemas.PartitionQueryRequest.properties.readTime.format
    - schemas.PartitionQueryRequest.properties.readTime.type
    
    The following keys were changed:
    - schemas.Empty.description
    
    #### firestore:v1beta2
    
    The following keys were changed:
    - schemas.Empty.description
    
    #### firestore:v1
    
    The following keys were changed:
    - schemas.Empty.description
    - schemas.GoogleFirestoreAdminV1Database.properties.concurrencyMode.enumDescriptions

--- src/apis/firestore/v1.ts
+++ src/apis/firestore/v1.ts
@@ -393,7 +393,7 @@ export namespace firestore_v1 {
     fieldTransforms?: Schema$FieldTransform[];
   }
   /**
-   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \} The JSON representation for `Empty` is empty JSON object `{\}`.
+   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \}
    */
   export interface Schema$Empty {}
   /**
