5aa2b4cc2b4aced133826a7d3e97f72036985fb0
Yoshi Automation <yoshi-automation@google.com>
2022-03-25 01:38:14 +0000

    fix(cloudasset): update the API
    #### cloudasset:v1
    
    The following keys were changed:
    - schemas.Empty.description

--- src/apis/cloudasset/v1.ts
+++ src/apis/cloudasset/v1.ts
@@ -420,7 +420,7 @@ export namespace cloudasset_v1 {
     policies?: Schema$PolicyInfo[];
   }
   /**
-   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \} The JSON representation for `Empty` is empty JSON object `{\}`.
+   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \}
    */
   export interface Schema$Empty {}
   /**
