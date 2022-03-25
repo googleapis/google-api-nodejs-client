0363de6f7d0dd46b30212911e5157a2da9c4a3c3
Yoshi Automation <yoshi-automation@google.com>
2022-03-25 01:38:16 +0000

    fix(secretmanager): update the API
    #### secretmanager:v1beta1
    
    The following keys were changed:
    - schemas.Empty.description
    
    #### secretmanager:v1
    
    The following keys were changed:
    - schemas.Empty.description

--- src/apis/secretmanager/v1.ts
+++ src/apis/secretmanager/v1.ts
@@ -245,7 +245,7 @@ export namespace secretmanager_v1 {
     etag?: string | null;
   }
   /**
-   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \} The JSON representation for `Empty` is empty JSON object `{\}`.
+   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \}
    */
   export interface Schema$Empty {}
   /**
