dd75f6b93371d51f96c63fca24ff489de2c4209b
Yoshi Automation <yoshi-automation@google.com>
2022-03-31 01:38:28 +0000

    fix(gkehub): update the API
    #### gkehub:v1alpha
    
    The following keys were changed:
    - schemas.Empty.description
    
    #### gkehub:v1alpha2
    
    The following keys were changed:
    - schemas.Empty.description
    
    #### gkehub:v2alpha
    
    The following keys were changed:
    - schemas.Empty.description
    
    #### gkehub:v1beta
    
    The following keys were changed:
    - schemas.Empty.description
    
    #### gkehub:v1beta1
    
    The following keys were changed:
    - schemas.Empty.description
    
    #### gkehub:v1
    
    The following keys were changed:
    - schemas.Empty.description

--- src/apis/gkehub/v1beta.ts
+++ src/apis/gkehub/v1beta.ts
@@ -706,7 +706,7 @@ export namespace gkehub_v1beta {
     syncToken?: string | null;
   }
   /**
-   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \} The JSON representation for `Empty` is empty JSON object `{\}`.
+   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \}
    */
   export interface Schema$Empty {}
   /**
