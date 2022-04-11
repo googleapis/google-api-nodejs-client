fc0f2e49dc81f54fec604d8d717db2f21780b062
Yoshi Automation <yoshi-automation@google.com>
2022-04-11 01:39:02 +0000

    fix(redis): update the API
    #### redis:v1beta1
    
    The following keys were changed:
    - schemas.Empty.description
    
    #### redis:v1
    
    The following keys were changed:
    - schemas.Empty.description

--- src/apis/redis/v1beta1.ts
+++ src/apis/redis/v1beta1.ts
@@ -126,7 +126,7 @@ export namespace redis_v1beta1 {
   }
 
   /**
-   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \} The JSON representation for `Empty` is empty JSON object `{\}`.
+   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \}
    */
   export interface Schema$Empty {}
   /**
