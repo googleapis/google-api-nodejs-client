c33fb217bf66f9dafbc2818deda57b4985f60199
Yoshi Automation <yoshi-automation@google.com>
2022-04-11 01:39:01 +0000

    fix(metastore): update the API
    #### metastore:v1alpha
    
    The following keys were changed:
    - schemas.Empty.description
    
    #### metastore:v1beta
    
    The following keys were changed:
    - schemas.Empty.description

--- src/apis/metastore/v1alpha.ts
+++ src/apis/metastore/v1alpha.ts
@@ -271,7 +271,7 @@ export namespace metastore_v1alpha {
     lakeResources?: {[key: string]: Schema$Lake} | null;
   }
   /**
-   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \} The JSON representation for Empty is empty JSON object {\}.
+   * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \}
    */
   export interface Schema$Empty {}
   /**
