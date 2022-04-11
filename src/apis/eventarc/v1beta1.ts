5a689bccfd2d2ca5925e62b8ad750ec117abc226
Yoshi Automation <yoshi-automation@google.com>
2022-04-11 01:39:00 +0000

    fix(eventarc): update the API
    #### eventarc:v1beta1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.filter.description
    
    #### eventarc:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.filter.description

--- src/apis/eventarc/v1beta1.ts
+++ src/apis/eventarc/v1beta1.ts
@@ -673,7 +673,7 @@ export namespace eventarc_v1beta1 {
      *
      *   // Do the magic
      *   const res = await eventarc.projects.locations.list({
-     *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like "displayName=tokyo", and is documented in more detail in [AIP-160](https://google.aip.dev/160).
+     *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
      *     // The resource that owns the locations collection, if applicable.
      *     name: 'projects/my-project',
@@ -798,7 +798,7 @@ export namespace eventarc_v1beta1 {
   export interface Params$Resource$Projects$Locations$List
     extends StandardParameters {
     /**
-     * A filter to narrow down results to a preferred subset. The filtering language accepts strings like "displayName=tokyo", and is documented in more detail in [AIP-160](https://google.aip.dev/160).
+     * A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      */
     filter?: string;
     /**
