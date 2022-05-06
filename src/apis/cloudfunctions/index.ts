a5bd561a6612852c6a75bca1d6b8217c4126c7c8
Yoshi Automation <yoshi-automation@google.com>
2022-05-06 01:37:24 +0000

    feat(cloudfunctions): update the API
    #### cloudfunctions:v2alpha
    
    The following keys were added:
    - schemas.EventTrigger.properties.channel.description
    - schemas.EventTrigger.properties.channel.type
    
    The following keys were changed:
    - schemas.AuditConfig.description
    - schemas.Binding.properties.members.description
    - schemas.SetIamPolicyRequest.properties.policy.description
    - schemas.TestIamPermissionsRequest.properties.permissions.description
    
    #### cloudfunctions:v2beta
    
    The following keys were added:
    - schemas.EventTrigger.properties.channel.description
    - schemas.EventTrigger.properties.channel.type
    
    The following keys were changed:
    - schemas.AuditConfig.description
    - schemas.Binding.properties.members.description
    - schemas.SetIamPolicyRequest.properties.policy.description
    - schemas.TestIamPermissionsRequest.properties.permissions.description
    
    #### cloudfunctions:v1
    
    The following keys were changed:
    - schemas.AuditConfig.description
    - schemas.Binding.properties.members.description
    - schemas.SetIamPolicyRequest.properties.policy.description
    - schemas.TestIamPermissionsRequest.properties.permissions.description

--- src/apis/cloudfunctions/index.ts
+++ src/apis/cloudfunctions/index.ts
@@ -16,12 +16,14 @@
 import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
 import {cloudfunctions_v1} from './v1';
 import {cloudfunctions_v1beta2} from './v1beta2';
+import {cloudfunctions_v2} from './v2';
 import {cloudfunctions_v2alpha} from './v2alpha';
 import {cloudfunctions_v2beta} from './v2beta';
 
 export const VERSIONS = {
   v1: cloudfunctions_v1.Cloudfunctions,
   v1beta2: cloudfunctions_v1beta2.Cloudfunctions,
+  v2: cloudfunctions_v2.Cloudfunctions,
   v2alpha: cloudfunctions_v2alpha.Cloudfunctions,
   v2beta: cloudfunctions_v2beta.Cloudfunctions,
 };
@@ -36,6 +38,10 @@ export function cloudfunctions(
 export function cloudfunctions(
   options: cloudfunctions_v1beta2.Options
 ): cloudfunctions_v1beta2.Cloudfunctions;
+export function cloudfunctions(version: 'v2'): cloudfunctions_v2.Cloudfunctions;
+export function cloudfunctions(
+  options: cloudfunctions_v2.Options
+): cloudfunctions_v2.Cloudfunctions;
 export function cloudfunctions(
   version: 'v2alpha'
 ): cloudfunctions_v2alpha.Cloudfunctions;
@@ -52,6 +58,7 @@ export function cloudfunctions<
   T =
     | cloudfunctions_v1.Cloudfunctions
     | cloudfunctions_v1beta2.Cloudfunctions
+    | cloudfunctions_v2.Cloudfunctions
     | cloudfunctions_v2alpha.Cloudfunctions
     | cloudfunctions_v2beta.Cloudfunctions
 >(
@@ -61,6 +68,8 @@ export function cloudfunctions<
     | cloudfunctions_v1.Options
     | 'v1beta2'
     | cloudfunctions_v1beta2.Options
+    | 'v2'
+    | cloudfunctions_v2.Options
     | 'v2alpha'
     | cloudfunctions_v2alpha.Options
     | 'v2beta'
@@ -73,6 +82,7 @@ const auth = new AuthPlus();
 export {auth};
 export {cloudfunctions_v1};
 export {cloudfunctions_v1beta2};
+export {cloudfunctions_v2};
 export {cloudfunctions_v2alpha};
 export {cloudfunctions_v2beta};
 export {
