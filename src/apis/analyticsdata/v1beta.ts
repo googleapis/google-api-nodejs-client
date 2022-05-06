c7cd341bc7a0f63ad62783ff0ec749c83bfc6cba
Yoshi Automation <yoshi-automation@google.com>
2022-05-06 01:37:21 +0000

    fix(analyticsdata): update the API
    #### analyticsdata:v1beta
    
    The following keys were changed:
    - resources.properties.methods.runRealtimeReport.description
    - resources.properties.methods.runReport.description

--- src/apis/analyticsdata/v1beta.ts
+++ src/apis/analyticsdata/v1beta.ts
@@ -1897,7 +1897,7 @@ export namespace analyticsdata_v1beta {
     }
 
     /**
-     * The Google Analytics Realtime API returns a customized report of realtime event data for your property. These reports show events and usage from the last 30 minutes.
+     * Returns a customized report of realtime event data for your property. Events appear in realtime reports seconds after they have been sent to the Google Analytics. Realtime reports show events and usage data for the periods of time ranging from the present moment to 30 minutes ago (up to 60 minutes for Google Analytics 360 properties). For a guide to constructing realtime requests & understanding responses, see [Creating a Realtime Report](https://developers.google.com/analytics/devguides/reporting/data/v1/realtime-basics).
      * @example
      * ```js
      * // Before running the sample:
@@ -2061,7 +2061,7 @@ export namespace analyticsdata_v1beta {
     }
 
     /**
-     * Returns a customized report of your Google Analytics event data. Reports contain statistics derived from data collected by the Google Analytics tracking code. The data returned from the API is as a table with columns for the requested dimensions and metrics. Metrics are individual measurements of user activity on your property, such as active users or event count. Dimensions break down metrics across some common criteria, such as country or event name.
+     * Returns a customized report of your Google Analytics event data. Reports contain statistics derived from data collected by the Google Analytics tracking code. The data returned from the API is as a table with columns for the requested dimensions and metrics. Metrics are individual measurements of user activity on your property, such as active users or event count. Dimensions break down metrics across some common criteria, such as country or event name. For a guide to constructing requests & understanding responses, see [Creating a Report](https://developers.google.com/analytics/devguides/reporting/data/v1/basics).
      * @example
      * ```js
      * // Before running the sample:
