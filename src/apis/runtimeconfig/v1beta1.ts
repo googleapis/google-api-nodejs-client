ed02d14a1b4693c945dac9049a4917bbda49bcef
Yoshi Automation <yoshi-automation@google.com>
2022-05-06 01:37:32 +0000

    fix(runtimeconfig): update the API
    #### runtimeconfig:v1beta1
    
    The following keys were changed:
    - resources.projects.resources.configs.methods.getIamPolicy.parameters.resource.description
    - resources.projects.resources.configs.methods.setIamPolicy.parameters.resource.description
    - resources.projects.resources.configs.methods.testIamPermissions.parameters.resource.description
    - resources.projects.resources.configs.resources.operations.methods.testIamPermissions.parameters.resource.description
    - resources.projects.resources.configs.resources.variables.methods.testIamPermissions.parameters.resource.description
    - resources.projects.resources.configs.resources.waiters.methods.testIamPermissions.parameters.resource.description

--- src/apis/runtimeconfig/v1beta1.ts
+++ src/apis/runtimeconfig/v1beta1.ts
@@ -852,7 +852,7 @@ export namespace runtimeconfig_v1beta1 {
      *   const res = await runtimeconfig.projects.configs.getIamPolicy({
      *     // Optional. The maximum policy version that will be used to format the policy. Valid values are 0, 1, and 3. Requests specifying an invalid value will be rejected. Requests for policies with any conditional role bindings must specify version 3. Policies with no conditional role bindings may specify any valid value or leave the field unset. The policy in the response might use the policy version that you specified, or it might use a lower policy version. For example, if you specify version 3, but the policy has no conditional role bindings, the response uses version 1. To learn which resources support conditions in their IAM policies, see the [IAM documentation](https://cloud.google.com/iam/help/conditions/resource-policies).
      *     'options.requestedPolicyVersion': 'placeholder-value',
-     *     // REQUIRED: The resource for which the policy is being requested. See the operation documentation for the appropriate value for this field.
+     *     // REQUIRED: The resource for which the policy is being requested. See [Resource names](https://cloud.google.com/apis/design/resource_names) for the appropriate value for this field.
      *     resource: 'projects/my-project/configs/my-config',
      *   });
      *   console.log(res.data);
@@ -1128,7 +1128,7 @@ export namespace runtimeconfig_v1beta1 {
      *
      *   // Do the magic
      *   const res = await runtimeconfig.projects.configs.setIamPolicy({
-     *     // REQUIRED: The resource for which the policy is being specified. See the operation documentation for the appropriate value for this field.
+     *     // REQUIRED: The resource for which the policy is being specified. See [Resource names](https://cloud.google.com/apis/design/resource_names) for the appropriate value for this field.
      *     resource: 'projects/my-project/configs/my-config',
      *
      *     // Request body metadata
@@ -1271,7 +1271,7 @@ export namespace runtimeconfig_v1beta1 {
      *
      *   // Do the magic
      *   const res = await runtimeconfig.projects.configs.testIamPermissions({
-     *     // REQUIRED: The resource for which the policy detail is being requested. See the operation documentation for the appropriate value for this field.
+     *     // REQUIRED: The resource for which the policy detail is being requested. See [Resource names](https://cloud.google.com/apis/design/resource_names) for the appropriate value for this field.
      *     resource: 'projects/my-project/configs/my-config',
      *
      *     // Request body metadata
@@ -1567,7 +1567,7 @@ export namespace runtimeconfig_v1beta1 {
      */
     'options.requestedPolicyVersion'?: number;
     /**
-     * REQUIRED: The resource for which the policy is being requested. See the operation documentation for the appropriate value for this field.
+     * REQUIRED: The resource for which the policy is being requested. See [Resource names](https://cloud.google.com/apis/design/resource_names) for the appropriate value for this field.
      */
     resource?: string;
   }
@@ -1589,7 +1589,7 @@ export namespace runtimeconfig_v1beta1 {
   export interface Params$Resource$Projects$Configs$Setiampolicy
     extends StandardParameters {
     /**
-     * REQUIRED: The resource for which the policy is being specified. See the operation documentation for the appropriate value for this field.
+     * REQUIRED: The resource for which the policy is being specified. See [Resource names](https://cloud.google.com/apis/design/resource_names) for the appropriate value for this field.
      */
     resource?: string;
 
@@ -1601,7 +1601,7 @@ export namespace runtimeconfig_v1beta1 {
   export interface Params$Resource$Projects$Configs$Testiampermissions
     extends StandardParameters {
     /**
-     * REQUIRED: The resource for which the policy detail is being requested. See the operation documentation for the appropriate value for this field.
+     * REQUIRED: The resource for which the policy detail is being requested. See [Resource names](https://cloud.google.com/apis/design/resource_names) for the appropriate value for this field.
      */
     resource?: string;
 
@@ -1794,7 +1794,7 @@ export namespace runtimeconfig_v1beta1 {
      *   // Do the magic
      *   const res =
      *     await runtimeconfig.projects.configs.operations.testIamPermissions({
-     *       // REQUIRED: The resource for which the policy detail is being requested. See the operation documentation for the appropriate value for this field.
+     *       // REQUIRED: The resource for which the policy detail is being requested. See [Resource names](https://cloud.google.com/apis/design/resource_names) for the appropriate value for this field.
      *       resource: 'projects/my-project/configs/my-config/operations/.*',
      *
      *       // Request body metadata
@@ -1924,7 +1924,7 @@ export namespace runtimeconfig_v1beta1 {
   export interface Params$Resource$Projects$Configs$Operations$Testiampermissions
     extends StandardParameters {
     /**
-     * REQUIRED: The resource for which the policy detail is being requested. See the operation documentation for the appropriate value for this field.
+     * REQUIRED: The resource for which the policy detail is being requested. See [Resource names](https://cloud.google.com/apis/design/resource_names) for the appropriate value for this field.
      */
     resource?: string;
 
@@ -2533,7 +2533,7 @@ export namespace runtimeconfig_v1beta1 {
      *   // Do the magic
      *   const res = await runtimeconfig.projects.configs.variables.testIamPermissions(
      *     {
-     *       // REQUIRED: The resource for which the policy detail is being requested. See the operation documentation for the appropriate value for this field.
+     *       // REQUIRED: The resource for which the policy detail is being requested. See [Resource names](https://cloud.google.com/apis/design/resource_names) for the appropriate value for this field.
      *       resource: 'projects/my-project/configs/my-config/variables/.*',
      *
      *       // Request body metadata
@@ -3005,7 +3005,7 @@ export namespace runtimeconfig_v1beta1 {
   export interface Params$Resource$Projects$Configs$Variables$Testiampermissions
     extends StandardParameters {
     /**
-     * REQUIRED: The resource for which the policy detail is being requested. See the operation documentation for the appropriate value for this field.
+     * REQUIRED: The resource for which the policy detail is being requested. See [Resource names](https://cloud.google.com/apis/design/resource_names) for the appropriate value for this field.
      */
     resource?: string;
 
@@ -3633,7 +3633,7 @@ export namespace runtimeconfig_v1beta1 {
      *
      *   // Do the magic
      *   const res = await runtimeconfig.projects.configs.waiters.testIamPermissions({
-     *     // REQUIRED: The resource for which the policy detail is being requested. See the operation documentation for the appropriate value for this field.
+     *     // REQUIRED: The resource for which the policy detail is being requested. See [Resource names](https://cloud.google.com/apis/design/resource_names) for the appropriate value for this field.
      *     resource: 'projects/my-project/configs/my-config/waiters/my-waiter',
      *
      *     // Request body metadata
@@ -3801,7 +3801,7 @@ export namespace runtimeconfig_v1beta1 {
   export interface Params$Resource$Projects$Configs$Waiters$Testiampermissions
     extends StandardParameters {
     /**
-     * REQUIRED: The resource for which the policy detail is being requested. See the operation documentation for the appropriate value for this field.
+     * REQUIRED: The resource for which the policy detail is being requested. See [Resource names](https://cloud.google.com/apis/design/resource_names) for the appropriate value for this field.
      */
     resource?: string;
 
