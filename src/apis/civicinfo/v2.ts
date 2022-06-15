d37e76b4dce08269a4338206870723920d99f5f9
Yoshi Automation <yoshi-automation@google.com>
2022-06-15 01:39:11 +0000

    feat(civicinfo): update the API
    #### civicinfo:v2
    
    The following keys were added:
    - schemas.Election.properties.shapeLookupBehavior.enum
    - schemas.Election.properties.shapeLookupBehavior.enumDescriptions
    - schemas.Election.properties.shapeLookupBehavior.type
    
    The following keys were changed:
    - schemas.GeocodingSummary.properties.featureType.enumDescriptions

--- src/apis/civicinfo/v2.ts
+++ src/apis/civicinfo/v2.ts
@@ -417,6 +417,7 @@ export namespace civicinfo_v2 {
      * The political division of the election. Represented as an OCD Division ID. Voters within these political jurisdictions are covered by this election. This is typically a state such as ocd-division/country:us/state:ca or for the midterms or general election the entire US (i.e. ocd-division/country:us).
      */
     ocdDivisionId?: string | null;
+    shapeLookupBehavior?: string | null;
   }
   /**
    * Information about individual election officials.
