0e93ad9798d7bdd1ecddfde087e29175837a3c7d
Yoshi Automation <yoshi-automation@google.com>
2022-06-15 01:39:10 +0000

    fix(bigqueryreservation): update the API
    #### bigqueryreservation:v1
    
    The following keys were changed:
    - schemas.Reservation.properties.slotCapacity.description

--- src/apis/bigqueryreservation/v1.ts
+++ src/apis/bigqueryreservation/v1.ts
@@ -294,7 +294,7 @@ export namespace bigqueryreservation_v1 {
      */
     name?: string | null;
     /**
-     * Minimum slots available to this reservation. A slot is a unit of computational power in BigQuery, and serves as the unit of parallelism. Queries using this reservation might use more slots during runtime if ignore_idle_slots is set to false. If the new reservation's slot capacity exceeds the project's slot capacity or if total slot capacity of the new reservation and its siblings exceeds the project's slot capacity, the request will fail with `google.rpc.Code.RESOURCE_EXHAUSTED`. NOTE: for reservations in US or EU multi-regions, slot capacity constraints are checked separately for default and auxiliary regions. See multi_region_auxiliary flag for more details.
+     * Minimum slots available to this reservation. A slot is a unit of computational power in BigQuery, and serves as the unit of parallelism. Queries using this reservation might use more slots during runtime if ignore_idle_slots is set to false. If total slot_capacity of the reservation and its siblings exceeds the total slot_count of all capacity commitments, the request will fail with `google.rpc.Code.RESOURCE_EXHAUSTED`. NOTE: for reservations in US or EU multi-regions, slot capacity constraints are checked separately for default and auxiliary regions. See multi_region_auxiliary flag for more details.
      */
     slotCapacity?: string | null;
     /**
