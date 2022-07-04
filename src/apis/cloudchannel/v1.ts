fb5b0705df5f18168ebda811ca54fff02dc4b1e1
Yoshi Automation <yoshi-automation@google.com>
2022-07-04 01:43:05 +0000

    fix(cloudchannel): update the API
    #### cloudchannel:v1
    
    The following keys were changed:
    - schemas.GoogleTypePostalAddress.description

--- src/apis/cloudchannel/v1.ts
+++ src/apis/cloudchannel/v1.ts
@@ -1861,7 +1861,7 @@ export namespace cloudchannel_v1 {
     units?: string | null;
   }
   /**
-   * Represents a postal address, e.g. for postal delivery or payments addresses. Given a postal address, a postal service can deliver items to a premise, P.O. Box or similar. It is not intended to model geographical locations (roads, towns, mountains). In typical usage an address would be created via user input or from importing existing data, depending on the type of process. Advice on address input / editing: - Use an i18n-ready address widget such as https://github.com/google/libaddressinput) - Users should not be presented with UI elements for input or editing of fields outside countries where that field is used. For more guidance on how to use this schema, please see: https://support.google.com/business/answer/6397478
+   * Represents a postal address, e.g. for postal delivery or payments addresses. Given a postal address, a postal service can deliver items to a premise, P.O. Box or similar. It is not intended to model geographical locations (roads, towns, mountains). In typical usage an address would be created via user input or from importing existing data, depending on the type of process. Advice on address input / editing: - Use an internationalization-ready address widget such as https://github.com/google/libaddressinput) - Users should not be presented with UI elements for input or editing of fields outside countries where that field is used. For more guidance on how to use this schema, please see: https://support.google.com/business/answer/6397478
    */
   export interface Schema$GoogleTypePostalAddress {
     /**
