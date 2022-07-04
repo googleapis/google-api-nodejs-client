9ffa647c8392b9fc3d33513fafb4b5957c231cc8
Yoshi Automation <yoshi-automation@google.com>
2022-07-04 01:43:11 +0000

    fix(sts): update the API
    #### sts:v1beta
    
    The following keys were changed:
    - schemas.GoogleIdentityStsV1betaExchangeTokenRequest.properties.options.description
    
    #### sts:v1
    
    The following keys were changed:
    - schemas.GoogleIdentityStsV1ExchangeTokenRequest.properties.options.description

--- src/apis/sts/v1.ts
+++ src/apis/sts/v1.ts
@@ -223,7 +223,7 @@ export namespace sts_v1 {
      */
     grantType?: string | null;
     /**
-     * A set of features that Security Token Service supports, in addition to the standard OAuth 2.0 token exchange, formatted as a serialized JSON object of Options.
+     * A set of features that Security Token Service supports, in addition to the standard OAuth 2.0 token exchange, formatted as a serialized JSON object of Options. The size of the parameter value must not exceed 4096 characters.
      */
     options?: string | null;
     /**
