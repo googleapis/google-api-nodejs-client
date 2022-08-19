bcb6e59002e24d3eb90609dbb4e8106d0137576d
Yoshi Automation <yoshi-automation@google.com>
2022-08-19 01:40:43 +0000

    fix(slides): update the API
    #### slides:v1
    
    The following keys were changed:
    - schemas.CreateSlideRequest.description
    - schemas.CreateSlideRequest.properties.insertionIndex.description
    - schemas.CreateSlideRequest.properties.objectId.description
    - schemas.CreateSlideRequest.properties.placeholderIdMappings.description
    - schemas.CreateSlideRequest.properties.slideLayoutReference.description

--- src/apis/slides/v1.ts
+++ src/apis/slides/v1.ts
@@ -406,23 +406,23 @@ export namespace slides_v1 {
     objectId?: string | null;
   }
   /**
-   * Creates a new slide.
+   * Creates a slide.
    */
   export interface Schema$CreateSlideRequest {
     /**
-     * The optional zero-based index indicating where to insert the slides. If you don't specify an index, the new slide is created at the end.
+     * The optional zero-based index indicating where to insert the slides. If you don't specify an index, the slide is created at the end.
      */
     insertionIndex?: number | null;
     /**
-     * A user-supplied object ID. If you specify an ID, it must be unique among all pages and page elements in the presentation. The ID must start with an alphanumeric character or an underscore (matches regex `[a-zA-Z0-9_]`); remaining characters may include those as well as a hyphen or colon (matches regex `[a-zA-Z0-9_-:]`). The length of the ID must not be less than 5 or greater than 50. If you don't specify an ID, a unique one is generated.
+     * A user-supplied object ID. If you specify an ID, it must be unique among all pages and page elements in the presentation. The ID must start with an alphanumeric character or an underscore (matches regex `[a-zA-Z0-9_]`); remaining characters may include those as well as a hyphen or colon (matches regex `[a-zA-Z0-9_-:]`). The ID length must be between 5 and 50 characters, inclusive. If you don't specify an ID, a unique one is generated.
      */
     objectId?: string | null;
     /**
-     * An optional list of object ID mappings from the placeholder(s) on the layout to the placeholder(s) that will be created on the new slide from that specified layout. Can only be used when `slide_layout_reference` is specified.
+     * An optional list of object ID mappings from the placeholder(s) on the layout to the placeholders that are created on the slide from the specified layout. Can only be used when `slide_layout_reference` is specified.
      */
     placeholderIdMappings?: Schema$LayoutPlaceholderIdMapping[];
     /**
-     * Layout reference of the slide to be inserted, based on the *current master*, which is one of the following: - The master of the previous slide index. - The master of the first slide, if the insertion_index is zero. - The first master in the presentation, if there are no slides. If the LayoutReference is not found in the current master, a 400 bad request error is returned. If you don't specify a layout reference, then the new slide will use the predefined layout `BLANK`.
+     * Layout reference of the slide to be inserted, based on the *current master*, which is one of the following: - The master of the previous slide index. - The master of the first slide, if the insertion_index is zero. - The first master in the presentation, if there are no slides. If the LayoutReference is not found in the current master, a 400 bad request error is returned. If you don't specify a layout reference, the slide uses the predefined `BLANK` layout.
      */
     slideLayoutReference?: Schema$LayoutReference;
   }
