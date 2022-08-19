b3d431cf9dc9b7cb4f7816b6a32780212458de49
Yoshi Automation <yoshi-automation@google.com>
2022-08-19 01:40:44 +0000

    feat(sqladmin): update the API
    #### sqladmin:v1beta4
    
    The following keys were added:
    - schemas.Settings.properties.connectorEnforcement.description
    - schemas.Settings.properties.connectorEnforcement.enum
    - schemas.Settings.properties.connectorEnforcement.enumDescriptions
    - schemas.Settings.properties.connectorEnforcement.type
    - schemas.Settings.properties.deletionProtectionEnabled.description
    - schemas.Settings.properties.deletionProtectionEnabled.type
    
    The following keys were changed:
    - schemas.BackupRun.properties.type.description
    - schemas.DatabaseInstance.properties.rootPassword.description
    
    #### sqladmin:v1
    
    The following keys were added:
    - schemas.Settings.properties.connectorEnforcement.description
    - schemas.Settings.properties.connectorEnforcement.enum
    - schemas.Settings.properties.connectorEnforcement.enumDescriptions
    - schemas.Settings.properties.connectorEnforcement.type
    - schemas.Settings.properties.deletionProtectionEnabled.description
    - schemas.Settings.properties.deletionProtectionEnabled.type
    
    The following keys were changed:
    - schemas.BackupRun.properties.type.description
    - schemas.DatabaseInstance.properties.rootPassword.description

--- src/apis/sqladmin/v1.ts
+++ src/apis/sqladmin/v1.ts
@@ -308,7 +308,7 @@ export namespace sqladmin_v1 {
      */
     status?: string | null;
     /**
-     * The type of this run; can be either "AUTOMATED" or "ON_DEMAND". This field defaults to "ON_DEMAND" and is ignored, when specified for insert requests.
+     * The type of this run; can be either "AUTOMATED" or "ON_DEMAND" or "FINAL". This field defaults to "ON_DEMAND" and is ignored, when specified for insert requests.
      */
     type?: string | null;
     /**
@@ -568,7 +568,7 @@ export namespace sqladmin_v1 {
      */
     replicaNames?: string[] | null;
     /**
-     * Initial root password. Use only on creation.
+     * Initial root password. Use only on creation. You must set root passwords before you can connect to PostgreSQL instances.
      */
     rootPassword?: string | null;
     /**
@@ -1498,6 +1498,10 @@ export namespace sqladmin_v1 {
      */
     collation?: string | null;
     /**
+     * Specifies if connections must use Cloud SQL connectors. Option values include the following: * `NOT_REQUIRED`: Cloud SQL instances can be connected without Cloud SQL Connectors. * `REQUIRED`: Only allow connections that use Cloud SQL Connectors. Note that using REQUIRED disables all existing authorized networks. If this field is not specified when creating a new instance, NOT_REQUIRED is used. If this field is not specified when patching or updating an existing instance, it is left unchanged in the instance.
+     */
+    connectorEnforcement?: string | null;
+    /**
      * Configuration specific to read replica instances. Indicates whether database flags for crash-safe replication are enabled. This property was only applicable to First Generation instances.
      */
     crashSafeReplicationEnabled?: boolean | null;
@@ -1517,6 +1521,10 @@ export namespace sqladmin_v1 {
      * The type of data disk: `PD_SSD` (default) or `PD_HDD`. Not used for First Generation instances.
      */
     dataDiskType?: string | null;
+    /**
+     * Configuration to protect against accidental instance deletion.
+     */
+    deletionProtectionEnabled?: boolean | null;
     /**
      * Deny maintenance periods
      */
