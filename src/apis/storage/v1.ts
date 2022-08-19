db8bc4e30d47c69309c88a1395f40b364d21e10c
Yoshi Automation <yoshi-automation@google.com>
2022-08-19 01:40:44 +0000

    fix(storage): update the API
    #### storage:v1
    
    The following keys were changed:
    - schemas.Object.properties.updated.description

--- src/apis/storage/v1.ts
+++ src/apis/storage/v1.ts
@@ -729,7 +729,7 @@ export namespace storage_v1 {
      */
     timeStorageClassUpdated?: string | null;
     /**
-     * The modification time of the object metadata in RFC 3339 format.
+     * The modification time of the object metadata in RFC 3339 format. Set initially to object creation time and then updated whenever any metadata of the object changes. This includes changes made by a requester, such as modifying custom metadata, as well as changes made by Cloud Storage on behalf of a requester, such as changing the storage class based on an Object Lifecycle Configuration.
      */
     updated?: string | null;
   }
