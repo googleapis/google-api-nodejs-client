98619ddfc12971a10fb9e5918b39bc4ef9a800c3
Yoshi Automation <yoshi-automation@google.com>
2022-08-19 01:40:45 +0000

    feat(vision): update the API
    #### vision:v1p1beta1
    
    The following keys were added:
    - schemas.GoogleCloudVisionV1p1beta1TextDetectionParams.properties.advancedOcrOptions.description
    - schemas.GoogleCloudVisionV1p1beta1TextDetectionParams.properties.advancedOcrOptions.items.type
    - schemas.GoogleCloudVisionV1p1beta1TextDetectionParams.properties.advancedOcrOptions.type
    
    #### vision:v1p2beta1
    
    The following keys were added:
    - schemas.GoogleCloudVisionV1p2beta1TextDetectionParams.properties.advancedOcrOptions.description
    - schemas.GoogleCloudVisionV1p2beta1TextDetectionParams.properties.advancedOcrOptions.items.type
    - schemas.GoogleCloudVisionV1p2beta1TextDetectionParams.properties.advancedOcrOptions.type
    
    #### vision:v1
    
    The following keys were added:
    - schemas.TextDetectionParams.properties.advancedOcrOptions.description
    - schemas.TextDetectionParams.properties.advancedOcrOptions.items.type
    - schemas.TextDetectionParams.properties.advancedOcrOptions.type

--- src/apis/vision/v1p1beta1.ts
+++ src/apis/vision/v1p1beta1.ts
@@ -1508,6 +1508,10 @@ export namespace vision_v1p1beta1 {
    */
   export interface Schema$GoogleCloudVisionV1p1beta1TextDetectionParams {
     /**
+     * A list of advanced OCR options to fine-tune OCR behavior.
+     */
+    advancedOcrOptions?: string[] | null;
+    /**
      * By default, Cloud Vision API only includes confidence score for DOCUMENT_TEXT_DETECTION result. Set the flag to true to include confidence score for TEXT_DETECTION as well.
      */
     enableTextDetectionConfidenceScore?: boolean | null;
