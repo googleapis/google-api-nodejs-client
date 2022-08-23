25af112f8450b69e8511f47a5c63586e4d54432a
Yoshi Automation <yoshi-automation@google.com>
2022-08-23 01:39:32 +0000

    feat(sqladmin)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### sqladmin:v1beta4
    
    The following keys were deleted:
    - schemas.Settings.properties.connectorEnforcement.description
    - schemas.Settings.properties.connectorEnforcement.enum
    - schemas.Settings.properties.connectorEnforcement.enumDescriptions
    - schemas.Settings.properties.connectorEnforcement.type
    
    #### sqladmin:v1
    
    The following keys were deleted:
    - schemas.Settings.properties.connectorEnforcement.description
    - schemas.Settings.properties.connectorEnforcement.enum
    - schemas.Settings.properties.connectorEnforcement.enumDescriptions
    - schemas.Settings.properties.connectorEnforcement.type

--- src/apis/sqladmin/v1beta4.ts
+++ src/apis/sqladmin/v1beta4.ts
@@ -1498,10 +1498,6 @@ export namespace sqladmin_v1beta4 {
      */
     collation?: string | null;
     /**
-     * Specifies if connections must use Cloud SQL connectors. Option values include the following: * `NOT_REQUIRED`: Cloud SQL instances can be connected without Cloud SQL Connectors. * `REQUIRED`: Only allow connections that use Cloud SQL Connectors. Note that using REQUIRED disables all existing authorized networks. If this field is not specified when creating a new instance, NOT_REQUIRED is used. If this field is not specified when patching or updating an existing instance, it is left unchanged in the instance.
-     */
-    connectorEnforcement?: string | null;
-    /**
      * Configuration specific to read replica instances. Indicates whether database flags for crash-safe replication are enabled. This property was only applicable to First Generation instances.
      */
     crashSafeReplicationEnabled?: boolean | null;
