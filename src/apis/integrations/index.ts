a417bf7764ce74cbc0ce4b6d7f44ae553a491b8b
Yoshi Automation <yoshi-automation@google.com>
2022-09-21 01:48:02 +0000

    fix(integrations): update the API

---
+++ src/apis/integrations/index.ts
@@ -0,0 +1,48 @@
+// Copyright 2020 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*! THIS FILE IS AUTO-GENERATED */
+
+import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
+import {integrations_v1alpha} from './v1alpha';
+
+export const VERSIONS = {
+  v1alpha: integrations_v1alpha.Integrations,
+};
+
+export function integrations(
+  version: 'v1alpha'
+): integrations_v1alpha.Integrations;
+export function integrations(
+  options: integrations_v1alpha.Options
+): integrations_v1alpha.Integrations;
+export function integrations<T = integrations_v1alpha.Integrations>(
+  this: GoogleConfigurable,
+  versionOrOptions: 'v1alpha' | integrations_v1alpha.Options
+) {
+  return getAPI<T>('integrations', versionOrOptions, VERSIONS, this);
+}
+
+const auth = new AuthPlus();
+export {auth};
+export {integrations_v1alpha};
+export {
+  AuthPlus,
+  GlobalOptions,
+  APIRequestContext,
+  GoogleConfigurable,
+  StreamMethodOptions,
+  GaxiosPromise,
+  MethodOptions,
+  BodyResponseCallback,
+} from 'googleapis-common';
