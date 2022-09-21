bf42a8687c0bdb858578f039ac1bbfe3e807f0b0
Yoshi Automation <yoshi-automation@google.com>
2022-09-21 01:48:07 +0000

    fix(recommender): update the API
    #### recommender:v1beta1
    
    The following keys were changed:
    - schemas.GoogleCloudRecommenderV1beta1Insight.description
    
    #### recommender:v1
    
    The following keys were changed:
    - schemas.GoogleCloudRecommenderV1Insight.description

--- src/apis/recommender/v1.ts
+++ src/apis/recommender/v1.ts
@@ -161,7 +161,7 @@ export namespace recommender_v1 {
     securityProjection?: Schema$GoogleCloudRecommenderV1SecurityProjection;
   }
   /**
-   * An insight along with the information used to derive the insight. The insight may have associated recomendations as well.
+   * An insight along with the information used to derive the insight. The insight may have associated recommendations as well.
    */
   export interface Schema$GoogleCloudRecommenderV1Insight {
     /**
