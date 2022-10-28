7fe069b84e28f84ed9d111246356797d9e874c5c
Yoshi Automation <yoshi-automation@google.com>
2022-10-28 19:07:08 +0000

    feat(youtube): update the API
    #### youtube:v3
    
    The following keys were added:
    - schemas.LiveBroadcastStatistics.properties.concurrentViewers.description
    - schemas.LiveBroadcastStatistics.properties.concurrentViewers.format
    - schemas.LiveBroadcastStatistics.properties.concurrentViewers.type

--- src/apis/youtube/v3.ts
+++ src/apis/youtube/v3.ts
@@ -2184,6 +2184,10 @@ export namespace youtube_v3 {
    */
   export interface Schema$LiveBroadcastStatistics {
     /**
+     * The number of viewers currently watching the broadcast. The property and its value will be present if the broadcast has current viewers and the broadcast owner has not hidden the viewcount for the video. Note that YouTube stops tracking the number of concurrent viewers for a broadcast when the broadcast ends. So, this property would not identify the number of viewers watching an archived video of a live broadcast that already ended.
+     */
+    concurrentViewers?: string | null;
+    /**
      * The total number of live chat messages currently on the broadcast. The property and its value will be present if the broadcast is public, has the live chat feature enabled, and has at least one message. Note that this field will not be filled after the broadcast ends. So this property would not identify the number of chat messages for an archived video of a completed live broadcast.
      */
     totalChatCount?: string | null;
