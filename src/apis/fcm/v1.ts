a29680d9815ad0e7df11439405ddd26b6b7042b8
Yoshi Automation <yoshi-automation@google.com>
2022-12-22 01:40:17 +0000

    fix(fcm): update the API
    #### fcm:v1
    
    The following keys were changed:
    - schemas.ApnsConfig.properties.headers.description
    - schemas.ApnsConfig.properties.payload.description

--- src/apis/fcm/v1.ts
+++ src/apis/fcm/v1.ts
@@ -288,11 +288,11 @@ export namespace fcm_v1 {
      */
     fcmOptions?: Schema$ApnsFcmOptions;
     /**
-     * HTTP request headers defined in Apple Push Notification Service. Refer to [APNs request headers](https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/sending_notification_requests_to_apns) for supported headers such as `apns-expiration` and `apns-priority`.
+     * HTTP request headers defined in Apple Push Notification Service. Refer to [APNs request headers](https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/sending_notification_requests_to_apns) for supported headers such as `apns-expiration` and `apns-priority`. The backend sets a default value for `apns-expiration` of 30 days and a default value for `apns-priority` of 10 if not explicitly set.
      */
     headers?: {[key: string]: string} | null;
     /**
-     * APNs payload as a JSON object, including both `aps` dictionary and custom payload. See [Payload Key Reference](https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/generating_a_remote_notification). If present, it overrides google.firebase.fcm.v1.Notification.title and google.firebase.fcm.v1.Notification.body. The backend sets a default value for `apns-expiration` of 30 days and a default value for `apns-priority` of 10 if not explicitly set.
+     * APNs payload as a JSON object, including both `aps` dictionary and custom payload. See [Payload Key Reference](https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/generating_a_remote_notification). If present, it overrides google.firebase.fcm.v1.Notification.title and google.firebase.fcm.v1.Notification.body.
      */
     payload?: {[key: string]: any} | null;
   }
