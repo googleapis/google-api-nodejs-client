86ee07116350c1e7e6ff311c1802349ddca65f95
Yoshi Automation <yoshi-automation@google.com>
2022-12-22 01:40:21 +0000

    fix(mybusinessplaceactions): update the API
    #### mybusinessplaceactions:v1
    
    The following keys were changed:
    - description

--- src/apis/mybusinessplaceactions/v1.ts
+++ src/apis/mybusinessplaceactions/v1.ts
@@ -102,7 +102,7 @@ export namespace mybusinessplaceactions_v1 {
   /**
    * My Business Place Actions API
    *
-   * The My Business Place Actions API provides an interface for managing place action links of a location on Google.
+   * The My Business Place Actions API provides an interface for managing place action links of a location on Google. Note - If you have a quota of 0 after enabling the API, please request for GBP API access.
    *
    * @example
    * ```js
