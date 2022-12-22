ae68b4f58d2ad6b2c99e362d1c2d3d68b02f99a3
Yoshi Automation <yoshi-automation@google.com>
2022-12-22 01:40:25 +0000

    feat(workflowexecutions): update the API
    #### workflowexecutions:v1beta
    
    The following keys were changed:
    - schemas.Execution.properties.callLogLevel.enumDescriptions
    - schemas.Execution.properties.status.description
    - schemas.Status.description
    
    #### workflowexecutions:v1
    
    The following keys were added:
    - schemas.Execution.properties.duration.description
    - schemas.Execution.properties.duration.format
    - schemas.Execution.properties.duration.readOnly
    - schemas.Execution.properties.duration.type
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.workflows.resources.executions.methods.get.parameters.view.enumDescriptions
    - resources.projects.resources.locations.resources.workflows.resources.executions.methods.list.parameters.view.enumDescriptions
    - schemas.Execution.properties.callLogLevel.enumDescriptions
    - schemas.Execution.properties.status.description
    - schemas.Status.description

--- src/apis/workflowexecutions/v1beta.ts
+++ src/apis/workflowexecutions/v1beta.ts
@@ -182,7 +182,7 @@ export namespace workflowexecutions_v1beta {
      */
     state?: string | null;
     /**
-     * Output only. Status tracks the current steps and progress data of this execution. \> **Preview:** This field is covered by the \> [Pre-GA Offerings Terms](https://cloud.google.com/terms/service-terms) of \> the Google Cloud Terms of Service. Pre-GA features might have limited \> support, and changes to pre-GA features might not be compatible with \> other pre-GA versions. For more information, see the \> [launch stage descriptions](https://cloud.google.com/products#product-launch-stages). \> This field is usable only if your project has access. See the \> [access request page](https://docs.google.com/forms/d/e/1FAIpQLSdgwrSV8Y4xZv_tvI6X2JEGX1-ty9yizv3_EAOVHWVKXvDLEA/viewform).
+     * Output only. Status tracks the current steps and progress data of this execution.
      */
     status?: Schema$Status;
     /**
@@ -247,7 +247,7 @@ export namespace workflowexecutions_v1beta {
     step?: string | null;
   }
   /**
-   * \> **Preview:** This field is covered by the \> [Pre-GA Offerings Terms](https://cloud.google.com/terms/service-terms) of \> the Google Cloud Terms of Service. Pre-GA features might have limited \> support, and changes to pre-GA features might not be compatible with \> other pre-GA versions. For more information, see the \> [launch stage descriptions](https://cloud.google.com/products#product-launch-stages). \> This field is usable only if your project has access. See the \> [access request page](https://docs.google.com/forms/d/e/1FAIpQLSdgwrSV8Y4xZv_tvI6X2JEGX1-ty9yizv3_EAOVHWVKXvDLEA/viewform). Represents the current status of this execution.
+   * Represents the current status of this execution.
    */
   export interface Schema$Status {
     /**
