72db393fca967caa3523f43f765e4e166c6c0894
Yoshi Automation <yoshi-automation@google.com>
2022-12-22 01:40:24 +0000

    fix(slides): update the API
    #### slides:v1
    
    The following keys were changed:
    - schemas.CreateImageRequest.properties.elementProperties.description
    - schemas.CreateImageRequest.properties.url.description

--- src/apis/slides/v1.ts
+++ src/apis/slides/v1.ts
@@ -273,7 +273,7 @@ export namespace slides_v1 {
    */
   export interface Schema$CreateImageRequest {
     /**
-     * The element properties for the image. When the aspect ratio of the provided size does not match the image aspect ratio, the image is scaled and centered with respect to the size in order to maintain aspect ratio. The provided transform is applied after this operation. The PageElementProperties.size property is optional. If you don't specify the size, the default size of the image is used. The PageElementProperties.transform property is optional. If you don't specify a transform, the image will be placed at the top left corner of the page.
+     * The element properties for the image. When the aspect ratio of the provided size does not match the image aspect ratio, the image is scaled and centered with respect to the size in order to maintain the aspect ratio. The provided transform is applied after this operation. The PageElementProperties.size property is optional. If you don't specify the size, the default size of the image is used. The PageElementProperties.transform property is optional. If you don't specify a transform, the image will be placed at the top-left corner of the page.
      */
     elementProperties?: Schema$PageElementProperties;
     /**
@@ -281,7 +281,7 @@ export namespace slides_v1 {
      */
     objectId?: string | null;
     /**
-     * The image URL. The image is fetched once at insertion time and a copy is stored for display inside the presentation. Images must be less than 50MB in size, cannot exceed 25 megapixels, and must be in one of PNG, JPEG, or GIF format. The provided URL can be at most 2 kB in length. The URL itself is saved with the image, and exposed via the Image.source_url field.
+     * The image URL. The image is fetched once at insertion time and a copy is stored for display inside the presentation. Images must be less than 50 MB in size, can't exceed 25 megapixels, and must be in one of PNG, JPEG, or GIF formats. The provided URL can be up to 2 KB in length. The URL is saved with the image, and exposed through the Image.source_url field.
      */
     url?: string | null;
   }
