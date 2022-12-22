24462a944f32722ec35076b922d35fc0dbe9345c
Yoshi Automation <yoshi-automation@google.com>
2022-12-22 01:40:14 +0000

    feat(datacatalog): update the API
    #### datacatalog:v1
    
    The following keys were added:
    - schemas.GoogleCloudDatacatalogV1ColumnSchema.properties.gcRule.description
    - schemas.GoogleCloudDatacatalogV1ColumnSchema.properties.gcRule.type

--- src/apis/datacatalog/v1.ts
+++ src/apis/datacatalog/v1.ts
@@ -295,6 +295,10 @@ export namespace datacatalog_v1 {
      */
     description?: string | null;
     /**
+     * Optional. Garbage collection policy for the column or column family. Applies to systems like Cloud Bigtable.
+     */
+    gcRule?: string | null;
+    /**
      * Optional. A column's mode indicates whether values in this column are required, nullable, or repeated. Only `NULLABLE`, `REQUIRED`, and `REPEATED` values are supported. Default mode is `NULLABLE`.
      */
     mode?: string | null;
