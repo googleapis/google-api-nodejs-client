21d0d5d201f9124df5e0f8370b870119f78d7598
Yoshi Automation <yoshi-automation@google.com>
2022-12-22 01:40:26 +0000

    fix(workloadmanager): update the API

---
+++ src/apis/workloadmanager/index.ts
@@ -0,0 +1,48 @@
+// Copyright 2020 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*! THIS FILE IS AUTO-GENERATED */
+
+import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
+import {workloadmanager_v1} from './v1';
+
+export const VERSIONS = {
+  v1: workloadmanager_v1.Workloadmanager,
+};
+
+export function workloadmanager(
+  version: 'v1'
+): workloadmanager_v1.Workloadmanager;
+export function workloadmanager(
+  options: workloadmanager_v1.Options
+): workloadmanager_v1.Workloadmanager;
+export function workloadmanager<T = workloadmanager_v1.Workloadmanager>(
+  this: GoogleConfigurable,
+  versionOrOptions: 'v1' | workloadmanager_v1.Options
+) {
+  return getAPI<T>('workloadmanager', versionOrOptions, VERSIONS, this);
+}
+
+const auth = new AuthPlus();
+export {auth};
+export {workloadmanager_v1};
+export {
+  AuthPlus,
+  GlobalOptions,
+  APIRequestContext,
+  GoogleConfigurable,
+  StreamMethodOptions,
+  GaxiosPromise,
+  MethodOptions,
+  BodyResponseCallback,
+} from 'googleapis-common';
