71a38e29ada3c23e319a378f4cb3f8f7e92089dd
Yoshi Automation <yoshi-automation@google.com>
2023-02-02 01:38:28 +0000

    fix(kmsinventory): update the API

---
+++ src/apis/kmsinventory/index.ts
@@ -0,0 +1,46 @@
+// Copyright 2020 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*! THIS FILE IS AUTO-GENERATED */
+
+import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
+import {kmsinventory_v1} from './v1';
+
+export const VERSIONS = {
+  v1: kmsinventory_v1.Kmsinventory,
+};
+
+export function kmsinventory(version: 'v1'): kmsinventory_v1.Kmsinventory;
+export function kmsinventory(
+  options: kmsinventory_v1.Options
+): kmsinventory_v1.Kmsinventory;
+export function kmsinventory<T = kmsinventory_v1.Kmsinventory>(
+  this: GoogleConfigurable,
+  versionOrOptions: 'v1' | kmsinventory_v1.Options
+) {
+  return getAPI<T>('kmsinventory', versionOrOptions, VERSIONS, this);
+}
+
+const auth = new AuthPlus();
+export {auth};
+export {kmsinventory_v1};
+export {
+  AuthPlus,
+  GlobalOptions,
+  APIRequestContext,
+  GoogleConfigurable,
+  StreamMethodOptions,
+  GaxiosPromise,
+  MethodOptions,
+  BodyResponseCallback,
+} from 'googleapis-common';
