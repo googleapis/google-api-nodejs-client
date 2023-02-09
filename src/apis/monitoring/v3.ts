98e55c08500949aef54fc8391d6e1a6201d2ab34
Yoshi Automation <yoshi-automation@google.com>
2023-02-09 01:38:03 +0000

    feat(monitoring): update the API
    #### monitoring:v1
    
    The following keys were added:
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.description
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.flatPath
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.httpMethod
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.id
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.parameterOrder
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.parameters.location.description
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.parameters.location.location
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.parameters.location.required
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.parameters.location.type
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.parameters.name.description
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.parameters.name.location
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.parameters.name.pattern
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.parameters.name.required
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.parameters.name.type
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.path
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.request.$ref
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.response.$ref
    - resources.projects.resources.location.resources.prometheus.resources.api.resources.v1.methods.query_exemplars.scopes
    - schemas.QueryExemplarsRequest.description
    - schemas.QueryExemplarsRequest.id
    - schemas.QueryExemplarsRequest.properties.end.description
    - schemas.QueryExemplarsRequest.properties.end.type
    - schemas.QueryExemplarsRequest.properties.query.description
    - schemas.QueryExemplarsRequest.properties.query.type
    - schemas.QueryExemplarsRequest.properties.start.description
    - schemas.QueryExemplarsRequest.properties.start.type
    - schemas.QueryExemplarsRequest.type
    
    #### monitoring:v3
    
    The following keys were changed:
    - schemas.Criteria.properties.policies.description
    - schemas.UptimeCheckConfig.properties.selectedRegions.items.enum
    - schemas.UptimeCheckConfig.properties.selectedRegions.items.enumDescriptions
    - schemas.UptimeCheckIp.properties.region.enum
    - schemas.UptimeCheckIp.properties.region.enumDescriptions

--- src/apis/monitoring/v3.ts
+++ src/apis/monitoring/v3.ts
@@ -534,7 +534,7 @@ export namespace monitoring_v3 {
    */
   export interface Schema$Criteria {
     /**
-     * The specific AlertPolicy names for the alert that should be snoozed. The format is: projects/[PROJECT_ID_OR_NUMBER]/alertPolicies/[POLICY_ID] There is a limit of 100 policies per snooze. This limit is checked during snooze creation.
+     * The specific AlertPolicy names for the alert that should be snoozed. The format is: projects/[PROJECT_ID_OR_NUMBER]/alertPolicies/[POLICY_ID] There is a limit of 16 policies per snooze. This limit is checked during snooze creation.
      */
     policies?: string[] | null;
   }
