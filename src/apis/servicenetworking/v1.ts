71074d9780c79fedea64081609ba7b471fb5d14c
Yoshi Automation <yoshi-automation@google.com>
2023-02-22 01:40:11 +0000

    feat(servicenetworking): update the API
    #### servicenetworking:v1beta
    
    The following keys were added:
    - schemas.BackendRule.properties.overridesByRequestProtocol.additionalProperties.$ref
    - schemas.BackendRule.properties.overridesByRequestProtocol.description
    - schemas.BackendRule.properties.overridesByRequestProtocol.type
    
    #### servicenetworking:v1
    
    The following keys were added:
    - schemas.BackendRule.properties.overridesByRequestProtocol.additionalProperties.$ref
    - schemas.BackendRule.properties.overridesByRequestProtocol.description
    - schemas.BackendRule.properties.overridesByRequestProtocol.type

--- src/apis/servicenetworking/v1.ts
+++ src/apis/servicenetworking/v1.ts
@@ -430,6 +430,10 @@ export namespace servicenetworking_v1 {
      * The number of seconds to wait for the completion of a long running operation. The default is no deadline.
      */
     operationDeadline?: number | null;
+    /**
+     * The map between request protocol and the backend address.
+     */
+    overridesByRequestProtocol?: {[key: string]: Schema$BackendRule} | null;
     pathTranslation?: string | null;
     /**
      * The protocol used for sending a request to the backend. The supported values are "http/1.1" and "h2". The default value is inferred from the scheme in the address field: SCHEME PROTOCOL http:// http/1.1 https:// http/1.1 grpc:// h2 grpcs:// h2 For secure HTTP backends (https://) that support HTTP/2, set this field to "h2" for improved performance. Configuring this field to non-default values is only supported for secure HTTP backends. This field will be ignored for all other backends. See https://www.iana.org/assignments/tls-extensiontype-values/tls-extensiontype-values.xhtml#alpn-protocol-ids for more details on the supported values.
