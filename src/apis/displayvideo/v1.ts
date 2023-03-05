95253da14d2087d714f9d452b8ecb2287c456ea2
Yoshi Automation <yoshi-automation@google.com>
2023-03-05 01:39:16 +0000

    fix(displayvideo): update the API
    #### displayvideo:v1
    
    The following keys were changed:
    - schemas.Asset.properties.mediaId.description
    
    #### displayvideo:v2
    
    The following keys were changed:
    - schemas.Asset.properties.mediaId.description

--- src/apis/displayvideo/v1.ts
+++ src/apis/displayvideo/v1.ts
@@ -412,7 +412,7 @@ export namespace displayvideo_v1 {
      */
     content?: string | null;
     /**
-     * Media ID of the uploaded asset. This is a unique identifier for the asset. This ID can be passed to other API calls, e.g. CreateCreative to associate the asset with a creative.
+     * Media ID of the uploaded asset. This is a unique identifier for the asset. This ID can be passed to other API calls, e.g. CreateCreative to associate the asset with a creative. **On April 5, 2023, the value of this ID will be updated. Before this date, we recommend that you stop using any cached media IDs when creating or updating creatives, and instead upload assets immediately before using them for creative production.** **After April 5, you can update cached media IDs to the new values by retrieving them from associated creative resources or re-uploading them.**
      */
     mediaId?: string | null;
   }
