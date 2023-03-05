e237a8796f822d18df714a37c1dbce69db116233
Yoshi Automation <yoshi-automation@google.com>
2023-03-05 01:39:20 +0000

    feat(workstations): update the API
    #### workstations:v1beta
    
    The following keys were added:
    - schemas.GceRegionalPersistentDisk.properties.sourceSnapshot.description
    - schemas.GceRegionalPersistentDisk.properties.sourceSnapshot.type

--- src/apis/workstations/v1beta.ts
+++ src/apis/workstations/v1beta.ts
@@ -300,6 +300,10 @@ export namespace workstations_v1beta {
      * Size of the disk in GB. Must be empty if source_snapshot is set.
      */
     sizeGb?: number | null;
+    /**
+     * Name of the snapshot to use as the source for the disk. If set, size_gb and fs_type must be empty.
+     */
+    sourceSnapshot?: string | null;
   }
   /**
    * A set of Compute Engine Shielded instance options.
