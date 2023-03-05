16975b72bdfb33784bad1c7f2a3dec787f80510b
Yoshi Automation <yoshi-automation@google.com>
2023-03-05 01:39:13 +0000

    fix(clouderrorreporting): update the API
    #### clouderrorreporting:v1beta1
    
    The following keys were changed:
    - resources.projects.resources.events.methods.report.description

--- src/apis/clouderrorreporting/v1beta1.ts
+++ src/apis/clouderrorreporting/v1beta1.ts
@@ -706,7 +706,7 @@ export namespace clouderrorreporting_v1beta1 {
     }
 
     /**
-     * Report an individual error event and record the event to a log. This endpoint accepts **either** an OAuth token, **or** an [API key](https://support.google.com/cloud/answer/6158862) for authentication. To use an API key, append it to the URL as the value of a `key` parameter. For example: `POST https://clouderrorreporting.googleapis.com/v1beta1/{projectName\}/events:report?key=123ABC456` **Note:** [Error Reporting] (https://cloud.google.com/error-reporting) is a global service built on Cloud Logging and doesn't analyze logs stored in regional log buckets or logs routed to other Google Cloud projects. For more information, see [Using Error Reporting with regionalized logs] (https://cloud.google.com/error-reporting/docs/regionalization).
+     * Report an individual error event and record the event to a log. This endpoint accepts **either** an OAuth token, **or** an [API key](https://support.google.com/cloud/answer/6158862) for authentication. To use an API key, append it to the URL as the value of a `key` parameter. For example: `POST https://clouderrorreporting.googleapis.com/v1beta1/{projectName\}/events:report?key=123ABC456` **Note:** [Error Reporting] (https://cloud.google.com/error-reporting) is a global service built on Cloud Logging and doesn't analyze logs stored in regional log buckets or logs routed to other Google Cloud projects.
      * @example
      * ```js
      * // Before running the sample:
