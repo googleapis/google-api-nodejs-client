69ceb5d94a86edcde19a4fcf87e9d689c1c6f48a
Yoshi Automation <yoshi-automation@google.com>
2023-03-05 01:39:17 +0000

    fix(docs): update the API
    #### docs:v1
    
    The following keys were changed:
    - schemas.InsertInlineImageRequest.properties.uri.description

--- src/apis/docs/v1.ts
+++ src/apis/docs/v1.ts
@@ -1159,7 +1159,7 @@ export namespace docs_v1 {
      */
     objectSize?: Schema$Size;
     /**
-     * The image URI. The image is fetched once at insertion time and a copy is stored for display inside the document. Images must be less than 50MB in size, cannot exceed 25 megapixels, and must be in one of PNG, JPEG, or GIF format. The provided URI can be at most 2 kB in length. The URI itself is saved with the image, and exposed via the ImageProperties.content_uri field.
+     * The image URI. The image is fetched once at insertion time and a copy is stored for display inside the document. Images must be less than 50MB in size, cannot exceed 25 megapixels, and must be in one of PNG, JPEG, or GIF format. The provided URI must be publicly accessible and at most 2 kB in length. The URI itself is saved with the image, and exposed via the ImageProperties.content_uri field.
      */
     uri?: string | null;
   }
