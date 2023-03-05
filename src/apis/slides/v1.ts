f62c1bc84b4c8742d9991f14766ca582ca544d19
Yoshi Automation <yoshi-automation@google.com>
2023-03-05 01:39:19 +0000

    fix(slides): update the API
    #### slides:v1
    
    The following keys were changed:
    - schemas.CreateImageRequest.properties.url.description

--- src/apis/slides/v1.ts
+++ src/apis/slides/v1.ts
@@ -281,7 +281,7 @@ export namespace slides_v1 {
      */
     objectId?: string | null;
     /**
-     * The image URL. The image is fetched once at insertion time and a copy is stored for display inside the presentation. Images must be less than 50 MB in size, can't exceed 25 megapixels, and must be in one of PNG, JPEG, or GIF formats. The provided URL can be up to 2 KB in length. The URL is saved with the image, and exposed through the Image.source_url field.
+     * The image URL. The image is fetched once at insertion time and a copy is stored for display inside the presentation. Images must be less than 50 MB in size, can't exceed 25 megapixels, and must be in one of PNG, JPEG, or GIF formats. The provided URL must be publicly accessible and up to 2 KB in length. The URL is saved with the image, and exposed through the Image.source_url field.
      */
     url?: string | null;
   }
