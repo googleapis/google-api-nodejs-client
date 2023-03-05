90642ba768cb5112edee9f811be500117cde5d36
Yoshi Automation <yoshi-automation@google.com>
2023-03-05 01:39:18 +0000

    fix(publicca): update the API

---
+++ src/apis/publicca/index.ts
@@ -0,0 +1,59 @@
+// Copyright 2020 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*! THIS FILE IS AUTO-GENERATED */
+
+import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
+import {publicca_v1alpha1} from './v1alpha1';
+import {publicca_v1beta1} from './v1beta1';
+
+export const VERSIONS = {
+  v1alpha1: publicca_v1alpha1.Publicca,
+  v1beta1: publicca_v1beta1.Publicca,
+};
+
+export function publicca(version: 'v1alpha1'): publicca_v1alpha1.Publicca;
+export function publicca(
+  options: publicca_v1alpha1.Options
+): publicca_v1alpha1.Publicca;
+export function publicca(version: 'v1beta1'): publicca_v1beta1.Publicca;
+export function publicca(
+  options: publicca_v1beta1.Options
+): publicca_v1beta1.Publicca;
+export function publicca<
+  T = publicca_v1alpha1.Publicca | publicca_v1beta1.Publicca
+>(
+  this: GoogleConfigurable,
+  versionOrOptions:
+    | 'v1alpha1'
+    | publicca_v1alpha1.Options
+    | 'v1beta1'
+    | publicca_v1beta1.Options
+) {
+  return getAPI<T>('publicca', versionOrOptions, VERSIONS, this);
+}
+
+const auth = new AuthPlus();
+export {auth};
+export {publicca_v1alpha1};
+export {publicca_v1beta1};
+export {
+  AuthPlus,
+  GlobalOptions,
+  APIRequestContext,
+  GoogleConfigurable,
+  StreamMethodOptions,
+  GaxiosPromise,
+  MethodOptions,
+  BodyResponseCallback,
+} from 'googleapis-common';
