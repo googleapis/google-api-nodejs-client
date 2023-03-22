d7cc7af77f48f52fa6f3867d6d53719d11d0e174
Yoshi Automation <yoshi-automation@google.com>
2023-03-22 01:37:26 +0000

    fix(appengine): update the API
    #### appengine:v1alpha
    
    The following keys were changed:
    - resources.apps.resources.operations.methods.list.description
    - resources.projects.resources.locations.resources.operations.methods.list.description
    
    #### appengine:v1beta
    
    The following keys were changed:
    - resources.apps.resources.operations.methods.list.description
    - resources.projects.resources.locations.resources.operations.methods.list.description
    
    #### appengine:v1
    
    The following keys were changed:
    - resources.apps.resources.operations.methods.list.description

--- src/apis/appengine/v1alpha.ts
+++ src/apis/appengine/v1alpha.ts
@@ -2928,7 +2928,7 @@ export namespace appengine_v1alpha {
     }
 
     /**
-     * Lists operations that match the specified filter in the request. If the server doesn't support this method, it returns UNIMPLEMENTED.NOTE: the name binding allows API services to override the binding to use different resource name schemes, such as users/x/operations. To override the binding, API services can add a binding such as "/v1/{name=users/x\}/operations" to their service configuration. For backwards compatibility, the default name includes the operations collection id, however overriding users must ensure the name binding is the parent resource, without the operations collection id.
+     * Lists operations that match the specified filter in the request. If the server doesn't support this method, it returns UNIMPLEMENTED.
      * @example
      * ```js
      * // Before running the sample:
@@ -3586,7 +3586,7 @@ export namespace appengine_v1alpha {
     }
 
     /**
-     * Lists operations that match the specified filter in the request. If the server doesn't support this method, it returns UNIMPLEMENTED.NOTE: the name binding allows API services to override the binding to use different resource name schemes, such as users/x/operations. To override the binding, API services can add a binding such as "/v1/{name=users/x\}/operations" to their service configuration. For backwards compatibility, the default name includes the operations collection id, however overriding users must ensure the name binding is the parent resource, without the operations collection id.
+     * Lists operations that match the specified filter in the request. If the server doesn't support this method, it returns UNIMPLEMENTED.
      * @example
      * ```js
      * // Before running the sample:
