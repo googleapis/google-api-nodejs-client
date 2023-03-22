4e8f1c79fd5313e72e38af47bd275b13947910bf
Yoshi Automation <yoshi-automation@google.com>
2023-03-22 01:37:35 +0000

    feat(notebooks): update the API
    #### notebooks:v1
    
    The following keys were added:
    - schemas.RuntimeSoftwareConfig.properties.mixerDisabled.description
    - schemas.RuntimeSoftwareConfig.properties.mixerDisabled.type
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.operations.methods.list.description

--- src/apis/notebooks/v1.ts
+++ src/apis/notebooks/v1.ts
@@ -1220,6 +1220,10 @@ export namespace notebooks_v1 {
      */
     kernels?: Schema$ContainerImage[];
     /**
+     * Bool indicating whether mixer client should be disabled. Default: False
+     */
+    mixerDisabled?: boolean | null;
+    /**
      * Cron expression in UTC timezone, used to schedule instance auto upgrade. Please follow the [cron format](https://en.wikipedia.org/wiki/Cron).
      */
     notebookUpgradeSchedule?: string | null;
@@ -7479,7 +7483,7 @@ export namespace notebooks_v1 {
     }
 
     /**
-     * Lists operations that match the specified filter in the request. If the server doesn't support this method, it returns `UNIMPLEMENTED`. NOTE: the `name` binding allows API services to override the binding to use different resource name schemes, such as `users/x/operations`. To override the binding, API services can add a binding such as `"/v1/{name=users/x\}/operations"` to their service configuration. For backwards compatibility, the default name includes the operations collection id, however overriding users must ensure the name binding is the parent resource, without the operations collection id.
+     * Lists operations that match the specified filter in the request. If the server doesn't support this method, it returns `UNIMPLEMENTED`.
      * @example
      * ```js
      * // Before running the sample:
