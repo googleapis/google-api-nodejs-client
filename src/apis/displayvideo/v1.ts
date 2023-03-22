5c7c0b63505ef31bf1450ed275fc90b6d664d0e8
Yoshi Automation <yoshi-automation@google.com>
2023-03-22 01:37:32 +0000

    fix(displayvideo): update the API
    #### displayvideo:v1
    
    The following keys were changed:
    - schemas.ExchangeConfigEnabledExchange.properties.exchange.enum
    - schemas.ExchangeConfigEnabledExchange.properties.exchange.enumDescriptions
    - schemas.ExchangeReviewStatus.properties.exchange.enum
    - schemas.ExchangeReviewStatus.properties.exchange.enumDescriptions
    - schemas.ExchangeTargetingOptionDetails.properties.exchange.enum
    - schemas.ExchangeTargetingOptionDetails.properties.exchange.enumDescriptions
    - schemas.GuaranteedOrder.properties.exchange.enum
    - schemas.GuaranteedOrder.properties.exchange.enumDescriptions
    - schemas.InventorySource.properties.exchange.enum
    - schemas.InventorySource.properties.exchange.enumDescriptions
    - schemas.LineItem.properties.targetingExpansion.description
    - schemas.LineItem.properties.warningMessages.items.enum
    - schemas.LineItem.properties.warningMessages.items.enumDescriptions
    - schemas.TargetingExpansionConfig.description
    - schemas.TargetingExpansionConfig.properties.excludeFirstPartyAudience.description
    - schemas.TargetingExpansionConfig.properties.targetingExpansionLevel.description
    
    #### displayvideo:v2
    
    The following keys were changed:
    - schemas.ExchangeAssignedTargetingOptionDetails.properties.exchange.enum
    - schemas.ExchangeAssignedTargetingOptionDetails.properties.exchange.enumDescriptions
    - schemas.ExchangeConfigEnabledExchange.properties.exchange.enum
    - schemas.ExchangeConfigEnabledExchange.properties.exchange.enumDescriptions
    - schemas.ExchangeReviewStatus.properties.exchange.enum
    - schemas.ExchangeReviewStatus.properties.exchange.enumDescriptions
    - schemas.ExchangeTargetingOptionDetails.properties.exchange.enum
    - schemas.ExchangeTargetingOptionDetails.properties.exchange.enumDescriptions
    - schemas.GuaranteedOrder.properties.exchange.enum
    - schemas.GuaranteedOrder.properties.exchange.enumDescriptions
    - schemas.InventorySource.properties.exchange.enum
    - schemas.InventorySource.properties.exchange.enumDescriptions
    - schemas.LineItem.properties.targetingExpansion.description
    - schemas.LineItem.properties.warningMessages.items.enum
    - schemas.LineItem.properties.warningMessages.items.enumDescriptions
    - schemas.TargetingExpansionConfig.description
    - schemas.TargetingExpansionConfig.properties.excludeFirstPartyAudience.description
    - schemas.TargetingExpansionConfig.properties.targetingExpansionLevel.description

--- src/apis/displayvideo/v1.ts
+++ src/apis/displayvideo/v1.ts
@@ -3525,7 +3525,7 @@ export namespace displayvideo_v1 {
      */
     reservationType?: string | null;
     /**
-     * The [targeting expansion](//support.google.com/displayvideo/answer/10191558) settings of the line item. This config is only applicable when eligible audience list targeting is assigned to the line item.
+     * The [targeting expansion](//support.google.com/displayvideo/answer/10191558) settings of the line item. This config is only applicable when eligible audience list targeting is assigned to the line item. Beginning **March 25, 2023**, these settings may represent the [optimized targeting feature](//support.google.com/displayvideo/answer/12060859) in place of targeting expansion. This feature will be rolled out to all partners by mid-April 2023.
      */
     targetingExpansion?: Schema$TargetingExpansionConfig;
     /**
@@ -4880,15 +4880,15 @@ export namespace displayvideo_v1 {
     displayName?: string | null;
   }
   /**
-   * Settings that control the targeting expansion of the line item. Targeting expansion allows the line item to reach a larger audience based on the original audience list and the targeting expansion level.
+   * Settings that control the targeting expansion of the line item. Targeting expansion allows the line item to reach a larger audience based on the original audience list and the targeting expansion level. Beginning **March 25, 2023**, these settings may represent the [optimized targeting feature](//support.google.com/displayvideo/answer/12060859) in place of targeting expansion. This feature will be rolled out to all partners by mid-April 2023.
    */
   export interface Schema$TargetingExpansionConfig {
     /**
-     * Required. Whether to exclude first-party audiences from use in targeting expansion or optimized targeting. Similar audiences of the excluded first-party lists will not be excluded. Only applicable when a first-party audience is positively targeted (directly or included in a combined audience), otherwise this selection will be ignored.
+     * Required. Whether to exclude first-party audiences from use in targeting expansion or optimized targeting. Similar audiences of the excluded first-party lists will not be excluded. Only applicable when a first-party audience is positively targeted (directly or included in a combined audience), otherwise this selection will be ignored. Beginning **March 25, 2023**, this field may be deprecated with the replacement of targeting expansion with [optimized targeting](//support.google.com/displayvideo/answer/12060859). Upon deprecation, this field will be set to `false`. If this field is set to `true` when deprecated, all positive first-party audience targeting assigned to this line item will be replaced with negative targeting of the same first-party audiences to ensure the continued exclusion of those audiences. This field will be deprecated for all partners by mid-April 2023.
      */
     excludeFirstPartyAudience?: boolean | null;
     /**
-     * Required. Magnitude of expansion for applicable targeting under this line item.
+     * Required. Magnitude of expansion for applicable targeting under this line item. Beginning **March 25, 2023**, the behavior of this field may change in the following ways with the replacement of targeting expansion with [optimized targeting](//support.google.com/displayvideo/answer/12060859): * This field will represent the optimized targeting checkbox, with a `NO_EXPANSION` value representing optimized targeting turned off and a `LEAST_EXPANSION` value representing optimized targeting turned on. * `NO_EXPANSION` will be the default value for the field and will be automatically assigned if you do not set the field. * If you set the field to any value other than `NO_EXPANSION`, it will automatically be set to `LEAST_EXPANSION`. This behavior will be rolled out to all partners by mid-April 2023.
      */
     targetingExpansionLevel?: string | null;
   }
