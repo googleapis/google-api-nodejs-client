f12859063b7039d2caba7dd3f443f52dd6448ae7
Yoshi Automation <yoshi-automation@google.com>
2023-03-22 01:37:28 +0000

    feat(cloudbuild): update the API
    #### cloudbuild:v1
    
    The following keys were added:
    - schemas.BuildOptions.properties.defaultLogsBucketBehavior.description
    - schemas.BuildOptions.properties.defaultLogsBucketBehavior.enum
    - schemas.BuildOptions.properties.defaultLogsBucketBehavior.enumDescriptions
    - schemas.BuildOptions.properties.defaultLogsBucketBehavior.type

--- src/apis/cloudbuild/v1.ts
+++ src/apis/cloudbuild/v1.ts
@@ -598,6 +598,10 @@ export namespace cloudbuild_v1 {
    */
   export interface Schema$BuildOptions {
     /**
+     * Optional. Option to specify how default logs buckets are setup.
+     */
+    defaultLogsBucketBehavior?: string | null;
+    /**
      * Requested disk size for the VM that runs the build. Note that this is *NOT* "disk free"; some of the space will be used by the operating system and build utilities. Also note that this is the minimum disk size that will be allocated for the build -- the build may run with a larger disk than requested. At present, the maximum disk size is 2000GB; builds that request more than the maximum are rejected with an error.
      */
     diskSizeGb?: string | null;
