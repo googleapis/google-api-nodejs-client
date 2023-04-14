84fa4f2d0f78440564de25b3c3668b38ab1cff0d
Yoshi Automation <yoshi-automation@google.com>
2023-04-14 01:37:27 +0000

    fix(testing): update the API
    #### testing:v1
    
    The following keys were changed:
    - schemas.TestSetup.properties.systrace.description

--- src/apis/testing/v1.ts
+++ src/apis/testing/v1.ts
@@ -1345,7 +1345,7 @@ export namespace testing_v1 {
      */
     networkProfile?: string | null;
     /**
-     * Deprecated: Systrace uses Python 2 which has been sunset 2020-01-01. Support of Systrace may stop at any time, at which point no Systrace file will be provided in the results. Systrace configuration for the run. If set a systrace will be taken, starting on test start and lasting for the configured duration. The systrace file thus obtained is put in the results bucket together with the other artifacts from the run.
+     * Systrace configuration for the run. Deprecated: Systrace used Python 2 which was sunsetted on 2020-01-01. Systrace is no longer supported in the Cloud Testing API, and no Systrace file will be provided in the results.
      */
     systrace?: Schema$SystraceSetup;
   }
