6ff8ff8eeaac6b0245abf3e1543e891ac96eff86
Yoshi Automation <yoshi-automation@google.com>
2023-05-02 01:37:58 +0000

    fix(gmail): update the API
    #### gmail:v1
    
    The following keys were changed:
    - resources.users.resources.messages.methods.import.description

--- src/apis/gmail/v1.ts
+++ src/apis/gmail/v1.ts
@@ -4127,7 +4127,7 @@ export namespace gmail_v1 {
     }
 
     /**
-     * Imports a message into only this user's mailbox, with standard email delivery scanning and classification similar to receiving via SMTP. This method doesn't perform SPF checks, so it might not work for some spam messages, such as those attempting to perform domain spoofing. This method does not send a message. Note: This function doesn't trigger forwarding rules or filters set up by the user.
+     * Imports a message into only this user's mailbox, with standard email delivery scanning and classification similar to receiving via SMTP. This method doesn't perform SPF checks, so it might not work for some spam messages, such as those attempting to perform domain spoofing. This method does not send a message.
      * @example
      * ```js
      * // Before running the sample:
