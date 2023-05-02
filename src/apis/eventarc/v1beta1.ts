643f428b779b1c47b79a6b0a2cdd4401323bcbd3
Yoshi Automation <yoshi-automation@google.com>
2023-05-02 01:37:57 +0000

    fix(eventarc): update the API
    #### eventarc:v1beta1
    
    The following keys were changed:
    - schemas.Location.description
    
    #### eventarc:v1
    
    The following keys were changed:
    - schemas.Location.description

--- src/apis/eventarc/v1beta1.ts
+++ src/apis/eventarc/v1beta1.ts
@@ -266,7 +266,7 @@ export namespace eventarc_v1beta1 {
     unreachable?: string[] | null;
   }
   /**
-   * A resource that represents Google Cloud Platform location.
+   * A resource that represents a Google Cloud location.
    */
   export interface Schema$Location {
     /**
