11d3f561ac7051d1b7caf997664d8c51923623b2
Yoshi Automation <yoshi-automation@google.com>
2023-05-02 01:38:00 +0000

    fix(privateca): update the API
    #### privateca:v1beta1
    
    The following keys were changed:
    - schemas.Location.description
    
    #### privateca:v1
    
    The following keys were changed:
    - schemas.Location.description

--- src/apis/privateca/v1.ts
+++ src/apis/privateca/v1.ts
@@ -913,7 +913,7 @@ export namespace privateca_v1 {
     operations?: Schema$Operation[];
   }
   /**
-   * A resource that represents Google Cloud Platform location.
+   * A resource that represents a Google Cloud location.
    */
   export interface Schema$Location {
     /**
