764711b0a069b7862eb1a0165864caa4fd5cb8e2
Yoshi Automation <yoshi-automation@google.com>
2023-07-10 01:42:45 +0000

    fix(analyticsdata): update the API
    #### analyticsdata:v1beta
    
    The following keys were changed:
    - description

--- src/apis/analyticsdata/v1beta.ts
+++ src/apis/analyticsdata/v1beta.ts
@@ -102,7 +102,7 @@ export namespace analyticsdata_v1beta {
   /**
    * Google Analytics Data API
    *
-   * Accesses report data in Google Analytics.
+   * Accesses report data in Google Analytics. Warning: Creating multiple Customer Applications, Accounts, or Projects to simulate or act as a single Customer Application, Account, or Project (respectively) or to circumvent Service-specific usage limits or quotas is a direct violation of Google Cloud Platform Terms of Service as well as Google APIs Terms of Service. These actions can result in immediate termination of your GCP project(s) without any warning.
    *
    * @example
    * ```js
