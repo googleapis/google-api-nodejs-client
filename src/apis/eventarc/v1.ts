10ada5b6f06e2f5c5a7826e5705cf44a9fca575d
Yoshi Automation <yoshi-automation@google.com>
2023-07-18 01:39:55 +0000

    fix(eventarc): update the API
    #### eventarc:v1
    
    The following keys were changed:
    - schemas.EventFilter.properties.operator.description

--- src/apis/eventarc/v1.ts
+++ src/apis/eventarc/v1.ts
@@ -288,7 +288,7 @@ export namespace eventarc_v1 {
      */
     attribute?: string | null;
     /**
-     * Optional. The operator used for matching the events with the value of the filter. If not specified, only events that have an exact key-value pair specified in the filter are matched. The only allowed value is `match-path-pattern`.
+     * Optional. The operator used for matching the events with the value of the filter. If not specified, only events that have an exact key-value pair specified in the filter are matched. The allowed values are `path_pattern` and `match-path-pattern`. `path_pattern` is only allowed for GCFv1 triggers.
      */
     operator?: string | null;
     /**
