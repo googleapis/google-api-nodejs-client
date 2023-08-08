385ee33cb84c5916fe0262c017ab35ad80703927
Yoshi Automation <yoshi-automation@google.com>
2023-08-08 21:54:59 +0000

    fix(kgsearch): update the API

--- src/apis/kgsearch/v1.ts
+++ src/apis/kgsearch/v1.ts
@@ -150,62 +150,6 @@ export namespace kgsearch_v1 {
 
     /**
      * Searches Knowledge Graph for entities that match the constraints. A list of matched entities will be returned in response, which will be in JSON-LD format and compatible with http://schema.org
-     * @example
-     * ```js
-     * // Before running the sample:
-     * // - Enable the API at:
-     * //   https://console.developers.google.com/apis/api/kgsearch.googleapis.com
-     * // - Login into gcloud by running:
-     * //   `$ gcloud auth application-default login`
-     * // - Install the npm module by running:
-     * //   `$ npm install googleapis`
-     *
-     * const {google} = require('googleapis');
-     * const kgsearch = google.kgsearch('v1');
-     *
-     * async function main() {
-     *   const auth = new google.auth.GoogleAuth({
-     *     // Scopes can be specified either as an array or as a single, space-delimited string.
-     *     scopes: [],
-     *   });
-     *
-     *   // Acquire an auth client, and bind it to all future calls
-     *   const authClient = await auth.getClient();
-     *   google.options({auth: authClient});
-     *
-     *   // Do the magic
-     *   const res = await kgsearch.entities.search({
-     *     // The list of entity id to be used for search instead of query string. To specify multiple ids in the HTTP request, repeat the parameter in the URL as in ...?ids=A&ids=B
-     *     ids: 'placeholder-value',
-     *     // Enables indenting of json results.
-     *     indent: 'placeholder-value',
-     *     // The list of language codes (defined in ISO 693) to run the query with, e.g. 'en'.
-     *     languages: 'placeholder-value',
-     *     // Limits the number of entities to be returned.
-     *     limit: 'placeholder-value',
-     *     // Enables prefix match against names and aliases of entities
-     *     prefix: 'placeholder-value',
-     *     // The literal query string for search.
-     *     query: 'placeholder-value',
-     *     // Restricts returned entities with these types, e.g. Person (as defined in http://schema.org/Person). If multiple types are specified, returned entities will contain one or more of these types.
-     *     types: 'placeholder-value',
-     *   });
-     *   console.log(res.data);
-     *
-     *   // Example response
-     *   // {
-     *   //   "@context": {},
-     *   //   "@type": {},
-     *   //   "itemListElement": []
-     *   // }
-     * }
-     *
-     * main().catch(e => {
-     *   console.error(e);
-     *   throw e;
-     * });
-     *
-     * ```
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
