fb596dd2cb6f6cb6f7a766dd90888af574ed50f8
Yoshi Automation <yoshi-automation@google.com>
2023-08-08 21:55:11 +0000

    fix(webfonts): update the API

--- src/apis/webfonts/v1.ts
+++ src/apis/webfonts/v1.ts
@@ -208,55 +208,6 @@ export namespace webfonts_v1 {
 
     /**
      * Retrieves the list of fonts currently served by the Google Fonts Developer API.
-     * @example
-     * ```js
-     * // Before running the sample:
-     * // - Enable the API at:
-     * //   https://console.developers.google.com/apis/api/webfonts.googleapis.com
-     * // - Login into gcloud by running:
-     * //   `$ gcloud auth application-default login`
-     * // - Install the npm module by running:
-     * //   `$ npm install googleapis`
-     *
-     * const {google} = require('googleapis');
-     * const webfonts = google.webfonts('v1');
-     *
-     * async function main() {
-     *   const auth = new google.auth.GoogleAuth({
-     *     // Scopes can be specified either as an array or as a single, space-delimited string.
-     *     scopes: [],
-     *   });
-     *
-     *   // Acquire an auth client, and bind it to all future calls
-     *   const authClient = await auth.getClient();
-     *   google.options({auth: authClient});
-     *
-     *   // Do the magic
-     *   const res = await webfonts.webfonts.list({
-     *     // Controls the font urls in `Webfont.files`, by default, static ttf fonts are sent.
-     *     capability: 'placeholder-value',
-     *     // Filters by Webfont.family, using literal match. If not set, returns all families
-     *     family: 'placeholder-value',
-     *     // Enables sorting of the list.
-     *     sort: 'placeholder-value',
-     *     // Filters by Webfont.subset, if subset is found in Webfont.subsets. If not set, returns all families.
-     *     subset: 'placeholder-value',
-     *   });
-     *   console.log(res.data);
-     *
-     *   // Example response
-     *   // {
-     *   //   "items": [],
-     *   //   "kind": "my_kind"
-     *   // }
-     * }
-     *
-     * main().catch(e => {
-     *   console.error(e);
-     *   throw e;
-     * });
-     *
-     * ```
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
