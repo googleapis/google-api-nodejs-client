0ca91b1e049958a17ab9a4fd7e707e03e81a52b0
Yoshi Automation <yoshi-automation@google.com>
2023-08-08 21:55:01 +0000

    fix(mybusinessqanda): update the API

--- src/apis/mybusinessqanda/v1.ts
+++ src/apis/mybusinessqanda/v1.ts
@@ -274,70 +274,6 @@ export namespace mybusinessqanda_v1 {
 
     /**
      * Adds a question for the specified location.
-     * @example
-     * ```js
-     * // Before running the sample:
-     * // - Enable the API at:
-     * //   https://console.developers.google.com/apis/api/mybusinessqanda.googleapis.com
-     * // - Login into gcloud by running:
-     * //   `$ gcloud auth application-default login`
-     * // - Install the npm module by running:
-     * //   `$ npm install googleapis`
-     *
-     * const {google} = require('googleapis');
-     * const mybusinessqanda = google.mybusinessqanda('v1');
-     *
-     * async function main() {
-     *   const auth = new google.auth.GoogleAuth({
-     *     // Scopes can be specified either as an array or as a single, space-delimited string.
-     *     scopes: [],
-     *   });
-     *
-     *   // Acquire an auth client, and bind it to all future calls
-     *   const authClient = await auth.getClient();
-     *   google.options({auth: authClient});
-     *
-     *   // Do the magic
-     *   const res = await mybusinessqanda.locations.questions.create({
-     *     // Required. The name of the location to write a question for.
-     *     parent: 'locations/my-location/questions',
-     *
-     *     // Request body metadata
-     *     requestBody: {
-     *       // request body parameters
-     *       // {
-     *       //   "author": {},
-     *       //   "createTime": "my_createTime",
-     *       //   "name": "my_name",
-     *       //   "text": "my_text",
-     *       //   "topAnswers": [],
-     *       //   "totalAnswerCount": 0,
-     *       //   "updateTime": "my_updateTime",
-     *       //   "upvoteCount": 0
-     *       // }
-     *     },
-     *   });
-     *   console.log(res.data);
-     *
-     *   // Example response
-     *   // {
-     *   //   "author": {},
-     *   //   "createTime": "my_createTime",
-     *   //   "name": "my_name",
-     *   //   "text": "my_text",
-     *   //   "topAnswers": [],
-     *   //   "totalAnswerCount": 0,
-     *   //   "updateTime": "my_updateTime",
-     *   //   "upvoteCount": 0
-     *   // }
-     * }
-     *
-     * main().catch(e => {
-     *   console.error(e);
-     *   throw e;
-     * });
-     *
-     * ```
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
@@ -423,46 +359,6 @@ export namespace mybusinessqanda_v1 {
 
     /**
      * Deletes a specific question written by the current user.
-     * @example
-     * ```js
-     * // Before running the sample:
-     * // - Enable the API at:
-     * //   https://console.developers.google.com/apis/api/mybusinessqanda.googleapis.com
-     * // - Login into gcloud by running:
-     * //   `$ gcloud auth application-default login`
-     * // - Install the npm module by running:
-     * //   `$ npm install googleapis`
-     *
-     * const {google} = require('googleapis');
-     * const mybusinessqanda = google.mybusinessqanda('v1');
-     *
-     * async function main() {
-     *   const auth = new google.auth.GoogleAuth({
-     *     // Scopes can be specified either as an array or as a single, space-delimited string.
-     *     scopes: [],
-     *   });
-     *
-     *   // Acquire an auth client, and bind it to all future calls
-     *   const authClient = await auth.getClient();
-     *   google.options({auth: authClient});
-     *
-     *   // Do the magic
-     *   const res = await mybusinessqanda.locations.questions.delete({
-     *     // Required. The name of the question to delete.
-     *     name: 'locations/my-location/questions/my-question',
-     *   });
-     *   console.log(res.data);
-     *
-     *   // Example response
-     *   // {}
-     * }
-     *
-     * main().catch(e => {
-     *   console.error(e);
-     *   throw e;
-     * });
-     *
-     * ```
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
@@ -548,60 +444,6 @@ export namespace mybusinessqanda_v1 {
 
     /**
      * Returns the paginated list of questions and some of its answers for a specified location. This operation is only valid if the specified location is verified.
-     * @example
-     * ```js
-     * // Before running the sample:
-     * // - Enable the API at:
-     * //   https://console.developers.google.com/apis/api/mybusinessqanda.googleapis.com
-     * // - Login into gcloud by running:
-     * //   `$ gcloud auth application-default login`
-     * // - Install the npm module by running:
-     * //   `$ npm install googleapis`
-     *
-     * const {google} = require('googleapis');
-     * const mybusinessqanda = google.mybusinessqanda('v1');
-     *
-     * async function main() {
-     *   const auth = new google.auth.GoogleAuth({
-     *     // Scopes can be specified either as an array or as a single, space-delimited string.
-     *     scopes: [],
-     *   });
-     *
-     *   // Acquire an auth client, and bind it to all future calls
-     *   const authClient = await auth.getClient();
-     *   google.options({auth: authClient});
-     *
-     *   // Do the magic
-     *   const res = await mybusinessqanda.locations.questions.list({
-     *     // Optional. How many answers to fetch per question. The default and maximum `answers_per_question` values are 10.
-     *     answersPerQuestion: 'placeholder-value',
-     *     // Optional. A filter constraining the questions to return. The only filter currently supported is "ignore_answered=true"
-     *     filter: 'placeholder-value',
-     *     // Optional. The order to return the questions. Valid options include 'update_time desc' and 'upvote_count desc', which will return the questions sorted descendingly by the requested field. The default sort order is 'update_time desc'.
-     *     orderBy: 'placeholder-value',
-     *     // Optional. How many questions to fetch per page. The default and maximum `page_size` values are 10.
-     *     pageSize: 'placeholder-value',
-     *     // Optional. If specified, the next page of questions is retrieved.
-     *     pageToken: 'placeholder-value',
-     *     // Required. The name of the location to fetch questions for.
-     *     parent: 'locations/my-location/questions',
-     *   });
-     *   console.log(res.data);
-     *
-     *   // Example response
-     *   // {
-     *   //   "nextPageToken": "my_nextPageToken",
-     *   //   "questions": [],
-     *   //   "totalSize": 0
-     *   // }
-     * }
-     *
-     * main().catch(e => {
-     *   console.error(e);
-     *   throw e;
-     * });
-     *
-     * ```
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
@@ -692,72 +534,6 @@ export namespace mybusinessqanda_v1 {
 
     /**
      * Updates a specific question written by the current user.
-     * @example
-     * ```js
-     * // Before running the sample:
-     * // - Enable the API at:
-     * //   https://console.developers.google.com/apis/api/mybusinessqanda.googleapis.com
-     * // - Login into gcloud by running:
-     * //   `$ gcloud auth application-default login`
-     * // - Install the npm module by running:
-     * //   `$ npm install googleapis`
-     *
-     * const {google} = require('googleapis');
-     * const mybusinessqanda = google.mybusinessqanda('v1');
-     *
-     * async function main() {
-     *   const auth = new google.auth.GoogleAuth({
-     *     // Scopes can be specified either as an array or as a single, space-delimited string.
-     *     scopes: [],
-     *   });
-     *
-     *   // Acquire an auth client, and bind it to all future calls
-     *   const authClient = await auth.getClient();
-     *   google.options({auth: authClient});
-     *
-     *   // Do the magic
-     *   const res = await mybusinessqanda.locations.questions.patch({
-     *     // Immutable. The unique name for the question. locations/x/questions/x This field will be ignored if set during question creation.
-     *     name: 'locations/my-location/questions/my-question',
-     *     // Required. The specific fields to update. Only question text can be updated.
-     *     updateMask: 'placeholder-value',
-     *
-     *     // Request body metadata
-     *     requestBody: {
-     *       // request body parameters
-     *       // {
-     *       //   "author": {},
-     *       //   "createTime": "my_createTime",
-     *       //   "name": "my_name",
-     *       //   "text": "my_text",
-     *       //   "topAnswers": [],
-     *       //   "totalAnswerCount": 0,
-     *       //   "updateTime": "my_updateTime",
-     *       //   "upvoteCount": 0
-     *       // }
-     *     },
-     *   });
-     *   console.log(res.data);
-     *
-     *   // Example response
-     *   // {
-     *   //   "author": {},
-     *   //   "createTime": "my_createTime",
-     *   //   "name": "my_name",
-     *   //   "text": "my_text",
-     *   //   "topAnswers": [],
-     *   //   "totalAnswerCount": 0,
-     *   //   "updateTime": "my_updateTime",
-     *   //   "upvoteCount": 0
-     *   // }
-     * }
-     *
-     * main().catch(e => {
-     *   console.error(e);
-     *   throw e;
-     * });
-     *
-     * ```
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
@@ -913,46 +689,6 @@ export namespace mybusinessqanda_v1 {
 
     /**
      * Deletes the answer written by the current user to a question.
-     * @example
-     * ```js
-     * // Before running the sample:
-     * // - Enable the API at:
-     * //   https://console.developers.google.com/apis/api/mybusinessqanda.googleapis.com
-     * // - Login into gcloud by running:
-     * //   `$ gcloud auth application-default login`
-     * // - Install the npm module by running:
-     * //   `$ npm install googleapis`
-     *
-     * const {google} = require('googleapis');
-     * const mybusinessqanda = google.mybusinessqanda('v1');
-     *
-     * async function main() {
-     *   const auth = new google.auth.GoogleAuth({
-     *     // Scopes can be specified either as an array or as a single, space-delimited string.
-     *     scopes: [],
-     *   });
-     *
-     *   // Acquire an auth client, and bind it to all future calls
-     *   const authClient = await auth.getClient();
-     *   google.options({auth: authClient});
-     *
-     *   // Do the magic
-     *   const res = await mybusinessqanda.locations.questions.answers.delete({
-     *     // Required. The name of the question to delete an answer for.
-     *     name: 'locations/my-location/questions/my-question',
-     *   });
-     *   console.log(res.data);
-     *
-     *   // Example response
-     *   // {}
-     * }
-     *
-     * main().catch(e => {
-     *   console.error(e);
-     *   throw e;
-     * });
-     *
-     * ```
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
@@ -1041,56 +777,6 @@ export namespace mybusinessqanda_v1 {
 
     /**
      * Returns the paginated list of answers for a specified question.
-     * @example
-     * ```js
-     * // Before running the sample:
-     * // - Enable the API at:
-     * //   https://console.developers.google.com/apis/api/mybusinessqanda.googleapis.com
-     * // - Login into gcloud by running:
-     * //   `$ gcloud auth application-default login`
-     * // - Install the npm module by running:
-     * //   `$ npm install googleapis`
-     *
-     * const {google} = require('googleapis');
-     * const mybusinessqanda = google.mybusinessqanda('v1');
-     *
-     * async function main() {
-     *   const auth = new google.auth.GoogleAuth({
-     *     // Scopes can be specified either as an array or as a single, space-delimited string.
-     *     scopes: [],
-     *   });
-     *
-     *   // Acquire an auth client, and bind it to all future calls
-     *   const authClient = await auth.getClient();
-     *   google.options({auth: authClient});
-     *
-     *   // Do the magic
-     *   const res = await mybusinessqanda.locations.questions.answers.list({
-     *     // Optional. The order to return the answers. Valid options include 'update_time desc' and 'upvote_count desc', which will return the answers sorted descendingly by the requested field. The default sort order is 'update_time desc'.
-     *     orderBy: 'placeholder-value',
-     *     // Optional. How many answers to fetch per page. The default and maximum `page_size` values are 10.
-     *     pageSize: 'placeholder-value',
-     *     // Optional. If specified, the next page of answers is retrieved.
-     *     pageToken: 'placeholder-value',
-     *     // Required. The name of the question to fetch answers for.
-     *     parent: 'locations/my-location/questions/my-question',
-     *   });
-     *   console.log(res.data);
-     *
-     *   // Example response
-     *   // {
-     *   //   "answers": [],
-     *   //   "nextPageToken": "my_nextPageToken",
-     *   //   "totalSize": 0
-     *   // }
-     * }
-     *
-     * main().catch(e => {
-     *   console.error(e);
-     *   throw e;
-     * });
-     *
-     * ```
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
@@ -1182,61 +868,6 @@ export namespace mybusinessqanda_v1 {
 
     /**
      * Creates an answer or updates the existing answer written by the user for the specified question. A user can only create one answer per question.
-     * @example
-     * ```js
-     * // Before running the sample:
-     * // - Enable the API at:
-     * //   https://console.developers.google.com/apis/api/mybusinessqanda.googleapis.com
-     * // - Login into gcloud by running:
-     * //   `$ gcloud auth application-default login`
-     * // - Install the npm module by running:
-     * //   `$ npm install googleapis`
-     *
-     * const {google} = require('googleapis');
-     * const mybusinessqanda = google.mybusinessqanda('v1');
-     *
-     * async function main() {
-     *   const auth = new google.auth.GoogleAuth({
-     *     // Scopes can be specified either as an array or as a single, space-delimited string.
-     *     scopes: [],
-     *   });
-     *
-     *   // Acquire an auth client, and bind it to all future calls
-     *   const authClient = await auth.getClient();
-     *   google.options({auth: authClient});
-     *
-     *   // Do the magic
-     *   const res = await mybusinessqanda.locations.questions.answers.upsert({
-     *     // Required. The name of the question to write an answer for.
-     *     parent: 'locations/my-location/questions/my-question',
-     *
-     *     // Request body metadata
-     *     requestBody: {
-     *       // request body parameters
-     *       // {
-     *       //   "answer": {}
-     *       // }
-     *     },
-     *   });
-     *   console.log(res.data);
-     *
-     *   // Example response
-     *   // {
-     *   //   "author": {},
-     *   //   "createTime": "my_createTime",
-     *   //   "name": "my_name",
-     *   //   "text": "my_text",
-     *   //   "updateTime": "my_updateTime",
-     *   //   "upvoteCount": 0
-     *   // }
-     * }
-     *
-     * main().catch(e => {
-     *   console.error(e);
-     *   throw e;
-     * });
-     *
-     * ```
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
