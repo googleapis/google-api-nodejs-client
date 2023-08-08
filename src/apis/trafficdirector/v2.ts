48a067ccd7a1fca2a817d18d3c2a5659345a743f
Yoshi Automation <yoshi-automation@google.com>
2023-08-08 21:55:10 +0000

    feat(trafficdirector): update the API
    #### trafficdirector:v2
    
    The following keys were added:
    - schemas.GoogleRE2.properties.maxProgramSize.deprecated
    - schemas.Node.properties.buildVersion.deprecated
    - schemas.StringMatcher.properties.regex.deprecated

--- src/apis/trafficdirector/v2.ts
+++ src/apis/trafficdirector/v2.ts
@@ -689,53 +689,6 @@ export namespace trafficdirector_v2 {
     }
 
     /**
-     * @example
-     * ```js
-     * // Before running the sample:
-     * // - Enable the API at:
-     * //   https://console.developers.google.com/apis/api/trafficdirector.googleapis.com
-     * // - Login into gcloud by running:
-     * //   `$ gcloud auth application-default login`
-     * // - Install the npm module by running:
-     * //   `$ npm install googleapis`
-     *
-     * const {google} = require('googleapis');
-     * const trafficdirector = google.trafficdirector('v2');
-     *
-     * async function main() {
-     *   const auth = new google.auth.GoogleAuth({
-     *     // Scopes can be specified either as an array or as a single, space-delimited string.
-     *     scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-     *   });
-     *
-     *   // Acquire an auth client, and bind it to all future calls
-     *   const authClient = await auth.getClient();
-     *   google.options({auth: authClient});
-     *
-     *   // Do the magic
-     *   const res = await trafficdirector.discovery.client_status({
-     *     // Request body metadata
-     *     requestBody: {
-     *       // request body parameters
-     *       // {
-     *       //   "nodeMatchers": []
-     *       // }
-     *     },
-     *   });
-     *   console.log(res.data);
-     *
-     *   // Example response
-     *   // {
-     *   //   "config": []
-     *   // }
-     * }
-     *
-     * main().catch(e => {
-     *   console.error(e);
-     *   throw e;
-     * });
-     *
-     * ```
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
