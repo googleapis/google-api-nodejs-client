42b1cf4af01b9c3b8697318f15d81337d7698a6b
Yoshi Automation <yoshi-automation@google.com>
2023-08-08 01:38:50 +0000

    fix(iam): update the API
    #### iam:v1
    
    The following keys were changed:
    - schemas.ServiceAccount.description

--- src/apis/iam/v1.ts
+++ src/apis/iam/v1.ts
@@ -838,7 +838,7 @@ export namespace iam_v1 {
     idpMetadataXml?: string | null;
   }
   /**
-   * An IAM service account. A service account is an account for an application or a virtual machine (VM) instance, not a person. You can use a service account to call Google APIs. To learn more, read the [overview of service accounts](https://cloud.google.com/iam/help/service-accounts/overview). When you create a service account, you specify the project ID that owns the service account, as well as a name that must be unique within the project. IAM uses these values to create an email address that identifies the service //
+   * An IAM service account. A service account is an account for an application or a virtual machine (VM) instance, not a person. You can use a service account to call Google APIs. To learn more, read the [overview of service accounts](https://cloud.google.com/iam/help/service-accounts/overview). When you create a service account, you specify the project ID that owns the service account, as well as a name that must be unique within the project. IAM uses these values to create an email address that identifies the service account. //
    */
   export interface Schema$ServiceAccount {
     /**
