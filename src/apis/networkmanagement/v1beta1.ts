6a0be5239af85d3de3a2baf2b989c38333a08cdf
Yoshi Automation <yoshi-automation@google.com>
2023-08-15 01:30:02 +0000

    feat(networkmanagement): update the API
    #### networkmanagement:v1beta1
    
    The following keys were added:
    - schemas.RouteInfo.properties.nccHubUri.description
    - schemas.RouteInfo.properties.nccHubUri.type
    - schemas.RouteInfo.properties.nccSpokeUri.description
    - schemas.RouteInfo.properties.nccSpokeUri.type
    - schemas.RouteInfo.properties.routeScope.description
    - schemas.RouteInfo.properties.routeScope.enum
    - schemas.RouteInfo.properties.routeScope.enumDescriptions
    - schemas.RouteInfo.properties.routeScope.type
    
    The following keys were changed:
    - schemas.AbortInfo.properties.cause.enum
    - schemas.AbortInfo.properties.cause.enumDescriptions
    - schemas.ForwardInfo.properties.target.enum
    - schemas.ForwardInfo.properties.target.enumDescriptions
    - schemas.Operation.properties.response.description
    - schemas.Policy.description
    - schemas.RouteInfo.properties.displayName.description
    - schemas.RouteInfo.properties.networkUri.description
    - schemas.RouteInfo.properties.nextHopType.enum
    - schemas.RouteInfo.properties.nextHopType.enumDescriptions
    - schemas.RouteInfo.properties.uri.description
    
    #### networkmanagement:v1
    
    The following keys were added:
    - schemas.RouteInfo.properties.nccHubUri.description
    - schemas.RouteInfo.properties.nccHubUri.type
    - schemas.RouteInfo.properties.nccSpokeUri.description
    - schemas.RouteInfo.properties.nccSpokeUri.type
    - schemas.RouteInfo.properties.routeScope.description
    - schemas.RouteInfo.properties.routeScope.enum
    - schemas.RouteInfo.properties.routeScope.enumDescriptions
    - schemas.RouteInfo.properties.routeScope.type
    
    The following keys were changed:
    - schemas.AbortInfo.properties.cause.enum
    - schemas.AbortInfo.properties.cause.enumDescriptions
    - schemas.ForwardInfo.properties.target.enum
    - schemas.ForwardInfo.properties.target.enumDescriptions
    - schemas.Operation.properties.response.description
    - schemas.Policy.description
    - schemas.RouteInfo.properties.displayName.description
    - schemas.RouteInfo.properties.networkUri.description
    - schemas.RouteInfo.properties.nextHopType.enum
    - schemas.RouteInfo.properties.nextHopType.enumDescriptions
    - schemas.RouteInfo.properties.uri.description

--- src/apis/networkmanagement/v1beta1.ts
+++ src/apis/networkmanagement/v1beta1.ts
@@ -866,7 +866,7 @@ export namespace networkmanagement_v1beta1 {
      */
     name?: string | null;
     /**
-     * The normal response of the operation in case of success. If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`. If the original method is standard `Get`/`Create`/`Update`, the response should be the resource. For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name. For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
+     * The normal, successful response of the operation. If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`. If the original method is standard `Get`/`Create`/`Update`, the response should be the resource. For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name. For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
      */
     response?: {[key: string]: any} | null;
   }
@@ -904,7 +904,7 @@ export namespace networkmanagement_v1beta1 {
     verb?: string | null;
   }
   /**
-   * An Identity and Access Management (IAM) policy, which specifies access controls for Google Cloud resources. A `Policy` is a collection of `bindings`. A `binding` binds one or more `members`, or principals, to a single `role`. Principals can be user accounts, service accounts, Google groups, and domains (such as G Suite). A `role` is a named list of permissions; each `role` can be an IAM predefined role or a user-created custom role. For some types of Google Cloud resources, a `binding` can also specify a `condition`, which is a logical expression that allows access to a resource only if the expression evaluates to `true`. A condition can add constraints based on attributes of the request, the resource, or both. To learn which resources support conditions in their IAM policies, see the [IAM documentation](https://cloud.google.com/iam/help/conditions/resource-policies). **JSON example:** { "bindings": [ { "role": "roles/resourcemanager.organizationAdmin", "members": [ "user:mike@example.com", "group:admins@example.com", "domain:google.com", "serviceAccount:my-project-id@appspot.gserviceaccount.com" ] \}, { "role": "roles/resourcemanager.organizationViewer", "members": [ "user:eve@example.com" ], "condition": { "title": "expirable access", "description": "Does not grant access after Sep 2020", "expression": "request.time < timestamp('2020-10-01T00:00:00.000Z')", \} \} ], "etag": "BwWWja0YfJA=", "version": 3 \} **YAML example:** bindings: - members: - user:mike@example.com - group:admins@example.com - domain:google.com - serviceAccount:my-project-id@appspot.gserviceaccount.com role: roles/resourcemanager.organizationAdmin - members: - user:eve@example.com role: roles/resourcemanager.organizationViewer condition: title: expirable access description: Does not grant access after Sep 2020 expression: request.time < timestamp('2020-10-01T00:00:00.000Z') etag: BwWWja0YfJA= version: 3 For a description of IAM and its features, see the [IAM documentation](https://cloud.google.com/iam/docs/).
+   * An Identity and Access Management (IAM) policy, which specifies access controls for Google Cloud resources. A `Policy` is a collection of `bindings`. A `binding` binds one or more `members`, or principals, to a single `role`. Principals can be user accounts, service accounts, Google groups, and domains (such as G Suite). A `role` is a named list of permissions; each `role` can be an IAM predefined role or a user-created custom role. For some types of Google Cloud resources, a `binding` can also specify a `condition`, which is a logical expression that allows access to a resource only if the expression evaluates to `true`. A condition can add constraints based on attributes of the request, the resource, or both. To learn which resources support conditions in their IAM policies, see the [IAM documentation](https://cloud.google.com/iam/help/conditions/resource-policies). **JSON example:** ``` { "bindings": [ { "role": "roles/resourcemanager.organizationAdmin", "members": [ "user:mike@example.com", "group:admins@example.com", "domain:google.com", "serviceAccount:my-project-id@appspot.gserviceaccount.com" ] \}, { "role": "roles/resourcemanager.organizationViewer", "members": [ "user:eve@example.com" ], "condition": { "title": "expirable access", "description": "Does not grant access after Sep 2020", "expression": "request.time < timestamp('2020-10-01T00:00:00.000Z')", \} \} ], "etag": "BwWWja0YfJA=", "version": 3 \} ``` **YAML example:** ``` bindings: - members: - user:mike@example.com - group:admins@example.com - domain:google.com - serviceAccount:my-project-id@appspot.gserviceaccount.com role: roles/resourcemanager.organizationAdmin - members: - user:eve@example.com role: roles/resourcemanager.organizationViewer condition: title: expirable access description: Does not grant access after Sep 2020 expression: request.time < timestamp('2020-10-01T00:00:00.000Z') etag: BwWWja0YfJA= version: 3 ``` For a description of IAM and its features, see the [IAM documentation](https://cloud.google.com/iam/docs/).
    */
   export interface Schema$Policy {
     /**
@@ -1003,7 +1003,7 @@ export namespace networkmanagement_v1beta1 {
      */
     destPortRanges?: string[] | null;
     /**
-     * Name of a Compute Engine route.
+     * Name of a route.
      */
     displayName?: string | null;
     /**
@@ -1011,7 +1011,15 @@ export namespace networkmanagement_v1beta1 {
      */
     instanceTags?: string[] | null;
     /**
-     * URI of a Compute Engine network.
+     * URI of a NCC Hub. NCC_HUB routes only.
+     */
+    nccHubUri?: string | null;
+    /**
+     * URI of a NCC Spoke. NCC_HUB routes only.
+     */
+    nccSpokeUri?: string | null;
+    /**
+     * URI of a Compute Engine network. NETWORK routes only.
      */
     networkUri?: string | null;
     /**
@@ -1031,6 +1039,10 @@ export namespace networkmanagement_v1beta1 {
      */
     protocols?: string[] | null;
     /**
+     * Indicates where route is applicable.
+     */
+    routeScope?: string | null;
+    /**
      * Type of route.
      */
     routeType?: string | null;
@@ -1043,7 +1055,7 @@ export namespace networkmanagement_v1beta1 {
      */
     srcPortRanges?: string[] | null;
     /**
-     * URI of a Compute Engine route. Dynamic route from cloud router does not have a URI. Advertised route from Google Cloud VPC to on-premises network also does not have a URI.
+     * URI of a route. Dynamic, peering static and peering dynamic routes do not have an URI. Advertised route from Google Cloud VPC to on-premises network also does not have an URI.
      */
     uri?: string | null;
   }
