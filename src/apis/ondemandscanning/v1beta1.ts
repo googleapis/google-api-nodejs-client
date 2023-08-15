5cdde79054a7b8f2274ab30257a159c78c4a10dc
Yoshi Automation <yoshi-automation@google.com>
2023-08-15 01:30:02 +0000

    fix(ondemandscanning): update the API
    #### ondemandscanning:v1beta1
    
    The following keys were changed:
    - schemas.Operation.properties.response.description
    
    #### ondemandscanning:v1
    
    The following keys were changed:
    - schemas.Operation.properties.response.description

--- src/apis/ondemandscanning/v1beta1.ts
+++ src/apis/ondemandscanning/v1beta1.ts
@@ -1003,7 +1003,7 @@ export namespace ondemandscanning_v1beta1 {
      */
     name?: string | null;
     /**
-     * The normal response of the operation in case of success. If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`. If the original method is standard `Get`/`Create`/`Update`, the response should be the resource. For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name. For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
+     * The normal, successful response of the operation. If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`. If the original method is standard `Get`/`Create`/`Update`, the response should be the resource. For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name. For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
      */
     response?: {[key: string]: any} | null;
   }
