ce98115988ee6f9fb95ffc28eae8ac73a72a860c
Yoshi Automation <yoshi-automation@google.com>
2023-08-15 01:29:58 +0000

    feat(composer): update the API
    #### composer:v1beta1
    
    The following keys were added:
    - schemas.Environment.properties.storageConfig.$ref
    - schemas.Environment.properties.storageConfig.description
    - schemas.StorageConfig.description
    - schemas.StorageConfig.id
    - schemas.StorageConfig.type
    
    The following keys were changed:
    - schemas.Operation.properties.response.description
    
    #### composer:v1
    
    The following keys were changed:
    - schemas.Operation.properties.response.description

--- src/apis/composer/v1beta1.ts
+++ src/apis/composer/v1beta1.ts
@@ -265,6 +265,10 @@ export namespace composer_v1beta1 {
      */
     state?: string | null;
     /**
+     * Optional. Storage configuration for this environment.
+     */
+    storageConfig?: Schema$StorageConfig;
+    /**
      * Output only. The time at which this environment was last modified.
      */
     updateTime?: string | null;
@@ -662,7 +666,7 @@ export namespace composer_v1beta1 {
      */
     name?: string | null;
     /**
-     * The normal response of the operation in case of success. If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`. If the original method is standard `Get`/`Create`/`Update`, the response should be the resource. For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name. For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
+     * The normal, successful response of the operation. If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`. If the original method is standard `Get`/`Create`/`Update`, the response should be the resource. For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name. For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
      */
     response?: {[key: string]: any} | null;
   }
@@ -952,6 +956,10 @@ export namespace composer_v1beta1 {
      */
     output?: string[] | null;
   }
+  /**
+   * The configuration for data storage in the environment.
+   */
+  export interface Schema$StorageConfig {}
   /**
    * Configuration for resources used by Airflow triggerers.
    */
