43c76669ab171cb91a44c992b121a3775e2616da
Yoshi Automation <yoshi-automation@google.com>
2023-08-15 01:29:59 +0000

    fix(datastream): update the API
    #### datastream:v1alpha1
    
    The following keys were changed:
    - schemas.Operation.properties.response.description
    
    #### datastream:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.streams.methods.run.description
    - schemas.Operation.properties.response.description

--- src/apis/datastream/v1.ts
+++ src/apis/datastream/v1.ts
@@ -826,7 +826,7 @@ export namespace datastream_v1 {
      */
     name?: string | null;
     /**
-     * The normal response of the operation in case of success. If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`. If the original method is standard `Get`/`Create`/`Update`, the response should be the resource. For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name. For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
+     * The normal, successful response of the operation. If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`. If the original method is standard `Get`/`Create`/`Update`, the response should be the resource. For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name. For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
      */
     response?: {[key: string]: any} | null;
   }
@@ -4194,7 +4194,7 @@ export namespace datastream_v1 {
     }
 
     /**
-     * Use this method to start, resume or recover a stream with a non default CDC strategy.
+     * Use this method to start, resume or recover a stream with a non default CDC strategy. NOTE: This feature is currently experimental.
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
