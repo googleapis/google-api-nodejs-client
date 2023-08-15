ffaf1efccd64be3348f2449d2ecdc03e574a1b71
Yoshi Automation <yoshi-automation@google.com>
2023-08-15 01:29:59 +0000

    feat(dataproc): update the API
    #### dataproc:v1
    
    The following keys were added:
    - resources.projects.resources.regions.resources.clusters.methods.delete.parameters.gracefulTerminationTimeout.description
    - resources.projects.resources.regions.resources.clusters.methods.delete.parameters.gracefulTerminationTimeout.format
    - resources.projects.resources.regions.resources.clusters.methods.delete.parameters.gracefulTerminationTimeout.location
    - resources.projects.resources.regions.resources.clusters.methods.delete.parameters.gracefulTerminationTimeout.type
    - schemas.InstanceGroupConfig.properties.minNumInstances.description
    - schemas.InstanceGroupConfig.properties.minNumInstances.format
    - schemas.InstanceGroupConfig.properties.minNumInstances.type
    
    The following keys were changed:
    - schemas.Operation.properties.response.description

--- src/apis/dataproc/v1.ts
+++ src/apis/dataproc/v1.ts
@@ -1166,6 +1166,10 @@ export namespace dataproc_v1 {
      */
     minCpuPlatform?: string | null;
     /**
+     * Optional. The minimum number of instances to create. If min_num_instances is set, min_num_instances is used for a criteria to decide the cluster. Cluster creation will be failed by being an error state if the total number of instances created is less than the min_num_instances. For example, given that num_instances = 5 and min_num_instances = 3, * if 4 instances are created and then registered successfully but one instance is failed, the failed VM will be deleted and the cluster will be resized to 4 instances in running state. * if 2 instances are created successfully and 3 instances are failed, the cluster will be in an error state and does not delete failed VMs for debugging. * if 2 instance are created and then registered successfully but 3 instances are failed to initialize, the cluster will be in an error state and does not delete failed VMs for debugging. NB: This can only be set for primary workers now.
+     */
+    minNumInstances?: number | null;
+    /**
      * Optional. The number of VM instances in the instance group. For HA cluster master_config groups, must be set to 3. For standard cluster master_config groups, must be set to 1.
      */
     numInstances?: number | null;
@@ -1789,7 +1793,7 @@ export namespace dataproc_v1 {
      */
     name?: string | null;
     /**
-     * The normal response of the operation in case of success. If the original method returns no data on success, such as Delete, the response is google.protobuf.Empty. If the original method is standard Get/Create/Update, the response should be the resource. For other methods, the response should have the type XxxResponse, where Xxx is the original method name. For example, if the original method name is TakeSnapshot(), the inferred response type is TakeSnapshotResponse.
+     * The normal, successful response of the operation. If the original method returns no data on success, such as Delete, the response is google.protobuf.Empty. If the original method is standard Get/Create/Update, the response should be the resource. For other methods, the response should have the type XxxResponse, where Xxx is the original method name. For example, if the original method name is TakeSnapshot(), the inferred response type is TakeSnapshotResponse.
      */
     response?: {[key: string]: any} | null;
   }
@@ -7501,6 +7505,10 @@ export namespace dataproc_v1 {
      * Optional. Specifying the cluster_uuid means the RPC should fail (with error NOT_FOUND) if cluster with specified UUID does not exist.
      */
     clusterUuid?: string;
+    /**
+     * Optional. The graceful termination timeout for the deletion of the cluster. Indicate the time the request will wait to complete the running jobs on the cluster before its forceful deletion. Default value is 0 indicating that the user has not enabled the graceful termination. Value can be between 60 second and 6 Hours, in case the graceful termination is enabled. (There is no separate flag to check the enabling or disabling of graceful termination, it can be checked by the values in the field).
+     */
+    gracefulTerminationTimeout?: string;
     /**
      * Required. The ID of the Google Cloud Platform project that the cluster belongs to.
      */
