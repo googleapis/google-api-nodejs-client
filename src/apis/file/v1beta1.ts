16d1c381e4003d3d13aef706556ca2a21f1b711e
Yoshi Automation <yoshi-automation@google.com>
2023-08-15 01:30:00 +0000

    fix(file): update the API
    #### file:v1beta1
    
    The following keys were changed:
    - schemas.Operation.properties.response.description
    - schemas.UpdatePolicy.properties.denyMaintenancePeriods.description
    
    #### file:v1
    
    The following keys were changed:
    - schemas.Operation.properties.response.description
    - schemas.UpdatePolicy.properties.denyMaintenancePeriods.description

--- src/apis/file/v1beta1.ts
+++ src/apis/file/v1beta1.ts
@@ -805,7 +805,7 @@ export namespace file_v1beta1 {
      */
     name?: string | null;
     /**
-     * The normal response of the operation in case of success. If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`. If the original method is standard `Get`/`Create`/`Update`, the response should be the resource. For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name. For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
+     * The normal, successful response of the operation. If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`. If the original method is standard `Get`/`Create`/`Update`, the response should be the resource. For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name. For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
      */
     response?: {[key: string]: any} | null;
   }
@@ -1002,7 +1002,7 @@ export namespace file_v1beta1 {
      */
     channel?: string | null;
     /**
-     * Deny Maintenance Period that is applied to resource to indicate when maintenance is forbidden. User can specify zero or more non-overlapping deny periods. Maximum number of deny_maintenance_periods expected is one.
+     * Deny Maintenance Period that is applied to resource to indicate when maintenance is forbidden. The protocol supports zero-to-many such periods, but the current SLM Rollout implementation only supports zero-to-one.
      */
     denyMaintenancePeriods?: Schema$DenyMaintenancePeriod[];
     /**
