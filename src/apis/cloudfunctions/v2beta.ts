1198c5ee84c9297fc4e644532374eaf72db999bb
Yoshi Automation <yoshi-automation@google.com>
2023-09-22 01:32:52 +0000

    feat(cloudfunctions): update the API
    #### cloudfunctions:v2alpha
    
    The following keys were added:
    - schemas.Date.description
    - schemas.Date.id
    - schemas.Date.properties.day.description
    - schemas.Date.properties.day.format
    - schemas.Date.properties.day.type
    - schemas.Date.properties.month.description
    - schemas.Date.properties.month.format
    - schemas.Date.properties.month.type
    - schemas.Date.properties.year.description
    - schemas.Date.properties.year.format
    - schemas.Date.properties.year.type
    - schemas.Date.type
    - schemas.Runtime.properties.decommissionDate.$ref
    - schemas.Runtime.properties.decommissionDate.description
    - schemas.Runtime.properties.deprecationDate.$ref
    - schemas.Runtime.properties.deprecationDate.description
    
    #### cloudfunctions:v2beta
    
    The following keys were added:
    - schemas.Date.description
    - schemas.Date.id
    - schemas.Date.properties.day.description
    - schemas.Date.properties.day.format
    - schemas.Date.properties.day.type
    - schemas.Date.properties.month.description
    - schemas.Date.properties.month.format
    - schemas.Date.properties.month.type
    - schemas.Date.properties.year.description
    - schemas.Date.properties.year.format
    - schemas.Date.properties.year.type
    - schemas.Date.type
    - schemas.Runtime.properties.decommissionDate.$ref
    - schemas.Runtime.properties.decommissionDate.description
    - schemas.Runtime.properties.deprecationDate.$ref
    - schemas.Runtime.properties.deprecationDate.description
    
    #### cloudfunctions:v2
    
    The following keys were added:
    - schemas.Date.description
    - schemas.Date.id
    - schemas.Date.properties.day.description
    - schemas.Date.properties.day.format
    - schemas.Date.properties.day.type
    - schemas.Date.properties.month.description
    - schemas.Date.properties.month.format
    - schemas.Date.properties.month.type
    - schemas.Date.properties.year.description
    - schemas.Date.properties.year.format
    - schemas.Date.properties.year.type
    - schemas.Date.type
    - schemas.Runtime.properties.decommissionDate.$ref
    - schemas.Runtime.properties.decommissionDate.description
    - schemas.Runtime.properties.deprecationDate.$ref
    - schemas.Runtime.properties.deprecationDate.description

--- src/apis/cloudfunctions/v2beta.ts
+++ src/apis/cloudfunctions/v2beta.ts
@@ -213,6 +213,23 @@ export namespace cloudfunctions_v2beta {
     workerPool?: string | null;
   }
   /**
+   * Represents a whole or partial calendar date, such as a birthday. The time of day and time zone are either specified elsewhere or are insignificant. The date is relative to the Gregorian Calendar. This can represent one of the following: * A full date, with non-zero year, month, and day values. * A month and day, with a zero year (for example, an anniversary). * A year on its own, with a zero month and a zero day. * A year and month, with a zero day (for example, a credit card expiration date). Related types: * google.type.TimeOfDay * google.type.DateTime * google.protobuf.Timestamp
+   */
+  export interface Schema$Date {
+    /**
+     * Day of a month. Must be from 1 to 31 and valid for the year and month, or 0 to specify a year by itself or a year and month where the day isn't significant.
+     */
+    day?: number | null;
+    /**
+     * Month of a year. Must be from 1 to 12, or 0 to specify a year without a month and day.
+     */
+    month?: number | null;
+    /**
+     * Year of the date. Must be from 1 to 9999, or 0 to specify a date without a year.
+     */
+    year?: number | null;
+  }
+  /**
    * Filters events based on exact matches on the CloudEvents attributes.
    */
   export interface Schema$EventFilter {
@@ -872,6 +889,14 @@ export namespace cloudfunctions_v2beta {
    * Describes a runtime and any special information (e.g., deprecation status) related to it.
    */
   export interface Schema$Runtime {
+    /**
+     * Decommission date for the runtime.
+     */
+    decommissionDate?: Schema$Date;
+    /**
+     * Deprecation date for the runtime.
+     */
+    deprecationDate?: Schema$Date;
     /**
      * The user facing name, eg 'Go 1.13', 'Node.js 12', etc.
      */
