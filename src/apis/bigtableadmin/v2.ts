3084883070788c7fab74ac454f286fb525ac3c35
Yoshi Automation <yoshi-automation@google.com>
2023-09-26 01:31:25 +0000

    fix(bigtableadmin): update the API
    #### bigtableadmin:v2
    
    The following keys were changed:
    - resources.projects.resources.instances.resources.clusters.resources.backups.methods.getIamPolicy.description
    - resources.projects.resources.instances.resources.clusters.resources.backups.methods.setIamPolicy.description
    - resources.projects.resources.instances.resources.clusters.resources.backups.methods.testIamPermissions.description
    - resources.projects.resources.instances.resources.tables.methods.getIamPolicy.description
    - resources.projects.resources.instances.resources.tables.methods.setIamPolicy.description
    - resources.projects.resources.instances.resources.tables.methods.testIamPermissions.description
    - schemas.Operation.properties.response.description
    - schemas.Policy.description

--- src/apis/bigtableadmin/v2.ts
+++ src/apis/bigtableadmin/v2.ts
@@ -955,7 +955,7 @@ export namespace bigtableadmin_v2 {
      */
     name?: string | null;
     /**
-     * The normal response of the operation in case of success. If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`. If the original method is standard `Get`/`Create`/`Update`, the response should be the resource. For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name. For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
+     * The normal, successful response of the operation. If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`. If the original method is standard `Get`/`Create`/`Update`, the response should be the resource. For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name. For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
      */
     response?: {[key: string]: any} | null;
   }
@@ -1033,7 +1033,7 @@ export namespace bigtableadmin_v2 {
     updateMask?: string | null;
   }
   /**
-   * An Identity and Access Management (IAM) policy, which specifies access controls for Google Cloud resources. A `Policy` is a collection of `bindings`. A `binding` binds one or more `members`, or principals, to a single `role`. Principals can be user accounts, service accounts, Google groups, and domains (such as G Suite). A `role` is a named list of permissions; each `role` can be an IAM predefined role or a user-created custom role. For some types of Google Cloud resources, a `binding` can also specify a `condition`, which is a logical expression that allows access to a resource only if the expression evaluates to `true`. A condition can add constraints based on attributes of the request, the resource, or both. To learn which resources support conditions in their IAM policies, see the [IAM documentation](https://cloud.google.com/iam/help/conditions/resource-policies). **JSON example:** { "bindings": [ { "role": "roles/resourcemanager.organizationAdmin", "members": [ "user:mike@example.com", "group:admins@example.com", "domain:google.com", "serviceAccount:my-project-id@appspot.gserviceaccount.com" ] \}, { "role": "roles/resourcemanager.organizationViewer", "members": [ "user:eve@example.com" ], "condition": { "title": "expirable access", "description": "Does not grant access after Sep 2020", "expression": "request.time < timestamp('2020-10-01T00:00:00.000Z')", \} \} ], "etag": "BwWWja0YfJA=", "version": 3 \} **YAML example:** bindings: - members: - user:mike@example.com - group:admins@example.com - domain:google.com - serviceAccount:my-project-id@appspot.gserviceaccount.com role: roles/resourcemanager.organizationAdmin - members: - user:eve@example.com role: roles/resourcemanager.organizationViewer condition: title: expirable access description: Does not grant access after Sep 2020 expression: request.time < timestamp('2020-10-01T00:00:00.000Z') etag: BwWWja0YfJA= version: 3 For a description of IAM and its features, see the [IAM documentation](https://cloud.google.com/iam/docs/).
+   * An Identity and Access Management (IAM) policy, which specifies access controls for Google Cloud resources. A `Policy` is a collection of `bindings`. A `binding` binds one or more `members`, or principals, to a single `role`. Principals can be user accounts, service accounts, Google groups, and domains (such as G Suite). A `role` is a named list of permissions; each `role` can be an IAM predefined role or a user-created custom role. For some types of Google Cloud resources, a `binding` can also specify a `condition`, which is a logical expression that allows access to a resource only if the expression evaluates to `true`. A condition can add constraints based on attributes of the request, the resource, or both. To learn which resources support conditions in their IAM policies, see the [IAM documentation](https://cloud.google.com/iam/help/conditions/resource-policies). **JSON example:** ``` { "bindings": [ { "role": "roles/resourcemanager.organizationAdmin", "members": [ "user:mike@example.com", "group:admins@example.com", "domain:google.com", "serviceAccount:my-project-id@appspot.gserviceaccount.com" ] \}, { "role": "roles/resourcemanager.organizationViewer", "members": [ "user:eve@example.com" ], "condition": { "title": "expirable access", "description": "Does not grant access after Sep 2020", "expression": "request.time < timestamp('2020-10-01T00:00:00.000Z')", \} \} ], "etag": "BwWWja0YfJA=", "version": 3 \} ``` **YAML example:** ``` bindings: - members: - user:mike@example.com - group:admins@example.com - domain:google.com - serviceAccount:my-project-id@appspot.gserviceaccount.com role: roles/resourcemanager.organizationAdmin - members: - user:eve@example.com role: roles/resourcemanager.organizationViewer condition: title: expirable access description: Does not grant access after Sep 2020 expression: request.time < timestamp('2020-10-01T00:00:00.000Z') etag: BwWWja0YfJA= version: 3 ``` For a description of IAM and its features, see the [IAM documentation](https://cloud.google.com/iam/docs/).
    */
   export interface Schema$Policy {
     /**
@@ -4147,7 +4147,7 @@ export namespace bigtableadmin_v2 {
     }
 
     /**
-     * Gets the access control policy for a Table resource. Returns an empty policy if the resource exists but does not have a policy set.
+     * Gets the access control policy for a Table or Backup resource. Returns an empty policy if the resource exists but does not have a policy set.
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
@@ -4413,7 +4413,7 @@ export namespace bigtableadmin_v2 {
     }
 
     /**
-     * Sets the access control policy on a Table resource. Replaces any existing policy.
+     * Sets the access control policy on a Table or Backup resource. Replaces any existing policy.
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
@@ -4502,7 +4502,7 @@ export namespace bigtableadmin_v2 {
     }
 
     /**
-     * Returns permissions that the caller has on the specified table resource.
+     * Returns permissions that the caller has on the specified Table or Backup resource.
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
@@ -5388,7 +5388,7 @@ export namespace bigtableadmin_v2 {
     }
 
     /**
-     * Gets the access control policy for a Table resource. Returns an empty policy if the resource exists but does not have a policy set.
+     * Gets the access control policy for a Table or Backup resource. Returns an empty policy if the resource exists but does not have a policy set.
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
@@ -5829,7 +5829,7 @@ export namespace bigtableadmin_v2 {
     }
 
     /**
-     * Sets the access control policy on a Table resource. Replaces any existing policy.
+     * Sets the access control policy on a Table or Backup resource. Replaces any existing policy.
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
@@ -5917,7 +5917,7 @@ export namespace bigtableadmin_v2 {
     }
 
     /**
-     * Returns permissions that the caller has on the specified table resource.
+     * Returns permissions that the caller has on the specified Table or Backup resource.
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
