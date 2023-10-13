1a2c5629e1344e9818d0be3495714ef08372f43a
Yoshi Automation <yoshi-automation@google.com>
2023-10-13 01:35:58 +0000

    fix(travelimpactmodel): update the API
    #### travelimpactmodel:v1
    
    The following keys were changed:
    - schemas.FlightWithEmissions.properties.emissionsGramsPerPax.description

--- src/apis/travelimpactmodel/v1.ts
+++ src/apis/travelimpactmodel/v1.ts
@@ -214,7 +214,7 @@ export namespace travelimpactmodel_v1 {
    */
   export interface Schema$FlightWithEmissions {
     /**
-     * Optional. Per-passenger emission estimate numbers. Will not be present if emissions could not be computed. For the list of reasons why emissions could not be computed, see ComputeFlightEmissions.
+     * Optional. Per-passenger emission estimate numbers. Will not be present if emissions could not be computed. For the list of reasons why emissions could not be computed, see ComputeFlightEmissions. Note this field is currently equivalent to ttw_emissions_grams_per_pax until TIM version 1.X.0 which will update this to be total wtw emissions aka wtt_emissions_grams_per_pax + ttw_emissions_grams_per_pax.
      */
     emissionsGramsPerPax?: Schema$EmissionsGramsPerPax;
     /**
