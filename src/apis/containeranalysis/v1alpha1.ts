7de4be7a03d8071712dd81a63146a7f89a73e3b1
Yoshi Automation <yoshi-automation@google.com>
2023-10-19 01:32:16 +0000

    feat(containeranalysis)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### containeranalysis:v1alpha1
    
    The following keys were added:
    - schemas.ContaineranalysisGoogleDevtoolsCloudbuildV1StorageSource.properties.sourceFetcher.description
    - schemas.ContaineranalysisGoogleDevtoolsCloudbuildV1StorageSource.properties.sourceFetcher.enum
    - schemas.ContaineranalysisGoogleDevtoolsCloudbuildV1StorageSource.properties.sourceFetcher.enumDescriptions
    - schemas.ContaineranalysisGoogleDevtoolsCloudbuildV1StorageSource.properties.sourceFetcher.type
    - schemas.VulnerabilityDetails.properties.extraDetails.description
    - schemas.VulnerabilityDetails.properties.extraDetails.type
    
    #### containeranalysis:v1
    
    The following keys were deleted:
    - schemas.VulnerabilityNote.properties.extraDetails.description
    - schemas.VulnerabilityNote.properties.extraDetails.type
    
    The following keys were added:
    - schemas.ContaineranalysisGoogleDevtoolsCloudbuildV1StorageSource.properties.sourceFetcher.description
    - schemas.ContaineranalysisGoogleDevtoolsCloudbuildV1StorageSource.properties.sourceFetcher.enum
    - schemas.ContaineranalysisGoogleDevtoolsCloudbuildV1StorageSource.properties.sourceFetcher.enumDescriptions
    - schemas.ContaineranalysisGoogleDevtoolsCloudbuildV1StorageSource.properties.sourceFetcher.type
    - schemas.VulnerabilityOccurrence.properties.extraDetails.description
    - schemas.VulnerabilityOccurrence.properties.extraDetails.type

--- src/apis/containeranalysis/v1alpha1.ts
+++ src/apis/containeranalysis/v1alpha1.ts
@@ -1298,6 +1298,10 @@ export namespace containeranalysis_v1alpha1 {
      * Cloud Storage object containing the source. This object must be a zipped (`.zip`) or gzipped archive file (`.tar.gz`) containing source to build.
      */
     object?: string | null;
+    /**
+     * Optional. Option to specify the tool to fetch the source file for the build.
+     */
+    sourceFetcher?: string | null;
   }
   /**
    * Location of the source manifest in Cloud Storage. This feature is in Preview; see description [here](https://github.com/GoogleCloudPlatform/cloud-builders/tree/master/gcs-fetcher).
@@ -3647,6 +3651,10 @@ export namespace containeranalysis_v1alpha1 {
      * The distro assigned severity for this vulnerability when that is available and note provider assigned severity when distro has not yet assigned a severity for this vulnerability. When there are multiple package issues for this vulnerability, they can have different effective severities because some might come from the distro and some might come from installed language packs (e.g. Maven JARs or Go binaries). For this reason, it is advised to use the effective severity on the PackageIssue level, as this field may eventually be deprecated. In the case where multiple PackageIssues have different effective severities, the one set here will be the highest severity of any of the PackageIssues.
      */
     effectiveSeverity?: string | null;
+    /**
+     * Occurrence-specific extra details about the vulnerability.
+     */
+    extraDetails?: string | null;
     /**
      * The set of affected locations and their fixes (if available) within the associated resource.
      */
