3cb71d54b11a56935122ae99afd0a983e3158d3f
Yoshi Automation <yoshi-automation@google.com>
2023-11-27 01:32:45 +0000

    feat(cloudfunctions): update the API
    #### cloudfunctions:v2alpha
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.functions.methods.generateUploadUrl.description
    
    #### cloudfunctions:v2beta
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.functions.methods.generateUploadUrl.description
    
    #### cloudfunctions:v1
    
    The following keys were added:
    - schemas.CloudFunction.properties.network.deprecated
    
    The following keys were changed:
    - schemas.CloudFunction.properties.network.description
    
    #### cloudfunctions:v2
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.functions.methods.generateUploadUrl.description

--- src/apis/cloudfunctions/v1.ts
+++ src/apis/cloudfunctions/v1.ts
@@ -272,7 +272,7 @@ export namespace cloudfunctions_v1 {
      */
     name?: string | null;
     /**
-     * The VPC Network that this cloud function can connect to. It can be either the fully-qualified URI, or the short name of the network resource. If the short network name is used, the network must belong to the same project. Otherwise, it must belong to a project within the same organization. The format of this field is either `projects/{project\}/global/networks/{network\}` or `{network\}`, where `{project\}` is a project id where the network is defined, and `{network\}` is the short name of the network. This field is mutually exclusive with `vpc_connector` and will be replaced by it. See [the VPC documentation](https://cloud.google.com/compute/docs/vpc) for more information on connecting Cloud projects.
+     * Deprecated: use vpc_connector
      */
     network?: string | null;
     /**
