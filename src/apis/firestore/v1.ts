3f05c7c45742425fc3343f6e4da6a4fd9f68b3c9
Yoshi Automation <yoshi-automation@google.com>
2023-11-27 01:32:48 +0000

    feat(firestore): update the API
    #### firestore:v1
    
    The following keys were added:
    - schemas.GoogleFirestoreAdminV1ListDatabasesResponse.properties.unreachable.description
    - schemas.GoogleFirestoreAdminV1ListDatabasesResponse.properties.unreachable.items.type
    - schemas.GoogleFirestoreAdminV1ListDatabasesResponse.properties.unreachable.type
    
    The following keys were changed:
    - schemas.GoogleFirestoreAdminV1ExportDocumentsRequest.properties.snapshotTime.description

--- src/apis/firestore/v1.ts
+++ src/apis/firestore/v1.ts
@@ -752,7 +752,7 @@ export namespace firestore_v1 {
      */
     outputUriPrefix?: string | null;
     /**
-     * The timestamp that corresponds to the version of the database to be exported. The timestamp must be rounded to the minute, in the past, and not older than 5 days. Please choose a reasonable timestamp based on prior knowledge on how long exports take as data at provided snapshot timestamp can expire during export. If specified, then the exported documents will represent a consistent view of the database at the provided time. Otherwise, there are no guarantees about the consistency of the exported documents.
+     * The timestamp that corresponds to the version of the database to be exported. The timestamp must be in the past, rounded to the minute and not older than earliestVersionTime. If specified, then the exported documents will represent a consistent view of the database at the provided time. Otherwise, there are no guarantees about the consistency of the exported documents.
      */
     snapshotTime?: string | null;
   }
@@ -1016,6 +1016,10 @@ export namespace firestore_v1 {
      * The databases in the project.
      */
     databases?: Schema$GoogleFirestoreAdminV1Database[];
+    /**
+     * In the event that data about individual databases cannot be listed they will be recorded here. An example entry might be: projects/some_project/locations/some_location This can happen if the Cloud Region that the Database resides in is currently unavailable. In this case we can't fetch all the details about the database. You may be able to get a more detailed error message (or possibly fetch the resource) by sending a 'Get' request for the resource or a 'List' request for the specific location.
+     */
+    unreachable?: string[] | null;
   }
   /**
    * The response for FirestoreAdmin.ListFields.
