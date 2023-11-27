2f7ff38cc99a315ad3bf7798f026ec9eb1790da8
Yoshi Automation <yoshi-automation@google.com>
2023-11-27 01:32:46 +0000

    feat(dataflow): update the API
    #### dataflow:v1b3
    
    The following keys were added:
    - schemas.Job.properties.satisfiesPzi.description
    - schemas.Job.properties.satisfiesPzi.readOnly
    - schemas.Job.properties.satisfiesPzi.type
    
    The following keys were changed:
    - schemas.Job.properties.requestedState.description

--- src/apis/dataflow/v1b3.ts
+++ src/apis/dataflow/v1b3.ts
@@ -1303,13 +1303,17 @@ export namespace dataflow_v1b3 {
      */
     replaceJobId?: string | null;
     /**
-     * The job's requested state. `UpdateJob` may be used to switch between the `JOB_STATE_STOPPED` and `JOB_STATE_RUNNING` states, by setting requested_state. `UpdateJob` may also be used to directly set a job's requested state to `JOB_STATE_CANCELLED` or `JOB_STATE_DONE`, irrevocably terminating the job if it has not already reached a terminal state.
+     * The job's requested state. Applies to `UpdateJob` requests. Set `requested_state` with `UpdateJob` requests to switch between the states `JOB_STATE_STOPPED` and `JOB_STATE_RUNNING`. You can also use `UpdateJob` requests to change a job's state from `JOB_STATE_RUNNING` to `JOB_STATE_CANCELLED`, `JOB_STATE_DONE`, or `JOB_STATE_DRAINED`. These states irrevocably terminate the job if it hasn't already reached a terminal state. This field has no effect on `CreateJob` requests.
      */
     requestedState?: string | null;
     /**
      * This field may ONLY be modified at runtime using the projects.jobs.update method to adjust job behavior. This field has no effect when specified at job creation.
      */
     runtimeUpdatableParams?: Schema$RuntimeUpdatableParams;
+    /**
+     * Output only. Reserved for future use. This field is set only in responses from the server; it is ignored if it is set in any requests.
+     */
+    satisfiesPzi?: boolean | null;
     /**
      * Reserved for future use. This field is set only in responses from the server; it is ignored if it is set in any requests.
      */
