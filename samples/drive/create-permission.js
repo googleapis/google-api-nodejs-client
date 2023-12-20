3336de2c401aed44e4d8f040a246608af871ebb8
Justin Szaro <justinszaro@gmail.com>
2023-12-20 18:42:07 -0500

    feat: include a create permissions sample in the drive folder

---
+++ samples/drive/create-permission.js
@@ -0,0 +1,53 @@
+// Copyright 2016 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const {google} = require('googleapis');
+const fs = require('fs');
+const os = require('os');
+const uuid = require('uuid');
+const path = require('path');
+const {authenticate} = require('@google-cloud/local-auth');
+
+const drive = google.drive('v3');
+
+async function runSample(fileId) {
+    // Obtain user credentials to use for the request
+    const auth = await authenticate({
+        keyfilePath: path.join(__dirname, '../oauth2.keys.json'),
+        scopes: [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file',
+        ],
+    });
+    google.options({auth});
+    
+    const res = await drive.permissions.create(
+        {
+            fileId: fileId,
+            requestBody: {
+                role: 'reader',
+                type: 'anyone',
+            },
+        }
+    );
+    console.log(res.data);
+    return res.data;
+}
+
+// if invoked directly (not tests), authenticate and run the samples
+if (module === require.main) {
+    const fileId = process.argv[2];
+    runSample(fileId).catch(console.error);
+}
