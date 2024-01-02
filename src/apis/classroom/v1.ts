9d2ed122022cd591742589f816a8064bf8deadb8
Yoshi Automation <yoshi-automation@google.com>
2024-01-02 01:32:32 +0000

    fix(classroom): update the API
    #### classroom:v1
    
    The following keys were changed:
    - resources.courses.resources.courseWork.methods.patch.parameters.updateMask.description
    - schemas.StudentSubmission.description

--- src/apis/classroom/v1.ts
+++ src/apis/classroom/v1.ts
@@ -1143,7 +1143,7 @@ export namespace classroom_v1 {
     userId?: string | null;
   }
   /**
-   * Student submission for course work. StudentSubmission items are generated when a CourseWork item is created. StudentSubmissions that have never been accessed (i.e. with `state` = NEW) may not have a creation time or update time.
+   * Student submission for course work. `StudentSubmission` items are generated when a `CourseWork` item is created. Student submissions that have never been accessed (i.e. with `state` = NEW) may not have a creation time or update time.
    */
   export interface Schema$StudentSubmission {
     /**
@@ -3494,7 +3494,7 @@ export namespace classroom_v1 {
      */
     id?: string;
     /**
-     * Mask that identifies which fields on the course work to update. This field is required to do an update. The update fails if invalid fields are specified. If a field supports empty values, it can be cleared by specifying it in the update mask and not in the CourseWork object. If a field that does not support empty values is included in the update mask and not set in the CourseWork object, an `INVALID_ARGUMENT` error is returned. The following fields may be specified by teachers: * `title` * `description` * `state` * `due_date` * `due_time` * `max_points` * `scheduled_time` * `submission_modification_mode` * `topic_id`
+     * Mask that identifies which fields on the course work to update. This field is required to do an update. The update fails if invalid fields are specified. If a field supports empty values, it can be cleared by specifying it in the update mask and not in the `CourseWork` object. If a field that does not support empty values is included in the update mask and not set in the `CourseWork` object, an `INVALID_ARGUMENT` error is returned. The following fields may be specified by teachers: * `title` * `description` * `state` * `due_date` * `due_time` * `max_points` * `scheduled_time` * `submission_modification_mode` * `topic_id`
      */
     updateMask?: string;
 
