dcc96db92ab88ca77c55843654d9a8d09af3375d
Yoshi Automation <yoshi-automation@google.com>
2024-02-02 01:30:42 +0000

    fix(memcache): update the API
    #### memcache:v1beta2
    
    The following keys were changed:
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1Instance.properties.maintenancePolicyNames.description
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.properties.maintenancePolicies.description
    
    #### memcache:v1
    
    The following keys were changed:
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1Instance.properties.maintenancePolicyNames.description
    - schemas.GoogleCloudSaasacceleratorManagementProvidersV1MaintenanceSettings.properties.maintenancePolicies.description

--- src/apis/memcache/v1beta2.ts
+++ src/apis/memcache/v1beta2.ts
@@ -301,7 +301,7 @@ export namespace memcache_v1beta2 {
      */
     labels?: {[key: string]: string} | null;
     /**
-     * Optional. The MaintenancePolicies that have been attached to the instance. The key must be of the type name of the oneof policy name defined in MaintenancePolicy, and the referenced policy must define the same policy type. For details, please refer to go/cloud-saas-mw-ug. Should not be set if maintenance_settings.maintenance_policies is set.
+     * Optional. The MaintenancePolicies that have been attached to the instance. The key must be of the type name of the oneof policy name defined in MaintenancePolicy, and the referenced policy must define the same policy type. For details, please refer to go/mr-user-guide. Should not be set if maintenance_settings.maintenance_policies is set.
      */
     maintenancePolicyNames?: {[key: string]: string} | null;
     /**
@@ -399,7 +399,7 @@ export namespace memcache_v1beta2 {
      */
     isRollback?: boolean | null;
     /**
-     * Optional. The MaintenancePolicies that have been attached to the instance. The key must be of the type name of the oneof policy name defined in MaintenancePolicy, and the embedded policy must define the same policy type. For details, please refer to go/cloud-saas-mw-ug. Should not be set if maintenance_policy_names is set. If only the name is needed, then only populate MaintenancePolicy.name.
+     * Optional. The MaintenancePolicies that have been attached to the instance. The key must be of the type name of the oneof policy name defined in MaintenancePolicy, and the embedded policy must define the same policy type. For details, please refer to go/mr-user-guide. Should not be set if maintenance_policy_names is set. If only the name is needed, then only populate MaintenancePolicy.name.
      */
     maintenancePolicies?: {[key: string]: Schema$MaintenancePolicy} | null;
   }
