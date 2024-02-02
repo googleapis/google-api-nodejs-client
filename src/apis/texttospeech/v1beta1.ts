1464272e8e41a476457834b5044b4019afe9e519
Yoshi Automation <yoshi-automation@google.com>
2024-02-02 01:30:45 +0000

    feat(texttospeech): update the API
    #### texttospeech:v1beta1
    
    The following keys were added:
    - schemas.GoogleCloudTexttospeechV1beta1SynthesizeLongAudioMetadata.properties.lastUpdateTime.deprecated
    - schemas.GoogleCloudTexttospeechV1beta1SynthesizeLongAudioMetadata.properties.lastUpdateTime.description
    - schemas.GoogleCloudTexttospeechV1beta1SynthesizeLongAudioMetadata.properties.lastUpdateTime.format
    - schemas.GoogleCloudTexttospeechV1beta1SynthesizeLongAudioMetadata.properties.lastUpdateTime.type
    - schemas.SynthesizeLongAudioMetadata.properties.lastUpdateTime.deprecated
    - schemas.SynthesizeLongAudioMetadata.properties.lastUpdateTime.description
    - schemas.SynthesizeLongAudioMetadata.properties.lastUpdateTime.format
    - schemas.SynthesizeLongAudioMetadata.properties.lastUpdateTime.type
    
    #### texttospeech:v1
    
    The following keys were added:
    - schemas.GoogleCloudTexttospeechV1SynthesizeLongAudioMetadata.properties.lastUpdateTime.deprecated
    - schemas.GoogleCloudTexttospeechV1SynthesizeLongAudioMetadata.properties.lastUpdateTime.description
    - schemas.GoogleCloudTexttospeechV1SynthesizeLongAudioMetadata.properties.lastUpdateTime.format
    - schemas.GoogleCloudTexttospeechV1SynthesizeLongAudioMetadata.properties.lastUpdateTime.type
    - schemas.SynthesizeLongAudioMetadata.properties.lastUpdateTime.deprecated
    - schemas.SynthesizeLongAudioMetadata.properties.lastUpdateTime.description
    - schemas.SynthesizeLongAudioMetadata.properties.lastUpdateTime.format
    - schemas.SynthesizeLongAudioMetadata.properties.lastUpdateTime.type

--- src/apis/texttospeech/v1beta1.ts
+++ src/apis/texttospeech/v1beta1.ts
@@ -175,6 +175,10 @@ export namespace texttospeech_v1beta1 {
    */
   export interface Schema$GoogleCloudTexttospeechV1beta1SynthesizeLongAudioMetadata {
     /**
+     * Deprecated. Do not use.
+     */
+    lastUpdateTime?: string | null;
+    /**
      * The progress of the most recent processing update in percentage, ie. 70.0%.
      */
     progressPercentage?: number | null;
@@ -264,6 +268,10 @@ export namespace texttospeech_v1beta1 {
    * Metadata for response returned by the `SynthesizeLongAudio` method.
    */
   export interface Schema$SynthesizeLongAudioMetadata {
+    /**
+     * Deprecated. Do not use.
+     */
+    lastUpdateTime?: string | null;
     /**
      * The progress of the most recent processing update in percentage, ie. 70.0%.
      */
