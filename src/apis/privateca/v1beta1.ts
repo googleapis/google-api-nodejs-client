f3ac40bb4cf2cc159cb5b3f20e1ed3ccd105a3e6
Yoshi Automation <yoshi-automation@google.com>
2024-02-07 01:29:18 +0000

    fix(privateca): update the API
    #### privateca:v1beta1
    
    The following keys were changed:
    - schemas.Binding.properties.role.description
    
    #### privateca:v1
    
    The following keys were changed:
    - schemas.Binding.properties.role.description

--- src/apis/privateca/v1beta1.ts
+++ src/apis/privateca/v1beta1.ts
@@ -163,7 +163,7 @@ export namespace privateca_v1beta1 {
      */
     members?: string[] | null;
     /**
-     * Role that is assigned to the list of `members`, or principals. For example, `roles/viewer`, `roles/editor`, or `roles/owner`.
+     * Role that is assigned to the list of `members`, or principals. For example, `roles/viewer`, `roles/editor`, or `roles/owner`. For an overview of the IAM roles and permissions, see the [IAM documentation](https://cloud.google.com/iam/docs/roles-overview). For a list of the available pre-defined roles, see [here](https://cloud.google.com/iam/docs/understanding-roles).
      */
     role?: string | null;
   }
