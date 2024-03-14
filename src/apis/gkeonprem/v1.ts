48de5d95cfaf5e402e75dfa49208fca30e6c6ef5
Yoshi Automation <yoshi-automation@google.com>
2024-03-14 01:29:51 +0000

    fix(gkeonprem): update the API
    #### gkeonprem:v1
    
    The following keys were changed:
    - schemas.VmwareNodeConfig.properties.imageType.description

--- src/apis/gkeonprem/v1.ts
+++ src/apis/gkeonprem/v1.ts
@@ -2601,7 +2601,7 @@ export namespace gkeonprem_v1 {
      */
     image?: string | null;
     /**
-     * Required. The OS image to be used for each node in a node pool. Currently `cos`, `ubuntu`, `ubuntu_containerd` and `windows` are supported.
+     * Required. The OS image to be used for each node in a node pool. Currently `cos`, `cos_cgv2`, `ubuntu`, `ubuntu_cgv2`, `ubuntu_containerd` and `windows` are supported.
      */
     imageType?: string | null;
     /**
