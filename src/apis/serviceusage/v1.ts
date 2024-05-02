41b76ee8d6beeeb3bbccdcbbcd0853f610a54171
Yoshi Automation <yoshi-automation@google.com>
2024-05-02 01:32:43 +0000

    feat(serviceusage)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### serviceusage:v1beta1
    
    The following keys were deleted:
    - schemas.Endpoint.properties.aliases.deprecated
    
    The following keys were changed:
    - schemas.Endpoint.properties.aliases.description
    - schemas.MethodSettings.properties.longRunning.description
    - schemas.MethodSettings.properties.selector.description
    
    #### serviceusage:v1
    
    The following keys were deleted:
    - schemas.Endpoint.properties.aliases.deprecated
    
    The following keys were changed:
    - schemas.Endpoint.properties.aliases.description
    - schemas.MethodSettings.properties.longRunning.description
    - schemas.MethodSettings.properties.selector.description

--- src/apis/serviceusage/v1.ts
+++ src/apis/serviceusage/v1.ts
@@ -758,7 +758,7 @@ export namespace serviceusage_v1 {
    */
   export interface Schema$Endpoint {
     /**
-     * Unimplemented. Dot not use. DEPRECATED: This field is no longer supported. Instead of using aliases, please specify multiple google.api.Endpoint for each of the intended aliases. Additional names that this endpoint will be hosted on.
+     * Aliases for this endpoint, these will be served by the same UrlMap as the parent endpoint, and will be provisioned in the GCP stack for the Regional Endpoints.
      */
     aliases?: string[] | null;
     /**
@@ -1469,11 +1469,11 @@ export namespace serviceusage_v1 {
      */
     autoPopulatedFields?: string[] | null;
     /**
-     * Describes settings to use for long-running operations when generating API methods for RPCs. Complements RPCs that use the annotations in google/longrunning/operations.proto. Example of a YAML configuration:: publishing: method_settings: - selector: google.cloud.speech.v2.Speech.BatchRecognize long_running: initial_poll_delay: seconds: 60 # 1 minute poll_delay_multiplier: 1.5 max_poll_delay: seconds: 360 # 6 minutes total_poll_timeout: seconds: 54000 # 90 minutes
+     * Describes settings to use for long-running operations when generating API methods for RPCs. Complements RPCs that use the annotations in google/longrunning/operations.proto. Example of a YAML configuration:: publishing: method_settings: - selector: google.cloud.speech.v2.Speech.BatchRecognize long_running: initial_poll_delay: 60s # 1 minute poll_delay_multiplier: 1.5 max_poll_delay: 360s # 6 minutes total_poll_timeout: 54000s # 90 minutes
      */
     longRunning?: Schema$LongRunning;
     /**
-     * The fully qualified name of the method, for which the options below apply. This is used to find the method to apply the options.
+     * The fully qualified name of the method, for which the options below apply. This is used to find the method to apply the options. Example: publishing: method_settings: - selector: google.storage.control.v2.StorageControl.CreateFolder # method settings for CreateFolder...
      */
     selector?: string | null;
   }
@@ -2121,6 +2121,7 @@ export namespace serviceusage_v1 {
           {
             url: (rootUrl + '/v1/{+name}:cancel').replace(/([^:]\/)\/+/g, '$1'),
             method: 'POST',
+            apiVersion: '',
           },
           options
         ),
@@ -2205,6 +2206,7 @@ export namespace serviceusage_v1 {
           {
             url: (rootUrl + '/v1/{+name}').replace(/([^:]\/)\/+/g, '$1'),
             method: 'DELETE',
+            apiVersion: '',
           },
           options
         ),
@@ -2288,6 +2290,7 @@ export namespace serviceusage_v1 {
           {
             url: (rootUrl + '/v1/{+name}').replace(/([^:]\/)\/+/g, '$1'),
             method: 'GET',
+            apiVersion: '',
           },
           options
         ),
@@ -2376,6 +2379,7 @@ export namespace serviceusage_v1 {
           {
             url: (rootUrl + '/v1/operations').replace(/([^:]\/)\/+/g, '$1'),
             method: 'GET',
+            apiVersion: '',
           },
           options
         ),
@@ -2514,6 +2518,7 @@ export namespace serviceusage_v1 {
               '$1'
             ),
             method: 'POST',
+            apiVersion: '',
           },
           options
         ),
@@ -2608,6 +2613,7 @@ export namespace serviceusage_v1 {
               '$1'
             ),
             method: 'GET',
+            apiVersion: '',
           },
           options
         ),
@@ -2694,6 +2700,7 @@ export namespace serviceusage_v1 {
               '$1'
             ),
             method: 'POST',
+            apiVersion: '',
           },
           options
         ),
@@ -2777,6 +2784,7 @@ export namespace serviceusage_v1 {
           {
             url: (rootUrl + '/v1/{+name}:enable').replace(/([^:]\/)\/+/g, '$1'),
             method: 'POST',
+            apiVersion: '',
           },
           options
         ),
@@ -2867,6 +2875,7 @@ export namespace serviceusage_v1 {
           {
             url: (rootUrl + '/v1/{+name}').replace(/([^:]\/)\/+/g, '$1'),
             method: 'GET',
+            apiVersion: '',
           },
           options
         ),
@@ -2960,6 +2969,7 @@ export namespace serviceusage_v1 {
               '$1'
             ),
             method: 'GET',
+            apiVersion: '',
           },
           options
         ),
