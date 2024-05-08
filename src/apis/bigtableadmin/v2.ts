d982ec20655fadb43440bffee63b342ba9163a80
Yoshi Automation <yoshi-automation@google.com>
2024-05-08 01:23:34 +0000

    feat(bigtableadmin): update the API
    #### bigtableadmin:v2
    
    The following keys were added:
    - schemas.Instance.properties.satisfiesPzi.description
    - schemas.Instance.properties.satisfiesPzi.readOnly
    - schemas.Instance.properties.satisfiesPzi.type
    
    The following keys were changed:
    - resources.projects.resources.instances.resources.tables.resources.authorizedViews.methods.list.parameters.view.description
    - schemas.CreateAuthorizedViewMetadata.properties.originalRequest.description

--- src/apis/bigtableadmin/v2.ts
+++ src/apis/bigtableadmin/v2.ts
@@ -512,7 +512,7 @@ export namespace bigtableadmin_v2 {
      */
     finishTime?: string | null;
     /**
-     * The request that prompted the initiation of this CreateInstance operation.
+     * The request that prompted the initiation of this CreateAuthorizedView operation.
      */
     originalRequest?: Schema$CreateAuthorizedViewRequest;
     /**
@@ -931,6 +931,10 @@ export namespace bigtableadmin_v2 {
      * The unique name of the instance. Values are of the form `projects/{project\}/instances/a-z+[a-z0-9]`.
      */
     name?: string | null;
+    /**
+     * Output only. Reserved for future use.
+     */
+    satisfiesPzi?: boolean | null;
     /**
      * Output only. Reserved for future use.
      */
@@ -7225,7 +7229,7 @@ export namespace bigtableadmin_v2 {
      */
     parent?: string;
     /**
-     * Optional. The resource_view to be applied to the returned views' fields. Default to NAME_ONLY.
+     * Optional. The resource_view to be applied to the returned AuthorizedViews' fields. Default to NAME_ONLY.
      */
     view?: string;
   }
