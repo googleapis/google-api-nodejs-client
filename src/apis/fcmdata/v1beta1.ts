34f4d08acd6c253676f801f76f0451b4897f867d
Yoshi Automation <yoshi-automation@google.com>
2024-06-06 01:35:30 +0000

    feat(fcmdata): update the API
    #### fcmdata:v1beta1
    
    The following keys were added:
    - schemas.GoogleFirebaseFcmDataV1beta1MessageOutcomePercents.properties.collapsed.description
    - schemas.GoogleFirebaseFcmDataV1beta1MessageOutcomePercents.properties.collapsed.format
    - schemas.GoogleFirebaseFcmDataV1beta1MessageOutcomePercents.properties.collapsed.type
    - schemas.GoogleFirebaseFcmDataV1beta1MessageOutcomePercents.properties.droppedTtlExpired.description
    - schemas.GoogleFirebaseFcmDataV1beta1MessageOutcomePercents.properties.droppedTtlExpired.format
    - schemas.GoogleFirebaseFcmDataV1beta1MessageOutcomePercents.properties.droppedTtlExpired.type

--- src/apis/fcmdata/v1beta1.ts
+++ src/apis/fcmdata/v1beta1.ts
@@ -226,6 +226,10 @@ export namespace fcmdata_v1beta1 {
    */
   export interface Schema$GoogleFirebaseFcmDataV1beta1MessageOutcomePercents {
     /**
+     * The percentage of accepted messages that were [collapsed](https://firebase.google.com/docs/cloud-messaging/concept-options#collapsible_and_non-collapsible_messages) by another message.
+     */
+    collapsed?: number | null;
+    /**
      * The percentage of all accepted messages that were successfully delivered to the device.
      */
     delivered?: number | null;
@@ -241,6 +245,10 @@ export namespace fcmdata_v1beta1 {
      * The percentage of accepted messages that were dropped due to [too many undelivered non-collapsible messages](https://firebase.google.com/docs/cloud-messaging/concept-options#collapsible_and_non-collapsible_messages). Specifically, each app instance can only have 100 pending messages stored on our servers for a device which is disconnected. When that device reconnects, those messages are delivered. When there are more than the maximum pending messages, we call [OnDeletedMessages()](https://firebase.google.com/docs/cloud-messaging/android/receive#override-ondeletedmessages) in our SDK instead of delivering the messages.
      */
     droppedTooManyPendingMessages?: number | null;
+    /**
+     * The percentage of accepted messages that expired because [Time To Live (TTL)](https://firebase.google.com/docs/cloud-messaging/concept-options#ttl) elapsed before the target device reconnected.
+     */
+    droppedTtlExpired?: number | null;
     /**
      * The percentage of messages accepted on this day that were not dropped and not delivered, due to the device being disconnected (as of the end of the America/Los_Angeles day when the message was sent to FCM). A portion of these messages will be delivered the next day when the device connects but others may be destined to devices that ultimately never reconnect.
      */
