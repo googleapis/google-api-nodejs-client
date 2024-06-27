0ed510f824b64536965b8090404efc6a59fc2407
Yoshi Automation <yoshi-automation@google.com>
2024-06-27 01:36:18 +0000

    feat(healthcare): update the API
    #### healthcare:v1beta1
    
    The following keys were added:
    - schemas.Dataset.properties.encryptionSpec.$ref
    - schemas.Dataset.properties.encryptionSpec.description
    - schemas.EncryptionSpec.description
    - schemas.EncryptionSpec.id
    - schemas.EncryptionSpec.properties.kmsKeyName.description
    - schemas.EncryptionSpec.properties.kmsKeyName.type
    - schemas.EncryptionSpec.type
    
    #### healthcare:v1
    
    The following keys were added:
    - schemas.Dataset.properties.encryptionSpec.$ref
    - schemas.Dataset.properties.encryptionSpec.description
    - schemas.EncryptionSpec.description
    - schemas.EncryptionSpec.id
    - schemas.EncryptionSpec.properties.kmsKeyName.description
    - schemas.EncryptionSpec.properties.kmsKeyName.type
    - schemas.EncryptionSpec.type
    
    The following keys were changed:
    - schemas.Dataset.properties.timeZone.description

--- src/apis/healthcare/v1beta1.ts
+++ src/apis/healthcare/v1beta1.ts
@@ -803,6 +803,10 @@ export namespace healthcare_v1beta1 {
    */
   export interface Schema$Dataset {
     /**
+     * Customer-managed encryption key spec for a Dataset. If set, this Dataset and all of its sub-resources will be secured by this key. If empty, the Dataset is secured by the default Google encryption key.
+     */
+    encryptionSpec?: Schema$EncryptionSpec;
+    /**
      * Identifier. Resource name of the dataset, of the form `projects/{project_id\}/locations/{location_id\}/datasets/{dataset_id\}`.
      */
     name?: string | null;
@@ -1069,6 +1073,15 @@ export namespace healthcare_v1beta1 {
    * A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); \}
    */
   export interface Schema$Empty {}
+  /**
+   * Represents a customer-managed encryption key spec that can be applied to a resource.
+   */
+  export interface Schema$EncryptionSpec {
+    /**
+     * Required. The resource name of customer-managed encryption key that is used to secure a resource and its sub-resources. Only the key in the same location as this dataset is allowed to be used for encryption. Format is: `projects/{project\}/locations/{location\}/keyRings/{keyRing\}/cryptoKeys/{key\}`
+     */
+    kmsKeyName?: string | null;
+  }
   /**
    * The candidate entities that an entity mention could link to.
    */
