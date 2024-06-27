8820658b6edd1bd094c00c18a44b78ee9e64befd
Yoshi Automation <yoshi-automation@google.com>
2024-06-27 01:36:22 +0000

    feat(storage): update the API
    #### storage:v1
    
    The following keys were added:
    - schemas.GoogleLongrunningListOperationsResponse.properties.kind.default
    - schemas.GoogleLongrunningListOperationsResponse.properties.kind.description
    - schemas.GoogleLongrunningListOperationsResponse.properties.kind.type
    - schemas.GoogleLongrunningOperation.properties.kind.default
    - schemas.GoogleLongrunningOperation.properties.kind.description
    - schemas.GoogleLongrunningOperation.properties.kind.type
    - schemas.GoogleLongrunningOperation.properties.selfLink.description
    - schemas.GoogleLongrunningOperation.properties.selfLink.type

--- src/apis/storage/v1.ts
+++ src/apis/storage/v1.ts
@@ -679,6 +679,10 @@ export namespace storage_v1 {
    */
   export interface Schema$GoogleLongrunningListOperationsResponse {
     /**
+     * The kind of item this is. For lists of operations, this is always storage#operations.
+     */
+    kind?: string | null;
+    /**
      * The continuation token, used to page through large result sets. Provide this value in a subsequent request to return the next page of results.
      */
     nextPageToken?: string | null;
@@ -700,6 +704,10 @@ export namespace storage_v1 {
      */
     error?: Schema$GoogleRpcStatus;
     /**
+     * The kind of item this is. For operations, this is always storage#operation.
+     */
+    kind?: string | null;
+    /**
      * Service-specific metadata associated with the operation. It typically contains progress information and common metadata such as create time. Some services might not provide such metadata. Any method that returns a long-running operation should document the metadata type, if any.
      */
     metadata?: {[key: string]: any} | null;
@@ -711,6 +719,10 @@ export namespace storage_v1 {
      * The normal response of the operation in case of success. If the original method returns no data on success, such as "Delete", the response is google.protobuf.Empty. If the original method is standard Get/Create/Update, the response should be the resource. For other methods, the response should have the type "XxxResponse", where "Xxx" is the original method name. For example, if the original method name is "TakeSnapshot()", the inferred response type is "TakeSnapshotResponse".
      */
     response?: {[key: string]: any} | null;
+    /**
+     * The link to this long running operation.
+     */
+    selfLink?: string | null;
   }
   /**
    * The "Status" type defines a logical error model that is suitable for different programming environments, including REST APIs and RPC APIs. It is used by [gRPC](https://github.com/grpc). Each "Status" message contains three pieces of data: error code, error message, and error details. You can find out more about this error model and how to work with it in the [API Design Guide](https://cloud.google.com/apis/design/errors).
