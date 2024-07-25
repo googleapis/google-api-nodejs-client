9cc58c6ef2636f8d85b04c80ec3c7f05243db45d
sofisl <55454395+sofisl@users.noreply.github.com>
2024-07-25 02:21:22 -0700

    Update synth.ts

--- src/generator/synth.ts
+++ src/generator/synth.ts
@@ -55,7 +55,7 @@ export async function synth(options: SynthOptions = {}) {
   if (!codeBotToken) {
     throw new Error('please include a CODE_BOT_TOKEN');
   }
-  only set these while running in the GitHub Actions environment
+  // only set these while running in the GitHub Actions environment
   if (process.env.GITHUB_ACTIONS) {
     await execa('git', ['config', '--global', 'user.email', 'yoshi-automation@google.com']);
     await execa('git', ['config', '--global', 'user.name', 'Yoshi Automation']);
