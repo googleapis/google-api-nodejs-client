aae3e45ef3cc7b906514bda018a8e1c9a2f53f8f
Yoshi Automation <yoshi-automation@google.com>
2024-07-26 01:34:33 +0000

    fix(androidmanagement): update the API
    #### androidmanagement:v1
    
    The following keys were changed:
    - resources.enterprises.resources.enrollmentTokens.methods.create.description
    - schemas.NonComplianceDetail.properties.nonComplianceReason.enumDescriptions
    - schemas.NonComplianceDetailCondition.properties.nonComplianceReason.enumDescriptions

--- src/apis/androidmanagement/v1.ts
+++ src/apis/androidmanagement/v1.ts
@@ -4792,7 +4792,7 @@ export namespace androidmanagement_v1 {
     }
 
     /**
-     * Creates an enrollment token for a given enterprise. It's up to the caller's responsibility to manage the lifecycle of newly created tokens and deleting them when they're not intended to be used anymore. Once an enrollment token has been created, it's not possible to retrieve the token's content anymore using AM API. It is recommended for EMMs to securely store the token if it's intended to be reused.
+     * Creates an enrollment token for a given enterprise. It's up to the caller's responsibility to manage the lifecycle of newly created tokens and deleting them when they're not intended to be used anymore.
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
