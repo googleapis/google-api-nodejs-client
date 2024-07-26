6b17fe359bd158703e5efb2a9f26266a43b48291
sofisl <55454395+sofisl@users.noreply.github.com>
2024-07-25 17:13:48 -0700

    Update synth.ts

--- src/generator/synth.ts
+++ src/generator/synth.ts
@@ -112,7 +112,7 @@ export async function synth(options: SynthOptions = {}) {
         title: `${prefix}: run the generator`,
         owner: 'googleapis',
         repo: 'google-api-nodejs-client',
-        head: `google-api-nodejs-client-autodisco:${branch}`,
+        head: `yoshi-code-bot:${branch}`,
         base: 'main',
         body: changelogs.join('\n\n').slice(0, 65000),
       },
