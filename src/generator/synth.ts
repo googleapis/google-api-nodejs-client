25e9ca52cff28d47a618fc483c615a176153d099
Sofia Leon <sofialeon@google.com>
2024-07-25 18:08:37 -0700

    chore: update linting

--- src/generator/synth.ts
+++ src/generator/synth.ts
@@ -57,9 +57,19 @@ export async function synth(options: SynthOptions = {}) {
   }
   // only set these while running in the GitHub Actions environment
   if (process.env.GITHUB_ACTIONS) {
-    await execa('git', ['config', '--global', 'user.email', 'yoshi-automation@google.com']);
+    await execa('git', [
+      'config',
+      '--global',
+      'user.email',
+      'yoshi-automation@google.com',
+    ]);
     await execa('git', ['config', '--global', 'user.name', 'Yoshi Automation']);
-    await execa('git', ['config', '--global', `url.https://${process.env.CODE_BOT_TOKEN}@github.com/.insteadOf`, 'https://github.com/']);
+    await execa('git', [
+      'config',
+      '--global',
+      `url.https://${process.env.CODE_BOT_TOKEN}@github.com/.insteadOf`,
+      'https://github.com/',
+    ]);
   }
   const dirs = files.filter(f => {
     return (
