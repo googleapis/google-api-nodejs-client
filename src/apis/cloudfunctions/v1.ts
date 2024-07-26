44f70bbc0443089c0f0a6d81d3713e5425f2ba99
Yoshi Automation <yoshi-automation@google.com>
2024-07-26 01:34:35 +0000

    fix(cloudfunctions): update the API
    #### cloudfunctions:v1
    
    The following keys were changed:
    - schemas.SourceRepository.properties.url.description

--- src/apis/cloudfunctions/v1.ts
+++ src/apis/cloudfunctions/v1.ts
@@ -995,7 +995,7 @@ export namespace cloudfunctions_v1 {
      */
     deployedUrl?: string | null;
     /**
-     * The URL pointing to the hosted repository where the function is defined. There are supported Cloud Source Repository URLs in the following formats: To refer to a specific commit: `https://source.developers.google.com/projects/x/repos/x/revisions/x/paths/x` To refer to a moveable alias (branch): `https://source.developers.google.com/projects/x/repos/x/moveable-aliases/x/paths/x` In particular, to refer to HEAD use `master` moveable alias. To refer to a specific fixed alias (tag): `https://source.developers.google.com/projects/x/repos/x/fixed-aliases/x/paths/x` You may omit `paths/x` if you want to use the main directory.
+     * The URL pointing to the hosted repository where the function is defined. There are supported Cloud Source Repository URLs in the following formats: To refer to a specific commit: `https://source.developers.google.com/projects/x/repos/x/revisions/x/paths/x` To refer to a moveable alias (branch): `https://source.developers.google.com/projects/x/repos/x/moveable-aliases/x/paths/x` In particular, to refer to HEAD use `master` moveable alias. To refer to a specific fixed alias (tag): `https://source.developers.google.com/projects/x/repos/x/fixed-aliases/x/paths/x` You may omit `paths/x` if you want to use the main directory. The function response may add an empty `/paths/` to the URL.
      */
     url?: string | null;
   }
