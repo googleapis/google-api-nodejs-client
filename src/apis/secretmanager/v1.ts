647d818c1b8ecef96f92ae53b2944109c3af6f6b
Yoshi Automation <yoshi-automation@google.com>
2024-08-27 01:35:31 +0000

    fix(secretmanager): update the API
    #### secretmanager:v1
    
    The following keys were changed:
    - schemas.Topic.properties.name.description

--- src/apis/secretmanager/v1.ts
+++ src/apis/secretmanager/v1.ts
@@ -582,7 +582,7 @@ export namespace secretmanager_v1 {
    */
   export interface Schema$Topic {
     /**
-     * Required. The resource name of the Pub/Sub topic that will be published to, in the following format: `projects/x/topics/x`. For publication to succeed, the Secret Manager service agent must have the `pubsub.topic.publish` permission on the topic. The Pub/Sub Publisher role (`roles/pubsub.publisher`) includes this permission.
+     * Identifier. The resource name of the Pub/Sub topic that will be published to, in the following format: `projects/x/topics/x`. For publication to succeed, the Secret Manager service agent must have the `pubsub.topic.publish` permission on the topic. The Pub/Sub Publisher role (`roles/pubsub.publisher`) includes this permission.
      */
     name?: string | null;
   }
