d28ff5dbd4995f9cd985511db3a4b87f2ad7fe20
Yoshi Automation <yoshi-automation@google.com>
2024-08-27 01:35:27 +0000

    fix(cloudcontrolspartner): update the API
    #### cloudcontrolspartner:v1beta
    
    The following keys were changed:
    - schemas.Customer.properties.displayName.description
    
    #### cloudcontrolspartner:v1
    
    The following keys were changed:
    - schemas.Customer.properties.displayName.description

--- src/apis/cloudcontrolspartner/v1.ts
+++ src/apis/cloudcontrolspartner/v1.ts
@@ -197,7 +197,7 @@ export namespace cloudcontrolspartner_v1 {
      */
     customerOnboardingState?: Schema$CustomerOnboardingState;
     /**
-     * Optional. The customer organization's display name. E.g. "Google".
+     * The customer organization's display name. E.g. "Google".
      */
     displayName?: string | null;
     /**
