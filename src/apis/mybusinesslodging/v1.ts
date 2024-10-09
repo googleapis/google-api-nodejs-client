ccafbfe63a38a3b0d7ca9d5c900d6d7a4fc36214
Yoshi Automation <yoshi-automation@google.com>
2024-10-09 01:36:04 +0000

    fix(mybusinesslodging): update the API
    #### mybusinesslodging:v1
    
    The following keys were changed:
    - schemas.TimeOfDay.properties.hours.description
    - schemas.TimeOfDay.properties.minutes.description
    - schemas.TimeOfDay.properties.nanos.description
    - schemas.TimeOfDay.properties.seconds.description

--- src/apis/mybusinesslodging/v1.ts
+++ src/apis/mybusinesslodging/v1.ts
@@ -2278,19 +2278,19 @@ export namespace mybusinesslodging_v1 {
    */
   export interface Schema$TimeOfDay {
     /**
-     * Hours of day in 24 hour format. Should be from 0 to 23. An API may choose to allow the value "24:00:00" for scenarios like business closing time.
+     * Hours of a day in 24 hour format. Must be greater than or equal to 0 and typically must be less than or equal to 23. An API may choose to allow the value "24:00:00" for scenarios like business closing time.
      */
     hours?: number | null;
     /**
-     * Minutes of hour of day. Must be from 0 to 59.
+     * Minutes of an hour. Must be greater than or equal to 0 and less than or equal to 59.
      */
     minutes?: number | null;
     /**
-     * Fractions of seconds in nanoseconds. Must be from 0 to 999,999,999.
+     * Fractions of seconds, in nanoseconds. Must be greater than or equal to 0 and less than or equal to 999,999,999.
      */
     nanos?: number | null;
     /**
-     * Seconds of minutes of the time. Must normally be from 0 to 59. An API may allow the value 60 if it allows leap-seconds.
+     * Seconds of a minute. Must be greater than or equal to 0 and typically must be less than or equal to 59. An API may allow the value 60 if it allows leap-seconds.
      */
     seconds?: number | null;
   }
