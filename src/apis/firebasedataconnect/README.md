14b27de562a717b94a7d43376e88b99cbd107f2f
Yoshi Automation <yoshi-automation@google.com>
2024-10-09 01:36:03 +0000

    fix(firebasedataconnect): update the API

---
+++ src/apis/firebasedataconnect/README.md
@@ -0,0 +1,28 @@
+<img src="https://avatars0.githubusercontent.com/u/1342004?v=3&s=96" alt="Google Inc. logo" title="Google" align="right" height="96" width="96"/>
+
+# firebasedataconnect
+
+> Firebase Data Connect is a relational database service for mobile and web apps that lets you build and scale using a fully-managed PostgreSQL database powered by Cloud SQL. The REST API lets developers manage the connections to their database, change the schema of their database, and query the database.
+
+## Installation
+
+```sh
+$ npm install @googleapis/firebasedataconnect
+```
+
+## Usage
+All documentation and usage information can be found on [GitHub](https://github.com/googleapis/google-api-nodejs-client).
+Information on classes can be found in [Googleapis Documentation](https://googleapis.dev/nodejs/googleapis/latest/firebasedataconnect/classes/Firebasedataconnect.html).
+
+## License
+This library is licensed under Apache 2.0. Full license text is available in [LICENSE](https://github.com/googleapis/google-api-nodejs-client/blob/main/LICENSE).
+
+## Contributing
+We love contributions! Before submitting a Pull Request, it's always good to start with a new issue first. To learn more, see [CONTRIBUTING](https://github.com/google/google-api-nodejs-client/blob/main/.github/CONTRIBUTING.md).
+
+## Questions/problems?
+* Ask your development related questions on [StackOverflow](http://stackoverflow.com/questions/tagged/google-api-nodejs-client).
+* If you've found an bug/issue, please [file it on GitHub](https://github.com/googleapis/google-api-nodejs-client/issues).
+
+
+*Crafted with ❤️ by the Google Node.js team*
---
+++ src/apis/firebasedataconnect/index.ts
@@ -0,0 +1,50 @@
+// Copyright 2020 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*! THIS FILE IS AUTO-GENERATED */
+
+import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
+import {firebasedataconnect_v1beta} from './v1beta';
+
+export const VERSIONS = {
+  v1beta: firebasedataconnect_v1beta.Firebasedataconnect,
+};
+
+export function firebasedataconnect(
+  version: 'v1beta'
+): firebasedataconnect_v1beta.Firebasedataconnect;
+export function firebasedataconnect(
+  options: firebasedataconnect_v1beta.Options
+): firebasedataconnect_v1beta.Firebasedataconnect;
+export function firebasedataconnect<
+  T = firebasedataconnect_v1beta.Firebasedataconnect,
+>(
+  this: GoogleConfigurable,
+  versionOrOptions: 'v1beta' | firebasedataconnect_v1beta.Options
+) {
+  return getAPI<T>('firebasedataconnect', versionOrOptions, VERSIONS, this);
+}
+
+const auth = new AuthPlus();
+export {auth};
+export {firebasedataconnect_v1beta};
+export {
+  AuthPlus,
+  GlobalOptions,
+  APIRequestContext,
+  GoogleConfigurable,
+  StreamMethodOptions,
+  GaxiosPromise,
+  MethodOptions,
+  BodyResponseCallback,
+} from 'googleapis-common';
