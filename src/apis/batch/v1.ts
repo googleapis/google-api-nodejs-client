2d95feb809ec5d495d7b9888d7d87ca5f724e6a0
Yoshi Automation <yoshi-automation@google.com>
2024-10-09 01:35:59 +0000

    fix(batch): update the API
    #### batch:v1
    
    The following keys were changed:
    - schemas.AllocationPolicy.properties.labels.description
    - schemas.InstancePolicy.properties.reservation.description
    - schemas.InstancePolicyOrTemplate.properties.instanceTemplate.description
    - schemas.Job.properties.labels.description

--- src/apis/batch/v1.ts
+++ src/apis/batch/v1.ts
@@ -448,7 +448,7 @@ export namespace batch_v1 {
      */
     instances?: Schema$InstancePolicyOrTemplate[];
     /**
-     * Labels applied to all VM instances and other resources created by AllocationPolicy. Labels could be user provided or system generated. You can assign up to 64 labels. [Google Compute Engine label restrictions](https://cloud.google.com/compute/docs/labeling-resources#restrictions) apply. Label names that start with "goog-" or "google-" are reserved.
+     * Custom labels to apply to the job and all the Compute Engine resources that both are created by this allocation policy and support labels. Use labels to group and describe the resources they are applied to. Batch automatically applies predefined labels and supports multiple `labels` fields for each job, which each let you apply custom labels to various resources. Label names that start with "goog-" or "google-" are reserved for predefined labels. For more information about labels with Batch, see [Organize resources using labels](https://cloud.google.com/batch/docs/organize-resources-using-labels).
      */
     labels?: {[key: string]: string} | null;
     /**
@@ -650,7 +650,7 @@ export namespace batch_v1 {
      */
     provisioningModel?: string | null;
     /**
-     * Optional. If specified, VMs will consume only the specified reservation. If not specified (default), VMs will consume any applicable reservation.
+     * Optional. If specified, VMs will consume only the specified reservation. If not specified (default), VMs will consume any applicable reservation. Additionally, VMs will not consume any reservation if "NO_RESERVATION" is specified.
      */
     reservation?: string | null;
   }
@@ -671,7 +671,7 @@ export namespace batch_v1 {
      */
     installOpsAgent?: boolean | null;
     /**
-     * Name of an instance template used to create VMs. Named the field as 'instance_template' instead of 'template' to avoid c++ keyword conflict.
+     * Name of an instance template used to create VMs. Named the field as 'instance_template' instead of 'template' to avoid C++ keyword conflict. Batch only supports global instance templates. You can specify the global instance template as a full or partial URL.
      */
     instanceTemplate?: string | null;
     /**
@@ -713,7 +713,7 @@ export namespace batch_v1 {
      */
     createTime?: string | null;
     /**
-     * Labels for the Job. Labels could be user provided or system generated. For example, "labels": { "department": "finance", "environment": "test" \} You can assign up to 64 labels. [Google Compute Engine label restrictions](https://cloud.google.com/compute/docs/labeling-resources#restrictions) apply. Label names that start with "goog-" or "google-" are reserved.
+     * Custom labels to apply to the job and any Cloud Logging [LogEntry](https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry) that it generates. Use labels to group and describe the resources they are applied to. Batch automatically applies predefined labels and supports multiple `labels` fields for each job, which each let you apply custom labels to various resources. Label names that start with "goog-" or "google-" are reserved for predefined labels. For more information about labels with Batch, see [Organize resources using labels](https://cloud.google.com/batch/docs/organize-resources-using-labels).
      */
     labels?: {[key: string]: string} | null;
     /**
