954f5e76812e5f7552d096109898cbd8c264128a
Yoshi Automation <yoshi-automation@google.com>
2024-11-02 01:36:11 +0000

    fix(androidmanagement): update the API
    #### androidmanagement:v1
    
    The following keys were changed:
    - schemas.EnrollmentToken.properties.allowPersonalUsage.enumDescriptions
    - schemas.ExtensionConfig.description
    - schemas.SigninDetail.properties.allowPersonalUsage.enumDescriptions

--- src/apis/androidmanagement/v1.ts
+++ src/apis/androidmanagement/v1.ts
@@ -1282,7 +1282,7 @@ export namespace androidmanagement_v1 {
     termsAndConditions?: Schema$TermsAndConditions[];
   }
   /**
-   * Configuration to enable an app as an extension app, with the capability of interacting with Android Device Policy offline. For Android versions 13 and above, extension apps are exempt from battery restrictions so will not be placed into the restricted App Standby Bucket (https://developer.android.com/topic/performance/appstandby#restricted-bucket). Extensions apps are also protected against users clearing their data or force-closing the application, although admins can continue to use the clear app data command on extension apps if needed for Android 13 and above.
+   * Configuration to enable an app as an extension app, with the capability of interacting with Android Device Policy offline. For Android versions 11 and above, extension apps are exempt from battery restrictions so will not be placed into the restricted App Standby Bucket (https://developer.android.com/topic/performance/appstandby#restricted-bucket). Extensions apps are also protected against users clearing their data or force-closing the application, although admins can continue to use the clear app data command on extension apps if needed for Android 11 and above.
    */
   export interface Schema$ExtensionConfig {
     /**
