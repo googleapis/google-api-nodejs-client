e70d4930a475344b4b6e969cd5b954eb6b3fba8c
Yoshi Automation <yoshi-automation@google.com>
2025-01-28 01:35:35 +0000

    fix(chromepolicy): update the API
    #### chromepolicy:v1
    
    The following keys were changed:
    - schemas.Proto2FileDescriptorProto.properties.syntax.description

--- src/apis/chromepolicy/v1.ts
+++ src/apis/chromepolicy/v1.ts
@@ -923,7 +923,7 @@ export namespace chromepolicy_v1 {
      */
     package?: string | null;
     /**
-     * The syntax of the proto file. The supported values are "proto2", "proto3", and "editions". If `edition` is present, this value must be "editions".
+     * The syntax of the proto file. The supported values are "proto2", "proto3", and "editions". If `edition` is present, this value must be "editions". WARNING: This field should only be used by protobuf plugins or special cases like the proto compiler. Other uses are discouraged and developers should rely on the protoreflect APIs for their client language.
      */
     syntax?: string | null;
   }
