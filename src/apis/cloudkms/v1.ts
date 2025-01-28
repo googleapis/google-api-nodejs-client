9553db27e201b6dc066dbbed3d85381efc3b5775
Yoshi Automation <yoshi-automation@google.com>
2025-01-28 01:35:36 +0000

    fix(cloudkms): update the API
    #### cloudkms:v1
    
    The following keys were changed:
    - endpoints
    - schemas.MacVerifyRequest.properties.macCrc32c.description

--- src/apis/cloudkms/v1.ts
+++ src/apis/cloudkms/v1.ts
@@ -1008,7 +1008,7 @@ export namespace cloudkms_v1 {
      */
     mac?: string | null;
     /**
-     * Optional. An optional CRC32C checksum of the MacVerifyRequest.mac. If specified, KeyManagementService will verify the integrity of the received MacVerifyRequest.mac using this checksum. KeyManagementService will report an error if the checksum verification fails. If you receive a checksum error, your client should verify that CRC32C(MacVerifyRequest.tag) is equal to MacVerifyRequest.mac_crc32c, and if so, perform a limited number of retries. A persistent mismatch may indicate an issue in your computation of the CRC32C checksum. Note: This field is defined as int64 for reasons of compatibility across different languages. However, it is a non-negative integer, which will never exceed 2^32-1, and can be safely downconverted to uint32 in languages that support this type.
+     * Optional. An optional CRC32C checksum of the MacVerifyRequest.mac. If specified, KeyManagementService will verify the integrity of the received MacVerifyRequest.mac using this checksum. KeyManagementService will report an error if the checksum verification fails. If you receive a checksum error, your client should verify that CRC32C(MacVerifyRequest.mac) is equal to MacVerifyRequest.mac_crc32c, and if so, perform a limited number of retries. A persistent mismatch may indicate an issue in your computation of the CRC32C checksum. Note: This field is defined as int64 for reasons of compatibility across different languages. However, it is a non-negative integer, which will never exceed 2^32-1, and can be safely downconverted to uint32 in languages that support this type.
      */
     macCrc32c?: string | null;
   }
