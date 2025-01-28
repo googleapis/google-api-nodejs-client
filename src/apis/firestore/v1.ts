b4bc88da5e1bd546fa700ec0817383abc1f800fb
Yoshi Automation <yoshi-automation@google.com>
2025-01-28 01:35:41 +0000

    feat(firestore): update the API
    #### firestore:v1beta1
    
    The following keys were changed:
    - schemas.FindNearest.properties.distanceThreshold.description
    - schemas.StructuredQuery.description
    
    #### firestore:v1
    
    The following keys were added:
    - schemas.GoogleFirestoreAdminV1Index.properties.density.description
    - schemas.GoogleFirestoreAdminV1Index.properties.density.enum
    - schemas.GoogleFirestoreAdminV1Index.properties.density.enumDescriptions
    - schemas.GoogleFirestoreAdminV1Index.properties.density.type
    - schemas.GoogleFirestoreAdminV1Index.properties.multikey.description
    - schemas.GoogleFirestoreAdminV1Index.properties.multikey.type
    
    The following keys were changed:
    - resources.projects.resources.databases.resources.operations.methods.cancel.description
    - schemas.FindNearest.properties.distanceThreshold.description
    - schemas.GoogleFirestoreAdminV1Index.properties.apiScope.enum
    - schemas.GoogleFirestoreAdminV1Index.properties.apiScope.enumDescriptions
    - schemas.StructuredQuery.description

--- src/apis/firestore/v1.ts
+++ src/apis/firestore/v1.ts
@@ -618,7 +618,7 @@ export namespace firestore_v1 {
      */
     distanceResultField?: string | null;
     /**
-     * Optional. Option to specify a threshold for which no less similar documents will be returned. The behavior of the specified `distance_measure` will affect the meaning of the distance threshold. Since DOT_PRODUCT distances increase when the vectors are more similar, the comparison is inverted. For EUCLIDEAN, COSINE: WHERE distance <= distance_threshold For DOT_PRODUCT: WHERE distance \>= distance_threshold
+     * Optional. Option to specify a threshold for which no less similar documents will be returned. The behavior of the specified `distance_measure` will affect the meaning of the distance threshold. Since DOT_PRODUCT distances increase when the vectors are more similar, the comparison is inverted. * For EUCLIDEAN, COSINE: WHERE distance <= distance_threshold * For DOT_PRODUCT: WHERE distance \>= distance_threshold
      */
     distanceThreshold?: number | null;
     /**
@@ -1079,9 +1079,17 @@ export namespace firestore_v1 {
      */
     apiScope?: string | null;
     /**
+     * Immutable. The density configuration of the index.
+     */
+    density?: string | null;
+    /**
      * The fields supported by this index. For composite indexes, this requires a minimum of 2 and a maximum of 100 fields. The last field entry is always for the field path `__name__`. If, on creation, `__name__` was not specified as the last field, it will be added automatically with the same direction as that of the last field defined. If the final field in a composite index is not directional, the `__name__` will be ordered ASCENDING (unless explicitly specified). For single field indexes, this will always be exactly one entry with a field path equal to the field path of the associated field.
      */
     fields?: Schema$GoogleFirestoreAdminV1IndexField[];
+    /**
+     * Optional. Whether the index is multikey. By default, the index is not multikey. For non-multikey indexes, none of the paths in the index definition reach or traverse an array, except via an explicit array index. For multikey indexes, at most one of the paths in the index definition reach or traverse an array, except via an explicit array index. Violations will result in errors. Note this field only applies to index with IGNITE_API ApiScope.
+     */
+    multikey?: boolean | null;
     /**
      * Output only. A server defined name for this index. The form of this name for composite indexes will be: `projects/{project_id\}/databases/{database_id\}/collectionGroups/{collection_id\}/indexes/{composite_index_id\}` For single field indexes, this field will be empty.
      */
@@ -1821,7 +1829,7 @@ export namespace firestore_v1 {
     structuredQuery?: Schema$StructuredQuery;
   }
   /**
-   * A Firestore query. The query stages are executed in the following order: 1. from 2. where 3. select 4. order_by + start_at + end_at 5. offset 6. limit
+   * A Firestore query. The query stages are executed in the following order: 1. from 2. where 3. select 4. order_by + start_at + end_at 5. offset 6. limit 7. find_nearest
    */
   export interface Schema$StructuredQuery {
     /**
@@ -6234,7 +6242,7 @@ export namespace firestore_v1 {
     }
 
     /**
-     * Starts asynchronous cancellation on a long-running operation. The server makes a best effort to cancel the operation, but success is not guaranteed. If the server doesn't support this method, it returns `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other methods to check whether the cancellation succeeded or whether the operation completed despite cancellation. On successful cancellation, the operation is not deleted; instead, it becomes an operation with an Operation.error value with a google.rpc.Status.code of 1, corresponding to `Code.CANCELLED`.
+     * Starts asynchronous cancellation on a long-running operation. The server makes a best effort to cancel the operation, but success is not guaranteed. If the server doesn't support this method, it returns `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other methods to check whether the cancellation succeeded or whether the operation completed despite cancellation. On successful cancellation, the operation is not deleted; instead, it becomes an operation with an Operation.error value with a google.rpc.Status.code of `1`, corresponding to `Code.CANCELLED`.
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
