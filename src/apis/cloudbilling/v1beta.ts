348cec643a6d10c63541b624d804abadd384277c
Yoshi Automation <yoshi-automation@google.com>
2025-01-28 01:35:36 +0000

    feat(cloudbilling): update the API
    #### cloudbilling:v1beta
    
    The following keys were changed:
    - schemas.Decimal.description
    
    #### cloudbilling:v1
    
    The following keys were added:
    - schemas.BillingAccount.properties.currencyCode.description
    - schemas.BillingAccount.properties.currencyCode.type

--- src/apis/cloudbilling/v1beta.ts
+++ src/apis/cloudbilling/v1beta.ts
@@ -418,7 +418,7 @@ export namespace cloudbilling_v1beta {
     virtualCpuCount?: string | null;
   }
   /**
-   * A representation of a decimal value, such as 2.5. Clients may convert values into language-native decimal formats, such as Java's BigDecimal or Python's decimal.Decimal. [BigDecimal]: https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/math/BigDecimal.html [decimal.Decimal]: https://docs.python.org/3/library/decimal.html
+   * A representation of a decimal value, such as 2.5. Clients may convert values into language-native decimal formats, such as Java's [BigDecimal](https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/math/BigDecimal.html) or Python's [decimal.Decimal](https://docs.python.org/3/library/decimal.html).
    */
   export interface Schema$Decimal {
     /**
