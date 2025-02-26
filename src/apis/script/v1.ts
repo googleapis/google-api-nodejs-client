875690461391be6d06aa4f4da2e47e509a7838a5
Yoshi Automation <yoshi-automation@google.com>
2025-02-26 01:36:46 +0000

    fix(script): update the API

--- src/apis/script/v1.ts
+++ src/apis/script/v1.ts
@@ -2344,7 +2344,6 @@ export namespace script_v1 {
     }
 
     /**
-     * Runs a function in an Apps Script project. The script project must be deployed for use with the Apps Script API and the calling application must share the same Cloud Platform project. This method requires authorization with an OAuth 2.0 token that includes at least one of the scopes listed in the [Authorization](#authorization-scopes) section; script projects that do not require authorization cannot be executed through this API. To find the correct scopes to include in the authentication token, open the script project **Overview** page and scroll down to "Project OAuth Scopes." The error `403, PERMISSION_DENIED: The caller does not have permission` indicates that the Cloud Platform project used to authorize the request is not the same as the one used by the script.
      *
      * @param params - Parameters for request
      * @param options - Optionally override request options, such as `url`, `method`, and `encoding`.
