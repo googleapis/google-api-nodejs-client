cd143438037c66e9cb97601b2177fb53bcbd4a32
Yoshi Automation <yoshi-automation@google.com>
2025-02-26 01:36:42 +0000

    feat(file): update the API
    #### file:v1beta1
    
    The following keys were added:
    - schemas.PromoteReplicaRequest.properties.peerInstance.description
    - schemas.PromoteReplicaRequest.properties.peerInstance.type
    
    The following keys were changed:
    - schemas.PerformanceConfig.properties.iopsPerTb.description
    - schemas.ReplicaConfig.properties.stateReasons.items.enum
    - schemas.ReplicaConfig.properties.stateReasons.items.enumDescriptions
    
    #### file:v1
    
    The following keys were added:
    - schemas.PromoteReplicaRequest.properties.peerInstance.description
    - schemas.PromoteReplicaRequest.properties.peerInstance.type
    
    The following keys were changed:
    - schemas.PerformanceConfig.properties.iopsPerTb.description
    - schemas.ReplicaConfig.properties.stateReasons.items.enum
    - schemas.ReplicaConfig.properties.stateReasons.items.enumDescriptions

--- src/apis/file/v1.ts
+++ src/apis/file/v1.ts
@@ -850,7 +850,7 @@ export namespace file_v1 {
      */
     fixedIops?: Schema$FixedIOPS;
     /**
-     * Provision IOPS dynamically based on the capacity of the instance. Provisioned read IOPS will be calculated by multiplying the capacity of the instance in TiB by the `iops_per_tb` value. For example, for a 2 TiB instance with an `iops_per_tb` value of 17000 the provisioned read IOPS will be 34000. If the calculated value is outside the supported range for the instance's capacity during instance creation, instance creation will fail with an `InvalidArgument` error. Similarly, if an instance capacity update would result in a value outside the supported range, the update will fail with an `InvalidArgument` error.
+     * Provision IOPS dynamically based on the capacity of the instance. Provisioned IOPS will be calculated by multiplying the capacity of the instance in TiB by the `iops_per_tb` value. For example, for a 2 TiB instance with an `iops_per_tb` value of 17000 the provisioned IOPS will be 34000. If the calculated value is outside the supported range for the instance's capacity during instance creation, instance creation will fail with an `InvalidArgument` error. Similarly, if an instance capacity update would result in a value outside the supported range, the update will fail with an `InvalidArgument` error.
      */
     iopsPerTb?: Schema$IOPSPerTB;
   }
@@ -882,7 +882,12 @@ export namespace file_v1 {
   /**
    * PromoteReplicaRequest promotes a Filestore standby instance (replica).
    */
-  export interface Schema$PromoteReplicaRequest {}
+  export interface Schema$PromoteReplicaRequest {
+    /**
+     * Optional. The resource name of the peer instance to promote, in the format `projects/{project_id\}/locations/{location_id\}/instances/{instance_id\}`. The peer instance is required if the operation is called on an active instance.
+     */
+    peerInstance?: string | null;
+  }
   /**
    * Replica configuration for the instance.
    */
