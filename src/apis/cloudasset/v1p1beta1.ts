d70aca1c64808da5e6b09bb3ddb9fbac5a9f8bd1
Yoshi Automation <yoshi-automation@google.com>
2025-03-17 01:36:42 +0000

    feat(cloudasset): update the API
    #### cloudasset:v1p1beta1
    
    The following keys were added:
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.description
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.items.type
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.type
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.description
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.items.type
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.type
    
    The following keys were changed:
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressSource.properties.resource.description
    - schemas.IamPolicySearchResult.properties.project.description
    
    #### cloudasset:v1p5beta1
    
    The following keys were added:
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.description
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.items.type
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.type
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.description
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.items.type
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.type
    
    The following keys were changed:
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressSource.properties.resource.description
    
    #### cloudasset:v1p7beta1
    
    The following keys were added:
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.description
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.items.type
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.type
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.description
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.items.type
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.type
    
    The following keys were changed:
    - schemas.GoogleCloudAssetV1p7beta1PartitionSpec.properties.partitionKey.enumDescriptions
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressSource.properties.resource.description
    
    #### cloudasset:v1beta1
    
    The following keys were added:
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.description
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.items.type
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.type
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.description
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.items.type
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.type
    
    The following keys were changed:
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressSource.properties.resource.description
    
    #### cloudasset:v1
    
    The following keys were added:
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.description
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.items.type
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressTo.properties.roles.type
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.description
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.items.type
    - schemas.GoogleIdentityAccesscontextmanagerV1IngressTo.properties.roles.type
    
    The following keys were changed:
    - resources.v1.methods.analyzeMove.parameters.destinationParent.description
    - schemas.GoogleCloudAssetV1BigQueryDestination.properties.partitionKey.enumDescriptions
    - schemas.GoogleIdentityAccesscontextmanagerV1EgressSource.properties.resource.description
    - schemas.IamPolicySearchResult.properties.project.description
    - schemas.PartitionSpec.properties.partitionKey.enumDescriptions

--- src/apis/cloudasset/v1p1beta1.ts
+++ src/apis/cloudasset/v1p1beta1.ts
@@ -610,7 +610,7 @@ export namespace cloudasset_v1p1beta1 {
      */
     accessLevel?: string | null;
     /**
-     * A Google Cloud resource that you want to allow to egress the perimeter. These resources can access data outside the perimeter. This field only supports projects. The project format is `projects/{project_number\}`. The resource can be in any Google Cloud organization, not just the organization where the perimeter is defined. You can't use `*` in this field to allow all Google Cloud resources.
+     * A Google Cloud resource from the service perimeter that you want to allow to access data outside the perimeter. This field supports only projects. The project format is `projects/{project_number\}`. You can't use `*` in this field to allow all Google Cloud resources.
      */
     resource?: string | null;
   }
@@ -630,6 +630,10 @@ export namespace cloudasset_v1p1beta1 {
      * A list of resources, currently only projects in the form `projects/`, that are allowed to be accessed by sources defined in the corresponding EgressFrom. A request matches if it contains a resource in this list. If `*` is specified for `resources`, then this EgressTo rule will authorize access to all resources outside the perimeter.
      */
     resources?: string[] | null;
+    /**
+     * IAM roles that represent the set of operations that the sources specified in the corresponding EgressFrom. are allowed to perform in this ServicePerimeter.
+     */
+    roles?: string[] | null;
   }
   /**
    * Defines the conditions under which an IngressPolicy matches a request. Conditions are based on information about the source of the request. The request must satisfy what is defined in `sources` AND identity related fields in order to match.
@@ -690,6 +694,10 @@ export namespace cloudasset_v1p1beta1 {
      * A list of resources, currently only projects in the form `projects/`, protected by this ServicePerimeter that are allowed to be accessed by sources defined in the corresponding IngressFrom. If a single `*` is specified, then access to all resources inside the perimeter are allowed.
      */
     resources?: string[] | null;
+    /**
+     * IAM roles that represent the set of operations that the sources specified in the corresponding IngressFrom are allowed to perform in this ServicePerimeter.
+     */
+    roles?: string[] | null;
   }
   /**
    * An allowed method or permission of a service specified in ApiOperation.
@@ -835,7 +843,7 @@ export namespace cloudasset_v1p1beta1 {
      */
     policy?: Schema$Policy;
     /**
-     * The project that the associated Google Cloud resource belongs to, in the form of `projects/{project_number\}`. If an IAM policy is set on a resource -- such as a Compute Engine instance or a Cloud Storage bucket -- the project field will indicate the project that contains the resource. If an IAM policy is set on a folder or orgnization, the project field will be empty.
+     * The project that the associated Google Cloud resource belongs to, in the form of `projects/{project_number\}`. If an IAM policy is set on a resource -- such as a Compute Engine instance or a Cloud Storage bucket -- the project field will indicate the project that contains the resource. If an IAM policy is set on a folder or organization, the project field will be empty.
      */
     project?: string | null;
     /**
