6a229bac381057a584778ac7448c29ef742b38e5
Yoshi Automation <yoshi-automation@google.com>
2025-03-17 01:36:50 +0000

    feat(serviceconsumermanagement): update the API
    #### serviceconsumermanagement:v1beta1
    
    The following keys were added:
    - schemas.Aspect.description
    - schemas.Aspect.id
    - schemas.Aspect.properties.kind.description
    - schemas.Aspect.properties.kind.type
    - schemas.Aspect.properties.spec.additionalProperties.description
    - schemas.Aspect.properties.spec.additionalProperties.type
    - schemas.Aspect.properties.spec.description
    - schemas.Aspect.properties.spec.type
    - schemas.Aspect.type
    - schemas.BackendRule.properties.loadBalancingPolicy.description
    - schemas.BackendRule.properties.loadBalancingPolicy.type
    - schemas.ExperimentalFeatures.properties.unversionedPackageDisabled.description
    - schemas.ExperimentalFeatures.properties.unversionedPackageDisabled.type
    - schemas.SelectiveGapicGeneration.properties.generateOmittedAsInternal.description
    - schemas.SelectiveGapicGeneration.properties.generateOmittedAsInternal.type
    - schemas.Service.properties.aspects.description
    - schemas.Service.properties.aspects.items.$ref
    - schemas.Service.properties.aspects.type
    
    The following keys were changed:
    - schemas.Documentation.properties.sectionOverrides.description
    - schemas.DocumentationRule.properties.disableReplacementWords.description
    - schemas.Page.properties.content.description
    - schemas.UsageRule.description
    - schemas.UsageRule.properties.allowUnregisteredCalls.description
    
    #### serviceconsumermanagement:v1
    
    The following keys were added:
    - schemas.Aspect.description
    - schemas.Aspect.id
    - schemas.Aspect.properties.kind.description
    - schemas.Aspect.properties.kind.type
    - schemas.Aspect.properties.spec.additionalProperties.description
    - schemas.Aspect.properties.spec.additionalProperties.type
    - schemas.Aspect.properties.spec.description
    - schemas.Aspect.properties.spec.type
    - schemas.Aspect.type
    - schemas.BackendRule.properties.loadBalancingPolicy.description
    - schemas.BackendRule.properties.loadBalancingPolicy.type
    - schemas.ExperimentalFeatures.properties.unversionedPackageDisabled.description
    - schemas.ExperimentalFeatures.properties.unversionedPackageDisabled.type
    - schemas.SelectiveGapicGeneration.properties.generateOmittedAsInternal.description
    - schemas.SelectiveGapicGeneration.properties.generateOmittedAsInternal.type
    - schemas.Service.properties.aspects.description
    - schemas.Service.properties.aspects.items.$ref
    - schemas.Service.properties.aspects.type
    
    The following keys were changed:
    - schemas.Documentation.properties.sectionOverrides.description
    - schemas.DocumentationRule.properties.disableReplacementWords.description
    - schemas.Page.properties.content.description
    - schemas.UsageRule.description
    - schemas.UsageRule.properties.allowUnregisteredCalls.description

--- src/apis/serviceconsumermanagement/v1.ts
+++ src/apis/serviceconsumermanagement/v1.ts
@@ -186,6 +186,19 @@ export namespace serviceconsumermanagement_v1 {
     tag?: string | null;
   }
   /**
+   * Aspect represents Generic aspect. It is used to configure an aspect without making direct changes to service.proto
+   */
+  export interface Schema$Aspect {
+    /**
+     * The type of this aspect configuration.
+     */
+    kind?: string | null;
+    /**
+     * Content of the configuration. The underlying schema should be defined by Aspect owners as protobuf message under `apiserving/configaspects/proto`.
+     */
+    spec?: {[key: string]: any} | null;
+  }
+  /**
    * Request to attach an existing project to the tenancy unit as a new tenant resource.
    */
   export interface Schema$AttachTenantProjectRequest {
@@ -307,6 +320,10 @@ export namespace serviceconsumermanagement_v1 {
      * The JWT audience is used when generating a JWT ID token for the backend. This ID token will be added in the HTTP "authorization" header, and sent to the backend.
      */
     jwtAudience?: string | null;
+    /**
+     * The load balancing policy used for connection to the application backend. Defined as an arbitrary string to accomondate custom load balancing policies supported by the underlying channel, but suggest most users use one of the standard policies, such as the default, "RoundRobin".
+     */
+    loadBalancingPolicy?: string | null;
     /**
      * Deprecated, do not use.
      */
@@ -568,7 +585,7 @@ export namespace serviceconsumermanagement_v1 {
      */
     rules?: Schema$DocumentationRule[];
     /**
-     * Specifies section and content to override boilerplate content provided by go/api-docgen. Currently overrides following sections: 1. rest.service.client_libraries
+     * Specifies section and content to override the boilerplate content. Currently overrides following sections: 1. rest.service.client_libraries
      */
     sectionOverrides?: Schema$Page[];
     /**
@@ -593,7 +610,7 @@ export namespace serviceconsumermanagement_v1 {
      */
     description?: string | null;
     /**
-     * String of comma or space separated case-sensitive words for which method/field name replacement will be disabled by go/api-docgen.
+     * String of comma or space separated case-sensitive words for which method/field name replacement will be disabled.
      */
     disableReplacementWords?: string | null;
     /**
@@ -713,6 +730,10 @@ export namespace serviceconsumermanagement_v1 {
      * Enables generation of asynchronous REST clients if `rest` transport is enabled. By default, asynchronous REST clients will not be generated. This feature will be enabled by default 1 month after launching the feature in preview packages.
      */
     restAsyncIoEnabled?: boolean | null;
+    /**
+     * Disables generation of an unversioned Python package for this client library. This means that the module names will need to be versioned in import statements. For example `import google.cloud.library_v2` instead of `import google.cloud.library`.
+     */
+    unversionedPackageDisabled?: boolean | null;
   }
   /**
    * A single field of a message type.
@@ -1271,7 +1292,7 @@ export namespace serviceconsumermanagement_v1 {
    */
   export interface Schema$Page {
     /**
-     * The Markdown content of the page. You can use (== include {path\} ==) to include content from a Markdown file. The content can be used to produce the documentation page such as HTML format page.
+     * The Markdown content of the page. You can use ```(== include {path\} ==)``` to include content from a Markdown file. The content can be used to produce the documentation page such as HTML format page.
      */
     content?: string | null;
     /**
@@ -1461,6 +1482,10 @@ export namespace serviceconsumermanagement_v1 {
    */
   export interface Schema$SelectiveGapicGeneration {
     /**
+     * Setting this to true indicates to the client generators that methods that would be excluded from the generation should instead be generated in a way that indicates these methods should not be consumed by end users. How this is expressed is up to individual language implementations to decide. Some examples may be: added annotations, obfuscated identifiers, or other language idiomatic patterns.
+     */
+    generateOmittedAsInternal?: boolean | null;
+    /**
      * An allowlist of the fully qualified names of RPCs that should be included on public client surfaces.
      */
     methods?: string[] | null;
@@ -1473,6 +1498,10 @@ export namespace serviceconsumermanagement_v1 {
      * A list of API interfaces exported by this service. Only the `name` field of the google.protobuf.Api needs to be provided by the configuration author, as the remaining fields will be derived from the IDL during the normalization process. It is an error to specify an API interface here which cannot be resolved against the associated IDL files.
      */
     apis?: Schema$Api[];
+    /**
+     * Configuration aspects. This is a repeated field to allow multiple aspects to be configured. The kind field in each ConfigAspect specifies the type of aspect. The spec field contains the configuration for that aspect. The schema for the spec field is defined by the backend service owners.
+     */
+    aspects?: Schema$Aspect[];
     /**
      * Auth configuration.
      */
@@ -1809,11 +1838,11 @@ export namespace serviceconsumermanagement_v1 {
     rules?: Schema$UsageRule[];
   }
   /**
-   * Usage configuration rules for the service. NOTE: Under development. Use this rule to configure unregistered calls for the service. Unregistered calls are calls that do not contain consumer project identity. (Example: calls that do not contain an API key). By default, API methods do not allow unregistered calls, and each method call must be identified by a consumer project identity. Use this rule to allow/disallow unregistered calls. Example of an API that wants to allow unregistered calls for entire service. usage: rules: - selector: "*" allow_unregistered_calls: true Example of a method that wants to allow unregistered calls. usage: rules: - selector: "google.example.library.v1.LibraryService.CreateBook" allow_unregistered_calls: true
+   * Usage configuration rules for the service.
    */
   export interface Schema$UsageRule {
     /**
-     * If true, the selected method allows unregistered calls, e.g. calls that don't identify any user or application.
+     *  Use this rule to configure unregistered calls for the service. Unregistered calls are calls that do not contain consumer project identity. (Example: calls that do not contain an API key). WARNING: By default, API methods do not allow unregistered calls, and each method call must be identified by a consumer project identity.
      */
     allowUnregisteredCalls?: boolean | null;
     /**
