5448212a42323a16755388809120ed04b8686fc0
Yoshi Automation <yoshi-automation@google.com>
2025-03-18 00:03:47 +0000

    feat(monitoring): update the API
    #### monitoring:v1
    
    The following keys were added:
    - schemas.Scorecard.properties.breakdowns.description
    - schemas.Scorecard.properties.breakdowns.items.$ref
    - schemas.Scorecard.properties.breakdowns.type
    - schemas.SingleViewGroup.properties.displayType.description
    - schemas.SingleViewGroup.properties.displayType.enum
    - schemas.SingleViewGroup.properties.displayType.enumDescriptions
    - schemas.SingleViewGroup.properties.displayType.type
    - schemas.TemplateVariableCondition.description
    - schemas.TemplateVariableCondition.id
    - schemas.TemplateVariableCondition.properties.comparator.description
    - schemas.TemplateVariableCondition.properties.comparator.enum
    - schemas.TemplateVariableCondition.properties.comparator.enumDescriptions
    - schemas.TemplateVariableCondition.properties.comparator.type
    - schemas.TemplateVariableCondition.properties.templateVariable.description
    - schemas.TemplateVariableCondition.properties.templateVariable.type
    - schemas.TemplateVariableCondition.properties.templateVariableValue.description
    - schemas.TemplateVariableCondition.properties.templateVariableValue.type
    - schemas.TemplateVariableCondition.type
    - schemas.VisibilityCondition.description
    - schemas.VisibilityCondition.id
    - schemas.VisibilityCondition.properties.templateVariableCondition.$ref
    - schemas.VisibilityCondition.properties.templateVariableCondition.description
    - schemas.VisibilityCondition.type
    - schemas.Widget.properties.visibilityCondition.$ref
    - schemas.Widget.properties.visibilityCondition.description
    
    #### monitoring:v3
    
    The following keys were added:
    - schemas.Criteria.properties.filter.description
    - schemas.Criteria.properties.filter.type
    - schemas.UptimeCheckConfig.properties.logCheckFailures.description
    - schemas.UptimeCheckConfig.properties.logCheckFailures.type
    
    The following keys were changed:
    - resources.projects.resources.snoozes.methods.list.parameters.filter.description
    - schemas.Criteria.properties.policies.description

--- src/apis/monitoring/v3.ts
+++ src/apis/monitoring/v3.ts
@@ -576,7 +576,11 @@ export namespace monitoring_v3 {
    */
   export interface Schema$Criteria {
     /**
-     * The specific AlertPolicy names for the alert that should be snoozed. The format is: projects/[PROJECT_ID_OR_NUMBER]/alertPolicies/[POLICY_ID] There is a limit of 16 policies per snooze. This limit is checked during snooze creation.
+     * Optional. The filter string to match on Alert fields when silencing the alerts. It follows the standard https://google.aip.dev/160 syntax. A filter string used to apply the snooze to specific incidents that have matching filter values. Filters can be defined for snoozes that apply to one alerting policy. Filters must be a string formatted as one or more resource labels with specific label values. If multiple resource labels are used, then they must be connected with an AND operator. For example, the following filter applies the snooze to incidents that have an instance ID of 1234567890 and a zone of us-central1-a: resource.labels.instance_id="1234567890" AND resource.labels.zone="us-central1-a"
+     */
+    filter?: string | null;
+    /**
+     * The specific AlertPolicy names for the alert that should be snoozed. The format is: projects/[PROJECT_ID_OR_NUMBER]/alertPolicies/[POLICY_ID] There is a limit of 16 policies per snooze. This limit is checked during snooze creation. Exactly 1 alert policy is required if filter is specified at the same time.
      */
     policies?: string[] | null;
   }
@@ -2386,6 +2390,10 @@ export namespace monitoring_v3 {
      * If this is true, then checks are made only from the 'internal_checkers'. If it is false, then checks are made only from the 'selected_regions'. It is an error to provide 'selected_regions' when is_internal is true, or to provide 'internal_checkers' when is_internal is false.
      */
     isInternal?: boolean | null;
+    /**
+     * To specify whether to log the results of failed probes to Cloud Logging.
+     */
+    logCheckFailures?: boolean | null;
     /**
      * The monitored resource (https://cloud.google.com/monitoring/api/resources) associated with the configuration. The following monitored resource types are valid for this field: uptime_url, gce_instance, gae_app, aws_ec2_instance, aws_elb_load_balancer k8s_service servicedirectory_service cloud_run_revision
      */
@@ -6232,7 +6240,7 @@ export namespace monitoring_v3 {
   export interface Params$Resource$Projects$Snoozes$List
     extends StandardParameters {
     /**
-     * Optional. Optional filter to restrict results to the given criteria. The following fields are supported. interval.start_time interval.end_timeFor example: ``` interval.start_time \> "2022-03-11T00:00:00-08:00" AND interval.end_time < "2022-03-12T00:00:00-08:00" ```
+     * Optional. Optional filter to restrict results to the given criteria. The following fields are supported. interval.start_time interval.end_timeFor example: interval.start_time \> "2022-03-11T00:00:00-08:00" AND interval.end_time < "2022-03-12T00:00:00-08:00"
      */
     filter?: string;
     /**
