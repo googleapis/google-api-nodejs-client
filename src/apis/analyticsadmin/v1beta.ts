7ac4ed3e6aedb24bf46f188a3e33badb40105db9
Yoshi Automation <yoshi-automation@google.com>
2025-03-18 00:03:39 +0000

    feat(analyticsadmin): update the API
    #### analyticsadmin:v1alpha
    
    The following keys were added:
    - schemas.GoogleAnalyticsAdminV1alphaChangeHistoryChangeChangeHistoryResource.properties.keyEvent.$ref
    - schemas.GoogleAnalyticsAdminV1alphaChangeHistoryChangeChangeHistoryResource.properties.keyEvent.description
    - schemas.GoogleAnalyticsAdminV1alphaDataSharingSettings.properties.sharingWithGoogleAnySalesEnabled.deprecated
    
    The following keys were changed:
    - schemas.GoogleAnalyticsAdminV1alphaDataSharingSettings.properties.sharingWithGoogleAnySalesEnabled.description
    - schemas.GoogleAnalyticsAdminV1alphaDataSharingSettings.properties.sharingWithGoogleAssignedSalesEnabled.description
    - schemas.GoogleAnalyticsAdminV1alphaDataSharingSettings.properties.sharingWithGoogleProductsEnabled.description
    - schemas.GoogleAnalyticsAdminV1alphaDataSharingSettings.properties.sharingWithGoogleSupportEnabled.description
    - schemas.GoogleAnalyticsAdminV1alphaDataSharingSettings.properties.sharingWithOthersEnabled.description
    - schemas.GoogleAnalyticsAdminV1alphaSearchChangeHistoryEventsRequest.properties.resourceType.items.enum
    - schemas.GoogleAnalyticsAdminV1alphaSearchChangeHistoryEventsRequest.properties.resourceType.items.enumDescriptions
    
    #### analyticsadmin:v1beta
    
    The following keys were added:
    - schemas.GoogleAnalyticsAdminV1betaDataSharingSettings.properties.sharingWithGoogleAnySalesEnabled.deprecated
    
    The following keys were changed:
    - schemas.GoogleAnalyticsAdminV1betaDataSharingSettings.properties.sharingWithGoogleAnySalesEnabled.description
    - schemas.GoogleAnalyticsAdminV1betaDataSharingSettings.properties.sharingWithGoogleAssignedSalesEnabled.description
    - schemas.GoogleAnalyticsAdminV1betaDataSharingSettings.properties.sharingWithGoogleProductsEnabled.description
    - schemas.GoogleAnalyticsAdminV1betaDataSharingSettings.properties.sharingWithGoogleSupportEnabled.description
    - schemas.GoogleAnalyticsAdminV1betaDataSharingSettings.properties.sharingWithOthersEnabled.description

--- src/apis/analyticsadmin/v1beta.ts
+++ src/apis/analyticsadmin/v1beta.ts
@@ -696,23 +696,23 @@ export namespace analyticsadmin_v1beta {
      */
     name?: string | null;
     /**
-     * Allows any of Google sales to access the data in order to suggest configuration changes to improve results.
+     * Deprecated. This field is no longer used and always returns false.
      */
     sharingWithGoogleAnySalesEnabled?: boolean | null;
     /**
-     * Allows Google sales teams that are assigned to the customer to access the data in order to suggest configuration changes to improve results. Sales team restrictions still apply when enabled.
+     * Allows Google access to your Google Analytics account data, including account usage and configuration data, product spending, and users associated with your Google Analytics account, so that Google can help you make the most of Google products, providing you with insights, offers, recommendations, and optimization tips across Google Analytics and other Google products for business. This field maps to the "Recommendations for your business" field in the Google Analytics Admin UI.
      */
     sharingWithGoogleAssignedSalesEnabled?: boolean | null;
     /**
-     * Allows Google to use the data to improve other Google products or services.
+     * Allows Google to use the data to improve other Google products or services. This fields maps to the "Google products & services" field in the Google Analytics Admin UI.
      */
     sharingWithGoogleProductsEnabled?: boolean | null;
     /**
-     * Allows Google support to access the data in order to help troubleshoot issues.
+     * Allows Google technical support representatives access to your Google Analytics data and account when necessary to provide service and find solutions to technical issues. This field maps to the "Technical support" field in the Google Analytics Admin UI.
      */
     sharingWithGoogleSupportEnabled?: boolean | null;
     /**
-     * Allows Google to share the data anonymously in aggregate form with others.
+     * Enable features like predictions, modeled data, and benchmarking that can provide you with richer business insights when you contribute aggregated measurement data. The data you share (including information about the property from which it is shared) is aggregated and de-identified before being used to generate business insights. This field maps to the "Modeling contributions & business insights" field in the Google Analytics Admin UI.
      */
     sharingWithOthersEnabled?: boolean | null;
   }
