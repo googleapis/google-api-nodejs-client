bb057485881c61f76e8ad2c9ea80d41d58004305
Yoshi Automation <yoshi-automation@google.com>
2025-04-19 01:36:41 +0000

    fix(content): update the API
    #### content:v2.1
    
    The following keys were changed:
    - schemas.Product.description

--- src/apis/content/v2.1.ts
+++ src/apis/content/v2.1.ts
@@ -4213,7 +4213,7 @@ export namespace content_v2_1 {
     suggestedPriceMicros?: string | null;
   }
   /**
-   *  Required product attributes are primarily defined by the product data specification. See the Product Data Specification Help Center article for information. Product data. After inserting, updating, or deleting a product, it may take several minutes before changes take effect.
+   *  Required product attributes are primarily defined by the product data specification. See the Product Data Specification Help Center article for information. Product data. After inserting, updating, or deleting a product, it may take several minutes before changes take effect. The following reference documentation lists the field names in the **camelCase** casing style while the Products Data Specification lists the names in the **snake_case** casing style.
    */
   export interface Schema$Product {
     /**
