68285147673208872ed4426c00b06dc13ce39fb2
Yoshi Automation <yoshi-automation@google.com>
2025-05-02 01:37:25 +0000

    feat(apigateway): update the API
    #### apigateway:v1beta
    
    The following keys were added:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.location
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.repeated
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.type
    
    #### apigateway:v1
    
    The following keys were added:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.location
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.repeated
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.type

--- src/apis/apigateway/v1beta.ts
+++ src/apis/apigateway/v1beta.ts
@@ -838,6 +838,10 @@ export namespace apigateway_v1beta {
   export interface Params$Resource$Projects$Locations$List
     extends StandardParameters {
     /**
+     * Optional. A list of extra location types that should be used as conditions for controlling the visibility of the locations.
+     */
+    extraLocationTypes?: string[];
+    /**
      * A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      */
     filter?: string;
