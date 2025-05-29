14f62911b9e30fff5d06ebeb370a9f13a1cece07
Yoshi Automation <yoshi-automation@google.com>
2025-05-29 01:37:42 +0000

    feat(places)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### places:v1
    
    The following keys were deleted:
    - schemas.GoogleMapsPlacesV1Review.properties.visitDate.$ref
    - schemas.GoogleMapsPlacesV1Review.properties.visitDate.description
    - schemas.GoogleMapsPlacesV1ReviewVisitDate.description
    - schemas.GoogleMapsPlacesV1ReviewVisitDate.id
    - schemas.GoogleMapsPlacesV1ReviewVisitDate.properties.month.description
    - schemas.GoogleMapsPlacesV1ReviewVisitDate.properties.month.format
    - schemas.GoogleMapsPlacesV1ReviewVisitDate.properties.month.type
    - schemas.GoogleMapsPlacesV1ReviewVisitDate.properties.year.description
    - schemas.GoogleMapsPlacesV1ReviewVisitDate.properties.year.format
    - schemas.GoogleMapsPlacesV1ReviewVisitDate.properties.year.type
    - schemas.GoogleMapsPlacesV1ReviewVisitDate.type
    
    The following keys were added:
    - schemas.GoogleMapsPlacesV1PlaceReviewSummary.properties.reviewsUri.description
    - schemas.GoogleMapsPlacesV1PlaceReviewSummary.properties.reviewsUri.type
    
    The following keys were changed:
    - schemas.GoogleMapsPlacesV1PlaceGoogleMapsLinks.properties.photosUri.description
    - schemas.GoogleMapsPlacesV1PlaceGoogleMapsLinks.properties.reviewsUri.description
    - schemas.GoogleMapsPlacesV1PlaceGoogleMapsLinks.properties.writeAReviewUri.description

--- src/apis/places/v1.ts
+++ src/apis/places/v1.ts
@@ -1029,7 +1029,7 @@ export namespace places_v1 {
      */
     directionsUri?: string | null;
     /**
-     * A link to show photos of this place. This link is currently not supported on Google Maps Mobile and only works on the web version of Google Maps.
+     * A link to show reviews of this place on Google Maps.
      */
     photosUri?: string | null;
     /**
@@ -1037,11 +1037,11 @@ export namespace places_v1 {
      */
     placeUri?: string | null;
     /**
-     * A link to show reviews of this place. This link is currently not supported on Google Maps Mobile and only works on the web version of Google Maps.
+     * A link to show reviews of this place on Google Maps.
      */
     reviewsUri?: string | null;
     /**
-     * A link to write a review for this place. This link is currently not supported on Google Maps Mobile and only works on the web version of Google Maps.
+     * A link to write a review for this place on Google Maps.
      */
     writeAReviewUri?: string | null;
   }
@@ -1226,6 +1226,10 @@ export namespace places_v1 {
      */
     flagContentUri?: string | null;
     /**
+     * A link to show reviews of this place on Google Maps.
+     */
+    reviewsUri?: string | null;
+    /**
      * The summary of user reviews.
      */
     text?: Schema$GoogleTypeLocalizedText;
@@ -1305,23 +1309,6 @@ export namespace places_v1 {
      * The localized text of the review.
      */
     text?: Schema$GoogleTypeLocalizedText;
-    /**
-     * The date when the author visited the place. This is trucated to month.
-     */
-    visitDate?: Schema$GoogleMapsPlacesV1ReviewVisitDate;
-  }
-  /**
-   * The date when the author visited the place. This is trucated to month.
-   */
-  export interface Schema$GoogleMapsPlacesV1ReviewVisitDate {
-    /**
-     * The month the author visited the place, e.g. 4. The value is between 1 and 12.
-     */
-    month?: number | null;
-    /**
-     * The year the author visited the place, e.g. 2025.
-     */
-    year?: number | null;
   }
   /**
    * Encapsulates a set of optional conditions to satisfy when calculating the routes.
