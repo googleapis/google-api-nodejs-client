e60128f56eecca8f1044cde7cd73cf5a3063b402
Yoshi Automation <yoshi-automation@google.com>
2025-08-04 01:39:36 +0000

    fix(securesourcemanager): update the API

---
+++ src/apis/securesourcemanager/index.ts
@@ -0,0 +1,49 @@
+// Copyright 2020 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*! THIS FILE IS AUTO-GENERATED */
+
+import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
+import {securesourcemanager_v1} from './v1';
+
+export const VERSIONS = {
+  v1: securesourcemanager_v1.Securesourcemanager,
+};
+
+export function securesourcemanager(
+  version: 'v1'
+): securesourcemanager_v1.Securesourcemanager;
+export function securesourcemanager(
+  options: securesourcemanager_v1.Options
+): securesourcemanager_v1.Securesourcemanager;
+export function securesourcemanager<
+  T = securesourcemanager_v1.Securesourcemanager,
+>(
+  this: GoogleConfigurable,
+  versionOrOptions: 'v1' | securesourcemanager_v1.Options
+) {
+  return getAPI<T>('securesourcemanager', versionOrOptions, VERSIONS, this);
+}
+
+const auth = new AuthPlus();
+export {auth};
+export {securesourcemanager_v1};
+export {
+  AuthPlus,
+  GlobalOptions,
+  APIRequestContext,
+  GoogleConfigurable,
+  StreamMethodOptions,
+  MethodOptions,
+  BodyResponseCallback,
+} from 'googleapis-common';
