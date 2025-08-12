d75502811e6528b9f951dfdd5308b7123880fbed
Yoshi Automation <yoshi-automation@google.com>
2025-08-12 01:37:13 +0000

    fix(vision): update the API
    #### vision:v1p1beta1
    
    The following keys were changed:
    - schemas.GoogleCloudVisionV1p1beta1Feature.properties.model.description
    
    #### vision:v1p2beta1
    
    The following keys were changed:
    - schemas.GoogleCloudVisionV1p2beta1Feature.properties.model.description
    
    #### vision:v1
    
    The following keys were changed:
    - schemas.Feature.properties.model.description

--- src/apis/vision/v1.ts
+++ src/apis/vision/v1.ts
@@ -685,7 +685,7 @@ export namespace vision_v1 {
      */
     maxResults?: number | null;
     /**
-     * Model to use for the feature. Supported values: "builtin/stable" (the default if unset) and "builtin/latest". `DOCUMENT_TEXT_DETECTION` and `TEXT_DETECTION` also support "builtin/weekly" for the bleeding edge release updated weekly.
+     * Model to use for the feature. Supported values: "builtin/stable" (the default if unset) and "builtin/latest". `DOCUMENT_TEXT_DETECTION` and `TEXT_DETECTION` also support "builtin/rc" for the latest release candidate.
      */
     model?: string | null;
     /**
