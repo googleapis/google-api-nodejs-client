bf04bf8b3bb19da477deecfc6f24ef9ff643b4f5
Yoshi Automation <yoshi-automation@google.com>
2025-08-12 01:37:13 +0000

    fix(monitoring): update the API
    #### monitoring:v3
    
    The following keys were changed:
    - schemas.MetricAbsence.properties.duration.description
    - schemas.MetricThreshold.properties.duration.description
    - schemas.MonitoringQueryLanguageCondition.properties.duration.description

--- src/apis/monitoring/v3.ts
+++ src/apis/monitoring/v3.ts
@@ -1381,7 +1381,7 @@ export namespace monitoring_v3 {
      */
     aggregations?: Schema$Aggregation[];
     /**
-     * The amount of time that a time series must fail to report new data to be considered failing. The minimum value of this field is 120 seconds. Larger values that are a multiple of a minute--for example, 240 or 300 seconds--are supported. If an invalid value is given, an error will be returned. The Duration.nanos field is ignored.
+     * Required. The amount of time that a time series must fail to report new data to be considered failing. The minimum value of this field is 120 seconds. Larger values that are a multiple of a minute--for example, 240 or 300 seconds--are supported. If an invalid value is given, an error will be returned.
      */
     duration?: string | null;
     /**
@@ -1497,7 +1497,7 @@ export namespace monitoring_v3 {
      */
     denominatorFilter?: string | null;
     /**
-     * The amount of time that a time series must violate the threshold to be considered failing. Currently, only values that are a multiple of a minute--e.g., 0, 60, 120, or 300 seconds--are supported. If an invalid value is given, an error will be returned. When choosing a duration, it is useful to keep in mind the frequency of the underlying time series data (which may also be affected by any alignments specified in the aggregations field); a good duration is long enough so that a single outlier does not generate spurious alerts, but short enough that unhealthy states are detected and alerted on quickly.
+     * Required. The amount of time that a time series must violate the threshold to be considered failing. Currently, only values that are a multiple of a minute--e.g., 0, 60, 120, or 300 seconds--are supported. If an invalid value is given, an error will be returned. When choosing a duration, it is useful to keep in mind the frequency of the underlying time series data (which may also be affected by any alignments specified in the aggregations field); a good duration is long enough so that a single outlier does not generate spurious alerts, but short enough that unhealthy states are detected and alerted on quickly.
      */
     duration?: string | null;
     /**
@@ -1590,7 +1590,7 @@ export namespace monitoring_v3 {
    */
   export interface Schema$MonitoringQueryLanguageCondition {
     /**
-     * The amount of time that a time series must violate the threshold to be considered failing. Currently, only values that are a multiple of a minute--e.g., 0, 60, 120, or 300 seconds--are supported. If an invalid value is given, an error will be returned. When choosing a duration, it is useful to keep in mind the frequency of the underlying time series data (which may also be affected by any alignments specified in the aggregations field); a good duration is long enough so that a single outlier does not generate spurious alerts, but short enough that unhealthy states are detected and alerted on quickly.
+     * Optional. The amount of time that a time series must violate the threshold to be considered failing. Currently, only values that are a multiple of a minute--e.g., 0, 60, 120, or 300 seconds--are supported. If an invalid value is given, an error will be returned. When choosing a duration, it is useful to keep in mind the frequency of the underlying time series data (which may also be affected by any alignments specified in the aggregations field); a good duration is long enough so that a single outlier does not generate spurious alerts, but short enough that unhealthy states are detected and alerted on quickly. The default value is zero.
      */
     duration?: string | null;
     /**
