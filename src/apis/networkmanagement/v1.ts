2e92cb8cc0d97df1712bf3e5d99563dc829fc3fb
Yoshi Automation <yoshi-automation@google.com>
2025-08-13 15:19:37 +0000

    feat(networkmanagement)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### networkmanagement:v1
    
    The following keys were deleted:
    - schemas.MonitoringPoint.properties.geoLocation.type
    - schemas.NetworkPath.properties.destinationGeoLocation.type
    
    The following keys were added:
    - schemas.DropInfo.properties.destinationGeolocationCode.description
    - schemas.DropInfo.properties.destinationGeolocationCode.type
    - schemas.DropInfo.properties.sourceGeolocationCode.description
    - schemas.DropInfo.properties.sourceGeolocationCode.type
    - schemas.FirewallInfo.properties.targetType.description
    - schemas.FirewallInfo.properties.targetType.enum
    - schemas.FirewallInfo.properties.targetType.enumDescriptions
    - schemas.FirewallInfo.properties.targetType.type
    - schemas.GeoLocation.description
    - schemas.GeoLocation.id
    - schemas.GeoLocation.properties.country.description
    - schemas.GeoLocation.properties.country.type
    - schemas.GeoLocation.properties.formattedAddress.description
    - schemas.GeoLocation.properties.formattedAddress.type
    - schemas.GeoLocation.type
    - schemas.InterconnectAttachmentInfo.description
    - schemas.InterconnectAttachmentInfo.id
    - schemas.InterconnectAttachmentInfo.properties.cloudRouterUri.description
    - schemas.InterconnectAttachmentInfo.properties.cloudRouterUri.type
    - schemas.InterconnectAttachmentInfo.properties.displayName.description
    - schemas.InterconnectAttachmentInfo.properties.displayName.type
    - schemas.InterconnectAttachmentInfo.properties.interconnectUri.description
    - schemas.InterconnectAttachmentInfo.properties.interconnectUri.type
    - schemas.InterconnectAttachmentInfo.properties.region.description
    - schemas.InterconnectAttachmentInfo.properties.region.type
    - schemas.InterconnectAttachmentInfo.properties.uri.description
    - schemas.InterconnectAttachmentInfo.properties.uri.type
    - schemas.InterconnectAttachmentInfo.type
    - schemas.MonitoringPoint.properties.geoLocation.$ref
    - schemas.MonitoringPoint.properties.upgradeAvailable.description
    - schemas.MonitoringPoint.properties.upgradeAvailable.readOnly
    - schemas.MonitoringPoint.properties.upgradeAvailable.type
    - schemas.NetworkMonitoringProvider.properties.errors.description
    - schemas.NetworkMonitoringProvider.properties.errors.items.type
    - schemas.NetworkMonitoringProvider.properties.errors.readOnly
    - schemas.NetworkMonitoringProvider.properties.errors.type
    - schemas.NetworkPath.properties.destinationGeoLocation.$ref
    - schemas.Step.properties.interconnectAttachment.$ref
    - schemas.Step.properties.interconnectAttachment.description
    
    The following keys were changed:
    - schemas.MonitoringPoint.properties.geoLocation.description
    - schemas.NetworkPath.properties.destinationGeoLocation.description
    - schemas.ProviderTag.properties.resourceType.enum
    - schemas.ProviderTag.properties.resourceType.enumDescriptions
    - schemas.Step.properties.state.enum
    - schemas.Step.properties.state.enumDeprecated
    - schemas.Step.properties.state.enumDescriptions

--- src/apis/networkmanagement/v1.ts
+++ src/apis/networkmanagement/v1.ts
@@ -445,6 +445,10 @@ export namespace networkmanagement_v1 {
      */
     cause?: string | null;
     /**
+     * Geolocation (region code) of the destination IP address (if relevant).
+     */
+    destinationGeolocationCode?: string | null;
+    /**
      * Destination IP address of the dropped packet (if relevant).
      */
     destinationIp?: string | null;
@@ -456,6 +460,10 @@ export namespace networkmanagement_v1 {
      * URI of the resource that caused the drop.
      */
     resourceUri?: string | null;
+    /**
+     * Geolocation (region code) of the source IP address (if relevant).
+     */
+    sourceGeolocationCode?: string | null;
     /**
      * Source IP address of the dropped packet (if relevant).
      */
@@ -658,6 +666,10 @@ export namespace networkmanagement_v1 {
      */
     targetTags?: string[] | null;
     /**
+     * Target type of the firewall rule.
+     */
+    targetType?: string | null;
+    /**
      * The URI of the firewall rule. This field is not applicable to implied VPC firewall rules.
      */
     uri?: string | null;
@@ -727,6 +739,19 @@ export namespace networkmanagement_v1 {
      * VIP of the forwarding rule.
      */
     vip?: string | null;
+  }
+  /**
+   * The geographical location of the MonitoringPoint.
+   */
+  export interface Schema$GeoLocation {
+    /**
+     * Country.
+     */
+    country?: string | null;
+    /**
+     * Formatted address.
+     */
+    formattedAddress?: string | null;
   }
   /**
    * For display only. Metadata associated with a Google Kubernetes Engine (GKE) cluster master.
@@ -849,6 +874,31 @@ export namespace networkmanagement_v1 {
     status?: string | null;
     /**
      * URI of a Compute Engine instance.
+     */
+    uri?: string | null;
+  }
+  /**
+   * For display only. Metadata associated with an Interconnect attachment.
+   */
+  export interface Schema$InterconnectAttachmentInfo {
+    /**
+     * URI of the Cloud Router to be used for dynamic routing.
+     */
+    cloudRouterUri?: string | null;
+    /**
+     * Name of an Interconnect attachment.
+     */
+    displayName?: string | null;
+    /**
+     * URI of the Interconnect where the Interconnect attachment is configured.
+     */
+    interconnectUri?: string | null;
+    /**
+     * Name of a Google Cloud region where the Interconnect attachment is configured.
+     */
+    region?: string | null;
+    /**
+     * URI of an Interconnect attachment.
      */
     uri?: string | null;
   }
@@ -1131,9 +1181,9 @@ export namespace networkmanagement_v1 {
      */
     errors?: string[] | null;
     /**
-     * Output only. The geographical location of the MonitoringPoint. Examples: - "New York, NY, USA" - "Berlin, Germany"
+     * Output only. The geographical location of the MonitoringPoint. ;
      */
-    geoLocation?: string | null;
+    geoLocation?: Schema$GeoLocation;
     /**
      * Output only. The host information of the MonitoringPoint.
      */
@@ -1166,6 +1216,10 @@ export namespace networkmanagement_v1 {
      * Output only. The time the MonitoringPoint was updated.
      */
     updateTime?: string | null;
+    /**
+     * Output only. Indicates if an upgrade is available for the MonitoringPoint.
+     */
+    upgradeAvailable?: boolean | null;
     /**
      * Output only. The type of upgrade available for the MonitoringPoint.
      */
@@ -1299,6 +1353,10 @@ export namespace networkmanagement_v1 {
      */
     createTime?: string | null;
     /**
+     * Output only. The list of error messages detected for the NetworkMonitoringProvider.
+     */
+    errors?: string[] | null;
+    /**
      * Output only. Identifier. Name of the resource. Format: `projects/{project\}/locations/{location\}/networkMonitoringProviders/{network_monitoring_provider\}`
      */
     name?: string | null;
@@ -1332,9 +1390,9 @@ export namespace networkmanagement_v1 {
      */
     destination?: string | null;
     /**
-     * Output only. Geographical location of the destination MonitoringPoint.
+     * Output only. Geographical location of the destination MonitoringPoint. ;
      */
-    destinationGeoLocation?: string | null;
+    destinationGeoLocation?: Schema$GeoLocation;
     /**
      * Output only. The display name of the network path.
      */
@@ -1911,6 +1969,10 @@ export namespace networkmanagement_v1 {
      * Display information of a Compute Engine instance.
      */
     instance?: Schema$InstanceInfo;
+    /**
+     * Display information of an interconnect attachment.
+     */
+    interconnectAttachment?: Schema$InterconnectAttachmentInfo;
     /**
      * Display information of the load balancers. Deprecated in favor of the `load_balancer_backend_info` field, not used in new tests.
      */
@@ -5741,6 +5803,7 @@ export namespace networkmanagement_v1 {
      *           // request body parameters
      *           // {
      *           //   "createTime": "my_createTime",
+     *           //   "errors": [],
      *           //   "name": "my_name",
      *           //   "providerType": "my_providerType",
      *           //   "providerUri": "my_providerUri",
@@ -6042,6 +6105,7 @@ export namespace networkmanagement_v1 {
      *   // Example response
      *   // {
      *   //   "createTime": "my_createTime",
+     *   //   "errors": [],
      *   //   "name": "my_name",
      *   //   "providerType": "my_providerType",
      *   //   "providerUri": "my_providerUri",
@@ -6400,7 +6464,7 @@ export namespace networkmanagement_v1 {
      *   //   "createTime": "my_createTime",
      *   //   "displayName": "my_displayName",
      *   //   "errors": [],
-     *   //   "geoLocation": "my_geoLocation",
+     *   //   "geoLocation": {},
      *   //   "host": {},
      *   //   "hostname": "my_hostname",
      *   //   "name": "my_name",
@@ -6409,6 +6473,7 @@ export namespace networkmanagement_v1 {
      *   //   "providerTags": [],
      *   //   "type": "my_type",
      *   //   "updateTime": "my_updateTime",
+     *   //   "upgradeAvailable": false,
      *   //   "upgradeType": "my_upgradeType",
      *   //   "version": "my_version"
      *   // }
@@ -6735,7 +6800,7 @@ export namespace networkmanagement_v1 {
      *   // {
      *   //   "createTime": "my_createTime",
      *   //   "destination": "my_destination",
-     *   //   "destinationGeoLocation": "my_destinationGeoLocation",
+     *   //   "destinationGeoLocation": {},
      *   //   "displayName": "my_displayName",
      *   //   "dualEnded": false,
      *   //   "monitoringEnabled": false,
