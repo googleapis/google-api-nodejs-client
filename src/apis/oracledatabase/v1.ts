8e7ab6c5c33d6daecb58e0257f8ae45fc050346c
Yoshi Automation <yoshi-automation@google.com>
2025-08-21 01:36:34 +0000

    fix(oracledatabase): update the API
    #### oracledatabase:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description

--- src/apis/oracledatabase/v1.ts
+++ src/apis/oracledatabase/v1.ts
@@ -2118,7 +2118,7 @@ export namespace oracledatabase_v1 {
      *
      *   // Do the magic
      *   const res = await oracledatabase.projects.locations.list({
-     *     // Optional. A list of extra location types that should be used as conditions for controlling the visibility of the locations.
+     *     // Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
      *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
@@ -2247,7 +2247,7 @@ export namespace oracledatabase_v1 {
   export interface Params$Resource$Projects$Locations$List
     extends StandardParameters {
     /**
-     * Optional. A list of extra location types that should be used as conditions for controlling the visibility of the locations.
+     * Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
      */
     extraLocationTypes?: string[];
     /**
