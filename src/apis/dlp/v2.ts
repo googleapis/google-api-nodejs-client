7abee60247f0884300048556fc91ff52578ddac1
Yoshi Automation <yoshi-automation@google.com>
2025-08-21 01:36:33 +0000

    feat(dlp): update the API
    #### dlp:v2
    
    The following keys were added:
    - schemas.Proto2BridgeMessageSet.deprecated
    - schemas.Proto2BridgeMessageSet.description
    - schemas.Proto2BridgeMessageSet.id
    - schemas.Proto2BridgeMessageSet.type
    - schemas.UtilStatusProto.description
    - schemas.UtilStatusProto.id
    - schemas.UtilStatusProto.properties.canonicalCode.description
    - schemas.UtilStatusProto.properties.canonicalCode.format
    - schemas.UtilStatusProto.properties.canonicalCode.type
    - schemas.UtilStatusProto.properties.code.description
    - schemas.UtilStatusProto.properties.code.format
    - schemas.UtilStatusProto.properties.code.type
    - schemas.UtilStatusProto.properties.message.description
    - schemas.UtilStatusProto.properties.message.type
    - schemas.UtilStatusProto.properties.messageSet.$ref
    - schemas.UtilStatusProto.properties.messageSet.description
    - schemas.UtilStatusProto.properties.space.description
    - schemas.UtilStatusProto.properties.space.type
    - schemas.UtilStatusProto.type

--- src/apis/dlp/v2.ts
+++ src/apis/dlp/v2.ts
@@ -5540,6 +5540,35 @@ export namespace dlp_v2 {
      */
     seconds?: number | null;
   }
+  /**
+   * This is proto2's version of MessageSet. DEPRECATED: DO NOT USE FOR NEW FIELDS. If you are using editions or proto2, please make your own extendable messages for your use case. If you are using proto3, please use `Any` instead. MessageSet was the implementation of extensions for proto1. When proto2 was introduced, extensions were implemented as a first-class feature. This schema for MessageSet was meant to be a "bridge" solution to migrate MessageSet-bearing messages from proto1 to proto2. This schema has been open-sourced only to facilitate the migration of Google products with MessageSet-bearing messages to open-source environments.
+   */
+  export interface Schema$Proto2BridgeMessageSet {}
+  /**
+   * Wire-format for a Status object
+   */
+  export interface Schema$UtilStatusProto {
+    /**
+     * copybara:strip_begin(b/383363683) copybara:strip_end_and_replace optional int32 canonical_code = 6;
+     */
+    canonicalCode?: number | null;
+    /**
+     * Numeric code drawn from the space specified below. Often, this is the canonical error space, and code is drawn from google3/util/task/codes.proto copybara:strip_begin(b/383363683) copybara:strip_end_and_replace optional int32 code = 1;
+     */
+    code?: number | null;
+    /**
+     * Detail message copybara:strip_begin(b/383363683) copybara:strip_end_and_replace optional string message = 3;
+     */
+    message?: string | null;
+    /**
+     * message_set associates an arbitrary proto message with the status. copybara:strip_begin(b/383363683) copybara:strip_end_and_replace optional proto2.bridge.MessageSet message_set = 5;
+     */
+    messageSet?: Schema$Proto2BridgeMessageSet;
+    /**
+     * copybara:strip_begin(b/383363683) Space to which this status belongs copybara:strip_end_and_replace optional string space = 2; // Space to which this status belongs
+     */
+    space?: string | null;
+  }
 
   export class Resource$Infotypes {
     context: APIRequestContext;
