96275ba7f8139c41d0a67d36e9d6517610404f76
Yoshi Automation <yoshi-automation@google.com>
2025-08-25 01:37:07 +0000

    fix(tpu): update the API
    #### tpu:v1alpha1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### tpu:v2alpha1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### tpu:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### tpu:v2
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description

--- src/apis/tpu/v1alpha1.ts
+++ src/apis/tpu/v1alpha1.ts
@@ -674,7 +674,7 @@ export namespace tpu_v1alpha1 {
      *
      *   // Do the magic
      *   const res = await tpu.projects.locations.list({
-     *     // Optional. A list of extra location types that should be used as conditions for controlling the visibility of the locations.
+     *     // Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
      *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
@@ -802,7 +802,7 @@ export namespace tpu_v1alpha1 {
   export interface Params$Resource$Projects$Locations$List
     extends StandardParameters {
     /**
-     * Optional. A list of extra location types that should be used as conditions for controlling the visibility of the locations.
+     * Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
      */
     extraLocationTypes?: string[];
     /**
--- src/apis/tpu/v2alpha1.ts
+++ src/apis/tpu/v2alpha1.ts
@@ -1476,7 +1476,7 @@ export namespace tpu_v2alpha1 {
      *
      *   // Do the magic
      *   const res = await tpu.projects.locations.list({
-     *     // Optional. A list of extra location types that should be used as conditions for controlling the visibility of the locations.
+     *     // Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
      *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
@@ -1616,7 +1616,7 @@ export namespace tpu_v2alpha1 {
   export interface Params$Resource$Projects$Locations$List
     extends StandardParameters {
     /**
-     * Optional. A list of extra location types that should be used as conditions for controlling the visibility of the locations.
+     * Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
      */
     extraLocationTypes?: string[];
     /**
