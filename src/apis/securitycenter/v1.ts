da138de0222573c987e45b46e64b0c7a203151ef
Yoshi Automation <yoshi-automation@google.com>
2025-08-26 01:36:52 +0000

    fix(securitycenter): update the API
    #### securitycenter:v1beta1
    
    The following keys were changed:
    - schemas.BigQueryDestination.description
    - schemas.ExportFindingsMetadata.properties.bigQueryDestination.description
    
    #### securitycenter:v1beta2
    
    The following keys were changed:
    - schemas.BigQueryDestination.description
    - schemas.ExportFindingsMetadata.properties.bigQueryDestination.description
    
    #### securitycenter:v1
    
    The following keys were changed:
    - schemas.BigQueryDestination.description
    - schemas.ExportFindingsMetadata.properties.bigQueryDestination.description

--- src/apis/securitycenter/v1.ts
+++ src/apis/securitycenter/v1.ts
@@ -694,7 +694,7 @@ export namespace securitycenter_v1 {
     resourceValueConfigs?: Schema$GoogleCloudSecuritycenterV1ResourceValueConfig[];
   }
   /**
-   * The destination big query dataset to export findings to.
+   * The destination BigQuery dataset to export findings to.
    */
   export interface Schema$BigQueryDestination {
     /**
@@ -1407,7 +1407,7 @@ export namespace securitycenter_v1 {
    */
   export interface Schema$ExportFindingsMetadata {
     /**
-     * Required. The destination big query dataset to export findings to.
+     * Required. The destination BigQuery dataset to export findings to.
      */
     bigQueryDestination?: Schema$BigQueryDestination;
     /**
