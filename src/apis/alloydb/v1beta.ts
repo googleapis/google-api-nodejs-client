9d4773c5b88d399d5f69c213b971837d2f64b9fb
Yoshi Automation <yoshi-automation@google.com>
2025-09-02 01:36:42 +0000

    fix(alloydb): update the API
    #### alloydb:v1alpha
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### alloydb:v1beta
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### alloydb:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    - schemas.Backup.properties.databaseVersion.enum
    - schemas.Backup.properties.databaseVersion.enumDeprecated
    - schemas.Backup.properties.databaseVersion.enumDescriptions
    - schemas.Cluster.properties.databaseVersion.enum
    - schemas.Cluster.properties.databaseVersion.enumDeprecated
    - schemas.Cluster.properties.databaseVersion.enumDescriptions
    - schemas.ClusterUpgradeDetails.properties.databaseVersion.enum
    - schemas.ClusterUpgradeDetails.properties.databaseVersion.enumDeprecated
    - schemas.ClusterUpgradeDetails.properties.databaseVersion.enumDescriptions
    - schemas.SupportedDatabaseFlag.properties.supportedDbVersions.items.enum
    - schemas.SupportedDatabaseFlag.properties.supportedDbVersions.items.enumDeprecated
    - schemas.SupportedDatabaseFlag.properties.supportedDbVersions.items.enumDescriptions
    - schemas.UpgradeClusterRequest.properties.version.enum
    - schemas.UpgradeClusterRequest.properties.version.enumDeprecated
    - schemas.UpgradeClusterRequest.properties.version.enumDescriptions
    - schemas.UpgradeClusterStatus.properties.sourceVersion.enum
    - schemas.UpgradeClusterStatus.properties.sourceVersion.enumDeprecated
    - schemas.UpgradeClusterStatus.properties.sourceVersion.enumDescriptions
    - schemas.UpgradeClusterStatus.properties.targetVersion.enum
    - schemas.UpgradeClusterStatus.properties.targetVersion.enumDeprecated
    - schemas.UpgradeClusterStatus.properties.targetVersion.enumDescriptions

--- src/apis/alloydb/v1beta.ts
+++ src/apis/alloydb/v1beta.ts
@@ -2763,7 +2763,7 @@ export namespace alloydb_v1beta {
      *
      *   // Do the magic
      *   const res = await alloydb.projects.locations.list({
-     *     // Optional. A list of extra location types that should be used as conditions for controlling the visibility of the locations.
+     *     // Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
      *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
@@ -2899,7 +2899,7 @@ export namespace alloydb_v1beta {
   export interface Params$Resource$Projects$Locations$List
     extends StandardParameters {
     /**
-     * Optional. A list of extra location types that should be used as conditions for controlling the visibility of the locations.
+     * Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
      */
     extraLocationTypes?: string[];
     /**
