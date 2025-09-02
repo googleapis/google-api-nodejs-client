79bd550cf638a020273f00c83fd45b935fde0d97
Yoshi Automation <yoshi-automation@google.com>
2025-09-02 01:36:43 +0000

    fix(dialogflow): update the API
    #### dialogflow:v2beta1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### dialogflow:v3beta1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description

--- src/apis/dialogflow/v2beta1.ts
+++ src/apis/dialogflow/v2beta1.ts
@@ -32147,7 +32147,7 @@ export namespace dialogflow_v2beta1 {
      *
      *   // Do the magic
      *   const res = await dialogflow.projects.locations.list({
-     *     // Optional. A list of extra location types that should be used as conditions for controlling the visibility of the locations.
+     *     // Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
      *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
@@ -32486,7 +32486,7 @@ export namespace dialogflow_v2beta1 {
   export interface Params$Resource$Projects$Locations$List
     extends StandardParameters {
     /**
-     * Optional. A list of extra location types that should be used as conditions for controlling the visibility of the locations.
+     * Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
      */
     extraLocationTypes?: string[];
     /**
