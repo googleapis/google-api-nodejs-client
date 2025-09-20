c82b4ad24a6b3a49e90054f8f9e0bdc0c8b6c512
Yoshi Automation <yoshi-automation@google.com>
2025-09-20 01:36:19 +0000

    fix(parallelstore): update the API
    #### parallelstore:v1beta
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### parallelstore:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description

--- src/apis/parallelstore/v1beta.ts
+++ src/apis/parallelstore/v1beta.ts
@@ -644,7 +644,7 @@ export namespace parallelstore_v1beta {
      *
      *   // Do the magic
      *   const res = await parallelstore.projects.locations.list({
-     *     // Optional. A list of extra location types that should be used as conditions for controlling the visibility of the locations.
+     *     // Optional. Unless explicitly documented otherwise, don't use this unsupported field which is primarily intended for internal usage.
      *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
@@ -773,7 +773,7 @@ export namespace parallelstore_v1beta {
   export interface Params$Resource$Projects$Locations$List
     extends StandardParameters {
     /**
-     * Optional. A list of extra location types that should be used as conditions for controlling the visibility of the locations.
+     * Optional. Unless explicitly documented otherwise, don't use this unsupported field which is primarily intended for internal usage.
      */
     extraLocationTypes?: string[];
     /**
