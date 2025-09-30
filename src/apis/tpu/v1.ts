21d125999ff7cdac616c61cd23c9d41725daf2f0
Yoshi Automation <yoshi-automation@google.com>
2025-09-30 01:36:50 +0000

    feat(tpu): update the API
    #### tpu:v1alpha1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### tpu:v2alpha1
    
    The following keys were added:
    - resources.projects.resources.locations.resources.queuedResources.methods.getMaintenanceInfo.description
    - resources.projects.resources.locations.resources.queuedResources.methods.getMaintenanceInfo.flatPath
    - resources.projects.resources.locations.resources.queuedResources.methods.getMaintenanceInfo.httpMethod
    - resources.projects.resources.locations.resources.queuedResources.methods.getMaintenanceInfo.id
    - resources.projects.resources.locations.resources.queuedResources.methods.getMaintenanceInfo.parameterOrder
    - resources.projects.resources.locations.resources.queuedResources.methods.getMaintenanceInfo.parameters.name.description
    - resources.projects.resources.locations.resources.queuedResources.methods.getMaintenanceInfo.parameters.name.location
    - resources.projects.resources.locations.resources.queuedResources.methods.getMaintenanceInfo.parameters.name.pattern
    - resources.projects.resources.locations.resources.queuedResources.methods.getMaintenanceInfo.parameters.name.required
    - resources.projects.resources.locations.resources.queuedResources.methods.getMaintenanceInfo.parameters.name.type
    - resources.projects.resources.locations.resources.queuedResources.methods.getMaintenanceInfo.path
    - resources.projects.resources.locations.resources.queuedResources.methods.getMaintenanceInfo.response.$ref
    - resources.projects.resources.locations.resources.queuedResources.methods.getMaintenanceInfo.scopes
    - schemas.BootDiskConfig.properties.storagePool.description
    - schemas.BootDiskConfig.properties.storagePool.type
    - schemas.GetMaintenanceInfoResponse.description
    - schemas.GetMaintenanceInfoResponse.id
    - schemas.GetMaintenanceInfoResponse.properties.nodeUpcomingMaintenances.description
    - schemas.GetMaintenanceInfoResponse.properties.nodeUpcomingMaintenances.items.$ref
    - schemas.GetMaintenanceInfoResponse.properties.nodeUpcomingMaintenances.type
    - schemas.GetMaintenanceInfoResponse.type
    - schemas.NodeUpcomingMaintenanceInfo.description
    - schemas.NodeUpcomingMaintenanceInfo.id
    - schemas.NodeUpcomingMaintenanceInfo.properties.nodeName.description
    - schemas.NodeUpcomingMaintenanceInfo.properties.nodeName.type
    - schemas.NodeUpcomingMaintenanceInfo.properties.nodeUid.description
    - schemas.NodeUpcomingMaintenanceInfo.properties.nodeUid.format
    - schemas.NodeUpcomingMaintenanceInfo.properties.nodeUid.type
    - schemas.NodeUpcomingMaintenanceInfo.properties.upcomingMaintenance.$ref
    - schemas.NodeUpcomingMaintenanceInfo.properties.upcomingMaintenance.description
    - schemas.NodeUpcomingMaintenanceInfo.type
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### tpu:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### tpu:v2
    
    The following keys were added:
    - schemas.BootDiskConfig.description
    - schemas.BootDiskConfig.id
    - schemas.BootDiskConfig.properties.customerEncryptionKey.$ref
    - schemas.BootDiskConfig.properties.customerEncryptionKey.description
    - schemas.BootDiskConfig.type
    - schemas.CustomerEncryptionKey.description
    - schemas.CustomerEncryptionKey.id
    - schemas.CustomerEncryptionKey.properties.kmsKeyName.description
    - schemas.CustomerEncryptionKey.properties.kmsKeyName.type
    - schemas.CustomerEncryptionKey.type
    - schemas.Node.properties.bootDiskConfig.$ref
    - schemas.Node.properties.bootDiskConfig.description
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description

--- src/apis/tpu/v1.ts
+++ src/apis/tpu/v1.ts
@@ -674,7 +674,7 @@ export namespace tpu_v1 {
      *
      *   // Do the magic
      *   const res = await tpu.projects.locations.list({
-     *     // Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
+     *     // Optional. Unless explicitly documented otherwise, don't use this unsupported field which is primarily intended for internal usage.
      *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
@@ -802,7 +802,7 @@ export namespace tpu_v1 {
   export interface Params$Resource$Projects$Locations$List
     extends StandardParameters {
     /**
-     * Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
+     * Optional. Unless explicitly documented otherwise, don't use this unsupported field which is primarily intended for internal usage.
      */
     extraLocationTypes?: string[];
     /**
