ffb27c3a42326d86ba27be209f3309d8b814e348
Yoshi Automation <yoshi-automation@google.com>
2025-09-30 16:52:19 +0000

    fix(authorizedbuyersmarketplace): update the API
    #### authorizedbuyersmarketplace:v1alpha
    
    The following keys were changed:
    - schemas.DataSegment.properties.cpmFee.description
    - schemas.SendRfpRequest.properties.inventorySizeTargeting.description
    
    #### authorizedbuyersmarketplace:v1beta
    
    The following keys were changed:
    - schemas.DataSegment.properties.cpmFee.description
    
    #### authorizedbuyersmarketplace:v1
    
    The following keys were changed:
    - schemas.SendRfpRequest.properties.inventorySizeTargeting.description

--- src/apis/authorizedbuyersmarketplace/v1beta.ts
+++ src/apis/authorizedbuyersmarketplace/v1beta.ts
@@ -133,7 +133,7 @@ export namespace authorizedbuyersmarketplace_v1beta {
    */
   export interface Schema$DataSegment {
     /**
-     * Required. This will be charged when other accounts use this data segment. For example, when other accounts add this data segment to a deal or auction package. Once set, the currency code cannot be changed.
+     * Optional. A fixed fee charged per thousand impressions. Once set, the currency code cannot be changed.
      */
     cpmFee?: Schema$Money;
     /**
