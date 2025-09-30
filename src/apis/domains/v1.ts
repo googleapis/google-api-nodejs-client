a8d45477ca5f13dab591fff1331a84a60bbb0880
Yoshi Automation <yoshi-automation@google.com>
2025-09-30 01:36:46 +0000

    feat(domains): update the API
    #### domains:v1alpha2
    
    The following keys were added:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.location
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.repeated
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.type
    
    The following keys were changed:
    - schemas.PostalAddress.description
    - schemas.PostalAddress.properties.addressLines.description
    - schemas.PostalAddress.properties.administrativeArea.description
    - schemas.PostalAddress.properties.locality.description
    - schemas.PostalAddress.properties.postalCode.description
    - schemas.PostalAddress.properties.sortingCode.description
    - schemas.PostalAddress.properties.sublocality.description
    - schemas.Registration.properties.issues.items.enum
    - schemas.Registration.properties.issues.items.enumDescriptions
    
    #### domains:v1beta1
    
    The following keys were added:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.location
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.repeated
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.type
    
    The following keys were changed:
    - schemas.PostalAddress.description
    - schemas.PostalAddress.properties.addressLines.description
    - schemas.PostalAddress.properties.administrativeArea.description
    - schemas.PostalAddress.properties.locality.description
    - schemas.PostalAddress.properties.postalCode.description
    - schemas.PostalAddress.properties.sortingCode.description
    - schemas.PostalAddress.properties.sublocality.description
    - schemas.Registration.properties.issues.items.enum
    - schemas.Registration.properties.issues.items.enumDescriptions
    
    #### domains:v1
    
    The following keys were added:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.location
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.repeated
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.type
    
    The following keys were changed:
    - schemas.PostalAddress.description
    - schemas.PostalAddress.properties.addressLines.description
    - schemas.PostalAddress.properties.administrativeArea.description
    - schemas.PostalAddress.properties.locality.description
    - schemas.PostalAddress.properties.postalCode.description
    - schemas.PostalAddress.properties.sortingCode.description
    - schemas.PostalAddress.properties.sublocality.description
    - schemas.Registration.properties.issues.items.enum
    - schemas.Registration.properties.issues.items.enumDescriptions

--- src/apis/domains/v1.ts
+++ src/apis/domains/v1.ts
@@ -719,15 +719,15 @@ export namespace domains_v1 {
     version?: number | null;
   }
   /**
-   * Represents a postal address. For example for postal delivery or payments addresses. Given a postal address, a postal service can deliver items to a premise, P.O. Box or similar. It is not intended to model geographical locations (roads, towns, mountains). In typical usage an address would be created by user input or from importing existing data, depending on the type of process. Advice on address input / editing: - Use an internationalization-ready address widget such as https://github.com/google/libaddressinput) - Users should not be presented with UI elements for input or editing of fields outside countries where that field is used. For more guidance on how to use this schema, see: https://support.google.com/business/answer/6397478
+   * Represents a postal address, such as for postal delivery or payments addresses. With a postal address, a postal service can deliver items to a premise, P.O. box, or similar. A postal address is not intended to model geographical locations like roads, towns, or mountains. In typical usage, an address would be created by user input or from importing existing data, depending on the type of process. Advice on address input or editing: - Use an internationalization-ready address widget such as https://github.com/google/libaddressinput. - Users should not be presented with UI elements for input or editing of fields outside countries where that field is used. For more guidance on how to use this schema, see: https://support.google.com/business/answer/6397478.
    */
   export interface Schema$PostalAddress {
     /**
-     * Unstructured address lines describing the lower levels of an address. Because values in address_lines do not have type information and may sometimes contain multiple values in a single field (For example "Austin, TX"), it is important that the line order is clear. The order of address lines should be "envelope order" for the country/region of the address. In places where this can vary (For example Japan), address_language is used to make it explicit (For example "ja" for large-to-small ordering and "ja-Latn" or "en" for small-to-large). This way, the most specific line of an address can be selected based on the language. The minimum permitted structural representation of an address consists of a region_code with all remaining information placed in the address_lines. It would be possible to format such an address very approximately without geocoding, but no semantic reasoning could be made about any of the address components until it was at least partially resolved. Creating an address only containing a region_code and address_lines, and then geocoding is the recommended way to handle completely unstructured addresses (as opposed to guessing which parts of the address should be localities or administrative areas).
+     * Unstructured address lines describing the lower levels of an address. Because values in `address_lines` do not have type information and may sometimes contain multiple values in a single field (for example, "Austin, TX"), it is important that the line order is clear. The order of address lines should be "envelope order" for the country or region of the address. In places where this can vary (for example, Japan), `address_language` is used to make it explicit (for example, "ja" for large-to-small ordering and "ja-Latn" or "en" for small-to-large). In this way, the most specific line of an address can be selected based on the language. The minimum permitted structural representation of an address consists of a `region_code` with all remaining information placed in the `address_lines`. It would be possible to format such an address very approximately without geocoding, but no semantic reasoning could be made about any of the address components until it was at least partially resolved. Creating an address only containing a `region_code` and `address_lines` and then geocoding is the recommended way to handle completely unstructured addresses (as opposed to guessing which parts of the address should be localities or administrative areas).
      */
     addressLines?: string[] | null;
     /**
-     * Optional. Highest administrative subdivision which is used for postal addresses of a country or region. For example, this can be a state, a province, an oblast, or a prefecture. Specifically, for Spain this is the province and not the autonomous community (For example "Barcelona" and not "Catalonia"). Many countries don't use an administrative area in postal addresses. For example in Switzerland this should be left unpopulated.
+     * Optional. Highest administrative subdivision which is used for postal addresses of a country or region. For example, this can be a state, a province, an oblast, or a prefecture. For Spain, this is the province and not the autonomous community (for example, "Barcelona" and not "Catalonia"). Many countries don't use an administrative area in postal addresses. For example, in Switzerland, this should be left unpopulated.
      */
     administrativeArea?: string | null;
     /**
@@ -735,7 +735,7 @@ export namespace domains_v1 {
      */
     languageCode?: string | null;
     /**
-     * Optional. Generally refers to the city/town portion of the address. Examples: US city, IT comune, UK post town. In regions of the world where localities are not well defined or do not fit into this structure well, leave locality empty and use address_lines.
+     * Optional. Generally refers to the city or town portion of the address. Examples: US city, IT comune, UK post town. In regions of the world where localities are not well defined or do not fit into this structure well, leave `locality` empty and use `address_lines`.
      */
     locality?: string | null;
     /**
@@ -743,7 +743,7 @@ export namespace domains_v1 {
      */
     organization?: string | null;
     /**
-     * Optional. Postal code of the address. Not all countries use or require postal codes to be present, but where they are used, they may trigger additional validation with other parts of the address (For example state/zip validation in the U.S.A.).
+     * Optional. Postal code of the address. Not all countries use or require postal codes to be present, but where they are used, they may trigger additional validation with other parts of the address (for example, state or zip code validation in the United States).
      */
     postalCode?: string | null;
     /**
@@ -759,11 +759,11 @@ export namespace domains_v1 {
      */
     revision?: number | null;
     /**
-     * Optional. Additional, country-specific, sorting code. This is not used in most regions. Where it is used, the value is either a string like "CEDEX", optionally followed by a number (For example "CEDEX 7"), or just a number alone, representing the "sector code" (Jamaica), "delivery area indicator" (Malawi) or "post office indicator" (For example Côte d'Ivoire).
+     * Optional. Additional, country-specific, sorting code. This is not used in most regions. Where it is used, the value is either a string like "CEDEX", optionally followed by a number (for example, "CEDEX 7"), or just a number alone, representing the "sector code" (Jamaica), "delivery area indicator" (Malawi) or "post office indicator" (Côte d'Ivoire).
      */
     sortingCode?: string | null;
     /**
-     * Optional. Sublocality of the address. For example, this can be neighborhoods, boroughs, districts.
+     * Optional. Sublocality of the address. For example, this can be a neighborhood, borough, or district.
      */
     sublocality?: string | null;
   }
@@ -1348,6 +1348,8 @@ export namespace domains_v1 {
      *
      *   // Do the magic
      *   const res = await domains.projects.locations.list({
+     *     // Optional. Unless explicitly documented otherwise, don't use this unsupported field which is primarily intended for internal usage.
+     *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
      *     // The resource that owns the locations collection, if applicable.
@@ -1473,6 +1475,10 @@ export namespace domains_v1 {
   }
   export interface Params$Resource$Projects$Locations$List
     extends StandardParameters {
+    /**
+     * Optional. Unless explicitly documented otherwise, don't use this unsupported field which is primarily intended for internal usage.
+     */
+    extraLocationTypes?: string[];
     /**
      * A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      */
