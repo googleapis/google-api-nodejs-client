7949ac395c2b4027edb9282751430e7eab106a65
Yoshi Automation <yoshi-automation@google.com>
2025-10-04 01:36:34 +0000

    feat(networkmanagement): update the API
    #### networkmanagement:v1beta1
    
    The following keys were added:
    - resources.organizations.resources.locations.resources.global.resources.operations.methods.list.parameters.returnPartialSuccess.description
    - resources.organizations.resources.locations.resources.global.resources.operations.methods.list.parameters.returnPartialSuccess.location
    - resources.organizations.resources.locations.resources.global.resources.operations.methods.list.parameters.returnPartialSuccess.type
    - resources.projects.resources.locations.resources.global.resources.operations.methods.list.parameters.returnPartialSuccess.description
    - resources.projects.resources.locations.resources.global.resources.operations.methods.list.parameters.returnPartialSuccess.location
    - resources.projects.resources.locations.resources.global.resources.operations.methods.list.parameters.returnPartialSuccess.type
    - schemas.ListOperationsResponse.properties.unreachable.description
    - schemas.ListOperationsResponse.properties.unreachable.items.type
    - schemas.ListOperationsResponse.properties.unreachable.type

--- src/apis/networkmanagement/v1beta1.ts
+++ src/apis/networkmanagement/v1beta1.ts
@@ -1006,6 +1006,10 @@ export namespace networkmanagement_v1beta1 {
      * A list of operations that matches the specified filter in the request.
      */
     operations?: Schema$Operation[];
+    /**
+     * Unordered list. Unreachable resources. Populated when the request sets `ListOperationsRequest.return_partial_success` and reads across collections e.g. when attempting to list all resources across all supported locations.
+     */
+    unreachable?: string[] | null;
   }
   /**
    * Response for the `ListVpcFlowLogsConfigs` method.
@@ -2865,13 +2869,16 @@ export namespace networkmanagement_v1beta1 {
      *       pageSize: 'placeholder-value',
      *       // The standard list page token.
      *       pageToken: 'placeholder-value',
+     *       // When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     *       returnPartialSuccess: 'placeholder-value',
      *     });
      *   console.log(res.data);
      *
      *   // Example response
      *   // {
      *   //   "nextPageToken": "my_nextPageToken",
-     *   //   "operations": []
+     *   //   "operations": [],
+     *   //   "unreachable": []
      *   // }
      * }
      *
@@ -3019,6 +3026,10 @@ export namespace networkmanagement_v1beta1 {
      * The standard list page token.
      */
     pageToken?: string;
+    /**
+     * When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     */
+    returnPartialSuccess?: boolean;
   }
 
   export class Resource$Organizations$Locations$Vpcflowlogsconfigs {
@@ -6216,6 +6227,8 @@ export namespace networkmanagement_v1beta1 {
      *       pageSize: 'placeholder-value',
      *       // The standard list page token.
      *       pageToken: 'placeholder-value',
+     *       // When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     *       returnPartialSuccess: 'placeholder-value',
      *     },
      *   );
      *   console.log(res.data);
@@ -6223,7 +6236,8 @@ export namespace networkmanagement_v1beta1 {
      *   // Example response
      *   // {
      *   //   "nextPageToken": "my_nextPageToken",
-     *   //   "operations": []
+     *   //   "operations": [],
+     *   //   "unreachable": []
      *   // }
      * }
      *
@@ -6371,6 +6385,10 @@ export namespace networkmanagement_v1beta1 {
      * The standard list page token.
      */
     pageToken?: string;
+    /**
+     * When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     */
+    returnPartialSuccess?: boolean;
   }
 
   export class Resource$Projects$Locations$Vpcflowlogsconfigs {
