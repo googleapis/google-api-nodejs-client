9a2d0f3ba4a83e1c23d151f322a6604d1b1b511e
Yoshi Automation <yoshi-automation@google.com>
2025-10-04 01:36:34 +0000

    feat(metastore): update the API
    #### metastore:v1alpha
    
    The following keys were added:
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.description
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.location
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.type
    - schemas.ListOperationsResponse.properties.unreachable.description
    - schemas.ListOperationsResponse.properties.unreachable.items.type
    - schemas.ListOperationsResponse.properties.unreachable.type
    
    #### metastore:v1beta
    
    The following keys were added:
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.description
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.location
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.type
    - schemas.ListOperationsResponse.properties.unreachable.description
    - schemas.ListOperationsResponse.properties.unreachable.items.type
    - schemas.ListOperationsResponse.properties.unreachable.type
    
    #### metastore:v1
    
    The following keys were added:
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.description
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.location
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.type
    - schemas.ListOperationsResponse.properties.unreachable.description
    - schemas.ListOperationsResponse.properties.unreachable.items.type
    - schemas.ListOperationsResponse.properties.unreachable.type

--- src/apis/metastore/v1alpha.ts
+++ src/apis/metastore/v1alpha.ts
@@ -784,6 +784,10 @@ export namespace metastore_v1alpha {
      * A list of operations that matches the specified filter in the request.
      */
     operations?: Schema$Operation[];
+    /**
+     * Unordered list. Unreachable resources. Populated when the request sets ListOperationsRequest.return_partial_success and reads across collections e.g. when attempting to list all resources across all supported locations.
+     */
+    unreachable?: string[] | null;
   }
   /**
    * Response message for DataprocMetastore.ListServices.
@@ -3608,13 +3612,16 @@ export namespace metastore_v1alpha {
      *     pageSize: 'placeholder-value',
      *     // The standard list page token.
      *     pageToken: 'placeholder-value',
+     *     // When set to true, operations that are reachable are returned as normal, and those that are unreachable are returned in the ListOperationsResponse.unreachable field.This can only be true when reading across collections e.g. when parent is set to "projects/example/locations/-".This field is not by default supported and will result in an UNIMPLEMENTED error if set unless explicitly documented otherwise in service or product specific documentation.
+     *     returnPartialSuccess: 'placeholder-value',
      *   });
      *   console.log(res.data);
      *
      *   // Example response
      *   // {
      *   //   "nextPageToken": "my_nextPageToken",
-     *   //   "operations": []
+     *   //   "operations": [],
+     *   //   "unreachable": []
      *   // }
      * }
      *
@@ -3760,6 +3767,10 @@ export namespace metastore_v1alpha {
      * The standard list page token.
      */
     pageToken?: string;
+    /**
+     * When set to true, operations that are reachable are returned as normal, and those that are unreachable are returned in the ListOperationsResponse.unreachable field.This can only be true when reading across collections e.g. when parent is set to "projects/example/locations/-".This field is not by default supported and will result in an UNIMPLEMENTED error if set unless explicitly documented otherwise in service or product specific documentation.
+     */
+    returnPartialSuccess?: boolean;
   }
 
   export class Resource$Projects$Locations$Services {
