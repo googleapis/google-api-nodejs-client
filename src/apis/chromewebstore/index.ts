b1170ccfc867264500d2674420eb04af676c0dbf
Yoshi Automation <yoshi-automation@google.com>
2025-10-09 21:49:39 +0000

    fix(chromewebstore): update the API

---
+++ src/apis/chromewebstore/index.ts
@@ -0,0 +1,47 @@
+// Copyright 2020 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*! THIS FILE IS AUTO-GENERATED */
+
+import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
+import {chromewebstore_v1_1} from './v1.1';
+
+export const VERSIONS = {
+  'v1.1': chromewebstore_v1_1.Chromewebstore,
+};
+
+export function chromewebstore(
+  version: 'v1.1'
+): chromewebstore_v1_1.Chromewebstore;
+export function chromewebstore(
+  options: chromewebstore_v1_1.Options
+): chromewebstore_v1_1.Chromewebstore;
+export function chromewebstore<T = chromewebstore_v1_1.Chromewebstore>(
+  this: GoogleConfigurable,
+  versionOrOptions: 'v1.1' | chromewebstore_v1_1.Options
+) {
+  return getAPI<T>('chromewebstore', versionOrOptions, VERSIONS, this);
+}
+
+const auth = new AuthPlus();
+export {auth};
+export {chromewebstore_v1_1};
+export {
+  AuthPlus,
+  GlobalOptions,
+  APIRequestContext,
+  GoogleConfigurable,
+  StreamMethodOptions,
+  MethodOptions,
+  BodyResponseCallback,
+} from 'googleapis-common';
