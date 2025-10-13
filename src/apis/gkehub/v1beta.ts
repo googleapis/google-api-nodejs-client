9e53904f4284c0c4132ebc420600c5ea49090701
Yoshi Automation <yoshi-automation@google.com>
2025-10-13 01:37:12 +0000

    feat(gkehub): update the API
    #### gkehub:v1alpha
    
    The following keys were added:
    - schemas.ConfigManagementMembershipState.properties.kubernetesApiServerVersion.description
    - schemas.ConfigManagementMembershipState.properties.kubernetesApiServerVersion.readOnly
    - schemas.ConfigManagementMembershipState.properties.kubernetesApiServerVersion.type
    
    #### gkehub:v2alpha
    
    The following keys were added:
    - schemas.ConfigManagementState.properties.kubernetesApiServerVersion.description
    - schemas.ConfigManagementState.properties.kubernetesApiServerVersion.readOnly
    - schemas.ConfigManagementState.properties.kubernetesApiServerVersion.type
    
    #### gkehub:v1beta
    
    The following keys were added:
    - schemas.ConfigManagementMembershipState.properties.kubernetesApiServerVersion.description
    - schemas.ConfigManagementMembershipState.properties.kubernetesApiServerVersion.readOnly
    - schemas.ConfigManagementMembershipState.properties.kubernetesApiServerVersion.type
    
    #### gkehub:v2beta
    
    The following keys were added:
    - schemas.ConfigManagementState.properties.kubernetesApiServerVersion.description
    - schemas.ConfigManagementState.properties.kubernetesApiServerVersion.readOnly
    - schemas.ConfigManagementState.properties.kubernetesApiServerVersion.type
    
    #### gkehub:v1
    
    The following keys were added:
    - schemas.ConfigManagementMembershipState.properties.kubernetesApiServerVersion.description
    - schemas.ConfigManagementMembershipState.properties.kubernetesApiServerVersion.readOnly
    - schemas.ConfigManagementMembershipState.properties.kubernetesApiServerVersion.type
    
    #### gkehub:v2
    
    The following keys were added:
    - schemas.ConfigManagementState.properties.kubernetesApiServerVersion.description
    - schemas.ConfigManagementState.properties.kubernetesApiServerVersion.readOnly
    - schemas.ConfigManagementState.properties.kubernetesApiServerVersion.type

--- src/apis/gkehub/v1beta.ts
+++ src/apis/gkehub/v1beta.ts
@@ -960,6 +960,10 @@ export namespace gkehub_v1beta {
      */
     hierarchyControllerState?: Schema$ConfigManagementHierarchyControllerState;
     /**
+     * Output only. The Kubernetes API server version of the cluster.
+     */
+    kubernetesApiServerVersion?: string | null;
+    /**
      * Output only. Membership configuration in the cluster. This represents the actual state in the cluster, while the MembershipSpec in the FeatureSpec represents the intended state
      */
     membershipSpec?: Schema$ConfigManagementMembershipSpec;
