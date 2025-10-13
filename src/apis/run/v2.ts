bff0db4d9b2f113e95ab12886141c4ba47a764cd
Yoshi Automation <yoshi-automation@google.com>
2025-10-13 01:37:13 +0000

    fix(run): update the API
    #### run:v2
    
    The following keys were changed:
    - schemas.GoogleCloudRunV2ResourceRequirements.properties.limits.description

--- src/apis/run/v2.ts
+++ src/apis/run/v2.ts
@@ -1053,7 +1053,7 @@ export namespace run_v2 {
      */
     cpuIdle?: boolean | null;
     /**
-     * Only `memory` and `cpu` keys in the map are supported. Notes: * The only supported values for CPU are '1', '2', '4', and '8'. Setting 4 CPU requires at least 2Gi of memory. For more information, go to https://cloud.google.com/run/docs/configuring/cpu. * For supported 'memory' values and syntax, go to https://cloud.google.com/run/docs/configuring/memory-limits
+     * Only `memory`, `cpu` and `nvidia.com/gpu` keys in the map are supported. Notes: * The only supported values for CPU are '1', '2', '4', and '8'. Setting 4 CPU requires at least 2Gi of memory. For more information, go to https://cloud.google.com/run/docs/configuring/cpu. * For supported 'memory' values and syntax, go to https://cloud.google.com/run/docs/configuring/memory-limits * The only supported 'nvidia.com/gpu' value is '1'.
      */
     limits?: {[key: string]: string} | null;
     /**
