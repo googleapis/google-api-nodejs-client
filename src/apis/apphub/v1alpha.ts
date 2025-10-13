bfda0e11c81e991106f11c8fbeb82c9e0a6bb982
Yoshi Automation <yoshi-automation@google.com>
2025-10-13 01:37:11 +0000

    feat(apphub): update the API
    #### apphub:v1alpha
    
    The following keys were added:
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.description
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.location
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.type
    - schemas.ListOperationsResponse.properties.unreachable.description
    - schemas.ListOperationsResponse.properties.unreachable.items.type
    - schemas.ListOperationsResponse.properties.unreachable.type
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### apphub:v1
    
    The following keys were added:
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.description
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.location
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.type
    - schemas.ListOperationsResponse.properties.unreachable.description
    - schemas.ListOperationsResponse.properties.unreachable.items.type
    - schemas.ListOperationsResponse.properties.unreachable.type
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description

--- src/apis/apphub/v1alpha.ts
+++ src/apis/apphub/v1alpha.ts
@@ -470,6 +470,10 @@ export namespace apphub_v1alpha {
      * A list of operations that matches the specified filter in the request.
      */
     operations?: Schema$Operation[];
+    /**
+     * Unordered list. Unreachable resources. Populated when the request sets `ListOperationsRequest.return_partial_success` and reads across collections e.g. when attempting to list all resources across all supported locations.
+     */
+    unreachable?: string[] | null;
   }
   /**
    * Response for ListServiceProjectAttachments.
@@ -1242,7 +1246,7 @@ export namespace apphub_v1alpha {
      *
      *   // Do the magic
      *   const res = await apphub.projects.locations.list({
-     *     // Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
+     *     // Optional. Unless explicitly documented otherwise, don't use this unsupported field which is primarily intended for internal usage.
      *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
@@ -1529,7 +1533,7 @@ export namespace apphub_v1alpha {
   export interface Params$Resource$Projects$Locations$List
     extends StandardParameters {
     /**
-     * Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
+     * Optional. Unless explicitly documented otherwise, don't use this unsupported field which is primarily intended for internal usage.
      */
     extraLocationTypes?: string[];
     /**
@@ -6382,13 +6386,16 @@ export namespace apphub_v1alpha {
      *     pageSize: 'placeholder-value',
      *     // The standard list page token.
      *     pageToken: 'placeholder-value',
+     *     // When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     *     returnPartialSuccess: 'placeholder-value',
      *   });
      *   console.log(res.data);
      *
      *   // Example response
      *   // {
      *   //   "nextPageToken": "my_nextPageToken",
-     *   //   "operations": []
+     *   //   "operations": [],
+     *   //   "unreachable": []
      *   // }
      * }
      *
@@ -6534,6 +6541,10 @@ export namespace apphub_v1alpha {
      * The standard list page token.
      */
     pageToken?: string;
+    /**
+     * When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     */
+    returnPartialSuccess?: boolean;
   }
 
   export class Resource$Projects$Locations$Serviceprojectattachments {
