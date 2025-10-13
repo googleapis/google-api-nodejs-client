867c5222ea8c5f478f94cf0efc22a5b36a503d46
Yoshi Automation <yoshi-automation@google.com>
2025-10-13 01:37:12 +0000

    feat(pubsublite): update the API
    #### pubsublite:v1
    
    The following keys were added:
    - resources.admin.resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.description
    - resources.admin.resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.location
    - resources.admin.resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.type
    - schemas.ListOperationsResponse.properties.unreachable.description
    - schemas.ListOperationsResponse.properties.unreachable.items.type
    - schemas.ListOperationsResponse.properties.unreachable.type

--- src/apis/pubsublite/v1.ts
+++ src/apis/pubsublite/v1.ts
@@ -295,6 +295,10 @@ export namespace pubsublite_v1 {
      * A list of operations that matches the specified filter in the request.
      */
     operations?: Schema$Operation[];
+    /**
+     * Unordered list. Unreachable resources. Populated when the request sets `ListOperationsRequest.return_partial_success` and reads across collections e.g. when attempting to list all resources across all supported locations.
+     */
+    unreachable?: string[] | null;
   }
   /**
    * Response for ListPartitionCursors
@@ -1090,13 +1094,16 @@ export namespace pubsublite_v1 {
      *     pageSize: 'placeholder-value',
      *     // The standard list page token.
      *     pageToken: 'placeholder-value',
+     *     // When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     *     returnPartialSuccess: 'placeholder-value',
      *   });
      *   console.log(res.data);
      *
      *   // Example response
      *   // {
      *   //   "nextPageToken": "my_nextPageToken",
-     *   //   "operations": []
+     *   //   "operations": [],
+     *   //   "unreachable": []
      *   // }
      * }
      *
@@ -1242,6 +1249,10 @@ export namespace pubsublite_v1 {
      * The standard list page token.
      */
     pageToken?: string;
+    /**
+     * When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     */
+    returnPartialSuccess?: boolean;
   }
 
   export class Resource$Admin$Projects$Locations$Reservations {
