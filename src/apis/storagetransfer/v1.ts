10923b1fadad692953ab372269f9dc3cd42411f8
Yoshi Automation <yoshi-automation@google.com>
2025-10-31 01:37:03 +0000

    feat(storagetransfer): update the API
    #### storagetransfer:v1
    
    The following keys were added:
    - resources.transferOperations.methods.list.parameters.returnPartialSuccess.description
    - resources.transferOperations.methods.list.parameters.returnPartialSuccess.location
    - resources.transferOperations.methods.list.parameters.returnPartialSuccess.type
    - schemas.ListOperationsResponse.properties.unreachable.description
    - schemas.ListOperationsResponse.properties.unreachable.items.type
    - schemas.ListOperationsResponse.properties.unreachable.type
    
    The following keys were changed:
    - schemas.ErrorLogEntry.description

--- src/apis/storagetransfer/v1.ts
+++ src/apis/storagetransfer/v1.ts
@@ -297,7 +297,7 @@ export namespace storagetransfer_v1 {
    */
   export interface Schema$Empty {}
   /**
-   * An entry describing an error that has occurred.
+   * LINT.IfChange An entry describing an error that has occurred.
    */
   export interface Schema$ErrorLogEntry {
     /**
@@ -429,6 +429,10 @@ export namespace storagetransfer_v1 {
      * A list of operations that matches the specified filter in the request.
      */
     operations?: Schema$Operation[];
+    /**
+     * Unordered list. Unreachable resources. Populated when the request sets `ListOperationsRequest.return_partial_success` and reads across collections e.g. when attempting to list all resources across all supported locations.
+     */
+    unreachable?: string[] | null;
   }
   /**
    * Response from ListTransferJobs.
@@ -3281,13 +3285,16 @@ export namespace storagetransfer_v1 {
      *     pageSize: 'placeholder-value',
      *     // The list page token.
      *     pageToken: 'placeholder-value',
+     *     // When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     *     returnPartialSuccess: 'placeholder-value',
      *   });
      *   console.log(res.data);
      *
      *   // Example response
      *   // {
      *   //   "nextPageToken": "my_nextPageToken",
-     *   //   "operations": []
+     *   //   "operations": [],
+     *   //   "unreachable": []
      *   // }
      * }
      *
@@ -3702,6 +3709,10 @@ export namespace storagetransfer_v1 {
      * The list page token.
      */
     pageToken?: string;
+    /**
+     * When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     */
+    returnPartialSuccess?: boolean;
   }
   export interface Params$Resource$Transferoperations$Pause
     extends StandardParameters {
