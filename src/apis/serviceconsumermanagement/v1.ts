d5529cf1f0ef6178aa85d5ca4abd6f88c27b7ceb
Yoshi Automation <yoshi-automation@google.com>
2025-10-31 01:37:02 +0000

    feat(serviceconsumermanagement): update the API
    #### serviceconsumermanagement:v1beta1
    
    The following keys were added:
    - schemas.V1Beta1ServiceIdentity.properties.projectRole.description
    - schemas.V1Beta1ServiceIdentity.properties.projectRole.type
    
    #### serviceconsumermanagement:v1
    
    The following keys were added:
    - resources.operations.methods.list.parameters.returnPartialSuccess.description
    - resources.operations.methods.list.parameters.returnPartialSuccess.location
    - resources.operations.methods.list.parameters.returnPartialSuccess.type
    - schemas.ListOperationsResponse.properties.unreachable.description
    - schemas.ListOperationsResponse.properties.unreachable.items.type
    - schemas.ListOperationsResponse.properties.unreachable.type
    - schemas.V1Beta1ServiceIdentity.properties.projectRole.description
    - schemas.V1Beta1ServiceIdentity.properties.projectRole.type

--- src/apis/serviceconsumermanagement/v1.ts
+++ src/apis/serviceconsumermanagement/v1.ts
@@ -1009,6 +1009,10 @@ export namespace serviceconsumermanagement_v1 {
      * A list of operations that matches the specified filter in the request.
      */
     operations?: Schema$Operation[];
+    /**
+     * Unordered list. Unreachable resources. Populated when the request sets `ListOperationsRequest.return_partial_success` and reads across collections e.g. when attempting to list all resources across all supported locations.
+     */
+    unreachable?: string[] | null;
   }
   /**
    * Response for the list request.
@@ -2068,6 +2072,10 @@ export namespace serviceconsumermanagement_v1 {
      */
     name?: string | null;
     /**
+     * The project-level IAM role defined in the service agent's grant configuration. This is the standard role intended for this service agent. This field is populated regardless of the `skip_role_attach` option in the request. If `skip_role_attach` is true, the caller can use this value to know which role they are responsible for granting.
+     */
+    projectRole?: string | null;
+    /**
      * The P4 service identity configuration tag. This must be defined in activation_grants. If not specified when creating the account, the tag is set to "default".
      */
     tag?: string | null;
@@ -2625,13 +2633,16 @@ export namespace serviceconsumermanagement_v1 {
      *     pageSize: 'placeholder-value',
      *     // The standard list page token.
      *     pageToken: 'placeholder-value',
+     *     // When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     *     returnPartialSuccess: 'placeholder-value',
      *   });
      *   console.log(res.data);
      *
      *   // Example response
      *   // {
      *   //   "nextPageToken": "my_nextPageToken",
-     *   //   "operations": []
+     *   //   "operations": [],
+     *   //   "unreachable": []
      *   // }
      * }
      *
@@ -2772,6 +2783,10 @@ export namespace serviceconsumermanagement_v1 {
      * The standard list page token.
      */
     pageToken?: string;
+    /**
+     * When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     */
+    returnPartialSuccess?: boolean;
   }
 
   export class Resource$Services {
