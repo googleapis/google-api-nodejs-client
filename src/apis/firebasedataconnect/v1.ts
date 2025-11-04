d3479abafc3c103cc774d35b431949396bf14a2b
Yoshi Automation <yoshi-automation@google.com>
2025-11-04 01:37:15 +0000

    fix(firebasedataconnect): update the API
    #### firebasedataconnect:v1beta
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.services.resources.schemas.methods.list.description
    
    #### firebasedataconnect:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.services.resources.schemas.methods.list.description

--- src/apis/firebasedataconnect/v1.ts
+++ src/apis/firebasedataconnect/v1.ts
@@ -5116,7 +5116,7 @@ export namespace firebasedataconnect_v1 {
     }
 
     /**
-     * Lists Schemas in a given project and location. Note that only `schemas/main` is supported, so this will always return at most one Schema.
+     * Lists Schemas in a given project and location.
      * @example
      * ```js
      * // Before running the sample:
