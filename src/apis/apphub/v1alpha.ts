abfdb5e99fb9701418b76faea83c3beb3032b27f
Yoshi Automation <yoshi-automation@google.com>
2025-11-04 01:37:14 +0000

    feat(apphub): update the API
    #### apphub:v1alpha
    
    The following keys were added:
    - schemas.FunctionalType.description
    - schemas.FunctionalType.id
    - schemas.FunctionalType.properties.type.description
    - schemas.FunctionalType.properties.type.enum
    - schemas.FunctionalType.properties.type.enumDescriptions
    - schemas.FunctionalType.properties.type.readOnly
    - schemas.FunctionalType.properties.type.type
    - schemas.FunctionalType.type
    - schemas.ServiceProperties.properties.functionalType.$ref
    - schemas.ServiceProperties.properties.functionalType.description
    - schemas.ServiceProperties.properties.functionalType.readOnly
    - schemas.WorkloadProperties.properties.functionalType.$ref
    - schemas.WorkloadProperties.properties.functionalType.description
    - schemas.WorkloadProperties.properties.functionalType.readOnly
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### apphub:v1
    
    The following keys were added:
    - schemas.FunctionalType.description
    - schemas.FunctionalType.id
    - schemas.FunctionalType.properties.type.description
    - schemas.FunctionalType.properties.type.enum
    - schemas.FunctionalType.properties.type.enumDescriptions
    - schemas.FunctionalType.properties.type.readOnly
    - schemas.FunctionalType.properties.type.type
    - schemas.FunctionalType.type
    - schemas.ServiceProperties.properties.functionalType.$ref
    - schemas.ServiceProperties.properties.functionalType.description
    - schemas.ServiceProperties.properties.functionalType.readOnly
    - schemas.WorkloadProperties.properties.functionalType.$ref
    - schemas.WorkloadProperties.properties.functionalType.description
    - schemas.WorkloadProperties.properties.functionalType.readOnly
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description

--- src/apis/apphub/v1alpha.ts
+++ src/apis/apphub/v1alpha.ts
@@ -395,6 +395,15 @@ export namespace apphub_v1alpha {
     unreachable?: string[] | null;
   }
   /**
+   * The functional type of a service or workload.
+   */
+  export interface Schema$FunctionalType {
+    /**
+     * Output only. The functional type of a service or workload.
+     */
+    type?: string | null;
+  }
+  /**
    * Response for ListApplications.
    */
   export interface Schema$ListApplicationsResponse {
@@ -745,6 +754,10 @@ export namespace apphub_v1alpha {
    */
   export interface Schema$ServiceProperties {
     /**
+     * Output only. The type of the service.
+     */
+    functionalType?: Schema$FunctionalType;
+    /**
      * Output only. The service project identifier that the underlying cloud resource resides in.
      */
     gcpProject?: string | null;
@@ -871,6 +884,10 @@ export namespace apphub_v1alpha {
    * Properties of an underlying compute resource represented by the Workload.
    */
   export interface Schema$WorkloadProperties {
+    /**
+     * Output only. The type of the workload.
+     */
+    functionalType?: Schema$FunctionalType;
     /**
      * Output only. The service project identifier that the underlying cloud resource resides in. Empty for non-cloud resources.
      */
@@ -1246,7 +1263,7 @@ export namespace apphub_v1alpha {
      *
      *   // Do the magic
      *   const res = await apphub.projects.locations.list({
-     *     // Optional. Unless explicitly documented otherwise, don't use this unsupported field which is primarily intended for internal usage.
+     *     // Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
      *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
@@ -1533,7 +1550,7 @@ export namespace apphub_v1alpha {
   export interface Params$Resource$Projects$Locations$List
     extends StandardParameters {
     /**
-     * Optional. Unless explicitly documented otherwise, don't use this unsupported field which is primarily intended for internal usage.
+     * Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
      */
     extraLocationTypes?: string[];
     /**
