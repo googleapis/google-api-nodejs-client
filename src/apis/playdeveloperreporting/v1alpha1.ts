b2edb0f27725e23f701d8bc938427fa05870fab3
Yoshi Automation <yoshi-automation@google.com>
2025-12-05 19:05:20 +0000

    fix(playdeveloperreporting): update the API

--- src/apis/playdeveloperreporting/v1alpha1.ts
+++ src/apis/playdeveloperreporting/v1alpha1.ts
@@ -1559,8 +1559,7 @@ export namespace playdeveloperreporting_v1alpha1 {
     }
   }
 
-  export interface Params$Resource$Apps$Fetchreleasefilteroptions
-    extends StandardParameters {
+  export interface Params$Resource$Apps$Fetchreleasefilteroptions extends StandardParameters {
     /**
      * Required. Name of the resource, i.e. app the filtering options are for. Format: apps/{app\}
      */
@@ -1921,15 +1920,13 @@ export namespace playdeveloperreporting_v1alpha1 {
     }
   }
 
-  export interface Params$Resource$Vitals$Anrrate$Get
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Anrrate$Get extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/anrRateMetricSet
      */
     name?: string;
   }
-  export interface Params$Resource$Vitals$Anrrate$Query
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Anrrate$Query extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/anrRateMetricSet
      */
@@ -2257,15 +2254,13 @@ export namespace playdeveloperreporting_v1alpha1 {
     }
   }
 
-  export interface Params$Resource$Vitals$Crashrate$Get
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Crashrate$Get extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/crashRateMetricSet
      */
     name?: string;
   }
-  export interface Params$Resource$Vitals$Crashrate$Query
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Crashrate$Query extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/crashRateMetricSet
      */
@@ -2605,15 +2600,13 @@ export namespace playdeveloperreporting_v1alpha1 {
     }
   }
 
-  export interface Params$Resource$Vitals$Errors$Counts$Get
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Errors$Counts$Get extends StandardParameters {
     /**
      * Required. Name of the errors metric set. Format: apps/{app\}/errorCountMetricSet
      */
     name?: string;
   }
-  export interface Params$Resource$Vitals$Errors$Counts$Query
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Errors$Counts$Query extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/errorCountMetricSet
      */
@@ -2831,8 +2824,7 @@ export namespace playdeveloperreporting_v1alpha1 {
     }
   }
 
-  export interface Params$Resource$Vitals$Errors$Issues$Search
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Errors$Issues$Search extends StandardParameters {
     /**
      * A selection predicate to retrieve only a subset of the issues. Counts in the returned error issues will only reflect occurrences that matched the filter. For filtering basics, please check [AIP-160](https://google.aip.dev/160). ** Supported field names:** * `apiLevel`: Matches error issues that occurred in the requested Android versions (specified as the numeric API level) only. Example: `apiLevel = 28 OR apiLevel = 29`. * `versionCode`: Matches error issues that occurred in the requested app version codes only. Example: `versionCode = 123 OR versionCode = 456`. * `deviceModel`: Matches error issues that occurred in the requested devices. Example: `deviceModel = "google/walleye" OR deviceModel = "google/marlin"`. * `deviceBrand`: Matches error issues that occurred in the requested device brands. Example: `deviceBrand = "Google". * `deviceType`: Matches error issues that occurred in the requested device types. Example: `deviceType = "PHONE"`. * `errorIssueType`: Matches error issues of the requested types only. Valid candidates: `CRASH`, `ANR`, `NON_FATAL`. Example: `errorIssueType = CRASH OR errorIssueType = ANR`. * `appProcessState`: Matches error issues on the process state of an app, indicating whether an app runs in the foreground (user-visible) or background. Valid candidates: `FOREGROUND`, `BACKGROUND`. Example: `appProcessState = FOREGROUND`. * `isUserPerceived`: Matches error issues that are user-perceived. It is not accompanied by any operators. Example: `isUserPerceived`. ** Supported operators:** * Comparison operators: The only supported comparison operator is equality. The filtered field must appear on the left hand side of the comparison. * Logical Operators: Logical operators `AND` and `OR` can be used to build complex filters following a conjunctive normal form (CNF), i.e., conjunctions of disjunctions. The `OR` operator takes precedence over `AND` so the use of parenthesis is not necessary when building CNF. The `OR` operator is only supported to build disjunctions that apply to the same field, e.g., `versionCode = 123 OR errorIssueType = ANR` is not a valid filter. ** Examples ** Some valid filtering expressions: * `versionCode = 123 AND errorIssueType = ANR` * `versionCode = 123 AND errorIssueType = OR errorIssueType = CRASH` * `versionCode = 123 AND (errorIssueType = OR errorIssueType = CRASH)`
      */
@@ -3141,8 +3133,7 @@ export namespace playdeveloperreporting_v1alpha1 {
     }
   }
 
-  export interface Params$Resource$Vitals$Errors$Reports$Search
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Errors$Reports$Search extends StandardParameters {
     /**
      * A selection predicate to retrieve only a subset of the reports. For filtering basics, please check [AIP-160](https://google.aip.dev/160). ** Supported field names:** * `apiLevel`: Matches error reports that occurred in the requested Android versions (specified as the numeric API level) only. Example: `apiLevel = 28 OR apiLevel = 29`. * `versionCode`: Matches error reports that occurred in the requested app version codes only. Example: `versionCode = 123 OR versionCode = 456`. * `deviceModel`: Matches error issues that occurred in the requested devices. Example: `deviceModel = "google/walleye" OR deviceModel = "google/marlin"`. * `deviceBrand`: Matches error issues that occurred in the requested device brands. Example: `deviceBrand = "Google". * `deviceType`: Matches error reports that occurred in the requested device types. Example: `deviceType = "PHONE"`. * `errorIssueType`: Matches error reports of the requested types only. Valid candidates: `CRASH`, `ANR`, `NON_FATAL`. Example: `errorIssueType = CRASH OR errorIssueType = ANR`. * `errorIssueId`: Matches error reports belonging to the requested error issue ids only. Example: `errorIssueId = 1234 OR errorIssueId = 4567`. * `errorReportId`: Matches error reports with the requested error report id. Example: `errorReportId = 1234 OR errorReportId = 4567`. * `appProcessState`: Matches error reports on the process state of an app, indicating whether an app runs in the foreground (user-visible) or background. Valid candidates: `FOREGROUND`, `BACKGROUND`. Example: `appProcessState = FOREGROUND`. * `isUserPerceived`: Matches error reports that are user-perceived. It is not accompanied by any operators. Example: `isUserPerceived`. ** Supported operators:** * Comparison operators: The only supported comparison operator is equality. The filtered field must appear on the left hand side of the comparison. * Logical Operators: Logical operators `AND` and `OR` can be used to build complex filters following a conjunctive normal form (CNF), i.e., conjunctions of disjunctions. The `OR` operator takes precedence over `AND` so the use of parenthesis is not necessary when building CNF. The `OR` operator is only supported to build disjunctions that apply to the same field, e.g., `versionCode = 123 OR versionCode = ANR`. The filter expression `versionCode = 123 OR errorIssueType = ANR` is not valid. ** Examples ** Some valid filtering expressions: * `versionCode = 123 AND errorIssueType = ANR` * `versionCode = 123 AND errorIssueType = OR errorIssueType = CRASH` * `versionCode = 123 AND (errorIssueType = OR errorIssueType = CRASH)`
      */
@@ -3557,15 +3548,13 @@ export namespace playdeveloperreporting_v1alpha1 {
     }
   }
 
-  export interface Params$Resource$Vitals$Excessivewakeuprate$Get
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Excessivewakeuprate$Get extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/excessiveWakeupRateMetricSet
      */
     name?: string;
   }
-  export interface Params$Resource$Vitals$Excessivewakeuprate$Query
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Excessivewakeuprate$Query extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/excessiveWakeupRateMetricSet
      */
@@ -3893,15 +3882,13 @@ export namespace playdeveloperreporting_v1alpha1 {
     }
   }
 
-  export interface Params$Resource$Vitals$Lmkrate$Get
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Lmkrate$Get extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/lmkRateMetricSet
      */
     name?: string;
   }
-  export interface Params$Resource$Vitals$Lmkrate$Query
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Lmkrate$Query extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/lmkRateMetricSet
      */
@@ -4229,15 +4216,13 @@ export namespace playdeveloperreporting_v1alpha1 {
     }
   }
 
-  export interface Params$Resource$Vitals$Slowrenderingrate$Get
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Slowrenderingrate$Get extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/slowRenderingRateMetricSet
      */
     name?: string;
   }
-  export interface Params$Resource$Vitals$Slowrenderingrate$Query
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Slowrenderingrate$Query extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/slowRenderingRateMetricSet
      */
@@ -4565,15 +4550,13 @@ export namespace playdeveloperreporting_v1alpha1 {
     }
   }
 
-  export interface Params$Resource$Vitals$Slowstartrate$Get
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Slowstartrate$Get extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/slowStartRateMetricSet
      */
     name?: string;
   }
-  export interface Params$Resource$Vitals$Slowstartrate$Query
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Slowstartrate$Query extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/slowStartRateMetricSet
      */
@@ -4903,15 +4886,13 @@ export namespace playdeveloperreporting_v1alpha1 {
     }
   }
 
-  export interface Params$Resource$Vitals$Stuckbackgroundwakelockrate$Get
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Stuckbackgroundwakelockrate$Get extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/stuckBackgroundWakelockRateMetricSet
      */
     name?: string;
   }
-  export interface Params$Resource$Vitals$Stuckbackgroundwakelockrate$Query
-    extends StandardParameters {
+  export interface Params$Resource$Vitals$Stuckbackgroundwakelockrate$Query extends StandardParameters {
     /**
      * Required. The resource name. Format: apps/{app\}/stuckBackgroundWakelockRateMetricSet
      */
