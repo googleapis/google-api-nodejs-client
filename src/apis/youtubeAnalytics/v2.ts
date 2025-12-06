89dea2385821afe476f6e7fa88d46632378bb4cc
Yoshi Automation <yoshi-automation@google.com>
2025-12-06 01:37:43 +0000

    fix(youtubeAnalytics): update the API

--- src/apis/youtubeAnalytics/v2.ts
+++ src/apis/youtubeAnalytics/v2.ts
@@ -819,8 +819,7 @@ export namespace youtubeAnalytics_v2 {
     }
   }
 
-  export interface Params$Resource$Groupitems$Delete
-    extends StandardParameters {
+  export interface Params$Resource$Groupitems$Delete extends StandardParameters {
     /**
      * The `id` parameter specifies the YouTube group item ID of the group item that is being deleted.
      */
@@ -830,8 +829,7 @@ export namespace youtubeAnalytics_v2 {
      */
     onBehalfOfContentOwner?: string;
   }
-  export interface Params$Resource$Groupitems$Insert
-    extends StandardParameters {
+  export interface Params$Resource$Groupitems$Insert extends StandardParameters {
     /**
      * This parameter can only be used in a properly authorized request. **Note:** This parameter is intended exclusively for YouTube content partners that own and manage many different YouTube channels. The `onBehalfOfContentOwner` parameter indicates that the request's authorization credentials identify a YouTube user who is acting on behalf of the content owner specified in the parameter value. It allows content owners to authenticate once and get access to all their video and channel data, without having to provide authentication credentials for each individual channel. The account that the user authenticates with must be linked to the specified YouTube content owner.
      */
