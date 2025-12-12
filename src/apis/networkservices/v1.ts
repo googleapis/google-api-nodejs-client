6959f6d0b87218fe5470a37cedf99afba2f8f27d
Yoshi Automation <yoshi-automation@google.com>
2025-12-12 01:38:27 +0000

    fix(networkservices): update the API
    #### networkservices:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.description
    - schemas.AuthzExtension.properties.wireFormat.description
    - schemas.AuthzExtension.properties.wireFormat.enumDescriptions
    - schemas.Gateway.properties.envoyHeaders.enumDescriptions
    - schemas.ListOperationsResponse.properties.unreachable.description
    - schemas.Mesh.properties.envoyHeaders.enumDescriptions

--- src/apis/networkservices/v1.ts
+++ src/apis/networkservices/v1.ts
@@ -203,7 +203,7 @@ export namespace networkservices_v1 {
      */
     updateTime?: string | null;
     /**
-     * Optional. The format of communication supported by the callout extension. If not specified, the default value `EXT_PROC_GRPC` is used.
+     * Optional. The format of communication supported by the callout extension. This field is supported only for regional `AuthzExtension` resources. If not specified, the default value `EXT_PROC_GRPC` is used. Global `AuthzExtension` resources use the `EXT_PROC_GRPC` wire format.
      */
     wireFormat?: string | null;
   }
@@ -1489,7 +1489,7 @@ export namespace networkservices_v1 {
      */
     operations?: Schema$Operation[];
     /**
-     * Unordered list. Unreachable resources. Populated when the request sets `ListOperationsRequest.return_partial_success` and reads across collections e.g. when attempting to list all resources across all supported locations.
+     * Unordered list. Unreachable resources. Populated when the request sets `ListOperationsRequest.return_partial_success` and reads across collections. For example, when attempting to list all resources across all supported locations.
      */
     unreachable?: string[] | null;
   }
@@ -12770,7 +12770,7 @@ export namespace networkservices_v1 {
      *     pageSize: 'placeholder-value',
      *     // The standard list page token.
      *     pageToken: 'placeholder-value',
-     *     // When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     *     // When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the ListOperationsResponse.unreachable field. This can only be `true` when reading across collections. For example, when `parent` is set to `"projects/example/locations/-"`. This field is not supported by default and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
      *     returnPartialSuccess: 'placeholder-value',
      *   });
      *   console.log(res.data);
@@ -12923,7 +12923,7 @@ export namespace networkservices_v1 {
      */
     pageToken?: string;
     /**
-     * When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     * When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the ListOperationsResponse.unreachable field. This can only be `true` when reading across collections. For example, when `parent` is set to `"projects/example/locations/-"`. This field is not supported by default and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
      */
     returnPartialSuccess?: boolean;
   }
