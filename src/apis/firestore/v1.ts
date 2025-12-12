2703fd00a8aa98ea28a486866c23bbd647628acc
Yoshi Automation <yoshi-automation@google.com>
2025-12-12 01:38:26 +0000

    fix(firestore): update the API
    #### firestore:v1
    
    The following keys were changed:
    - resources.projects.resources.databases.resources.operations.methods.list.parameters.returnPartialSuccess.description
    - schemas.GoogleFirestoreAdminV1EncryptionConfig.description
    - schemas.GoogleLongrunningListOperationsResponse.properties.unreachable.description

--- src/apis/firestore/v1.ts
+++ src/apis/firestore/v1.ts
@@ -949,7 +949,7 @@ export namespace firestore_v1 {
    */
   export interface Schema$GoogleFirestoreAdminV1EnableUserCredsRequest {}
   /**
-   * Encryption configuration for a new database being created from another source. The source could be a Backup .
+   * Encryption configuration for a new database being created from another source. The source could be a Backup or a PitrSnapshot.
    */
   export interface Schema$GoogleFirestoreAdminV1EncryptionConfig {
     /**
@@ -1568,7 +1568,7 @@ export namespace firestore_v1 {
      */
     operations?: Schema$GoogleLongrunningOperation[];
     /**
-     * Unordered list. Unreachable resources. Populated when the request sets `ListOperationsRequest.return_partial_success` and reads across collections e.g. when attempting to list all resources across all supported locations.
+     * Unordered list. Unreachable resources. Populated when the request sets `ListOperationsRequest.return_partial_success` and reads across collections. For example, when attempting to list all resources across all supported locations.
      */
     unreachable?: string[] | null;
   }
@@ -9419,7 +9419,7 @@ export namespace firestore_v1 {
      *     pageSize: 'placeholder-value',
      *     // The standard list page token.
      *     pageToken: 'placeholder-value',
-     *     // When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     *     // When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the ListOperationsResponse.unreachable field. This can only be `true` when reading across collections. For example, when `parent` is set to `"projects/example/locations/-"`. This field is not supported by default and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
      *     returnPartialSuccess: 'placeholder-value',
      *   });
      *   console.log(res.data);
@@ -9579,7 +9579,7 @@ export namespace firestore_v1 {
      */
     pageToken?: string;
     /**
-     * When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     * When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the ListOperationsResponse.unreachable field. This can only be `true` when reading across collections. For example, when `parent` is set to `"projects/example/locations/-"`. This field is not supported by default and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
      */
     returnPartialSuccess?: boolean;
   }
