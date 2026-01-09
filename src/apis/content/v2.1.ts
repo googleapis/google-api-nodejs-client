961e3729e8dd9a81499a0681e70c519799e3f135
Yoshi Automation <yoshi-automation@google.com>
2026-01-09 01:38:12 +0000

    fix(content): update the API
    #### content:v2.1
    
    The following keys were changed:
    - schemas.ProductView.properties.clickPotentialRank.description

--- src/apis/content/v2.1.ts
+++ src/apis/content/v2.1.ts
@@ -5298,7 +5298,7 @@ export namespace content_v2_1 {
      */
     clickPotential?: string | null;
     /**
-     * Rank of the product based on its click potential. A product with `click_potential_rank` 1 has the highest click potential among the merchant's products that fulfill the search query conditions.
+     * Normalized click potential of the product. Values range from 1 to 1000, where 1 is the highest click potential and 1000 is the theoretical lowest.
      */
     clickPotentialRank?: string | null;
     /**
