21b70356333383373c03cfc4db3032fa6f85c01c
Yoshi Automation <yoshi-automation@google.com>
2026-01-28 19:46:16 +0000

    feat(merchantapi): update the API
    #### merchantapi:inventories_v1
    
    The following keys were added:
    - schemas.InventoryLoyaltyProgram.description
    - schemas.InventoryLoyaltyProgram.id
    - schemas.InventoryLoyaltyProgram.properties.cashbackForFutureUse.$ref
    - schemas.InventoryLoyaltyProgram.properties.cashbackForFutureUse.description
    - schemas.InventoryLoyaltyProgram.properties.loyaltyPoints.description
    - schemas.InventoryLoyaltyProgram.properties.loyaltyPoints.format
    - schemas.InventoryLoyaltyProgram.properties.loyaltyPoints.type
    - schemas.InventoryLoyaltyProgram.properties.memberPriceEffectiveInterval.$ref
    - schemas.InventoryLoyaltyProgram.properties.memberPriceEffectiveInterval.description
    - schemas.InventoryLoyaltyProgram.properties.price.$ref
    - schemas.InventoryLoyaltyProgram.properties.price.description
    - schemas.InventoryLoyaltyProgram.properties.programLabel.description
    - schemas.InventoryLoyaltyProgram.properties.programLabel.type
    - schemas.InventoryLoyaltyProgram.properties.shippingLabel.description
    - schemas.InventoryLoyaltyProgram.properties.shippingLabel.type
    - schemas.InventoryLoyaltyProgram.properties.tierLabel.description
    - schemas.InventoryLoyaltyProgram.properties.tierLabel.type
    - schemas.InventoryLoyaltyProgram.type
    - schemas.LocalInventoryAttributes.properties.loyaltyPrograms.description
    - schemas.LocalInventoryAttributes.properties.loyaltyPrograms.items.$ref
    - schemas.LocalInventoryAttributes.properties.loyaltyPrograms.type
    - schemas.RegionalInventoryAttributes.properties.loyaltyPrograms.description
    - schemas.RegionalInventoryAttributes.properties.loyaltyPrograms.items.$ref
    - schemas.RegionalInventoryAttributes.properties.loyaltyPrograms.type
    
    #### merchantapi:issueresolution_v1
    
    The following keys were changed:
    - resources.issueresolution.methods.triggeraction.description
    
    #### merchantapi:issueresolution_v1beta
    
    The following keys were changed:
    - resources.issueresolution.methods.triggeraction.description
    
    #### merchantapi:notifications_v1
    
    The following keys were changed:
    - resources.accounts.resources.notificationsubscriptions.methods.create.description
    - schemas.NotificationSubscription.description
    
    #### merchantapi:notifications_v1beta
    
    The following keys were changed:
    - resources.accounts.resources.notificationsubscriptions.methods.create.description
    - schemas.NotificationSubscription.description
    
    #### merchantapi:products_v1
    
    The following keys were changed:
    - schemas.LoyaltyProgram.description
    
    #### merchantapi:products_v1beta
    
    The following keys were changed:
    - schemas.LoyaltyProgram.description

--- src/apis/merchantapi/notifications_v1.ts
+++ src/apis/merchantapi/notifications_v1.ts
@@ -142,7 +142,7 @@ export namespace merchantapi_notifications_v1 {
     notificationSubscriptions?: Schema$NotificationSubscription[];
   }
   /**
-   * Represents a notification subscription owned by a Merchant account.
+   * Represents a notification subscription owned by a Merchant account. See [Decode notifications](/merchant/api/guides/accounts/notifications#decode_notifications) for information on how to decode the notification payload and how to interpret its contents.
    */
   export interface Schema$NotificationSubscription {
     /**
@@ -246,7 +246,7 @@ export namespace merchantapi_notifications_v1 {
     }
 
     /**
-     * Creates a notification subscription for a business. For standalone or subaccounts accounts, the business can create a subscription for self. For MCAs, the business can create a subscription for all managed accounts or for a specific subaccount. We will allow the following types of notification subscriptions to exist together (per business as a subscriber per event type): 1. Subscription for all managed accounts + subscription for self. 2. Multiple "partial" subscriptions for managed accounts + subscription for self. we will not allow (per business as a subscriber per event type): 1. Multiple self subscriptions. 2. Multiple "all managed accounts" subscriptions. 3. "All managed accounts" subscription and partial subscriptions at the same time. 4. Multiple partial subscriptions for the same target account.
+     * Creates a notification subscription for a business. For standalone or subaccounts accounts, the business can create a subscription for self. For MCAs, the business can create a subscription for all managed accounts or for a specific subaccount. See [Decode notifications](/merchant/api/guides/accounts/notifications#decode_notifications) for information on how to decode the notification payload and how to interpret its contents. We will allow the following types of notification subscriptions to exist together (per business as a subscriber per event type): 1. Subscription for all managed accounts + subscription for self. 2. Multiple "partial" subscriptions for managed accounts + subscription for self. we will not allow (per business as a subscriber per event type): 1. Multiple self subscriptions. 2. Multiple "all managed accounts" subscriptions. 3. "All managed accounts" subscription and partial subscriptions at the same time. 4. Multiple partial subscriptions for the same target account.
      * @example
      * ```js
      * // Before running the sample:
