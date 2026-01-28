8bbc504412bad297d548219d43bfdc80862f6c46
Yoshi Automation <yoshi-automation@google.com>
2026-01-28 19:46:18 +0000

    fix(servicenetworking): update the API
    #### servicenetworking:v1beta
    
    The following keys were changed:
    - schemas.Service.properties.endpoints.description
    - schemas.Service.properties.monitoredResources.description
    
    #### servicenetworking:v1
    
    The following keys were changed:
    - schemas.Service.properties.endpoints.description
    - schemas.Service.properties.monitoredResources.description

--- src/apis/servicenetworking/v1beta.ts
+++ src/apis/servicenetworking/v1beta.ts
@@ -1873,7 +1873,7 @@ export namespace servicenetworking_v1beta {
      */
     documentation?: Schema$Documentation;
     /**
-     * Configuration for network endpoints. If this is empty, then an endpoint with the same name as the service is automatically generated to service all defined APIs.
+     * Configuration for network endpoints. If this is empty, then an endpoint with the same name as the service is automatically generated to service all defined APIs. WARNING: Defining any entries in the `endpoints` list disables the automatic generation of default endpoint variations (e.g., `{service\}.clients6.google.com`, `content-{service\}.googleapis.com`, and mTLS variants like `{service\}.mtls.googleapis.com`). To retain these default variations, you are required to explicitly include your main service endpoint (e.g., `myservice.googleapis.com`) in this list alongside any other custom endpoints (like REP, GFE, etc.).
      */
     endpoints?: Schema$Endpoint[];
     /**
@@ -1901,7 +1901,7 @@ export namespace servicenetworking_v1beta {
      */
     metrics?: Schema$MetricDescriptor[];
     /**
-     * Defines the monitored resources used by this service. This is required by the Service.monitoring and Service.logging configurations.
+     * Defines the monitored resources used by this service. This is required by the `Service.monitoring` and `Service.logging` configurations.
      */
     monitoredResources?: Schema$MonitoredResourceDescriptor[];
     /**
