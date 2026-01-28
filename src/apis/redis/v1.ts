8155ea1aba0460b716cead54ff8c01e82e6a8979
Yoshi Automation <yoshi-automation@google.com>
2026-01-28 19:46:18 +0000

    feat(redis): update the API
    #### redis:v1beta1
    
    The following keys were added:
    - schemas.MachineConfiguration.properties.baselineSlots.description
    - schemas.MachineConfiguration.properties.baselineSlots.format
    - schemas.MachineConfiguration.properties.baselineSlots.type
    - schemas.MachineConfiguration.properties.maxReservationSlots.description
    - schemas.MachineConfiguration.properties.maxReservationSlots.format
    - schemas.MachineConfiguration.properties.maxReservationSlots.type
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.description
    - schemas.ConfigBasedSignalData.properties.signalType.enum
    - schemas.ConfigBasedSignalData.properties.signalType.enumDescriptions
    - schemas.DatabaseResourceHealthSignalData.properties.signalType.enum
    - schemas.DatabaseResourceHealthSignalData.properties.signalType.enumDeprecated
    - schemas.DatabaseResourceHealthSignalData.properties.signalType.enumDescriptions
    - schemas.DatabaseResourceRecommendationSignalData.properties.signalType.enum
    - schemas.DatabaseResourceRecommendationSignalData.properties.signalType.enumDeprecated
    - schemas.DatabaseResourceRecommendationSignalData.properties.signalType.enumDescriptions
    - schemas.DatabaseResourceSignalData.properties.signalType.enum
    - schemas.DatabaseResourceSignalData.properties.signalType.enumDescriptions
    - schemas.ListOperationsResponse.properties.unreachable.description
    
    #### redis:v1
    
    The following keys were added:
    - schemas.MachineConfiguration.properties.baselineSlots.description
    - schemas.MachineConfiguration.properties.baselineSlots.format
    - schemas.MachineConfiguration.properties.baselineSlots.type
    - schemas.MachineConfiguration.properties.maxReservationSlots.description
    - schemas.MachineConfiguration.properties.maxReservationSlots.format
    - schemas.MachineConfiguration.properties.maxReservationSlots.type
    
    The following keys were changed:
    - resources.projects.resources.locations.resources.operations.methods.list.parameters.returnPartialSuccess.description
    - schemas.ConfigBasedSignalData.properties.signalType.enum
    - schemas.ConfigBasedSignalData.properties.signalType.enumDescriptions
    - schemas.DatabaseResourceHealthSignalData.properties.signalType.enum
    - schemas.DatabaseResourceHealthSignalData.properties.signalType.enumDeprecated
    - schemas.DatabaseResourceHealthSignalData.properties.signalType.enumDescriptions
    - schemas.DatabaseResourceRecommendationSignalData.properties.signalType.enum
    - schemas.DatabaseResourceRecommendationSignalData.properties.signalType.enumDeprecated
    - schemas.DatabaseResourceRecommendationSignalData.properties.signalType.enumDescriptions
    - schemas.DatabaseResourceSignalData.properties.signalType.enum
    - schemas.DatabaseResourceSignalData.properties.signalType.enumDescriptions
    - schemas.ListOperationsResponse.properties.unreachable.description

--- src/apis/redis/v1.ts
+++ src/apis/redis/v1.ts
@@ -1508,7 +1508,7 @@ export namespace redis_v1 {
      */
     operations?: Schema$Operation[];
     /**
-     * Unordered list. Unreachable resources. Populated when the request sets `ListOperationsRequest.return_partial_success` and reads across collections e.g. when attempting to list all resources across all supported locations.
+     * Unordered list. Unreachable resources. Populated when the request sets `ListOperationsRequest.return_partial_success` and reads across collections. For example, when attempting to list all resources across all supported locations.
      */
     unreachable?: string[] | null;
   }
@@ -1542,9 +1542,17 @@ export namespace redis_v1 {
    */
   export interface Schema$MachineConfiguration {
     /**
+     * Optional. Baseline slots for BigQuery Reservations. Baseline slots are in increments of 50.
+     */
+    baselineSlots?: string | null;
+    /**
      * The number of CPUs. Deprecated. Use vcpu_count instead. TODO(b/342344482) add proto validations again after bug fix.
      */
     cpuCount?: number | null;
+    /**
+     * Optional. Max slots for BigQuery Reservations. Max slots are in increments of 50.
+     */
+    maxReservationSlots?: string | null;
     /**
      * Memory size in bytes. TODO(b/342344482) add proto validations again after bug fix.
      */
@@ -7143,7 +7151,7 @@ export namespace redis_v1 {
      *     pageSize: 'placeholder-value',
      *     // The standard list page token.
      *     pageToken: 'placeholder-value',
-     *     // When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     *     // When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the ListOperationsResponse.unreachable field. This can only be `true` when reading across collections. For example, when `parent` is set to `"projects/example/locations/-"`. This field is not supported by default and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
      *     returnPartialSuccess: 'placeholder-value',
      *   });
      *   console.log(res.data);
@@ -7290,7 +7298,7 @@ export namespace redis_v1 {
      */
     pageToken?: string;
     /**
-     * When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
+     * When set to `true`, operations that are reachable are returned as normal, and those that are unreachable are returned in the ListOperationsResponse.unreachable field. This can only be `true` when reading across collections. For example, when `parent` is set to `"projects/example/locations/-"`. This field is not supported by default and will result in an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product specific documentation.
      */
     returnPartialSuccess?: boolean;
   }
