efc3146f59ce0e221e7f9b8e04ed68ecd9b9ca6a
Yoshi Automation <yoshi-automation@google.com>
2026-01-28 19:46:11 +0000

    feat(apikeys)!: update the API
    BREAKING CHANGE: This release has breaking changes.
    
    #### apikeys:v2
    
    The following keys were deleted:
    - schemas.V2Key.properties.etag.readOnly
    
    The following keys were changed:
    - schemas.V2Key.properties.etag.description

--- src/apis/apikeys/v2.ts
+++ src/apis/apikeys/v2.ts
@@ -253,7 +253,7 @@ export namespace apikeys_v2 {
      */
     displayName?: string | null;
     /**
-     * Output only. A checksum computed by the server based on the current value of the Key resource. This may be sent on update and delete requests to ensure the client has an up-to-date value before proceeding. See https://google.aip.dev/154.
+     * A checksum computed by the server based on the current value of the Key resource. This may be sent on update and delete requests to ensure the client has an up-to-date value before proceeding. See https://google.aip.dev/154.
      */
     etag?: string | null;
     /**
