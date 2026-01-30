c946496c26c5bba031a995f78c6364ec958be503
Yoshi Automation <yoshi-automation@google.com>
2026-01-30 01:40:18 +0000

    feat(containeranalysis): update the API
    #### containeranalysis:v1alpha1
    
    The following keys were added:
    - schemas.Discovered.properties.lastVulnerabilityUpdateTime.description
    - schemas.Discovered.properties.lastVulnerabilityUpdateTime.format
    - schemas.Discovered.properties.lastVulnerabilityUpdateTime.type
    
    #### containeranalysis:v1beta1
    
    The following keys were added:
    - schemas.Discovered.properties.lastVulnerabilityUpdateTime.description
    - schemas.Discovered.properties.lastVulnerabilityUpdateTime.format
    - schemas.Discovered.properties.lastVulnerabilityUpdateTime.type
    
    #### containeranalysis:v1
    
    The following keys were added:
    - schemas.DiscoveryOccurrence.properties.lastVulnerabilityUpdateTime.description
    - schemas.DiscoveryOccurrence.properties.lastVulnerabilityUpdateTime.format
    - schemas.DiscoveryOccurrence.properties.lastVulnerabilityUpdateTime.type

--- src/apis/containeranalysis/v1.ts
+++ src/apis/containeranalysis/v1.ts
@@ -1845,6 +1845,10 @@ export namespace containeranalysis_v1 {
      */
     lastScanTime?: string | null;
     /**
+     * The last time vulnerability scan results changed.
+     */
+    lastVulnerabilityUpdateTime?: string | null;
+    /**
      * The status of an SBOM generation.
      */
     sbomStatus?: Schema$SBOMStatus;
