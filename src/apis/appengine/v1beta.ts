defebaa8dc5a717e2f4238c060667837c7cc57e3
Yoshi Automation <yoshi-automation@google.com>
2026-01-30 01:40:17 +0000

    fix(appengine): update the API
    #### appengine:v1alpha
    
    The following keys were changed:
    - resources.apps.resources.locations.methods.list.description
    - resources.projects.resources.locations.methods.list.description
    
    #### appengine:v1beta
    
    The following keys were changed:
    - resources.apps.resources.locations.methods.list.description
    - resources.projects.resources.locations.methods.list.description
    
    #### appengine:v1
    
    The following keys were changed:
    - resources.apps.resources.locations.methods.list.description

--- src/apis/appengine/v1beta.ts
+++ src/apis/appengine/v1beta.ts
@@ -5856,7 +5856,7 @@ export namespace appengine_v1beta {
     }
 
     /**
-     * Lists information about the supported locations for this service.
+     * Lists information about the supported locations for this service. This method can be called in two ways: List all public locations: Use the path GET /v1/locations. List project-visible locations: Use the path GET /v1/projects/{project_id\}/locations. This may include public locations as well as private or other locations specifically visible to the project.
      * @example
      * ```js
      * // Before running the sample:
@@ -8915,7 +8915,7 @@ export namespace appengine_v1beta {
     }
 
     /**
-     * Lists information about the supported locations for this service.
+     * Lists information about the supported locations for this service. This method can be called in two ways: List all public locations: Use the path GET /v1/locations. List project-visible locations: Use the path GET /v1/projects/{project_id\}/locations. This may include public locations as well as private or other locations specifically visible to the project.
      * @example
      * ```js
      * // Before running the sample:
