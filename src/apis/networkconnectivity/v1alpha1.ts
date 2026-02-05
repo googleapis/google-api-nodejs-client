96b0b22654a6b421b6987a5ef07d675cdf9ce97d
Yoshi Automation <yoshi-automation@google.com>
2026-02-05 01:39:24 +0000

    fix(networkconnectivity): update the API
    #### networkconnectivity:v1alpha1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.description

--- src/apis/networkconnectivity/v1alpha1.ts
+++ src/apis/networkconnectivity/v1alpha1.ts
@@ -796,7 +796,7 @@ export namespace networkconnectivity_v1alpha1 {
     }
 
     /**
-     * Lists information about the supported locations for this service.
+     * Lists information about the supported locations for this service. This method can be called in two ways: * **List all public locations:** Use the path `GET /v1/locations`. * **List project-visible locations:** Use the path `GET /v1/projects/{project_id\}/locations`. This may include public locations as well as private or other locations specifically visible to the project.
      * @example
      * ```js
      * // Before running the sample:
