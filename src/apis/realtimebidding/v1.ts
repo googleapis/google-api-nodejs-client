cc0a646bcc8989405126b5087dda149e6464fab5
Yoshi Automation <yoshi-automation@google.com>
2026-03-27 01:41:03 +0000

    fix(realtimebidding): update the API
    #### realtimebidding:v1
    
    The following keys were changed:
    - resources.bidders.resources.pretargetingConfigs.methods.patch.parameters.name.description
    - schemas.NumericTargetingDimension.properties.excludedIds.description
    - schemas.NumericTargetingDimension.properties.includedIds.description
    - schemas.PretargetingConfig.description
    - schemas.PretargetingConfig.properties.allowedUserTargetingModes.description
    - schemas.PretargetingConfig.properties.billingId.description
    - schemas.PretargetingConfig.properties.displayName.description
    - schemas.PretargetingConfig.properties.excludedContentLabelIds.description
    - schemas.PretargetingConfig.properties.geoTargeting.description
    - schemas.PretargetingConfig.properties.includedCreativeDimensions.description
    - schemas.PretargetingConfig.properties.includedFormats.description
    - schemas.PretargetingConfig.properties.includedLanguages.description
    - schemas.PretargetingConfig.properties.includedMobileOperatingSystemIds.description
    - schemas.PretargetingConfig.properties.includedPlatforms.description
    - schemas.PretargetingConfig.properties.includedUserIdTypes.description
    - schemas.PretargetingConfig.properties.interstitialTargeting.description
    - schemas.PretargetingConfig.properties.maximumQps.description
    - schemas.PretargetingConfig.properties.minimumViewabilityDecile.description
    - schemas.PretargetingConfig.properties.name.description
    - schemas.PretargetingConfig.properties.state.description
    - schemas.PretargetingConfig.properties.state.enumDescriptions
    - schemas.PretargetingConfig.properties.userListTargeting.description
    - schemas.PretargetingConfig.properties.verticalTargeting.description
    - schemas.PretargetingConfig.properties.webTargeting.description

--- src/apis/realtimebidding/v1.ts
+++ src/apis/realtimebidding/v1.ts
@@ -868,11 +868,11 @@ export namespace realtimebidding_v1 {
    */
   export interface Schema$NumericTargetingDimension {
     /**
-     * The IDs excluded in a configuration.
+     * The IDs excluded in a config.
      */
     excludedIds?: string[] | null;
     /**
-     * The IDs included in a configuration.
+     * The IDs included in a config.
      */
     includedIds?: string[] | null;
   }
@@ -948,11 +948,11 @@ export namespace realtimebidding_v1 {
     httpCookie?: Schema$HttpCookieEvidence;
   }
   /**
-   * Pretargeting configuration: a set of targeting dimensions applied at the pretargeting stage of the RTB funnel. These control which inventory a bidder will receive bid requests for.
+   * Pretargeting config: a set of targeting dimensions applied at the pretargeting stage of the RTB funnel. These control which inventory a bidder will receive bid requests for.
    */
   export interface Schema$PretargetingConfig {
     /**
-     * Targeting modes included by this configuration. A bid request must allow all the specified targeting modes. An unset value allows all bid requests to be sent, regardless of which targeting modes they allow.
+     * Targeting modes included by this config. A bid request must allow all the specified targeting modes. An unset value allows all bid requests to be sent, regardless of which targeting modes they allow.
      */
     allowedUserTargetingModes?: string[] | null;
     /**
@@ -960,23 +960,23 @@ export namespace realtimebidding_v1 {
      */
     appTargeting?: Schema$AppTargeting;
     /**
-     * Output only. The identifier that corresponds to this pretargeting configuration that helps buyers track and attribute their spend across their own arbitrary divisions. If a bid request matches more than one configuration, the buyer chooses which billing_id to attribute each of their bids.
+     * Output only. The identifier that corresponds to this pretargeting config that helps buyers track and attribute their spend across their own arbitrary divisions. If a bid request matches more than one config, the buyer chooses which billing_id to attribute each of their bids.
      */
     billingId?: string | null;
     /**
-     * The diplay name associated with this configuration. This name must be unique among all the pretargeting configurations a bidder has.
+     * The diplay name associated with this config. This name must be unique among all the pretargeting configs a bidder has.
      */
     displayName?: string | null;
     /**
-     * The sensitive content category label IDs excluded in this configuration. Bid requests for inventory with any of the specified content label IDs will not be sent. Refer to this file https://storage.googleapis.com/adx-rtb-dictionaries/content-labels.txt for category IDs.
+     * The sensitive content category label IDs excluded in this config. Bid requests for inventory with any of the specified content label IDs will not be sent. Refer to this file https://storage.googleapis.com/adx-rtb-dictionaries/content-labels.txt for category IDs.
      */
     excludedContentLabelIds?: string[] | null;
     /**
-     * The geos included or excluded in this configuration defined in https://storage.googleapis.com/adx-rtb-dictionaries/geo-table.csv
+     * The geos included or excluded in this config defined in https://storage.googleapis.com/adx-rtb-dictionaries/geo-table.csv
      */
     geoTargeting?: Schema$NumericTargetingDimension;
     /**
-     * Creative dimensions included by this configuration. Only bid requests eligible for at least one of the specified creative dimensions will be sent. An unset value allows all bid requests to be sent, regardless of creative dimension.
+     * Creative dimensions included by this config. Only bid requests eligible for at least one of the specified creative dimensions will be sent. An unset value allows all bid requests to be sent, regardless of creative dimension.
      */
     includedCreativeDimensions?: Schema$CreativeDimensions[];
     /**
@@ -984,27 +984,27 @@ export namespace realtimebidding_v1 {
      */
     includedEnvironments?: string[] | null;
     /**
-     * Creative formats included by this configuration. Only bid requests eligible for at least one of the specified creative formats will be sent. An unset value will allow all bid requests to be sent, regardless of format.
+     * Creative formats included by this config. Only bid requests eligible for at least one of the specified creative formats will be sent. An unset value will allow all bid requests to be sent, regardless of format.
      */
     includedFormats?: string[] | null;
     /**
-     * The languages included in this configuration, represented by their language code. See https://developers.google.com/adwords/api/docs/appendix/languagecodes.
+     * The languages included in this config, represented by their language code. See https://developers.google.com/adwords/api/docs/appendix/languagecodes.
      */
     includedLanguages?: string[] | null;
     /**
-     * The mobile operating systems included in this configuration as defined in https://storage.googleapis.com/adx-rtb-dictionaries/mobile-os.csv
+     * The mobile operating systems included in this config as defined in https://storage.googleapis.com/adx-rtb-dictionaries/mobile-os.csv
      */
     includedMobileOperatingSystemIds?: string[] | null;
     /**
-     * The platforms included by this configration. Bid requests for devices with the specified platform types will be sent. An unset value allows all bid requests to be sent, regardless of platform.
+     * The platforms included by this config. Bid requests for devices with the specified platform types will be sent. An unset value allows all bid requests to be sent, regardless of platform.
      */
     includedPlatforms?: string[] | null;
     /**
-     * User identifier types included in this configuration. At least one of the user identifier types specified in this list must be available for the bid request to be sent.
+     * User identifier types included in this config. At least one of the user identifier types specified in this list must be available for the bid request to be sent.
      */
     includedUserIdTypes?: string[] | null;
     /**
-     * The interstitial targeting specified for this configuration. The unset value will allow bid requests to be sent regardless of whether they are for interstitials or not.
+     * The interstitial targeting specified for this config. The unset value will allow bid requests to be sent regardless of whether they are for interstitials or not.
      */
     interstitialTargeting?: string | null;
     /**
@@ -1012,15 +1012,15 @@ export namespace realtimebidding_v1 {
      */
     invalidGeoIds?: string[] | null;
     /**
-     * The maximum QPS threshold for this configuration. The bidder should receive no more than this number of bid requests matching this configuration per second across all their bidding endpoints among all trading locations. Further information available at https://developers.google.com/authorized-buyers/rtb/peer-guide
+     * The maximum QPS threshold for this config. The bidder should receive no more than this number of bid requests matching this config per second across all their bidding endpoints among all trading locations. Further information available at https://developers.google.com/authorized-buyers/rtb/peer-guide
      */
     maximumQps?: string | null;
     /**
-     * The targeted minimum viewability decile, ranging in values [0, 10]. A value of 5 means that the configuration will only match adslots for which we predict at least 50% viewability. Values \> 10 will be rounded down to 10. An unset value or a value of 0 indicates that bid requests will be sent regardless of viewability.
+     * The targeted minimum viewability decile, ranging in values [0, 10]. A value of 5 means that the config will only match adslots for which we predict at least 50% viewability. Values \> 10 will be rounded down to 10. An unset value or a value of 0 indicates that bid requests will be sent regardless of viewability.
      */
     minimumViewabilityDecile?: number | null;
     /**
-     * Output only. Name of the pretargeting configuration that must follow the pattern `bidders/{bidder_account_id\}/pretargetingConfigs/{config_id\}`
+     * Output only. Name of the pretargeting config that must follow the pattern `bidders/{bidder_account_id\}/pretargetingConfigs/{config_id\}`
      */
     name?: string | null;
     /**
@@ -1028,19 +1028,19 @@ export namespace realtimebidding_v1 {
      */
     publisherTargeting?: Schema$StringTargetingDimension;
     /**
-     * Output only. The state of this pretargeting configuration.
+     * Output only. The state of this pretargeting config.
      */
     state?: string | null;
     /**
-     * The remarketing lists included or excluded in this configuration as defined in UserList.
+     * The remarketing lists included or excluded in this config as defined in UserList.
      */
     userListTargeting?: Schema$NumericTargetingDimension;
     /**
-     * The verticals included or excluded in this configuration as defined in https://developers.google.com/authorized-buyers/rtb/downloads/publisher-verticals
+     * The verticals included or excluded in this config as defined in https://developers.google.com/authorized-buyers/rtb/downloads/publisher-verticals
      */
     verticalTargeting?: Schema$NumericTargetingDimension;
     /**
-     * Targeting on a subset of site inventory. If WEB is listed in included_environments, the specified targeting is applied. A maximum of 50,000 site URLs can be targeted. An unset value for targeting allows all web-based bid requests to be sent. Sites can either be targeting positively (bid requests will be sent only if the destination site is listed in the targeting dimension) or negatively (bid requests will be sent only if the destination site is not listed in the pretargeting configuration).
+     * Targeting on a subset of site inventory. If WEB is listed in included_environments, the specified targeting is applied. A maximum of 50,000 site URLs can be targeted. An unset value for targeting allows all web-based bid requests to be sent. Sites can either be targeting positively (bid requests will be sent only if the destination site is listed in the targeting dimension) or negatively (bid requests will be sent only if the destination site is not listed in the pretargeting config).
      */
     webTargeting?: Schema$StringTargetingDimension;
   }
@@ -3720,7 +3720,7 @@ export namespace realtimebidding_v1 {
      *
      *   // Do the magic
      *   const res = await realtimebidding.bidders.pretargetingConfigs.patch({
-     *     // Output only. Name of the pretargeting configuration that must follow the pattern `bidders/{bidder_account_id\}/pretargetingConfigs/{config_id\}`
+     *     // Output only. Name of the pretargeting config that must follow the pattern `bidders/{bidder_account_id\}/pretargetingConfigs/{config_id\}`
      *     name: 'bidders/my-bidder/pretargetingConfigs/my-pretargetingConfig',
      *     // Field mask to use for partial in-place updates.
      *     updateMask: 'placeholder-value',
@@ -4644,7 +4644,7 @@ export namespace realtimebidding_v1 {
   }
   export interface Params$Resource$Bidders$Pretargetingconfigs$Patch extends StandardParameters {
     /**
-     * Output only. Name of the pretargeting configuration that must follow the pattern `bidders/{bidder_account_id\}/pretargetingConfigs/{config_id\}`
+     * Output only. Name of the pretargeting config that must follow the pattern `bidders/{bidder_account_id\}/pretargetingConfigs/{config_id\}`
      */
     name?: string;
     /**
