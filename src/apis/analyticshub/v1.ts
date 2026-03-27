f718b401204a575d401a27acdf34ef9fc7943443
Yoshi Automation <yoshi-automation@google.com>
2026-03-27 01:40:55 +0000

    feat(analyticshub): update the API
    #### analyticshub:v1
    
    The following keys were added:
    - schemas.BigtableConfig.description
    - schemas.BigtableConfig.id
    - schemas.BigtableConfig.properties.appProfileId.description
    - schemas.BigtableConfig.properties.appProfileId.type
    - schemas.BigtableConfig.properties.serviceAccountEmail.description
    - schemas.BigtableConfig.properties.serviceAccountEmail.type
    - schemas.BigtableConfig.properties.table.description
    - schemas.BigtableConfig.properties.table.type
    - schemas.BigtableConfig.properties.writeMetadata.description
    - schemas.BigtableConfig.properties.writeMetadata.type
    - schemas.BigtableConfig.type
    - schemas.GooglePubsubV1Subscription.properties.bigtableConfig.$ref
    - schemas.GooglePubsubV1Subscription.properties.bigtableConfig.description

--- src/apis/analyticshub/v1.ts
+++ src/apis/analyticshub/v1.ts
@@ -241,6 +241,27 @@ export namespace analyticshub_v1 {
     selectedResources?: Schema$SelectedResource[];
   }
   /**
+   * Configuration for a Bigtable subscription. The Pub/Sub message will be written to a Bigtable row as follows: - row key: subscription name and message ID delimited by #. - columns: message bytes written to a single column family "data" with an empty-string column qualifier. - cell timestamp: the message publish timestamp.
+   */
+  export interface Schema$BigtableConfig {
+    /**
+     * Optional. The app profile to use for the Bigtable writes. If not specified, the "default" application profile will be used. The app profile must use single-cluster routing.
+     */
+    appProfileId?: string | null;
+    /**
+     * Optional. The service account to use to write to Bigtable. The subscription creator or updater that specifies this field must have `iam.serviceAccounts.actAs` permission on the service account. If not specified, the Pub/Sub [service agent]({$universe.dns_names.final_documentation_domain\}/iam/docs/service-agents), service-{project_number\}@gcp-sa-pubsub.iam.gserviceaccount.com, is used.
+     */
+    serviceAccountEmail?: string | null;
+    /**
+     * Optional. The unique name of the table to write messages to. Values are of the form `projects//instances//tables/`.
+     */
+    table?: string | null;
+    /**
+     * Optional. When true, write the subscription name, message_id, publish_time, attributes, and ordering_key to additional columns in the table under the pubsub_metadata column family. The subscription name, message_id, and publish_time fields are put in their own columns while all other message properties (other than data) are written to a JSON object in the attributes column.
+     */
+    writeMetadata?: boolean | null;
+  }
+  /**
    * Associates `members`, or principals, with a `role`.
    */
   export interface Schema$Binding {
@@ -542,6 +563,10 @@ export namespace analyticshub_v1 {
      * Optional. If delivery to BigQuery is used with this subscription, this field is used to configure it.
      */
     bigqueryConfig?: Schema$BigQueryConfig;
+    /**
+     * Optional. If delivery to Bigtable is used with this subscription, this field is used to configure it.
+     */
+    bigtableConfig?: Schema$BigtableConfig;
     /**
      * Optional. If delivery to Google Cloud Storage is used with this subscription, this field is used to configure it.
      */
