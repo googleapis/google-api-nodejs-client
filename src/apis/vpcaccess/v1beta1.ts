92a920b051eb627f104d9a81a162362b561d6305
Yoshi Automation <yoshi-automation@google.com>
2026-05-11 01:42:24 +0000

    fix(vpcaccess): update the API
    #### vpcaccess:v1beta1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.description
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### vpcaccess:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.description
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description

--- src/apis/vpcaccess/v1beta1.ts
+++ src/apis/vpcaccess/v1beta1.ts
@@ -392,7 +392,7 @@ export namespace vpcaccess_v1beta1 {
     }
 
     /**
-     * Lists information about the supported locations for this service. This method lists locations based on the resource scope provided in the [ListLocationsRequest.name] field: * **Global locations**: If `name` is empty, the method lists the public locations available to all projects. * **Project-specific locations**: If `name` follows the format `projects/{project\}`, the method lists locations visible to that specific project. This includes public, private, or other project-specific locations enabled for the project. For gRPC and client library implementations, the resource name is passed as the `name` field. For direct service calls, the resource name is incorporated into the request path based on the specific service implementation and version.
+     * Lists information about the supported locations for this service. This method lists locations based on the resource scope provided in the ListLocationsRequest.name field: * **Global locations**: If `name` is empty, the method lists the public locations available to all projects. * **Project-specific locations**: If `name` follows the format `projects/{project\}`, the method lists locations visible to that specific project. This includes public, private, or other project-specific locations enabled for the project. For gRPC and client library implementations, the resource name is passed as the `name` field. For direct service calls, the resource name is incorporated into the request path based on the specific service implementation and version.
      * @example
      * ```js
      * // Before running the sample:
@@ -422,7 +422,7 @@ export namespace vpcaccess_v1beta1 {
      *
      *   // Do the magic
      *   const res = await vpcaccess.projects.locations.list({
-     *     // Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
+     *     // Optional. Do not use this field unless explicitly documented otherwise. This is primarily for internal usage.
      *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
@@ -542,7 +542,7 @@ export namespace vpcaccess_v1beta1 {
 
   export interface Params$Resource$Projects$Locations$List extends StandardParameters {
     /**
-     * Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
+     * Optional. Do not use this field unless explicitly documented otherwise. This is primarily for internal usage.
      */
     extraLocationTypes?: string[];
     /**
