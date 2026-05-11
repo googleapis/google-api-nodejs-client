d6e4dcf2cc0069e8ec9332c4cf030cf31f996bf9
Yoshi Automation <yoshi-automation@google.com>
2026-05-11 01:42:24 +0000

    fix(managedidentities): update the API
    #### managedidentities:v1alpha1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    
    #### managedidentities:v1beta1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description

--- src/apis/managedidentities/v1alpha1.ts
+++ src/apis/managedidentities/v1alpha1.ts
@@ -1452,7 +1452,7 @@ export namespace managedidentities_v1alpha1 {
      *
      *   // Do the magic
      *   const res = await managedidentities.projects.locations.list({
-     *     // Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
+     *     // Optional. Do not use this field unless explicitly documented otherwise. This is primarily for internal usage.
      *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
@@ -1579,7 +1579,7 @@ export namespace managedidentities_v1alpha1 {
   }
   export interface Params$Resource$Projects$Locations$List extends StandardParameters {
     /**
-     * Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
+     * Optional. Do not use this field unless explicitly documented otherwise. This is primarily for internal usage.
      */
     extraLocationTypes?: string[];
     /**
--- src/apis/managedidentities/v1beta1.ts
+++ src/apis/managedidentities/v1beta1.ts
@@ -1477,7 +1477,7 @@ export namespace managedidentities_v1beta1 {
      *
      *   // Do the magic
      *   const res = await managedidentities.projects.locations.list({
-     *     // Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
+     *     // Optional. Do not use this field unless explicitly documented otherwise. This is primarily for internal usage.
      *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
@@ -1604,7 +1604,7 @@ export namespace managedidentities_v1beta1 {
   }
   export interface Params$Resource$Projects$Locations$List extends StandardParameters {
     /**
-     * Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
+     * Optional. Do not use this field unless explicitly documented otherwise. This is primarily for internal usage.
      */
     extraLocationTypes?: string[];
     /**
