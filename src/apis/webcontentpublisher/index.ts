43dc0ceeb22ec2db228a1f718bf0b58b6fd6a4d9
Yoshi Automation <yoshi-automation@google.com>
2026-05-28 01:41:15 +0000

    fix(webcontentpublisher): update the API

---
+++ src/apis/webcontentpublisher/index.ts
@@ -0,0 +1,49 @@
+// Copyright 2020 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*! THIS FILE IS AUTO-GENERATED */
+
+import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
+import {webcontentpublisher_v1} from './v1';
+
+export const VERSIONS = {
+  v1: webcontentpublisher_v1.Webcontentpublisher,
+};
+
+export function webcontentpublisher(
+  version: 'v1'
+): webcontentpublisher_v1.Webcontentpublisher;
+export function webcontentpublisher(
+  options: webcontentpublisher_v1.Options
+): webcontentpublisher_v1.Webcontentpublisher;
+export function webcontentpublisher<
+  T = webcontentpublisher_v1.Webcontentpublisher,
+>(
+  this: GoogleConfigurable,
+  versionOrOptions: 'v1' | webcontentpublisher_v1.Options
+) {
+  return getAPI<T>('webcontentpublisher', versionOrOptions, VERSIONS, this);
+}
+
+const auth = new AuthPlus();
+export {auth};
+export {webcontentpublisher_v1};
+export {
+  AuthPlus,
+  GlobalOptions,
+  APIRequestContext,
+  GoogleConfigurable,
+  StreamMethodOptions,
+  MethodOptions,
+  BodyResponseCallback,
+} from 'googleapis-common';
