4bd3b0381fa899b3ae3c21484e910100a3594f94
Yoshi Automation <yoshi-automation@google.com>
2026-05-28 01:41:11 +0000

    fix(datamigration): update the API
    #### datamigration:v1beta1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.description
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    - schemas.QuotaFailureViolation.properties.apiService.description
    
    #### datamigration:v1
    
    The following keys were changed:
    - resources.projects.resources.locations.methods.list.description
    - resources.projects.resources.locations.methods.list.parameters.extraLocationTypes.description
    - schemas.EntityIssue.properties.type.enum
    - schemas.EntityIssue.properties.type.enumDescriptions
    - schemas.QuotaFailureViolation.properties.apiService.description

--- src/apis/datamigration/v1beta1.ts
+++ src/apis/datamigration/v1beta1.ts
@@ -795,7 +795,7 @@ export namespace datamigration_v1beta1 {
    */
   export interface Schema$QuotaFailureViolation {
     /**
-     * The API Service from which the `QuotaFailure.Violation` orginates. In some cases, Quota issues originate from an API Service other than the one that was called. In other words, a dependency of the called API Service could be the cause of the `QuotaFailure`, and this field would have the dependency API service name. For example, if the called API is Kubernetes Engine API (container.googleapis.com), and a quota violation occurs in the Kubernetes Engine API itself, this field would be "container.googleapis.com". On the other hand, if the quota violation occurs when the Kubernetes Engine API creates VMs in the Compute Engine API (compute.googleapis.com), this field would be "compute.googleapis.com".
+     * The API Service from which the `QuotaFailure.Violation` originates. In some cases, Quota issues originate from an API Service other than the one that was called. In other words, a dependency of the called API Service could be the cause of the `QuotaFailure`, and this field would have the dependency API service name. For example, if the called API is Kubernetes Engine API (container.googleapis.com), and a quota violation occurs in the Kubernetes Engine API itself, this field would be "container.googleapis.com". On the other hand, if the quota violation occurs when the Kubernetes Engine API creates VMs in the Compute Engine API (compute.googleapis.com), this field would be "compute.googleapis.com".
      */
     apiService?: string | null;
     /**
@@ -1237,7 +1237,7 @@ export namespace datamigration_v1beta1 {
     }
 
     /**
-     * Lists information about the supported locations for this service. This method lists locations based on the resource scope provided in the [ListLocationsRequest.name] field: * **Global locations**: If `name` is empty, the method lists the public locations available to all projects. * **Project-specific locations**: If `name` follows the format `projects/{project\}`, the method lists locations visible to that specific project. This includes public, private, or other project-specific locations enabled for the project. For gRPC and client library implementations, the resource name is passed as the `name` field. For direct service calls, the resource name is incorporated into the request path based on the specific service implementation and version.
+     * Lists information about the supported locations for this service. This method lists locations based on the resource scope provided in the ListLocationsRequest.name field: * **Global locations**: If `name` is empty, the method lists the public locations available to all projects. * **Project-specific locations**: If `name` follows the format `projects/{project\}`, the method lists locations visible to that specific project. This includes public, private, or other project-specific locations enabled for the project. For gRPC and client library implementations, the resource name is passed as the `name` field. For direct service calls, the resource name is incorporated into the request path based on the specific service implementation and version.
      * @example
      * ```js
      * // Before running the sample:
@@ -1267,7 +1267,7 @@ export namespace datamigration_v1beta1 {
      *
      *   // Do the magic
      *   const res = await datamigration.projects.locations.list({
-     *     // Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
+     *     // Optional. Do not use this field unless explicitly documented otherwise. This is primarily for internal usage.
      *     extraLocationTypes: 'placeholder-value',
      *     // A filter to narrow down results to a preferred subset. The filtering language accepts strings like `"displayName=tokyo"`, and is documented in more detail in [AIP-160](https://google.aip.dev/160).
      *     filter: 'placeholder-value',
@@ -1394,7 +1394,7 @@ export namespace datamigration_v1beta1 {
   }
   export interface Params$Resource$Projects$Locations$List extends StandardParameters {
     /**
-     * Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented otherwise. This is primarily for internal usage.
+     * Optional. Do not use this field unless explicitly documented otherwise. This is primarily for internal usage.
      */
     extraLocationTypes?: string[];
     /**
