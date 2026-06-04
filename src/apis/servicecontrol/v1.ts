bf1b759e9eacb300d0bb837823406b7e42b20fa2
Yoshi Automation <yoshi-automation@google.com>
2026-06-04 21:11:40 +0000

    feat(servicecontrol): update the API
    #### servicecontrol:v1
    
    The following keys were added:
    - schemas.Auth.properties.credentialId.description
    - schemas.Auth.properties.credentialId.type
    
    #### servicecontrol:v2
    
    The following keys were added:
    - schemas.Auth.properties.credentialId.description
    - schemas.Auth.properties.credentialId.type

--- src/apis/servicecontrol/v1.ts
+++ src/apis/servicecontrol/v1.ts
@@ -284,6 +284,10 @@ export namespace servicecontrol_v1 {
      */
     claims?: {[key: string]: any} | null;
     /**
+     * Identifies the client credential id used for authentication. credential_id is in the format of AUTH_METHOD:IDENTIFIER, e.g. "serviceaccount:XXXXX, apikey:XXXXX" where the format of the IDENTIFIER can vary for different AUTH_METHODs.
+     */
+    credentialId?: string | null;
+    /**
      * Attributes of the OAuth token associated with the request.
      */
     oauth?: Schema$Oauth;
