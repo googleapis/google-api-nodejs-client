f10c71e632d89dc04e35e3655fdc744aa9531995
Yoshi Automation <yoshi-automation@google.com>
2026-08-03 01:53:41 +0000

    feat(places): update the API
    #### places:v1
    
    The following keys were added:
    - schemas.GoogleMapsPlacesV1Place.properties.entrances.description
    - schemas.GoogleMapsPlacesV1Place.properties.entrances.items.$ref
    - schemas.GoogleMapsPlacesV1Place.properties.entrances.type
    - schemas.GoogleMapsPlacesV1Place.properties.navigationPoints.description
    - schemas.GoogleMapsPlacesV1Place.properties.navigationPoints.items.$ref
    - schemas.GoogleMapsPlacesV1Place.properties.navigationPoints.type
    - schemas.GoogleMapsPlacesV1PlaceEntrance.description
    - schemas.GoogleMapsPlacesV1PlaceEntrance.id
    - schemas.GoogleMapsPlacesV1PlaceEntrance.properties.location.$ref
    - schemas.GoogleMapsPlacesV1PlaceEntrance.properties.location.description
    - schemas.GoogleMapsPlacesV1PlaceEntrance.properties.tags.description
    - schemas.GoogleMapsPlacesV1PlaceEntrance.properties.tags.items.enum
    - schemas.GoogleMapsPlacesV1PlaceEntrance.properties.tags.items.enumDescriptions
    - schemas.GoogleMapsPlacesV1PlaceEntrance.properties.tags.items.type
    - schemas.GoogleMapsPlacesV1PlaceEntrance.properties.tags.type
    - schemas.GoogleMapsPlacesV1PlaceEntrance.type
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.description
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.id
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.displayName.$ref
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.displayName.description
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.location.$ref
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.location.description
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.navigationPointToken.description
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.navigationPointToken.type
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.travelModes.description
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.travelModes.items.enum
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.travelModes.items.enumDescriptions
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.travelModes.items.type
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.travelModes.type
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.usages.description
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.usages.items.enum
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.usages.items.enumDescriptions
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.usages.items.type
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.properties.usages.type
    - schemas.GoogleMapsPlacesV1PlaceNavigationPoint.type

--- src/apis/places/v1.ts
+++ src/apis/places/v1.ts
@@ -680,6 +680,10 @@ export namespace places_v1 {
      */
     editorialSummary?: Schema$GoogleTypeLocalizedText;
     /**
+     * Entrances for this destination.
+     */
+    entrances?: Schema$GoogleMapsPlacesV1PlaceEntrance[];
+    /**
      * The summary of amenities near the EV charging station.
      */
     evChargeAmenitySummary?: Schema$GoogleMapsPlacesV1PlaceEvChargeAmenitySummary;
@@ -767,6 +771,10 @@ export namespace places_v1 {
      * A human-readable phone number for the place, in national format.
      */
     nationalPhoneNumber?: string | null;
+    /**
+     * Navigation points for this destination.
+     */
+    navigationPoints?: Schema$GoogleMapsPlacesV1PlaceNavigationPoint[];
     /**
      * A summary of points of interest near the place.
      */
@@ -1044,6 +1052,19 @@ export namespace places_v1 {
     name?: string | null;
   }
   /**
+   * An entrance is a single latitude/longitude coordinate pair that defines the location of an entry and exit point for a place.
+   */
+  export interface Schema$GoogleMapsPlacesV1PlaceEntrance {
+    /**
+     * The location of the entrance.
+     */
+    location?: Schema$GoogleTypeLatLng;
+    /**
+     * A list of tags that describe the entrance.
+     */
+    tags?: string[] | null;
+  }
+  /**
    * The summary of amenities near the EV charging station. This only applies to places with type `electric_vehicle_charging_station`. The `overview` field is guaranteed to be provided while the other fields are optional.
    */
   export interface Schema$GoogleMapsPlacesV1PlaceEvChargeAmenitySummary {
@@ -1113,6 +1134,31 @@ export namespace places_v1 {
      * A link to write a review for this place on Google Maps.
      */
     writeAReviewUri?: string | null;
+  }
+  /**
+   * A navigation point is a location next to a road where navigation can end.
+   */
+  export interface Schema$GoogleMapsPlacesV1PlaceNavigationPoint {
+    /**
+     * The display name of this navigation point. For example, "5th Ave" or "Gate B".
+     */
+    displayName?: Schema$GoogleTypeLocalizedText;
+    /**
+     * A point next to the road segment where navigation should end. The point is intentionally slightly offset from the road's centerline to clearly mark the side of the road where the place is located.
+     */
+    location?: Schema$GoogleTypeLatLng;
+    /**
+     * A token that can be used to identify this navigation point.
+     */
+    navigationPointToken?: string | null;
+    /**
+     * Travel modes that are appropriate for this navigation point.
+     */
+    travelModes?: string[] | null;
+    /**
+     * Lists `usages` supported by this navigation point. If empty, it does not necessarily mean its usage is restricted in any way. All navigation points can be used for general navigation.
+     */
+    usages?: string[] | null;
   }
   /**
    * A summary of points of interest near the place.
@@ -2164,6 +2210,7 @@ export namespace places_v1 {
      *   //   "dineIn": false,
      *   //   "displayName": {},
      *   //   "editorialSummary": {},
+     *   //   "entrances": [],
      *   //   "evChargeAmenitySummary": {},
      *   //   "evChargeOptions": {},
      *   //   "formattedAddress": "my_formattedAddress",
@@ -2186,6 +2233,7 @@ export namespace places_v1 {
      *   //   "movedPlaceId": "my_movedPlaceId",
      *   //   "name": "my_name",
      *   //   "nationalPhoneNumber": "my_nationalPhoneNumber",
+     *   //   "navigationPoints": [],
      *   //   "neighborhoodSummary": {},
      *   //   "openingDate": {},
      *   //   "outdoorSeating": false,
