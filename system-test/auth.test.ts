f0ed6097fc29fe367b94cd5d45e38888f7fbc13d
Daniel Bruce <djbruce@google.com>
2026-09-02 10:34:20 -0400

    Undo the skipped test

--- system-test/auth.test.ts
+++ system-test/auth.test.ts
@@ -41,8 +41,7 @@ describe('google.auth', async () => {
       assert.strictEqual(projectId, 'foo-project-id');
     });
 
-    it.skip('uses the last configured client settings', async () => {
-      // TODO: Unskip this later when we resolve this flakey test issue.
+    it('uses the last configured client settings', async () => {
       let authClient = await google.auth.getClient();
       authClient = await google.auth.getClient({
         scopes: ['https://www.googleapis.com/auth/cloud-platform'],
