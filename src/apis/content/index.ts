d5908f2f914287fd7a3457c8ec12af8b157ef2b9
Yoshi Automation <yoshi-automation@google.com>
2026-09-04 01:33:30 +0000

    fix(content): update the API

--- src/apis/content/index.ts
+++ src/apis/content/index.ts
@@ -14,28 +14,23 @@
 /*! THIS FILE IS AUTO-GENERATED */
 
 import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
-import {content_v2_1} from './v2.1';
 import {content_v2} from './v2';
 
 export const VERSIONS = {
-  'v2.1': content_v2_1.Content,
   v2: content_v2.Content,
 };
 
-export function content(version: 'v2.1'): content_v2_1.Content;
-export function content(options: content_v2_1.Options): content_v2_1.Content;
 export function content(version: 'v2'): content_v2.Content;
 export function content(options: content_v2.Options): content_v2.Content;
-export function content<T = content_v2_1.Content | content_v2.Content>(
+export function content<T = content_v2.Content>(
   this: GoogleConfigurable,
-  versionOrOptions: 'v2.1' | content_v2_1.Options | 'v2' | content_v2.Options
+  versionOrOptions: 'v2' | content_v2.Options
 ) {
   return getAPI<T>('content', versionOrOptions, VERSIONS, this);
 }
 
 const auth = new AuthPlus();
 export {auth};
-export {content_v2_1};
 export {content_v2};
 export {
   AuthPlus,
