b735a92e7f1026ffd5f97144906bc73c23fd705a
Yoshi Automation <yoshi-automation@google.com>
2026-09-10 21:48:13 +0000

    fix(auditmanager): update the API

---
+++ src/apis/auditmanager/index.ts
@@ -0,0 +1,45 @@
+// Copyright 2020 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*! THIS FILE IS AUTO-GENERATED */
+
+import {AuthPlus, getAPI, GoogleConfigurable} from 'googleapis-common';
+import {auditmanager_v1} from './v1';
+
+export const VERSIONS = {
+  v1: auditmanager_v1.Auditmanager,
+};
+
+export function auditmanager(version: 'v1'): auditmanager_v1.Auditmanager;
+export function auditmanager(
+  options: auditmanager_v1.Options
+): auditmanager_v1.Auditmanager;
+export function auditmanager<T = auditmanager_v1.Auditmanager>(
+  this: GoogleConfigurable,
+  versionOrOptions: 'v1' | auditmanager_v1.Options
+) {
+  return getAPI<T>('auditmanager', versionOrOptions, VERSIONS, this);
+}
+
+const auth = new AuthPlus();
+export {auth};
+export {auditmanager_v1};
+export {
+  AuthPlus,
+  GlobalOptions,
+  APIRequestContext,
+  GoogleConfigurable,
+  StreamMethodOptions,
+  MethodOptions,
+  BodyResponseCallback,
+} from 'googleapis-common';
