0aab3cc08cc2386573397e01175b175d277028f9
Yoshi Automation <yoshi-automation@google.com>
2026-09-10 01:37:28 +0000

    feat(cloudtasks): update the API
    #### cloudtasks:v2beta2
    
    The following keys were changed:
    - schemas.HttpTarget.description
    
    #### cloudtasks:v2beta3
    
    The following keys were changed:
    - schemas.HttpTarget.description
    
    #### cloudtasks:v2
    
    The following keys were added:
    - resources.projects.resources.locations.resources.operations.methods.get.description
    - resources.projects.resources.locations.resources.operations.methods.get.flatPath
    - resources.projects.resources.locations.resources.operations.methods.get.httpMethod
    - resources.projects.resources.locations.resources.operations.methods.get.id
    - resources.projects.resources.locations.resources.operations.methods.get.parameterOrder
    - resources.projects.resources.locations.resources.operations.methods.get.parameters.name.description
    - resources.projects.resources.locations.resources.operations.methods.get.parameters.name.location
    - resources.projects.resources.locations.resources.operations.methods.get.parameters.name.pattern
    - resources.projects.resources.locations.resources.operations.methods.get.parameters.name.required
    - resources.projects.resources.locations.resources.operations.methods.get.parameters.name.type
    - resources.projects.resources.locations.resources.operations.methods.get.path
    - resources.projects.resources.locations.resources.operations.methods.get.response.$ref
    - resources.projects.resources.locations.resources.operations.methods.get.scopes
    - schemas.Operation.description
    - schemas.Operation.id
    - schemas.Operation.properties.done.description
    - schemas.Operation.properties.done.type
    - schemas.Operation.properties.error.$ref
    - schemas.Operation.properties.error.description
    - schemas.Operation.properties.metadata.additionalProperties.description
    - schemas.Operation.properties.metadata.additionalProperties.type
    - schemas.Operation.properties.metadata.description
    - schemas.Operation.properties.metadata.type
    - schemas.Operation.properties.name.description
    - schemas.Operation.properties.name.type
    - schemas.Operation.properties.response.additionalProperties.description
    - schemas.Operation.properties.response.additionalProperties.type
    - schemas.Operation.properties.response.description
    - schemas.Operation.properties.response.type
    - schemas.Operation.type
    
    The following keys were changed:
    - schemas.HttpTarget.description

--- src/apis/cloudtasks/v2beta3.ts
+++ src/apis/cloudtasks/v2beta3.ts
@@ -403,7 +403,7 @@ export namespace cloudtasks_v2beta3 {
     url?: string | null;
   }
   /**
-   * HTTP target. When specified as a Queue, all the tasks with [HttpRequest] will be overridden according to the target.
+   * HTTP target. When specified at the Queue level, all tasks with HttpRequest are overridden according to the target.
    */
   export interface Schema$HttpTarget {
     /**
