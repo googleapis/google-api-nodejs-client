780041a8c50476ded2b84ee8ed9843f3b13936e7
Yoshi Automation <yoshi-automation@google.com>
2026-09-11 01:36:15 +0000

    feat(apigateway): update the API
    #### apigateway:v1beta
    
    The following keys were added:
    - schemas.ApigatewayGateway.properties.effectiveStreamingMode.description
    - schemas.ApigatewayGateway.properties.effectiveStreamingMode.enum
    - schemas.ApigatewayGateway.properties.effectiveStreamingMode.enumDescriptions
    - schemas.ApigatewayGateway.properties.effectiveStreamingMode.readOnly
    - schemas.ApigatewayGateway.properties.effectiveStreamingMode.type
    - schemas.ApigatewayGateway.properties.streamingMode.description
    - schemas.ApigatewayGateway.properties.streamingMode.enum
    - schemas.ApigatewayGateway.properties.streamingMode.enumDescriptions
    - schemas.ApigatewayGateway.properties.streamingMode.type
    
    #### apigateway:v1
    
    The following keys were added:
    - schemas.ApigatewayGateway.properties.effectiveStreamingMode.description
    - schemas.ApigatewayGateway.properties.effectiveStreamingMode.enum
    - schemas.ApigatewayGateway.properties.effectiveStreamingMode.enumDescriptions
    - schemas.ApigatewayGateway.properties.effectiveStreamingMode.readOnly
    - schemas.ApigatewayGateway.properties.effectiveStreamingMode.type
    - schemas.ApigatewayGateway.properties.streamingMode.description
    - schemas.ApigatewayGateway.properties.streamingMode.enum
    - schemas.ApigatewayGateway.properties.streamingMode.enumDescriptions
    - schemas.ApigatewayGateway.properties.streamingMode.type

--- src/apis/apigateway/v1.ts
+++ src/apis/apigateway/v1.ts
@@ -330,6 +330,10 @@ export namespace apigateway_v1 {
      */
     displayName?: string | null;
     /**
+     * Output only. The streaming mode this gateway is actually served with, which the service resolves at creation from `streaming_mode`, the referenced API Config, and the platform default at the time. Read this rather than `streaming_mode` to determine whether a gateway supports response streaming.
+     */
+    effectiveStreamingMode?: string | null;
+    /**
      * Optional. Resource labels to represent user-provided metadata. Refer to cloud documentation on labels for more details. https://cloud.google.com/compute/docs/labeling-resources
      */
     labels?: {[key: string]: string} | null;
@@ -341,6 +345,10 @@ export namespace apigateway_v1 {
      * Output only. The current state of the Gateway.
      */
     state?: string | null;
+    /**
+     * Optional. Immutable. Requests response streaming for a new gateway. An attempt to change it on update is rejected. If unset, the service selects the mode. This field records only what was requested and is never modified by the service; read `effective_streaming_mode` for the mode the gateway is served with.
+     */
+    streamingMode?: string | null;
     /**
      * Output only. Updated time.
      */
@@ -3601,9 +3609,11 @@ export namespace apigateway_v1 {
      *       //   "createTime": "my_createTime",
      *       //   "defaultHostname": "my_defaultHostname",
      *       //   "displayName": "my_displayName",
+     *       //   "effectiveStreamingMode": "my_effectiveStreamingMode",
      *       //   "labels": {},
      *       //   "name": "my_name",
      *       //   "state": "my_state",
+     *       //   "streamingMode": "my_streamingMode",
      *       //   "updateTime": "my_updateTime"
      *       // }
      *     },
@@ -3895,9 +3905,11 @@ export namespace apigateway_v1 {
      *   //   "createTime": "my_createTime",
      *   //   "defaultHostname": "my_defaultHostname",
      *   //   "displayName": "my_displayName",
+     *   //   "effectiveStreamingMode": "my_effectiveStreamingMode",
      *   //   "labels": {},
      *   //   "name": "my_name",
      *   //   "state": "my_state",
+     *   //   "streamingMode": "my_streamingMode",
      *   //   "updateTime": "my_updateTime"
      *   // }
      * }
@@ -4333,9 +4345,11 @@ export namespace apigateway_v1 {
      *       //   "createTime": "my_createTime",
      *       //   "defaultHostname": "my_defaultHostname",
      *       //   "displayName": "my_displayName",
+     *       //   "effectiveStreamingMode": "my_effectiveStreamingMode",
      *       //   "labels": {},
      *       //   "name": "my_name",
      *       //   "state": "my_state",
+     *       //   "streamingMode": "my_streamingMode",
      *       //   "updateTime": "my_updateTime"
      *       // }
      *     },
