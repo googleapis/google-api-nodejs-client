573d533d6a9a7bb4880ef0053f1c209ace7e4833
Yoshi Automation <yoshi-automation@google.com>
2026-09-16 01:35:53 +0000

    feat(datamigration): update the API
    #### datamigration:v1
    
    The following keys were added:
    - schemas.MySqlConnectionProfile.properties.privateConnectivity.$ref
    - schemas.MySqlConnectionProfile.properties.privateConnectivity.description
    - schemas.MySqlConnectionProfile.properties.privateServiceConnectConnectivity.$ref
    - schemas.MySqlConnectionProfile.properties.privateServiceConnectConnectivity.description
    - schemas.MySqlConnectionProfile.properties.staticServiceIpConnectivity.$ref
    - schemas.MySqlConnectionProfile.properties.staticServiceIpConnectivity.description
    - schemas.PerformanceConfig.properties.loadParallelLevel.description
    - schemas.PerformanceConfig.properties.loadParallelLevel.enum
    - schemas.PerformanceConfig.properties.loadParallelLevel.enumDescriptions
    - schemas.PerformanceConfig.properties.loadParallelLevel.type
    
    The following keys were changed:
    - schemas.MigrationJob.properties.performanceConfig.description
    - schemas.PostgreSqlConnectionProfile.properties.privateServiceConnectConnectivity.description

--- src/apis/datamigration/v1.ts
+++ src/apis/datamigration/v1.ts
@@ -1841,7 +1841,7 @@ export namespace datamigration_v1 {
      */
     originalMigrationName?: string | null;
     /**
-     * Optional. Data dump parallelism settings used by the migration.
+     * Optional. Data dump and load parallelism settings used by the migration.
      */
     performanceConfig?: Schema$PerformanceConfig;
     /**
@@ -2051,9 +2051,21 @@ export namespace datamigration_v1 {
      */
     port?: number | null;
     /**
+     * Private connectivity.
+     */
+    privateConnectivity?: Schema$PrivateConnectivity;
+    /**
+     * Private Service Connect connectivity.
+     */
+    privateServiceConnectConnectivity?: Schema$PrivateServiceConnectConnectivity;
+    /**
      * SSL configuration for the destination to connect to the source database.
      */
     ssl?: Schema$SslConfig;
+    /**
+     * Static Service IP connectivity.
+     */
+    staticServiceIpConnectivity?: Schema$StaticServiceIpConnectivity;
     /**
      * Required. The username that Database Migration Service will use to connect to the database. The value is encrypted when stored in Database Migration Service.
      */
@@ -2246,6 +2258,10 @@ export namespace datamigration_v1 {
      * Initial dump parallelism level.
      */
     dumpParallelLevel?: string | null;
+    /**
+     * Optional. Initial load parallelism level.
+     */
+    loadParallelLevel?: string | null;
   }
   /**
    * An Identity and Access Management (IAM) policy, which specifies access controls for Google Cloud resources. A `Policy` is a collection of `bindings`. A `binding` binds one or more `members`, or principals, to a single `role`. Principals can be user accounts, service accounts, Google groups, and domains (such as G Suite). A `role` is a named list of permissions; each `role` can be an IAM predefined role or a user-created custom role. For some types of Google Cloud resources, a `binding` can also specify a `condition`, which is a logical expression that allows access to a resource only if the expression evaluates to `true`. A condition can add constraints based on attributes of the request, the resource, or both. To learn which resources support conditions in their IAM policies, see the [IAM documentation](https://cloud.google.com/iam/help/conditions/resource-policies). **JSON example:** ``` { "bindings": [ { "role": "roles/resourcemanager.organizationAdmin", "members": [ "user:mike@example.com", "group:admins@example.com", "domain:google.com", "serviceAccount:my-project-id@appspot.gserviceaccount.com" ] \}, { "role": "roles/resourcemanager.organizationViewer", "members": [ "user:eve@example.com" ], "condition": { "title": "expirable access", "description": "Does not grant access after Sep 2020", "expression": "request.time < timestamp('2020-10-01T00:00:00.000Z')", \} \} ], "etag": "BwWWja0YfJA=", "version": 3 \} ``` **YAML example:** ``` bindings: - members: - user:mike@example.com - group:admins@example.com - domain:google.com - serviceAccount:my-project-id@appspot.gserviceaccount.com role: roles/resourcemanager.organizationAdmin - members: - user:eve@example.com role: roles/resourcemanager.organizationViewer condition: title: expirable access description: Does not grant access after Sep 2020 expression: request.time < timestamp('2020-10-01T00:00:00.000Z') etag: BwWWja0YfJA= version: 3 ``` For a description of IAM and its features, see the [IAM documentation](https://cloud.google.com/iam/docs/).
@@ -2364,7 +2380,7 @@ export namespace datamigration_v1 {
      */
     privateConnectivity?: Schema$PrivateConnectivity;
     /**
-     * Private service connect connectivity.
+     * Private Service Connect connectivity.
      */
     privateServiceConnectConnectivity?: Schema$PrivateServiceConnectConnectivity;
     /**
