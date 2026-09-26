a92a66db30e42d41824fde0091790dce161b7a71
Yoshi Automation <yoshi-automation@google.com>
2026-09-26 01:36:11 +0000

    fix(sts): update the API
    #### sts:v1beta
    
    The following keys were changed:
    - schemas.GoogleIdentityStsV1Options.properties.bindCertFingerprint.description
    
    #### sts:v1
    
    The following keys were changed:
    - resources.organizations.resources.locations.resources.workloadIdentityPools.resources.openid.methods.getJwks.description
    - resources.organizations.resources.locations.resources.workloadIdentityPools.resources.openid.methods.getJwks.parameters.name.description
    - resources.projects.resources.locations.resources.workloadIdentityPools.resources.openid.methods.getJwks.description
    - resources.projects.resources.locations.resources.workloadIdentityPools.resources.openid.methods.getJwks.parameters.name.description
    - schemas.GoogleIdentityStsV1Jwk.description
    - schemas.GoogleIdentityStsV1Jwks.properties.keys.description
    - schemas.GoogleIdentityStsV1OpenIdProviderConfig.description
    - schemas.GoogleIdentityStsV1OpenIdProviderConfig.properties.id_token_signing_alg_values_supported.description
    - schemas.GoogleIdentityStsV1OpenIdProviderConfig.properties.jwks_uri.description
    - schemas.GoogleIdentityStsV1OpenIdProviderConfig.properties.response_types_supported.description
    - schemas.GoogleIdentityStsV1OpenIdProviderConfig.properties.subject_types_supported.description
    - schemas.GoogleIdentityStsV1Options.properties.bindCertFingerprint.description

--- src/apis/sts/v1beta.ts
+++ src/apis/sts/v1beta.ts
@@ -269,7 +269,7 @@ export namespace sts_v1beta {
      */
     accessBoundary?: Schema$GoogleIdentityStsV1AccessBoundary;
     /**
-     * The unpadded, url-escaped, base64-encoded SHA-256 hash of the certificate's DER encoding. It must be 43 characters long. The resulting token will be bound to this value.
+     * The unpadded, URL-escaped, base64-encoded SHA-256 hash of the certificate's DER encoding. It must be 43 characters long. The resulting token will be bound to this value.
      */
     bindCertFingerprint?: string | null;
     /**
